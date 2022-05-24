8c874f988e34adc6afbf85fa590c772f972d1c5a
Raskolnikov <jerry.tann@endress.com>
2022-05-24 12:13:23 +0800

    ✨ solve fizzbuzz

--- FizzBuzz/fizzBuzz.rb
+++ FizzBuzz/fizzBuzz.rb
@@ -1,5 +1,19 @@
 # @param {Integer} n
 # @return {String[]}
 def fizz_buzz(n)
-    
+    res = []
+    for i in 1..n
+        if i % 3 == 0 && i % 5 == 0
+            res << "FizzBuzz"
+        elsif i % 3 == 0
+            res << "Fizz"
+        elsif i % 5 == 0
+            res << "Buzz"
+        else 
+            res << i.to_s
+        end
+    end
+    res
 end
+
+puts "#{fizz_buzz(10)}"
