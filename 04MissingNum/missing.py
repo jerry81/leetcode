8c16d4ea509475a28d7cd253b5f45442f2b84cc4
Raskolnikov <jerry.tann@endress.com>
2022-05-28 19:31:40 +0800

    ✨ start missing number problem

---
+++ 04MissingNum/missing.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return
+        
+s = Solution()
+nums = [3,0,1]
+print(f"expect 2 {s.missingNumber(nums)}")
