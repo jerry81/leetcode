1295f2d2057326a9f5a990f2c16f98089dd56e20
Raskolnikov <jerry.tann@endress.com>
2022-05-28 19:35:45 +0800

    ✅ pass example cases

--- 04MissingNum/missing.py
+++ 04MissingNum/missing.py
@@ -3,8 +3,19 @@ from typing import List
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        return
+        l = len(nums)
+        sor = sorted(nums)
+        for i in range(l):
+            if i != sor[i]:
+                return i 
+        return l
         
 s = Solution()
 nums = [3,0,1]
+print(f"expect 2 {s.missingNumber(nums)}")
+
+nums = [9,6,4,2,3,5,7,0,1]
+print(f"expect 8 {s.missingNumber(nums)}")
+
+nums = [0,1]
 print(f"expect 2 {s.missingNumber(nums)}")
