6eab91a744f532a76cd0fecd5e53b4e65a0f6369
Raskolnikov <jerry.tann@endress.com>
2022-05-29 13:34:39 +0800

    ✨ work on a medium problem of the day

---
+++ 05MaxProductOfWordLen/max_product.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def max_uniq(self, word, words):
+      can = []
+      for w in words:
+          contained = False
+          for c in word:
+              if c in w:
+                  contained = True
+                  continue
+          if not contained:
+              can.append(w)
+      can.sort(key = lambda x: len(x), reverse=True)
+      return can[0] if len(can) > 0 else None
+
+    def maxProduct(self, words: List[str]) -> int:
+        
+
+s = Solution()
+words = ["abcw","baz","foo","bar","xtfn","abcdef"]
+print(f"expect xtfn {s.max_uniq(words[0],words)}")
+words = ["abcw","baz","foo","bar","xtfn","abcdef"]
+print(f"expect 16 {s.maxProduct(words)}")
+words = ["a","ab","abc","d","cd","bcd","abcd"]
+print(f"expect 4 {s.maxProduct(words)}")
+words = ["a","aa","aaa","aaaa"]
+print(f"expect 0 {s.maxProduct(words)}")
