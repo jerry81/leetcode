6429af93ffe8b8d366ec3d7af10acbe5926b1e88
Raskolnikov <jerry.tann@endress.com>
2022-05-30 09:35:26 +0800

    ✨ start long division problem

---
+++ 06DivideTwoIntegers/divide2Ints.py
@@ -0,0 +1,11 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        return
+
+s = Solution()
+dividend = 10
+divisor = 3 
+print(f"expect 3 {s.divide(dividend, divisor)}")
+dividend = 7
+divisor = -3
+print(f"expect -2 {s.divide(dividend, divisor)}")
