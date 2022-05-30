676527580d29505fd6b2f55ad8ddd94a0eaeffa6
Raskolnikov <jerry.tann@endress.com>
2022-05-30 09:39:38 +0800

    ✨ pass example tests

--- 06DivideTwoIntegers/divide2Ints.py
+++ 06DivideTwoIntegers/divide2Ints.py
@@ -1,6 +1,15 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        return
+        count = 0
+        neg = False
+        if (dividend < 0 or divisor < 0) and not (dividend < 0 and divisor < 0):
+            neg = True
+        divisor = abs(divisor)
+        dividend = abs(dividend)
+        while dividend > divisor:
+          dividend -= divisor 
+          count+=1
+        return -count if neg else count
 
 s = Solution()
 dividend = 10
