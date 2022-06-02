65682fddb72ca211a714cb565e9c615c0f1252d5
Raskolnikov <jerry.tann@endress.com>
2022-06-02 08:48:15 +0800

    ✅ pass example tests

--- 09TransposeMatrix/transposeMatrix.py
+++ 09TransposeMatrix/transposeMatrix.py
@@ -3,7 +3,16 @@ from typing import List
 
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        return 
+        yl = len(matrix)
+        xl = len(matrix[0])
+        transposed = [ [0]*yl for _ in range(xl)]
+        print(f"transposed is {transposed}")
+        for y,i in enumerate(matrix):
+            for x,j in enumerate(i):
+                print(f"y is {y} x is {x}")
+                transposed[x][y] = j
+        return transposed
+            
 
 s = Solution()
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
