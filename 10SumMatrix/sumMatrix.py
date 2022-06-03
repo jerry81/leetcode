1e6e4fec1629823e3ec5b61780e0952389eccf5b
Raskolnikov <jerry.tann@endress.com>
2022-06-03 19:55:10 +0800

    ✨ add memoization

--- 10SumMatrix/sumMatrix.py
+++ 10SumMatrix/sumMatrix.py
@@ -6,13 +6,23 @@ class NumMatrix:
     
     def __init__(self, matrix: List[List[int]]):
         self.matrix = matrix
+        self.memo = {}
+    
+    def make_key(self,row1,col1,row2,col2):
+      return f"{row1},{col1},{row2},{col2}"
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        key = self.make_key(row1,col1,row2,col2)
         lines = self.matrix[row1:(row2+1)]
         tot = 0
-        for l in lines:
-            tot += sum(l[col1:(col2+1)])
-        return tot
+        try:
+          tot = self.memo[key]
+          return tot
+        except:
+          for l in lines:
+              tot += sum(l[col1:(col2+1)])
+          self.memo = tot
+          return tot
 
 nm = NumMatrix([[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]])
 print(f"nm.matrix is {nm.matrix}")
