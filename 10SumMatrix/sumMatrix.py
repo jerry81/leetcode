7e688a570b65bf0ffdce147855a37431a17325b2
Raskolnikov <jerry.tann@endress.com>
2022-06-03 19:43:29 +0800

    ✅ pass example test, but can't pass performance test

--- 10SumMatrix/sumMatrix.py
+++ 10SumMatrix/sumMatrix.py
@@ -8,10 +8,16 @@ class NumMatrix:
         self.matrix = matrix
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return
+        lines = self.matrix[row1:(row2+1)]
+        tot = 0
+        for l in lines:
+            tot += sum(l[col1:(col2+1)])
+        return tot
 
-nm = NumMatrix([[[[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]], [2, 1, 4, 3], [1, 1, 2, 2], [1, 2, 2, 4]])
+nm = NumMatrix([[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]])
 print(f"nm.matrix is {nm.matrix}")
+out1 = nm.sumRegion(2, 1, 4, 3); 
+print(f"expect 8 {out1}")
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
 # param_1 = obj.sumRegion(row1,col1,row2,col2)
