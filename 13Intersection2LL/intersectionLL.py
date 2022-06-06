e36a38b652c6004aa783d1d316cb2be9664c3ecf
Raskolnikov <jerry.tann@endress.com>
2022-06-06 20:30:13 +0800

    🎨 rework the problem

--- 13Intersection2LL/intersectionLL.py
+++ 13Intersection2LL/intersectionLL.py
@@ -6,56 +6,43 @@ class ListNode:
      def __init__(self, x):
          self.val = x
          self.next = None
+
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        lookup = {}
-        print(f"headA is {headA}")
-        print(f"headB is {headB}")
-        lookup[headA.val] = True
-        nA = headA.next
-        idx = 0
-        while nA is not None:
-            lookup[nA.val] = idx
-            nA = nA.next
-            idx+=1
+        la = 1
+        head = headA
+        while head.next is not None:
+            la += 1
+            head = head.next
+        lb = 1
         head = headB
-        idxs = []
-        lookup2 = {}
-        while head is not None:
-          try:
-              i = lookup[head.val]
-              idxs.append(i)
-              lookup2[i] = head
-              head = head.next 
-          except: 
-              head = head.next
-        if len(idxs) > 0:
-            return lookup2[min(idxs)]
-        return None
+        while head.next is not None:
+            lb += 1
+            head = head.next
+        print(f"la is {la} lb is {lb}")
         
 
 
 s = Solution()
 a1 = ListNode(4)
 a2 = ListNode(1)
-a3 = ListNode(8)
-a4 = ListNode(4)
-a5 = ListNode(5)
 a1.next = a2 
-a2.next = a3 
-a3.next = a4
-a4.next = a5
+
 
 b1 = ListNode(5)
 b2 = ListNode(6)
 b3 = ListNode(1)
-b4 = ListNode(8)
-b5 = ListNode(4)
-b6 = ListNode(5)
+
 b1.next = b2
 b2.next = b3
-b3.next = b4
-b4.next = b5
-b5.next = b6
+
+
+c4 = ListNode(8)
+c5 = ListNode(4)
+c6 = ListNode(5)
+b3.next = c4
+a2.next = c4
+c4.next = c5
+c5.next = c6
 
 print(f"expect 8 {s.getIntersectionNode(a1, b1)}")
