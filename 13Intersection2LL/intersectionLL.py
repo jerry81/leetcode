23506ef64181fa3e3a7bff120431c93e0e8b34ce
Raskolnikov <jerry.tann@endress.com>
2022-06-06 19:40:20 +0800

    ✨ intersection of 2 linked lists

---
+++ 13Intersection2LL/intersectionLL.py
@@ -0,0 +1,51 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
+class Solution:
+    lookup = {}
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        self.lookup[headA.val] = True
+        nA = headA.next
+        while nA is not None:
+            self.lookup[nA.val] = True
+        head = headB
+        while head is not None:
+          try:
+              self.lookup[head.val]
+              return head.val
+          except: 
+              head = head.next
+        return None
+        
+
+
+s = Solution()
+a1 = ListNode(4)
+a2 = ListNode(1)
+a3 = ListNode(8)
+a4 = ListNode(4)
+a5 = ListNode(5)
+a1.next = a2 
+a2.next = a3 
+a3.next = a4
+a4.next = a5
+
+b1 = ListNode(5)
+b2 = ListNode(6)
+b3 = ListNode(1)
+b4 = ListNode(8)
+b5 = ListNode(4)
+b6 = ListNode(5)
+b1.next = b2
+b2.next = b3
+b3.next = b4
+b4.next = b5
+b5.next = b6
+
+s.getIntersectionNode(a1, b1)
