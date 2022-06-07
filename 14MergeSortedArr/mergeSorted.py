94ea9f453d33aa26043ef72ec5ff47b748bdce4d
Raskolnikov <jerry.tann@endress.com>
2022-06-07 08:45:47 +0800

    ✨ setup test cases

--- 14MergeSortedArr/mergeSorted.py
+++ 14MergeSortedArr/mergeSorted.py
@@ -6,4 +6,25 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        
+
+s = Solution()
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+s.merge(nums1, m, nums2, n)
+print(f"expect [1,2,2,3,5,6] {nums1}")
+
+nums1 = [1]
+m = 1
+nums2 = []
+n = 0
+s.merge(nums1, m, nums2, n)
+print(f"expect [1] {nums1}")
+
+nums1 = []
+m = 0
+nums2 = [1]
+n = 1
+s.merge(nums1, m, nums2, n)
+print(f"expect [1] {nums1}")
