1cf98360fa752e137ac942d7b80bbd80b8a799bc
Raskolnikov <jerry.tann@endress.com>
2022-06-07 12:59:46 +0800

    🐛 another broken case

--- 14MergeSortedArr/mergeSorted.py
+++ 14MergeSortedArr/mergeSorted.py
@@ -61,3 +61,10 @@ nums2 = [1]
 n = 1
 s.merge(nums1, m, nums2, n)
 print(f"expect [1,2] {nums1}")
+
+nums1 = [-1,0,0,3,3,3,0,0,0]
+m = 6
+nums2 = [1,2,2]
+n = 3
+s.merge(nums1,m,nums2,n)
+print(f"expect [-1,0,0,1,2,2,3,3,3] {nums1}")
