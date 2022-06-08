dcbfa307f987b94bd6e1fa92ed25560d496d866a
Raskolnikov <jerry.tann@endress.com>
2022-06-08 14:11:07 +0800

    ✨ remove palindromic initial

---
+++ 15RemovePalindromic/removepalindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removePalindromeSub(self, s: str) -> int:
+        return
+
+sol = Solution()
+
+
+s = "ababa"
+out = 1
+print(f"expect {out} {sol.removePalindromeSub(s)}")
+s = "abb"
+out = 2
+print(f"expect {out} {sol.removePalindromeSub(s)}")
+s = "baabb"
+out = 2
+print(f"expect {out} {sol.removePalindromeSub(s)}")
