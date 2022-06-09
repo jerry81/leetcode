339bcd9a4040319989c5d803812b7e7d25a90f9e
Raskolnikov <jerry.tann@endress.com>
2022-06-09 11:11:00 +0800

    ✅ pass broken test

--- 16TwoSum/twosum.py
+++ 16TwoSum/twosum.py
@@ -2,18 +2,18 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        accum = 0
+        accum = None
         while accum != target:
-            accum = 0
             for i1 in range(len(numbers)):
                 accum = numbers[i1]
                 if accum > target:
                     break
-                for i2 in range(i1,len(numbers)):
+                for i2 in range(i1+1,len(numbers)):
                   accum = numbers[i1] + numbers[i2]
                   if accum > target:
                       break 
                   if accum == target:
+                      print(f"returning ")
                       return [i1+1,i2+1]
         return 
 
@@ -31,5 +31,10 @@ print(f"expect {Output} {sol.twoSum(numbers, target)}")
 
 numbers = [-1,0]
 target = -1
+Output = [1,2]
+print(f"expect {Output} {sol.twoSum(numbers, target)}")
+
+numbers = [0,0,3,4]
+target = 0
 Output = [1,2]
 print(f"expect {Output} {sol.twoSum(numbers, target)}")
