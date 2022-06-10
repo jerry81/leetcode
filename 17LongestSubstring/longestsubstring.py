2eb95285e22d08a924582587b732ec767621b177
Raskolnikov <jerry.tann@endress.com>
2022-06-10 11:35:17 +0800

    ✨ longest substring, initial

---
+++ 17LongestSubstring/longestsubstring.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        return 
+
+sol = Solution()
+
+
+s = "abcabcbb"
+Output = 3
+print(f"expect {Output} {sol.lengthOfLongestSubstring(s)}")
+
+s = "bbbbb"
+Output = 1
+print(f"expect {Output} {sol.lengthOfLongestSubstring(s)}")
+
+s = "pwwkew"
+Output = 3
+print(f"expect {Output} {sol.lengthOfLongestSubstring(s)}")
