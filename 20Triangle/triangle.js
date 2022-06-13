e04990c7b08bcf8cfa988a6a797ec608377a38b4
Raskolnikov <jerry.tann@endress.com>
2022-06-13 12:42:10 +0800

    ✅ confirm tree built correctly

--- 20Triangle/triangle.js
+++ 20Triangle/triangle.js
@@ -2,33 +2,49 @@
  * @param {number[][]} triangle
  * @return {number}
  */
- function buildTree(triangle) {
-    h = triangle.length 
+function buildTree(triangle) {
+    h = triangle.length
     previousNodes = []
     for (let i = h - 1; i >= 0; i--) {
         curRow = triangle[i]
         nextNodes = []
         for (let j = 0; j < curRow.length; j++) {
-          children = []
-          if (previousNodes.length > 0) {
-            children = [previousNodes[j],previousNodes[j+1]]
-          }
-          newNode = {v: triangle[i][j], c: children}
-          nextNodes.push(newNode)
+            children = []
+            if (previousNodes.length > 0) {
+                children = [previousNodes[j], previousNodes[j + 1]]
+            }
+            newNode = { v: triangle[i][j], c: children }
+            nextNodes.push(newNode)
         }
         previousNodes = nextNodes
     }
     console.error(`previousNodes is ${JSON.stringify(previousNodes)}`)
- }
- var minimumTotal = function(triangle) {
+}
+var minimumTotal = function (triangle) {
     // build tree
     buildTree(triangle)
 };
 
-triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+triangle = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
 Output = 11
 console.error(`expect ${Output}, ${minimumTotal(triangle)}`)
 
 triangle = [[-10]]
 Output = -10
 console.error(`expect ${Output}, ${minimumTotal(triangle)}`)
+
+// [{ 
+//     "v": 2, "c": [{ "v": 3, 
+//                     "c": [{
+//                          "v": 6, "c": [{
+//                              "v": 4, "c": [] }, { 
+//                                 "v": 1, "c": [] }] },
+//                           { "v": 5, "c": [{ 
+//                             "v": 1, "c": [] },
+//                              { "v": 8, "c": [] }] }] },
+//                  { "v": 4, "c": [{ 
+//                     "v": 5, "c": [{ 
+//                         "v": 1, "c": [] }, { "v": 8, "c": [] }] },
+//                          { "v": 7, "c": [{ 
+//                             "v": 8, "c": [] }, { "v": 3, "c": [] }]
+//                          }] }] }]
