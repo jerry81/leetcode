9162aba5b7bbfd32647668fbe19940f1eec5999e
Raskolnikov <jerry.tann@endress.com>
2022-06-14 17:01:33 +0800

    ✨ start delete 2 strings problem

---
+++ 21Delete2Strings/delete2strings.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+ var minDistance = function(word1, word2) {
+    // 
+};
+
+let word1 = "sea"
+let word2 = "eat"
+let Output = 2
+console.log(`expect ${Output} ${minDistance(word1, word2)}`)
+
+word1 = "leetcode"
+word2 = "etco"
+Output = 4
+console.log(`expect ${Output} ${minDistance(word1, word2)}`)
