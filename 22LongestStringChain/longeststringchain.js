8d67679cc01aba11e26765fe9fba3753d508b4fe
Raskolnikov <jerry.tann@endress.com>
2022-06-15 12:46:45 +0800

    📝 recursion analysis

--- 22LongestStringChain/longeststringchain.js
+++ 22LongestStringChain/longeststringchain.js
@@ -32,9 +32,37 @@
     }
 }
 
+function lsc(words, memo = {}) {
+    if (words.length == 0) {
+      return 0 
+    }
+
+    if (words.length == 1) {
+        return 1 
+    }
+
+    if (words.length == 2) {
+        return 
+    }
+
+    /* 
+    given [abc, abcd, abce, abcdefg, abcefgh, abcdefgh, zzzzzzzzz]
+    [abc, abcd, abce, abcdefg, abcefgh, abcdefgh, zzzzzzzzz]
+    next iteration should try zzzzzzzzz with a reduced set
+    should also try without zzzzzzzzz 
+    abc -> [abc, abcd] [abcdefg,abcefgh,abcdefgh]
+    abc -> [abc abce] [abcdefg,abcefgh,abcdefgh]
+    [abc,abcd] -> [abc,abcd] [abcdefg,abcdefgh]
+    [abc,abce] -> [abc,abce] [abcefgh,abcdefgh]
+    [abc,abcd,abcdefg] -> [abcdefgh]
+    [abc,abce,abcefgh] -> [abcdefgh]
+    */
+}
+
 var longestStrChain = function(words) {
     let sorted = words.sort((x,b) => x.length - b.length)
     let cur = sorted[sorted.length-1]
+    // must consider all possible chains 
     let l = 1
     for (let j = sorted.length-2; j >= 0; j--) {
       let a = sorted[j]
@@ -63,4 +91,10 @@ console.log(`expect ${Output}, ${longestStrChain(words)}`)
 words = ["abcd","dbqca"]
 Output = 1
 
+console.log(`expect ${Output}, ${longestStrChain(words)}`)
+
+words = ["a","aa","aab","aabb","bbaac"]
+Output = 4
+// must handle case where the longest string doesn't contain the last item 
+// only way is to find all chains 
 console.log(`expect ${Output}, ${longestStrChain(words)}`)
