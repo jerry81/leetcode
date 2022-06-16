c9f750b2f578026818c6127c12564c892ad8ebf6
Raskolnikov <jerry.tann@endress.com>
2022-06-17 07:43:16 +0800

    rotate array initial

---
+++ 27RotateArray/rotate_array.rb
@@ -0,0 +1,13 @@
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Void} Do not return anything, modify nums in-place instead.
+def rotate(nums, k)
+    
+end
+
+nums = [1,2,3,4,5,6,7]
+k = 3
+expect = [5,6,7,1,2,3,4]
+
+Input: nums = [-1,-100,3,99], k = 2
+expect = [3,99,-1,-100]
