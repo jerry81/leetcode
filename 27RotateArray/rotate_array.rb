1732c89c8dfba4bc85de12143dd69d4faa158869
Raskolnikov <jerry.tann@endress.com>
2022-06-17 20:49:31 +0800

    ✨ rotate array

--- 27RotateArray/rotate_array.rb
+++ 27RotateArray/rotate_array.rb
@@ -2,12 +2,23 @@
 # @param {Integer} k
 # @return {Void} Do not return anything, modify nums in-place instead.
 def rotate(nums, k)
-    
+    res = nums.clone
+
+    res.each_with_index do |x,i|
+        nums[(i+k) % res.size] = x
+    end
+    nums
 end
 
 nums = [1,2,3,4,5,6,7]
+# 0,1,2,3,4,5,6
 k = 3
 expect = [5,6,7,1,2,3,4]
-
-Input: nums = [-1,-100,3,99], k = 2
+# 4,5,6,0,1,2,3
+puts "expect #{expect} #{rotate(nums,k)}"
+puts "nums is now #{nums}"
+nums = [-1,-100,3,99] 
+k = 2
 expect = [3,99,-1,-100]
+puts "expect #{expect} #{rotate(nums,k)}"
+puts "nums is #{nums}"
