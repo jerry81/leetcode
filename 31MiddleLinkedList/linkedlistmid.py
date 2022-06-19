d60744f460dd07a125dfb561cedc72603a2e74d5
Raskolnikov <jerry.tann@endress.com>
2022-06-19 08:34:45 +0800

    ✨ linkedlist mid initial

---
+++ 31MiddleLinkedList/linkedlistmid.py
@@ -0,0 +1,13 @@
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return 
+
