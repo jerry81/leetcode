de88a6cfaaf04ec1ebfbced1bf72d91e1d636aae
Raskolnikov <jerry.tann@endress.com>
2022-06-20 18:48:41 +0800

    ✨ setup test case

--- 35MaxAreaIsland/maxareaisland.py
+++ 35MaxAreaIsland/maxareaisland.py
@@ -1,8 +1,16 @@
 from typing import List
 
-
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         return 
 
 sol = Solution()
+
+grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+expect = 6
+print(f"expect {expect} {sol.maxAreaOfIsland(grid)}")
+
+grid = [[0,0,0,0,0,0,0,0]]
+expect = 0
+print(f"expect {expect} {sol.maxAreaOfIsland(grid)}")
+
