6bbd7e2449a2a3732aee57f13fc88959052724c4
Raskolnikov <jerry.tann@endress.com>
2022-06-20 12:27:40 +0800

    ✨ setup local testing

--- 33PermutationInString/permutationinstring.py
+++ 33PermutationInString/permutationinstring.py
@@ -1,3 +1,16 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         return 
+
+sol = Solution()
+s1 = "ab"
+s2 = "eidbaooo"
+expect = "True"
+# Explanation: s2 contains one permutation of s1 ("ba").
+print(f"expect {expect} {sol.checkInclusion(s1,s2)}")
+
+s1 = "ab"
+s2 = "eidboaoo"
+expect = "false"
+print(f"expect {expect} {sol.checkInclusion(s1,s2)}")
+ 
