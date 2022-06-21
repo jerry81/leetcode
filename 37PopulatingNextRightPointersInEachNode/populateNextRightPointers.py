7fa3981972f4d9814cab44e0950c2323b614a11d
Raskolnikov <jerry.tann@endress.com>
2022-06-21 20:36:55 +0800

    ✨ impl a BFS

--- 37PopulatingNextRightPointersInEachNode/populateNextRightPointers.py
+++ 37PopulatingNextRightPointersInEachNode/populateNextRightPointers.py
@@ -12,5 +12,21 @@ from typing import Optional
 
 
 class Solution:
+    def flattenNodes(self,nodes,roots):
+      # bfs
+      newroots = []
+      if len(roots) == 0:
+        return
+      for r in roots:
+        nodes.append(r.val)
+        if r.left is not None:
+          newroots.append(r.left)
+        if r.right is not None:
+          newroots.append(r.right)
+      self.flattenNodes(nodes,newroots)
+
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        return 
+        # first enumerate as flat list
+        nodes = []
+        self.flattenNodes(nodes,root)
+        print(f"nodes is now {nodes}")
