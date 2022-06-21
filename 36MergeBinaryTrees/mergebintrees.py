d21bde192946fb16ded752d70854558d8ec3a59d
Raskolnikov <jerry.tann@endress.com>
2022-06-21 11:37:31 +0800

    ✅ pass all tests

--- 36MergeBinaryTrees/mergebintrees.py
+++ 36MergeBinaryTrees/mergebintrees.py
@@ -11,16 +11,23 @@ class TreeNode:
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
         newroot = TreeNode()
-        if root1.val is not None and root2.val is not None:
-            newroot.val = root1.val + root2.val 
-        elif root1.val is not None:
+        if root1 is None and root2 is None:
+            return None 
+        elif root1 is not None and root2 is not None:
+          if root1.val is not None and root2.val is not None:
+              newroot.val = root1.val + root2.val 
+        elif root1 is not None:
             newroot.val = root1.val 
-        elif root2.val is not None:
+        elif root2 is not None:
             newroot.val = root2.val 
         else:
             return None
-        newleft = self.mergeTrees(root1.left, root2.left)
-        newright = self.mergeTrees(root1.right,root2.right)
+        left1 = None if root1 is None else root1.left 
+        left2 = None if root2 is None else root2.left
+        right1 = None if root1 is None else root1.right
+        right2 = None if root2 is None else root2.right
+        newleft = self.mergeTrees(left1, left2)
+        newright = self.mergeTrees(right1,right2)
         newroot.left = newleft 
         newroot.right = newright
         return newroot
