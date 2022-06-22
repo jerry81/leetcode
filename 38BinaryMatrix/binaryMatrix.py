f81b64b4595ca7d8bcc3ef897e1cc12246067535
Raskolnikov <jerry.tann@endress.com>
2022-06-22 12:44:05 +0800

    ✨ setup local testing

--- 38BinaryMatrix/binaryMatrix.py
+++ 38BinaryMatrix/binaryMatrix.py
@@ -4,3 +4,13 @@ from typing import List
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         return 
+
+sol = Solution()
+
+mat = [[0,0,0],[0,1,0],[0,0,0]]
+expect = [[0,0,0],[0,1,0],[0,0,0]]
+print(f"expect {expect} {sol.updateMatrix(mat)}")
+
+mat = [[0,0,0],[0,1,0],[1,1,1]]
+expect = [[0,0,0],[0,1,0],[1,2,1]]
+print(f"expect {expect} {sol.updateMatrix(mat)}")
