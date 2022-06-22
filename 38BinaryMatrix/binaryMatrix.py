82289c74c3537e58e58a94ebed4825e2b3861407
Raskolnikov <jerry.tann@endress.com>
2022-06-23 07:38:01 +0800

    📝 package up the answer

--- 38BinaryMatrix/binaryMatrix.py
+++ 38BinaryMatrix/binaryMatrix.py
@@ -3,11 +3,12 @@ from typing import List
 
 
 class Solution:
-    def getMinDist(self,x,y,grid,memo,max_x,max_y,visited=defaultdict(bool)):
+    def getMinDist(self,x,y,grid,memo,max_x,max_y,visited=defaultdict(bool),updated=[]):
         if memo[f"{y},{x}"] != -1:
             return memo[f"{y},{x}"]
         if grid[y][x] == 0:
             memo[f"{y},{x}"] = 0
+            updated[y][x] = 0
             visited[f"{y},{x}"] = True
             return 0
 
@@ -20,8 +21,9 @@ class Solution:
               elif grid[y][x-1] == 0:
                 left = 0
                 memo[f"{y},{x-1}"] = 0
+                updated[y][x-1] = 0
               elif visited[f"{y},{x-1}"] is None:
-                left = self.getMinDist(x-1,y,grid,memo,max_x,max_y)
+                left = self.getMinDist(x-1,y,grid,memo,max_x,max_y, updated = updated)
               visited[f"{y},{x-1}"] = True
               compareArr.append(left)
               
@@ -32,8 +34,9 @@ class Solution:
               elif grid[y-1][x] == 0:
                 up = 0
                 memo[f"{y-1},{x}"] = 0
+                updated[y-1][x] = 0
               elif visited[f"{y-1},{x}"] is None:
-                up = self.getMinDist(x,y-1,grid,memo,max_x,max_y)
+                up = self.getMinDist(x,y-1,grid,memo,max_x,max_y, updated = updated)
               visited[f"{y-1},{x}"] = True
               compareArr.append(up)
               
@@ -44,8 +47,9 @@ class Solution:
                 elif grid[y][x+1] == 0:
                     right = 0
                     memo[f"{y},{x+1}"] = 0
+                    updated[y][x+1] = 0
                 else:
-                    right = self.getMinDist(x+1,y,grid,memo,max_x,max_y)
+                    right = self.getMinDist(x+1,y,grid,memo,max_x,max_y, updated = updated)
                 visited[f"{y},{x+1}"] = True
                 compareArr.append(right)
         if y+1 < max_y:
@@ -55,27 +59,28 @@ class Solution:
                 elif grid[y+1][x] == 0:
                     down = 0
                     memo[f"{y+1},{x}"] = 0
+                    updated[y+1][x] = 0
                 else:
-                    down = self.getMinDist(x,y+1,grid,memo,max_x,max_y)
+                    down = self.getMinDist(x,y+1,grid,memo,max_x,max_y, updated = updated)
                 visited[f"{y+1},{x}"] = True
                 compareArr.append(down)
 
         res = 1 + min(compareArr)
         memo[f"{y},{x}"] = res 
+        updated[y][x] = res
         return res 
         
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         max_y = len(mat)
         max_x = len(mat[0])
-        updated = [[0]*len(mat) for _ in range(len(mat[0]))] # init 2d matrix 
+        updated = [[0]*len(mat[0]) for _ in range(len(mat))] # init 2d matrix 
         memo = defaultdict(lambda: -1)
         for y in range(len(mat)):
             line = mat[y]
             for x in range(len(line)):
                 if memo[f"{y},{x}"] == -1:
-                    self.getMinDist(x,y,mat,memo,max_x,max_y)
-        
-        return 
+                    self.getMinDist(x,y,mat,memo,max_x,max_y, updated=updated)
+        return updated
 
 sol = Solution()
 
