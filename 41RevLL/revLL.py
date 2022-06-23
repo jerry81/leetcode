8ab36930d982fed7b0f5187396be0ceab7b0bfa8
Raskolnikov <jerry.tann@endress.com>
2022-06-23 19:25:56 +0800

    ✨ reverse a linked list, initial

---
+++ 41RevLL/revLL.py
@@ -0,0 +1,11 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import Optional
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return 
