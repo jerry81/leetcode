cef104700953e55088e6bf5656bf0289fc74d0ec
Raskolnikov <jerry.tann@endress.com>
2022-06-23 19:50:16 +0800

    ✅ pass test case

--- 41RevLL/revLL.py
+++ 41RevLL/revLL.py
@@ -3,6 +3,9 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+    def __str__(self):
+        return f"val:{self.val}, next: {self.next}"
+
 from typing import Optional
 
 
@@ -14,9 +17,32 @@ class Solution:
             return head 
         
         prev = None 
+        newHead = None 
         while head is not None:
-          tail = head.next 
-          head.next = prev  
-          prev = head 
+          # " break off " head 
+          tail = head.next
+          ntail = newHead
+          if newHead is None:
+            newHead = head
+            newHead.next = None
+          else:
+            head.next = None
+            newHead = head 
+            newHead.next = ntail
           head = tail
-        return tail 
+        return newHead 
+
+"""
+1,2,3
+1 
+2,3 
+
+2,1
+3
+3,2,1
+"""
+n2 = ListNode(3)
+n1 = ListNode(2,n2)
+n0 = ListNode(1,n1)
+sol = Solution()
+print(f"expect something {sol.reverseList(n0)}")
