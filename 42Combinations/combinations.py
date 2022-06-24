be0e1685943622f105354c68fe088bc72ee43d84
Raskolnikov <jerry.tann@endress.com>
2022-06-24 11:28:46 +0800

    ✨ cominations:setup tests

--- 42Combinations/combinations.py
+++ 42Combinations/combinations.py
@@ -4,3 +4,22 @@ from typing import List
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         return 
+
+sol = Solution()
+
+n = 4
+k = 2
+expect = [\
+  [2,4],\
+  [3,4],\
+  [2,3],\
+  [1,2],\
+  [1,3],\
+  [1,4],\
+]
+print(f"expect {expect} {sol.combine(n,k)}")
+
+n = 1
+k = 1
+expect = [[1]]
+print(f"expect {expect} {sol.combine(n,k)}")
