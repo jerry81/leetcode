2c2af396bddadc2c84c130f55b7ed6fe0bc8eeab
Raskolnikov <jerry.tann@endress.com>
2022-06-25 17:04:08 +0800

    ✨ setup tests

--- 44LetterCasePermutation/lettercase.py
+++ 44LetterCasePermutation/lettercase.py
@@ -4,3 +4,12 @@ from typing import List
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         return 
+
+sol = Solution()
+s = "a1b2"
+expect = ["a1b2","a1B2","A1b2","A1B2"]
+print(f"expect {expect}\nsol is {sol.letterCasePermutation(s)}")
+
+s = "3z4"
+expect = ["3z4","3Z4"]
+print(f"expect {expect}\nsol is {sol.letterCasePermutation(s)}")
