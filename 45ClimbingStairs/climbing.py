b0f98de540527d0a10951d76a855b9b395f563b0
Raskolnikov <jerry.tann@endress.com>
2022-06-25 20:49:49 +0800

    ✨setup tests

--- 45ClimbingStairs/climbing.py
+++ 45ClimbingStairs/climbing.py
@@ -1,3 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         return 
+
+s = Solution()
+n = 2
+a = 2
+
+print(f"a is {a}\ns is {s.climbStairs(a)}")
+
+n = 3
+a = 3
+print(f"a is {a}\ns is {s.climbStairs(a)}")
