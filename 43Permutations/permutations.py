ab35d7b2c9c0a8e0325d86fef1368d65b5a15bfe
Raskolnikov <jerry.tann@endress.com>
2022-06-25 12:48:10 +0800

    ✨ setup more tests

--- 43Permutations/permutations.py
+++ 43Permutations/permutations.py
@@ -1,9 +1,19 @@
 from typing import List
 class Solution:
+    def getNext(self, nums):
+        # 1,2,3 -> 1,3,2
+        for i in range(len(nums)-1, 1, -1):
+            if nums[i-1] < nums[i]:
+              print(f"swap! {nums[i-1]} and {nums[i]}")
+        return 
+
     def permute(self, nums: List[int]) -> List[List[int]]:
         return 
 
+
+
 sol = Solution()
+print(f"getNext([1,2,3]) is {sol.getNext([1,2,3])}")
 nums = [1,2,3]
 ex = [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 print(f"expect {ex}\n{sol.permute(nums)}")
@@ -15,3 +25,16 @@ print(f"expect {ex}\n{sol.permute(nums)}")
 nums = [1]
 ex = [[1]]
 print(f"expect {ex}\n{sol.permute(nums)}")
+
+nums = [1,2,3,4]
+ex = [[1,2,3,4],[1,2,4,3],[1,3,2,4],[1,3,4,2],[1,4,2,3],[1,4,3,2],\
+    [2,1,3,4],[2,1,4,3],[2,3,1,4],[2,3,4,1],[2,4,1,3],[2,4,3,1],\
+    [3,1,2,4],[3,1,4,2],[3,2,1,4],[3,2,4,1],[3,4,1,2],[3,4,2,1],\
+    [4,1,2,3],[4,1,3,2],[4,2,1,3],[4,2,3,1],[4,3,1,2],[4,3,2,1]]
+print(f"expect {ex}\n{sol.permute(nums)}")
+
+arr1 = [1,2,3]
+arr2 = [1,2,3]
+print(f"equals test {arr1==arr2}")
+arr3 = [3,2,1]
+print(f"equals test {arr1==arr3}")
