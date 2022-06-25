1f55174d9e21f42dc50a2f0345e53f1ab05884f4
Raskolnikov <jerry.tann@endress.com>
2022-06-25 21:44:05 +0800

    ✨ it was the fibonnacci...

--- 45ClimbingStairs/climbing.py
+++ 45ClimbingStairs/climbing.py
@@ -1,10 +1,18 @@
+from collections import defaultdict
+
+
 class Solution:
+    def fib(self,n,memo):
+       if memo[n]:
+        return memo[n]
+       memo[n] = memo[n-1] + memo[n-2]
+       return memo[n]
     def climbStairs(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-        return (self.climbStairs(n-1) - self.climbStairs(n-2)) + self.climbStairs(n-1)
+      memo=defaultdict(lambda: 0)
+      memo[0] = 0 
+      memo[1] = 1 
+      memo[2] = 2
+      return self.fib(n, memo)
 
 s = Solution()
 n = 2
@@ -20,50 +28,82 @@ n = 3
 a = 3
 print(f"a is {a}\ns is {s.climbStairs(n)}")
 """
-1,1,1
-2,1
+1,1,1   all 1s
+2,1     one 1 one 2
 1,2
 """
+
 n = 4
 a = 5
+
 print(f"a is {a}\ns is {s.climbStairs(n)}")
 """
-1,1,1,1
-2,1,1
+1,1,1,1  all ones
+2,1,1    one 2
 1,2,1
 1,1,2
-2,2
+2,2      two 2s 
 """
 n = 5
 a = 8
 print(f"a is {a}\ns is {s.climbStairs(n)}")
 """
-1,1,1,1,1
-2,1,1,1
+1,1,1,1,1   
+2,1,1,1   one 2
 1,2,1,1
 1,1,2,1
 1,1,1,2
-2,2,1
+2,2,1     two 2s
 2,1,2
 1,2,2
 """
 n=6
-a=12
+a=13
 print(f"a is {a}\ns is {s.climbStairs(n)}")
 """
 1,1,1,1,1,1
-2,1,1,1,1
+2,1,1,1,1    one 2
 1,2,1,1,1
 1,1,2,1,1
 1,1,1,2,1
 1,1,1,1,2
-2,2,1,1
+2,2,1,1      two 2s
 2,1,2,1
 1,2,2,1
 1,1,2,2
 2,1,1,2
-2,2,2
+1,2,1,2
+2,2,2         three 2s
 """
+n=7
+"""
+1,1,1,1,1,1,1
+2,1,1,1,1,1
+1,2,1,1,1,1
+1,1,2,1,1,1
+1,1,1,2,1,1
+1,1,1,1,2,1
+1,1,1,1,1,2
+2,2,1,1,1
+2,1,2,1,1
+2,1,1,2,1
+2,1,1,1,2
+1,2,2,1,1
+1,2,1,2,1
+1,2,1,1,2
+1,1,2,2,1
+1,1,2,1,2
+1,1,1,2,2
+1,2,2,2
+2,1,2,2
+2,2,1,2
+2,2,2,1
+"""
+a = 21
+print(f"a is {a}\ns is {s.climbStairs(n)}")
+
+n = 10 
+a = "bigger"
 print(f"a is {a}\ns is {s.climbStairs(n)}")
 
 """
@@ -73,3 +113,4 @@ print(f"a is {a}\ns is {s.climbStairs(n)}")
 2,2,1,1,1,1
 2,2,2,1
 """
+
