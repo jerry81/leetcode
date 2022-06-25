65907ae2d3e33d6491322f82c7696acc63694a8e
Raskolnikov <jerry.tann@endress.com>
2022-06-25 10:09:25 +0800

    ✨ trying simple approach

--- 42Combinations/combinations.py
+++ 42Combinations/combinations.py
@@ -1,16 +1,34 @@
 from typing import List
-
+from collections import defaultdict
 
 class Solution:
+    def nextCombination(self, n: int, k:int, cur:List):
+        ret = cur.copy()
+        for i in range(k):
+          idx = k-1-i
+          rightmost = ret[idx]
+          offset = n-idx
+          if (rightmost <= offset):
+            ret[idx] += 1
+            return ret
+          else: 
+            # if idx == 0:
+            break
+            # 1,4 
+            # -> 2,3
+            # 2,4
+            # -> 3,4
+        return None
+
     def combine(self, n: int, k: int) -> List[List[int]]:
-        ret = []
-        for iter in range(k):
-          for i in range(1,n+1):
-              for j in range(i,n+1):
-                  if (j != i):
-                    ret.append([i,j])
-        print (f"ret is {ret}")
-        return ret
+        res = []
+        first = list(range(1,k+1))
+        res.append(first)
+        cur = self.nextCombination(n,k,first)
+        while cur != None:
+            res.append(cur)
+            cur = self.nextCombination(n,k,cur) 
+        return res 
 
 sol = Solution()
 
@@ -39,6 +57,36 @@ expect = [\
   [1,3,4],\
   [2,3,4]
 ]
+
+"""
+1,2,3
+1,2,4
+1,3,4
+2,3,4
+"""
+
+"""
+1,2
+1,3
+1,4
+2,3
+2,4
+3,4
+"""
+
+"""
+1,2,3 bigpivot = 0, pivot = 2
+1,2,4 
+1,2,5 
+1,2,6
+1,3,4 bigpivot = 0, pivot 1
+1,3,5
+1,3,6
+1,4,5 bigpivot 0 pivot 1
+1,4,6
+1,5,6
+2,3,4 bigpivot 1 
+"""
 
 # n choose k (aka binomial coefficient)
 # n!/(k!(n-k)!)
