8246c0f86c2b38eac297d536fe03b3fda21ec114
Raskolnikov <jerry.tann@endress.com>
2022-06-25 22:02:34 +0800

    ✨ setup tests

--- 46HouseRobber/houserobber.py
+++ 46HouseRobber/houserobber.py
@@ -4,3 +4,12 @@ from typing import List
 class Solution:
     def rob(self, nums: List[int]) -> int:
         return 
+
+s=Solution()
+nums = [1,2,3,1]
+a= 4
+print(f"a {a}\n{s.rob(nums)}")
+
+nums = [2,7,9,3,1]
+a = 12
+print(f"a {a}\n{s.rob(nums)}")
