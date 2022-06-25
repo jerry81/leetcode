49c70a28a53d7ebbe3e162e45fb9457a15b457eb
Raskolnikov <jerry.tann@endress.com>
2022-06-25 14:06:06 +0800

    🐛 ✅ fix logic error

--- 43Permutations/permutations.py
+++ 43Permutations/permutations.py
@@ -1,19 +1,32 @@
 from typing import List
 class Solution:
     def getNext(self, nums):
-        # 1,3,2 -> 2,1,3
-        for i in range(len(nums)-1, 0, -1):
-            for j in range(i-1,-1,-1):
-              if nums[j] < nums[i]:
-                nums[j],nums[i] = nums[i],nums[j]
-                subs = nums[j+1:]
-                nums[j+1:] = subs[::-1]
-                return nums
+        # 2,5,6,4 expect 2,6,4,5
+        #1,3,4,2 expect 1,4,2,3
+        # 2 vs 4
+        # 2 vs 3
+        # 2 vs 1
+        # vs 
+        # 2 vs 4
+        # 4 vs 3
+        # swap 
+        # 1,3,2
+        # 1,3,2
+        # 2,1,3
+        for pivot in range(len(nums)-2,-1,-1):
+          for i in range(len(nums)-1, pivot, -1):
+            # 2,1,0
+                  if nums[pivot] < nums[i]:
+                    nums[i],nums[pivot] = nums[pivot],nums[i]
+                    subs = nums[pivot+1:]
+                    nums[pivot+1:] = subs[::-1]
+                    return nums
         return None
 
     def permute(self, nums: List[int]) -> List[List[int]]:
         nextres = nums.copy()
         nextres.sort()
+        print(f"starting is {nextres}")
         res = []
         while nextres is not None:
           curRes = nextres.copy()
@@ -26,6 +39,10 @@ class Solution:
 sol = Solution()
 print(f"getNext([1,2,3]) is {sol.getNext([1,2,3])}")
 print(f"getNext([1,3,2]) is {sol.getNext([1,3,2])}")
+print(f"getNext[2,5,6,4] is {sol.getNext([2,5,6,4])}")
+
+print(f"getNext[1,3,4,2] is {sol.getNext([1,3,4,2])}")
+
 nums = [1,2,3]
 ex = [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 print(f"expect {ex}\n{sol.permute(nums)}")
@@ -44,6 +61,11 @@ ex = [[1,2,3,4],[1,2,4,3],[1,3,2,4],[1,3,4,2],[1,4,2,3],[1,4,3,2],\
     [3,1,2,4],[3,1,4,2],[3,2,1,4],[3,2,4,1],[3,4,1,2],[3,4,2,1],\
     [4,1,2,3],[4,1,3,2],[4,2,1,3],[4,2,3,1],[4,3,1,2],[4,3,2,1]]
 print(f"expect {ex}\n{sol.permute(nums)}")
+print(f"expect 24 {len(sol.permute(nums))}")
+
+nums = [5,4,6,2]
+ex = 24
+print(f"expect {ex}\n{len(sol.permute(nums))}")
 
 nums = [0,1,-1]
 ex = [[1]]
