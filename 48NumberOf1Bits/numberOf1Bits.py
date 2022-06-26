771d6a6964d8a0e4e177deaff1b7bc1b9949f073
Raskolnikov <jerry.tann@endress.com>
2022-06-26 11:55:45 +0800

    🐛 fix tests  ✅ pass tests

--- 48NumberOf1Bits/numberOf1Bits.py
+++ 48NumberOf1Bits/numberOf1Bits.py
@@ -1,3 +1,22 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        return 
+        as_b = format(n,'032b')
+        one_count = 0
+        for i in as_b:
+            if i == '1':
+                one_count+=1
+        return one_count
+
+s = Solution()
+n = 0b00000000000000000000000000001011
+a = 3
+print(f"expect {a}\ns is {s.hammingWeight(n)}")
+
+n = 0b00000000000000000000000010000000
+a = 1
+print(f"expect {a}\ns is {s.hammingWeight(n)}")
+
+
+n = 0b11111111111111111111111111111101
+a = 31
+print(f"expect {a}\ns is {s.hammingWeight(n)}")
