4593da365288863bd822beb2d03f3dcfe253e758
Raskolnikov <jerry.tann@endress.com>
2022-06-26 13:58:26 +0800

    ✅ pass example tests

--- 49ReverseBits/reversebits.py
+++ 49ReverseBits/reversebits.py
@@ -1,3 +1,15 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        return 
+        as_b = format(n,"032b")
+        rev = list(as_b)[::-1]
+        return int("".join(rev),2) # TIL - convert binary string to int 
+
+s = Solution()
+n = 0b00000010100101000001111010011100
+ret = 964176192 # (00111001011110000010100101000000)
+print(f"expect is {ret}\ns is {s.reverseBits(n)}")
+
+n = 0b11111111111111111111111111111101
+ret = 3221225471 # (10111111111111111111111111111111)
+print(f"expect is {ret}\ns is {s.reverseBits(n)}")
+
