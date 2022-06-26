6729c28c59426d3eca50e9721f80e63118361b88
Raskolnikov <jerry.tann@endress.com>
2022-06-26 14:04:35 +0800

    ✨ setup tests

--- 50SingleNumber/singleNumber.py
+++ 50SingleNumber/singleNumber.py
@@ -3,3 +3,16 @@ from typing import List
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         return 
+
+s = Solution()
+nums = [2,2,1]
+a = 1
+print(f"a is {a}\ns is {s.singleNumber(nums)}")
+
+nums = [4,1,2,1,2]
+a = 4
+print(f"a is {a}\ns is {s.singleNumber(nums)}")
+
+nums = [1]
+a = 1
+print(f"a is {a}\ns is {s.singleNumber(nums)}")
