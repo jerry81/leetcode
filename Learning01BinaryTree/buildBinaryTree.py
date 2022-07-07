297fadaf091963c83ea8344d18cd98cb8c8a7614
Raskolnikov <jerry.tann@endress.com>
2022-07-08 07:35:30 +0800

    ✨ analyze more cases

--- Learning01BinaryTree/buildBinaryTree.py
+++ Learning01BinaryTree/buildBinaryTree.py
@@ -55,10 +55,54 @@ inorder until root reached
 [15,20,7]
 [15,7,20]
 - 15 is left most of the remaining tree
-- 
+- iterate until 20 
+- this is the new left with 20 being R of the root 
 
         3
     9      20
         15    7
+
+- bigger example
+
+               8
+        4             12
+    2      6       10        14
+1     3  5  7   9     11  13    15
+
+postorder = [ 1 3 2 5 7 6 4 9 11 10 13 15 14 12 8 ] 
+inorder = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15]
+inorder starts with leftmost 
+postorder starts with bottomest leftmost leaf 
+
+with holes
+
+               8
+        4             12
+    2      6       10        14
+      3  5  7   9    11   13    15
+
+post =  3 2 5 7 6 4 9 10 13 15 14 12 8
+in = 2 3 4 5 6 7 8 9 10 12 13 14 15
+inorder and post meet at first left parent 
+post[0] = in[1]
+so post[0] is R leaf of in[1]
+new leaf = 2 
+[5,7,6,4..
+[4,5,6,7...
+
+post[0] = 5
+indexOf in(5) = 1
+
+              8
+        4             12
+    2      6       10        14
+ 1       5  7           13    15
+in = 1 2 4 5 6 7 8 10 11 12 13 14 15
+post = 1 2 5 7 6 4 11 10 13 15 14 12 8
+
+first find intersection to post[0]
+in - 0
+so we have a left leaf 
+
 """
 
