440630982c1cf0c128232b30f113345e113882e6
Raskolnikov <jerry.tann@endress.com>
2022-07-14 12:44:37 +0800

    ✅ even digits, pass all tests

--- Learning03Array101/evendigits.py
+++ Learning03Array101/evendigits.py
@@ -33,4 +33,8 @@ from typing import List
 
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
-        return 
+        res = 0
+        for n in nums:
+          if len(str(n)) % 2 == 0:
+            res+=1
+        return res
