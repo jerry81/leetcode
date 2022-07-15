ea5300e7c074f9f113d07af41f35f9750b83990b
Raskolnikov <jerry.tann@endress.com>
2022-07-15 18:37:22 +0800

    🐛 add a failing test case

--- Learning03Array101/move0s.py
+++ Learning03Array101/move0s.py
@@ -24,9 +24,23 @@ Constraints:
 Follow up: Could you minimize the total number of operations done?
 """
 
+from typing import List
+
+
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+        l = len(nums)
+        for i in range(l):
+            item = nums[i]
+            if item == 0:
+                for j in range(i,l-1):
+                    nums[j] = nums[j+1]
+                nums[-1] = 0
+
+s = Solution()
+test = [0,0,1]
+s.moveZeroes(test)
+print(f"res is {test}")
