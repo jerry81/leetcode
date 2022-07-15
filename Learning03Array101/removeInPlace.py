809e565856a216e0def3799ce48f56b8697c1924
Raskolnikov <jerry.tann@endress.com>
2022-07-15 22:12:04 +0800

    🐛  should probably read instructions better

--- Learning03Array101/removeInPlace.py
+++ Learning03Array101/removeInPlace.py
@@ -3,4 +3,15 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        return 
+        ptr = 0
+        for i in range(len(nums)):
+          n = nums[i]
+          if n != val:
+            nums[ptr] = n
+            ptr+=1
+        return ptr
+
+s=Solution()
+arr = [3,2,2,3]
+s.removeElement(arr,3)
+print(f"mod is {arr}")
