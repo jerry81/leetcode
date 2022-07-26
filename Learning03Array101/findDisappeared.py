152b704a9af235ab358a6bcc93901c38d20fbbdd
Raskolnikov <jerry.tann@endress.com>
2022-07-26 10:46:57 +0800

    ✅ pass all tests

--- Learning03Array101/findDisappeared.py
+++ Learning03Array101/findDisappeared.py
@@ -23,9 +23,19 @@ n == nums.length
 Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
 """
 
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        return
+      # dict of numbers 1 to n
+      n = len(nums)
+      l = defaultdict(bool)
+      res = []
+      for i in nums:
+        l[i] = True
+      for i in range(1,n+1):
+        if not l[i]:
+          res.append(i)
+      return res
