f13d7662022acde96573408913c0cb6dad344d12
Raskolnikov <jerry.tann@endress.com>
2022-08-04 11:07:31 +0800

    📝 official solution explanation

--- Learning05ArrayAndString/find_pivot_index.py
+++ Learning05ArrayAndString/find_pivot_index.py
@@ -73,3 +73,16 @@ class Solution:
           return i
 
       return -1
+
+      """
+      official solution: only loops once (if you don't count sum function)
+class Solution(object):
+    def pivotIndex(self, nums):
+        S = sum(nums)
+        leftsum = 0
+        for i, x in enumerate(nums):
+            if leftsum == (S - leftsum - x):
+                return i
+            leftsum += x
+        return -1
+      """
