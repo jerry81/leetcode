8b496febb2701e232ce90712e118347b6027d15a
Raskolnikov <jerry.tann@endress.com>
2022-08-11 10:42:47 +0800

    ✨ spiral first attempt

--- Learning05ArrayAndString/spiral.py
+++ Learning05ArrayAndString/spiral.py
@@ -28,4 +28,52 @@ from typing import List
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-      return
+      if len(matrix) == 0:
+        return []
+      flattened = []
+      direction = 'R'
+      curx = 0
+      cury = 0
+      mx = len(matrix[0]) - 1
+      my = len(matrix) - 1
+      mnx = 0
+      mny = 0
+      targetLen = len(matrix) * len(matrix[0])
+      while len(flattened) < targetLen:
+        flattened.append(matrix[cury][curx])
+        if direction == 'R':
+          if (curx+1) <= mx:
+            curx+=1
+            continue
+          else:
+            mx-=1
+            direction = 'D'
+            cury+=1
+            continue
+        if direction == 'D':
+          if (cury+1) <= my:
+            cury+=1
+            continue
+          else:
+            my-=1
+            direction = 'L'
+            curx-=1
+            continue
+        if direction == 'L':
+          if (curx-1) >= mnx:
+            curx-=1
+            continue
+          else:
+            mnx+=1
+            direction = 'U'
+            cury-=1
+            continue
+        if direction = 'U':
+          if (cury-1) >= mny:
+            cury-=1
+            continue
+          else:
+            mny+=1
+            direction = 'R'
+            curx+=1
+       return flattened
