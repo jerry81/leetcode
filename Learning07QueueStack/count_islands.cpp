d6745c29e147595297a93930255f4689b6abfcc3
Raskolnikov <jerry.tann@endress.com>
2022-09-06 14:27:28 +0800

    ✨ try c++ queue

--- Learning07QueueStack/count_islands.cpp
+++ Learning07QueueStack/count_islands.cpp
@@ -35,11 +35,20 @@ grid[i][j] is '0' or '1'.
 
 #include <iostream>
 #include <vector> // make modules available, CLASSPATH in java
+#include <queue>
 using namespace std; // saves us from prepending std:: corresponds to "import" in java
 
+struct item {
+  int row;
+  int col;
+};
 class Solution {
 public:
     int numIslands(vector<vector<char> >& grid) {
+      vector<vector<bool> > visited;
+      int rows = grid.size();
+      int cols = grid.at(0).size(); // dangerous
+      cerr << "rows is " << rows << " cols is " << cols << endl;
       for (vector<char> line : grid) {
         for (char item : line) {
           cerr << "item is " << item << endl;
@@ -54,12 +63,28 @@ int main() {
   Solution s = Solution();
   vector<vector<char> > v;
   vector<char> l1;
+  item i;
+  i.col = 0;
+  i.row = 0;
+  queue<item> q;
+  q.push(i);
+  item i2;
+  i2.col = 1;
+  i2.row = 1;
+  q.push(i2);
+  while (!q.empty()) {
+    cerr << "front col is " << q.front().col << endl;
+    q.pop();
+  }
   l1.push_back('1');
   l1.push_back('1');
   l1.push_back('1');
   l1.push_back('1');
   l1.push_back('0');
   v.push_back(l1);
+
+
+
   // v.push_back({'1','1','0','1','0'});
   // v.push_back({'1','1','0','0','0'});
   // v.push_back({'0','0','0','0','0'});
