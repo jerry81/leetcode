91477071d7bc2a6ddbdce5de95e6ebadc51ded0c
Raskolnikov <jerry.tann@endress.com>
2022-09-06 11:38:10 +0800

    ✨ TIL: g++ with c++11 has syntax sugars

--- Learning07QueueStack/count_islands.cpp
+++ Learning07QueueStack/count_islands.cpp
@@ -33,12 +33,37 @@ n == grid[i].length
 grid[i][j] is '0' or '1'.
 */
 
+#include <iostream>
 #include <vector> // make modules available, CLASSPATH in java
 using namespace std; // saves us from prepending std:: corresponds to "import" in java
 
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-
+    int numIslands(vector<vector<char> >& grid) {
+      for (vector<char> line : grid) {
+        for (char item : line) {
+          cerr << "item is " << item << endl;
+        }
+      }
+      return 0;
     }
 };
+
+int main() {
+  cerr << "starting up!" <<endl;
+  Solution s = Solution();
+  vector<vector<char> > v;
+  vector<char> l1;
+  l1.push_back('1');
+  l1.push_back('1');
+  l1.push_back('1');
+  l1.push_back('1');
+  l1.push_back('0');
+  v.push_back(l1);
+  // v.push_back({'1','1','0','1','0'});
+  // v.push_back({'1','1','0','0','0'});
+  // v.push_back({'0','0','0','0','0'});
+  int res = s.numIslands(v);
+  cerr << " res is " << res << endl;
+  return 0;
+}
