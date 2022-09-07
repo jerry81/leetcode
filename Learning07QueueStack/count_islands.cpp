029998a516c05d0d83420b47ce7d6a21b41f37d0
Raskolnikov <jerry.tann@endress.com>
2022-09-07 11:00:38 +0800

    🐛 inf loop on first submission

--- Learning07QueueStack/count_islands.cpp
+++ Learning07QueueStack/count_islands.cpp
@@ -38,87 +38,121 @@ grid[i][j] is '0' or '1'.
 #include <queue>
 using namespace std; // saves us from prepending std:: corresponds to "import" in java
 
-struct Item {
+struct Item
+{
   int row;
   int col;
 };
-class Solution {
+class Solution
+{
 public:
-    vector<vector<bool> > visited;
-    vector<vector<char> > sgrid;
-    int boundR;
-    int boundC;
+  vector<vector<bool>> visited;
+  vector<vector<char>> sgrid;
+  int boundR;
+  int boundC;
 
-    void bfs(int row, int col) {
-      if (visited.at(row).at(col)) {
-        return;
-      }
-
-      // mark visited
-      // neighbors
-      queue<Item> q;
-      Item start;
-      start.row = row;
-      start.col = col;
-      visited.at(row).at(col) = true;
-      q.push(start);
-      while (!q.empty()) {
-        Item cur = q.front();
-        // add u l r d
-        int ur = row-1;
-        int dr = row+1;
-        int lc = col-1;
-        int rc = col+1;
-        if ((ur >= 0) && (!visited.at(ur).at(col))) {
-        }
-        if ((dr <= 0) && (!visited.at(dr).at(col))) {
-        }
-        if ((lc >= 0) && (!visited.at(row).at(lc))) {
-        }
-        if ((rc >= 0) && (!visited.at(row).at(rc))) {
-        }
-        q.pop();
-      }
-    }
-
-    int numIslands(vector<vector<char> >& grid) {
-      sgrid = grid;
-      int rows = grid.size();
-      if (rows < 1) {
-        return 0;
-      }
-
-      int cols = grid.at(0).size();
-      for (int i = 0; i < rows; ++i) {
-        vector<bool> row;
-        for (int j = 0; j < cols; ++j) {
-          row.push_back(false);
-        }
-        visited.push_back(row);
-      }
-
-      for (vector<bool> visitedrow : visited) {
-        for (bool item : visitedrow) {
-          cerr << "visited item is " << item << endl;
-        }
-      }
-
-      for (int r = 0; r < rows; ++r) {
-        for (int c = 0; c < cols; ++c) {
-          Item i;
-          i.col = c;
-          i.row = r;
-        }
-      }
-      // for each col and row
+  int bfs(int row, int col)
+  {
+    if (visited.at(row).at(col))
+    {
       return 0;
     }
+
+    // mark visited
+    // neighbors
+    queue<Item> q;
+    Item start;
+    start.row = row;
+    start.col = col;
+    visited.at(row).at(col) = true;
+    q.push(start);
+    while (!q.empty())
+    {
+      Item cur = q.front();
+      // add u l r d
+      int ur = cur.row - 1;
+      int dr = cur.row + 1;
+      int lc = cur.col - 1;
+      int rc = cur.col + 1;
+      if ((ur >= 0) && (!visited.at(ur).at(cur.col)))
+      {
+        Item next;
+        next.row = ur;
+        next.col = cur.col;
+        q.push(next);
+      }
+      if ((dr < boundR) && (!visited.at(dr).at(cur.col)))
+      {
+        Item next;
+        next.row = dr;
+        next.col = cur.col;
+        q.push(next);
+      }
+      if ((lc >= 0) && (!visited.at(cur.row).at(lc)))
+      {
+        Item next;
+        next.row = cur.row;
+        next.col = lc;
+        q.push(next);
+      }
+      if ((rc < boundC) && (!visited.at(cur.row).at(rc)))
+      {
+        Item next;
+        next.row = cur.row;
+        next.col = rc;
+        q.push(next);
+      }
+      q.pop();
+    }
+    return 1;
+  }
+
+  int numIslands(vector<vector<char>> &grid)
+  {
+    sgrid = grid;
+    boundR = grid.size();
+    if (boundR < 1)
+    {
+      return 0;
+    }
+
+    boundC = grid.at(0).size();
+    for (int i = 0; i < boundR; ++i)
+    {
+      vector<bool> row;
+      for (int j = 0; j < boundC; ++j)
+      {
+        row.push_back(false);
+      }
+      visited.push_back(row);
+    }
+
+    for (vector<bool> visitedrow : visited)
+    {
+      for (bool item : visitedrow)
+      {
+        cerr << "visited item is " << item << endl;
+      }
+    }
+
+    int total = 0;
+    for (int r = 0; r < boundR; ++r)
+    {
+      for (int c = 0; c < boundC; ++c)
+      {
+        total += bfs(r,c);
+      }
+    }
+    // for each col and row
+    return total;
+  }
 };
 
-int main() {
-  cerr << "starting up!" <<endl;
+int main()
+{
+  cerr << "starting up!" << endl;
   Solution s = Solution();
-  vector<vector<char> > v;
+  vector<vector<char>> v;
   vector<char> l1;
   Item i;
   i.col = 0;
@@ -129,7 +163,8 @@ int main() {
   i2.col = 1;
   i2.row = 1;
   q.push(i2);
-  while (!q.empty()) {
+  while (!q.empty())
+  {
     cerr << "front col is " << q.front().col << endl;
     q.pop();
   }
@@ -140,15 +175,14 @@ int main() {
   l1.push_back('0');
   v.push_back(l1);
 
-
-
   // v.push_back({'1','1','0','1','0'});
   // v.push_back({'1','1','0','0','0'});
   // v.push_back({'0','0','0','0','0'});
   int res = s.numIslands(v);
   cerr << " res is " << res << endl;
 
-  for (int i = 0; i < 5; ++i) {
+  for (int i = 0; i < 5; ++i)
+  {
     cerr << "item is " << v.at(0).at(i) << endl;
   }
 
