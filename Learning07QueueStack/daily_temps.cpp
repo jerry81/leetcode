1db1e064e20daa6cc2188ca65ed6552141e8b43e
Raskolnikov <jerry.tann@endress.com>
2022-09-15 14:46:42 +0800

    🐛 debugging

--- Learning07QueueStack/daily_temps.cpp
+++ Learning07QueueStack/daily_temps.cpp
@@ -1,5 +1,8 @@
 /*
-Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+Given an array of integers temperatures represents the daily temperatures,
+return an array answer such that answer[i] is the number of days you have to
+wait after the ith day to get a warmer temperature. If there is no future day
+for which this is possible, keep answer[i] == 0 instead.
 
 
 
@@ -22,52 +25,65 @@ Constraints:
 1 <= temperatures.length <= 105
 30 <= temperatures[i] <= 100
 
-If the temperature is say, 70 today, then in the future a warmer temperature must be either
-71, 72, 73, ..., 99, or 100. We could remember when all of them occur next.
+If the temperature is say, 70 today, then in the future a warmer temperature
+must be either 71, 72, 73, ..., 99, or 100. We could remember when all of them
+occur next.
 */
 
-#include <vector>
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 using namespace std;
 class Solution {
-public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-      size_t size = temperatures.size();
-      cerr << "size is " << size << endl;
-      // work backwards
-      // unordered map of indexes: temps
-      // write index
-      int arr[size];
-      int lookup[size];
-      vector<int> sol;
-      for (int i = (size-1); i > -1; --i) {
-        int temp = temperatures.back();
-        temperatures.pop_back();
-        lookup[i] = temp;
-        if (i == (size-1)) {
-          arr[i] = 0;
-        }
+ public:
+  vector<int> dailyTemperatures(vector<int>& temperatures) {
+    size_t size = temperatures.size();
+    cerr << "size is " << size << endl;
+    // work backwards
+    // unordered map of indexes: temps
+    // write index
+    int arr[size];
+    int lookup[size];
+    vector<int> sol;
+    for (int i = (size - 1); i > -1; --i) {
+      int temp = temperatures.back();
+      temperatures.pop_back();
+      lookup[i] = temp;
+      arr[i] = 0;
 
-        for (int j = i; j < size; ++j) {
-          int cmp = lookup[j];
-          if (temp < cmp) {
-            arr[i] = j - i;
-          }
+      for (int j = i; j < size; ++j) {
+        int cmp = lookup[j];
+        if (temp < cmp) {
+          cerr << "j-i " << j - i << endl;
+          arr[i] = j - i;
         }
       }
-      for (int i = 0; i < size; ++i) {
-        sol.push_back(arr[i]);
-      }
-
-      return sol;
     }
+    for (int i = 0; i < size; ++i) {
+      sol.push_back(arr[i]);
+    }
+
+    return sol;
+  }
 };
 
 int main() {
   Solution s;
   vector<int> t;
-  t.push_back(1);
-  s.dailyTemperatures(t);
+  t.push_back(73);
+  t.push_back(74);
+  t.push_back(75);
+
+  t.push_back(71);
+
+  t.push_back(69);
+  t.push_back(72);
+  t.push_back(76);
+  t.push_back(73);
+
+  vector<int> sol = s.dailyTemperatures(t);
+  for (int s : sol) {
+    cerr << "s is " << s << endl;
+  }
   return 0;
 }
