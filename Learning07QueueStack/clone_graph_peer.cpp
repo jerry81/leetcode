7b66ae121854452c887182464ced04033db4a178
Raskolnikov <jerry.tann@endress.com>
2022-09-18 12:43:11 +0800

    📝 code review

--- Learning07QueueStack/clone_graph_peer.cpp
+++ Learning07QueueStack/clone_graph_peer.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-	void dfscopy(Node * node, Node * copy,vector<Node*> &vis)
+	void dfscopy(Node * node, Node * copy,vector<Node*> &vis) // vector used as visited
 	{
-	  vis[copy->val]=copy;
-	  for(auto x:node->neighbors)
+	  vis[copy->val]=copy; // vector can use array notation
+	  for(auto x:node->neighbors) // auto typing?
 	  {
 
-		if(vis[x->val]==NULL)
+		if(vis[x->val]==NULL) // not already visited
 		{
 		  Node * newNode =new Node(x->val);
 		  (copy->neighbors).push_back(newNode);
-		  dfscopy(x,newNode,vis);
+		  dfscopy(x,newNode,vis); // recurse
 		}
 
 		else
-		{
-		  (copy->neighbors).push_back(vis[x->val]);
+		{ // already visited
+		  (copy->neighbors).push_back(vis[x->val]); // still must add to neighbors (from visited map)
 		}
 	  }
 	}
