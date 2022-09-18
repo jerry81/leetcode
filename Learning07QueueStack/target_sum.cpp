eab21cb2828864eb21e59ffd36f2c10d5d440554
Raskolnikov <jerry.tann@endress.com>
2022-09-18 13:37:39 +0800

    🎨 shrink solution

--- Learning07QueueStack/target_sum.cpp
+++ Learning07QueueStack/target_sum.cpp
@@ -2,10 +2,12 @@
 
 You are given an integer array nums and an integer target.
 
-You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
+You want to build an expression out of nums by adding one of the symbols '+' and
+'-' before each integer in nums and then concatenate all the integers.
 
-For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
-Return the number of different expressions that you can build, which evaluates to target.
+For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and
+concatenate them to build the expression "+2-1". Return the number of different
+expressions that you can build, which evaluates to target.
 
 
 
@@ -13,10 +15,8 @@ Example 1:
 
 Input: nums = [1,1,1,1,1], target = 3
 Output: 5
-Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
--1 + 1 + 1 + 1 + 1 = 3
-+1 - 1 + 1 + 1 + 1 = 3
-+1 + 1 - 1 + 1 + 1 = 3
+Explanation: There are 5 ways to assign symbols to make the sum of nums be
+target 3. -1 + 1 + 1 + 1 + 1 = 3 +1 - 1 + 1 + 1 + 1 = 3 +1 + 1 - 1 + 1 + 1 = 3
 +1 + 1 + 1 - 1 + 1 = 3
 +1 + 1 + 1 + 1 - 1 = 3
 Example 2:
@@ -33,23 +33,21 @@ Constraints:
 -1000 <= target <= 1000
 
 */
+
 #include <vector>
 
 using namespace std;
 class Solution {
-private:
+ private:
   int targetR(vector<int>& nums, int sum, int pointer, int target) {
-    if (pointer == (nums.size())) {
-      return sum == target;
-    } else {
-      return targetR(nums, sum+nums[pointer], pointer+1, target) + targetR(nums,sum-nums[pointer], pointer+1, target);
-    }
+    if (pointer == nums.size()) return sum == target;
+
+    return targetR(nums, sum + nums[pointer], pointer+1, target) +
+           targetR(nums, sum - nums[pointer], pointer+1, target);
   }
 
-public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-      // binary tree?
-      reverse(nums.begin(), nums.end());
-      return targetR(nums, 0, 0, target);
-    }
+ public:
+  int findTargetSumWays(vector<int>& nums, int target) {
+    return targetR(nums, 0, 0, target);
+  }
 };
