a4a1813956153f4d919a69ebe423a9728235167f
Raskolnikov <jerry.tann@endress.com>
2023-02-16 15:37:06 +0800

    ✅ pass more tests

--- Exercises/60AddtoArray-FormOfInteger/addToArray.cpp
+++ Exercises/60AddtoArray-FormOfInteger/addToArray.cpp
@@ -5,10 +5,12 @@ Easy
 2K
 185
 Companies
-The array-form of an integer num is an array representing its digits in left to right order.
+The array-form of an integer num is an array representing its digits in left to
+right order.
 
 For example, for num = 1321, the array form is [1,3,2,1].
-Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
+Given num, the array-form of an integer, and an integer k, return the array-form
+of the integer num + k.
 
 
 
@@ -44,55 +46,71 @@ Acceptance Rate
 
 */
 
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-   class Solution {
-public:
-    vector<int> addToArrayForm(vector<int>& num, int k) {
-      // use long addition
-      vector<int> ret;
-      string asS = to_string(k);
-      bool carry = false;
-      for (int i = 0; i < asS.size(); ++i) {
-        int curD = asS[asS.size() - i - 1] - '0';
-        int oIdx = num.size() - i - 1;
-        int curSum = num[oIdx] + curD + carry;
-        if (curSum >= 10) {
-          int partialSum = curSum - 10;
-          ret.push_back(partialSum);
-          carry = true;
-        } else {
-          ret.push_back(curSum);
-          carry = false;
-        }
-      }
-      for (int i = asS.size(); i < num.size(); ++i) {
-        int oIdx = num.size() - i - 1;
-        int curSum = num[oIdx] + carry;
-        if (curSum >= 10) {
-          int partialSum = curSum - 10;
-          ret.push_back(partialSum);
-          carry = true;
-        } else {
-          ret.push_back(curSum);
-          carry = false;
-        }
+class Solution {
+ public:
+  vector<int> addToArrayForm(vector<int>& num, int k) {
+    // use long addition
 
+    vector<int> ret;
+    string asS = to_string(k);
+    bool carry = false;
+    for (int i = 0; i < asS.size(); ++i) {
+      int curD = asS[asS.size() - i - 1] - '0';
+      int oIdx = num.size() - i - 1;
+      if (oIdx < 0) {
+        ret.push_back(curD + carry);
+        carry = false;
+        continue;
       }
-       if (carry) ret.push_back(1);
-       reverse(ret.begin(), ret.end());
-        return ret;
+      int curSum = num[oIdx] + curD + carry;
+      if (curSum >= 10) {
+        int partialSum = curSum - 10;
+        ret.push_back(partialSum);
+        carry = true;
+      } else {
+        ret.push_back(curSum);
+        carry = false;
+      }
     }
-
+    for (int i = asS.size(); i < num.size(); ++i) {
+      int oIdx = num.size() - i - 1;
+      int curSum = num[oIdx] + carry;
+      if (curSum >= 10) {
+        int partialSum = curSum - 10;
+        ret.push_back(partialSum);
+        carry = true;
+      } else {
+        ret.push_back(curSum);
+        carry = false;
+      }
+    }
+    if (carry) ret.push_back(1);
+    reverse(ret.begin(), ret.end());
+    return ret;
+  }
 };
 
 int main() {
   Solution s;
   vector<int> test1;
-  test1 = {1,2,0,0};
+  test1 = {1, 2, 0, 0};
   s.addToArrayForm(test1, 34);
+  vector<int> test2;
+  test2 = {};
+  vector<int> result = s.addToArrayForm(test2, 23);
+  for (int i : result) {
+    cerr << "i is " << i << endl;
+  }
+  vector<int> test3;
+  test3 = {9,9,3};
+  result = s.addToArrayForm(test3, 7);
+  for (int i : result) {
+    cerr << "i is " << i << endl;
+  }
   return 0;
 }
