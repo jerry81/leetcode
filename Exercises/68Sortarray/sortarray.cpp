6c9f95a8f0135e7a9f2273ed1b9b1328d44d725e
Raskolnikov <jerry.tann@endress.com>
2023-03-01 12:36:04 +0800

    ✨ daily, initial

---
+++ Exercises/68Sortarray/sortarray.cpp
@@ -0,0 +1,50 @@
+/*
+
+Given an array of integers nums, sort the array in ascending order and return it.
+
+You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+Example 2:
+
+Input: nums = [5,1,1,2,0,0]
+Output: [0,0,1,1,2,5]
+Explanation: Note that the values of nums are not necessairly unique.
+
+
+Constraints:
+
+1 <= nums.length <= 5 * 104
+-5 * 104 <= nums[i] <= 5 * 104
+Accepted
+371.9K
+Submissions
+634.4K
+Acceptance Rate
+58.6%
+
+*/
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+
+    }
+};
+
+int main () {
+  Solution s;
+  vector<int> input1 = {5,2,3,1};
+  cerr << "expect [1,2,3,5] " << s.sortArray(input1);
+  return 0;
+}
