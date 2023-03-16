c0a14bd8a98a52b6b83358400b525eb4d986d301
Raskolnikov <jerry.tann@endress.com>
2023-03-16 16:48:33 +0800

    🐛 fix some turrible bugs

--- Exercises/76-100/77ConstructBinaryTreeInorderPostorder/constructBTreeInPost.cpp
+++ Exercises/76-100/77ConstructBinaryTreeInorderPostorder/constructBTreeInPost.cpp
@@ -59,6 +59,55 @@ struct TreeNode {
   TreeNode(int x, TreeNode* left, TreeNode* right)
       : val(x), left(left), right(right) {}
 };
+/*
+
+106. Construct Binary Tree from Inorder and Postorder Traversal
+Medium
+5.9K
+87
+Companies
+Given two integer arrays inorder and postorder where inorder is the inorder
+traversal of a binary tree and postorder is the postorder traversal of the same
+tree, construct and return the binary tree.
+
+
+
+Example 1:
+
+
+Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+Output: [3,9,20,null,null,15,7]
+Example 2:
+
+Input: inorder = [-1], postorder = [-1]
+Output: [-1]
+
+
+Constraints:
+
+1 <= inorder.length <= 3000
+postorder.length == inorder.length
+-3000 <= inorder[i], postorder[i] <= 3000
+inorder and postorder consist of unique values.
+Each value of postorder also appears in inorder.
+inorder is guaranteed to be the inorder traversal of the tree.
+postorder is guaranteed to be the postorder traversal of the tree.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
 
 class Solution {
   vector<int> ino;
@@ -76,14 +125,16 @@ class Solution {
     int pivot = poo[p2];
     root->val = pivot;
     int pivotino;
-    for (int i = p1; i <= p2; ++i) {
+    for (int i = i1; i <= i2; ++i) {
       if (ino[i] == pivot) {
         pivotino = i;
         break;
       }
     }
-    root->left = buildTreeR(i1, pivotino-1, p1, pivotino-1);
-    root->right = buildTreeR(pivotino+1, i2, pivotino, p2-1);
+    int interval =pivotino - i1;
+    int ppivot = p1 + interval;
+    root->left = buildTreeR(i1, pivotino-1, p1, ppivot-1);
+    root->right = buildTreeR(pivotino+1, i2,ppivot, p2-1);
     return root;
   }
 
@@ -99,3 +150,16 @@ class Solution {
     // split into next recursive iteration on remaining subarrays
   }
 };
+
+// wrong answer
+/*
+[1,2,3,4]
+postorder =
+[3,4,2,1]
+Use Testcase
+Output
+[1,null,2,null,4,null,4]
+Expected
+[1,null,2,null,4,3]
+
+*/
