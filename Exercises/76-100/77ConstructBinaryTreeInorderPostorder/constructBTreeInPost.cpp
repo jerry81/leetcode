b5a316ff4eced6493cb962405ae76fbb8d1e9f1e
Raskolnikov <jerry.tann@endress.com>
2023-03-16 15:26:27 +0800

    ✨ first attempt, daily

--- Exercises/76-100/77ConstructBinaryTreeInorderPostorder/constructBTreeInPost.cpp
+++ Exercises/76-100/77ConstructBinaryTreeInorderPostorder/constructBTreeInPost.cpp
@@ -5,7 +5,9 @@ Medium
 5.9K
 87
 Companies
-Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
+Given two integer arrays inorder and postorder where inorder is the inorder
+traversal of a binary tree and postorder is the postorder traversal of the same
+tree, construct and return the binary tree.
 
 
 
@@ -40,7 +42,8 @@ postorder is guaranteed to be the postorder traversal of the tree.
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 
@@ -48,16 +51,50 @@ postorder is guaranteed to be the postorder traversal of the tree.
 using namespace std;
 
 struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
 class Solution {
-public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+  vector<int> ino;
+  vector<int> poo;
+  TreeNode* buildTreeR(int p1, int p2) {
+    TreeNode* root;
+    if (p2-p1 == 0) return nullptr;
 
+    // special case - single item
+    if (p2-p1 == 1) {
+      root->val = poo[p2];
+      return root;
     }
+
+    int pivot = poo[p2];
+    root->val = pivot;
+    int pivotino;
+    for (int i = p1; i <= p2; ++i) {
+      if (ino[i] == pivot) {
+        pivotino = i;
+        break;
+      }
+    }
+    root->left = buildTreeR(p1, pivotino-1);
+    root->right = buildTreeR(pivotino+1, p2);
+    return root;
+  }
+
+ public:
+  TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+    // using recursion
+    ino = inorder;
+    poo = postorder;
+    // special case - no items
+    return buildTreeR(0, inorder.size() - 1);
+
+    // make root from postorder last item
+    // split into next recursive iteration on remaining subarrays
+  }
 };
