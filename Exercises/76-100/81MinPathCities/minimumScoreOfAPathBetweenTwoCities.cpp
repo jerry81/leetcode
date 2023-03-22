e143fdef8ab2aba14789c5e816e8d38c6a0fcd96
Raskolnikov <jerry.tann@endress.com>
2023-03-22 17:06:57 +0800

    ✅  pass all, 🐛 egregious issues!

--- Exercises/76-100/81MinPathCities/minimumScoreOfAPathBetweenTwoCities.cpp
+++ Exercises/76-100/81MinPathCities/minimumScoreOfAPathBetweenTwoCities.cpp
@@ -115,7 +115,6 @@ int bfsFindMin() {
   queue<Node*> neighbors;
   neighbors.push(start);
   while (!neighbors.empty()) {
-    queue<Node*> nneighbors;
     Node* cur = neighbors.front();
     visited[cur->id] = true;
     neighbors.pop();
@@ -123,10 +122,9 @@ int bfsFindMin() {
       if (a.second < res) res = a.second;
 
       if (!visited[a.first]) {
-        nneighbors.push(nodes[a.first]);
+        neighbors.push(nodes[a.first]);
       }
     }
-    neighbors = nneighbors;
   }
   return res;
 }
@@ -143,5 +141,7 @@ int main() {
   Solution s;
   vector<vector<int>> test1 = {{12,7,2151},{7,2,7116},{11,14,8450},{11,2,9954},{1,11,3307},{10,7,3561},{10,1,4986},{11,7,7674},{14,2,1764},{11,12,6608},{14,7,1070},{9,8,2287},{14,12,6559},{1,2,1450},{2,12,9165}};
   cerr << "expect 1070 " <<  s.minScore(14, test1) << endl;
+  vector<vector<int>> test2 = {{6,2,7},{3,7,2},{9,6,5},{2,4,9},{7,8,7},{8,4,5},{6,1,10}};
+  cerr << "expect 2 " << s.minScore(9, test2) << endl;
   return 0;
 }
