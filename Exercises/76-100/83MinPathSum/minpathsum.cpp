f7f639e1e144ba190f4c08dc6ec5bcf1267e6aa1
Raskolnikov <jerry.tann@endress.com>
2023-03-27 12:15:54 +0800

    🎨 try bfs

--- Exercises/76-100/83MinPathSum/minpathsum.cpp
+++ Exercises/76-100/83MinPathSum/minpathsum.cpp
@@ -40,15 +40,58 @@ Acceptance Rate
 
 #include <vector>
 #include <iostream>
+#include <unordered_map>
+#include <queue>
 using namespace std;
 
 
 class Solution {
+struct Point {
+  int x = -1;
+  int y = -1;
+  string to_hash() {
+    return y+","+x;
+  }
+};
 vector<vector<int>> dists;
 vector<vector<int>> original;
 int w;
 int h;
 void pathBFS() {
+  unordered_map<string, bool> visited; // is it needed?
+  Point root;
+  root.x = 0;
+  root.y = 0;
+  queue<Point> neighbors;
+  neighbors.push(root);
+  while (!neighbors.empty()) {
+    Point cur = neighbors.front();
+    neighbors.pop();
+    int curx = cur.x;
+    int cury = cur.y;
+    int lx = curx - 1;
+    int rx = curx + 1;
+    int uy = cury - 1;
+    int dy = cury + 1;
+    int left = 0;
+    if (lx >= 0) left = original[cury][lx];
+    int top = 0;
+    if (uy >= 0) top = original[uy][curx];
+    int minp = min(left,top);
+    dists[cury][curx] = original[cury][curx] + minp;
+    if (rx < w) {
+      Point right;
+      right.x = rx;
+      right.y = cury;
+      neighbors.push(right);
+    }
+    if (dy < h) {
+      Point down;
+      down.x = curx;
+      down.y = dy;
+      neighbors.push(down);
+    }
+  }
 }
 public:
     int minPathSum(vector<vector<int>>& grid) {
