f28327ee8600cd605edfa1bc68fe95f54a51af5d
Raskolnikov <jerry.tann@endress.com>
2023-03-28 12:46:26 +0800

    📝 try doing 30 days first - didn't work

--- Exercises/76-100/84MinCostForTix/minCostTickets.cpp
+++ Exercises/76-100/84MinCostForTix/minCostTickets.cpp
@@ -5,7 +5,9 @@ Medium
 5.8K
 98
 Companies
-You have planned some train traveling one year in advance. The days of the year in which you will travel are given as an integer array days. Each day is an integer from 1 to 365.
+You have planned some train traveling one year in advance. The days of the year
+in which you will travel are given as an integer array days. Each day is an
+integer from 1 to 365.
 
 Train tickets are sold in three different ways:
 
@@ -14,8 +16,9 @@ a 7-day pass is sold for costs[1] dollars, and
 a 30-day pass is sold for costs[2] dollars.
 The passes allow that many days of consecutive travel.
 
-For example, if we get a 7-day pass on day 2, then we can travel for 7 days: 2, 3, 4, 5, 6, 7, and 8.
-Return the minimum number of dollars you need to travel every day in the given list of days.
+For example, if we get a 7-day pass on day 2, then we can travel for 7 days: 2,
+3, 4, 5, 6, 7, and 8. Return the minimum number of dollars you need to travel
+every day in the given list of days.
 
 
 
@@ -23,19 +26,20 @@ Example 1:
 
 Input: days = [1,4,6,7,8,20], costs = [2,7,15]
 Output: 11
-Explanation: For example, here is one way to buy passes that lets you travel your travel plan:
-On day 1, you bought a 1-day pass for costs[0] = $2, which covered day 1.
-On day 3, you bought a 7-day pass for costs[1] = $7, which covered days 3, 4, ..., 9.
-On day 20, you bought a 1-day pass for costs[0] = $2, which covered day 20.
-In total, you spent $11 and covered all the days of your travel.
-Example 2:
+Explanation: For example, here is one way to buy passes that lets you travel
+your travel plan: On day 1, you bought a 1-day pass for costs[0] = $2, which
+covered day 1. On day 3, you bought a 7-day pass for costs[1] = $7, which
+covered days 3, 4, ..., 9. On day 20, you bought a 1-day pass for costs[0] = $2,
+which covered day 20. In total, you spent $11 and covered all the days of your
+travel. Example 2:
 
 Input: days = [1,2,3,4,5,6,7,8,9,10,30,31], costs = [2,7,15]
 Output: 17
-Explanation: For example, here is one way to buy passes that lets you travel your travel plan:
-On day 1, you bought a 30-day pass for costs[2] = $15 which covered days 1, 2, ..., 30.
-On day 31, you bought a 1-day pass for costs[0] = $2 which covered day 31.
-In total, you spent $17 and covered all the days of your travel.
+Explanation: For example, here is one way to buy passes that lets you travel
+your travel plan: On day 1, you bought a 30-day pass for costs[2] = $15 which
+covered days 1, 2, ..., 30. On day 31, you bought a 1-day pass for costs[0] = $2
+which covered day 31. In total, you spent $17 and covered all the days of your
+travel.
 
 
 Constraints:
@@ -54,70 +58,82 @@ Acceptance Rate
 
 */
 
-#include <vector>
-#include <queue>
 #include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
 
 class Solution {
-int result = INT_MAX;
-vector<int> _costs;
-void traverse(vector<int> remaining, int accum) {
-  if (accum > result) return;
+  int result = INT_MAX;
+  vector<int> _costs;
+  void traverse(vector<int> remaining, int accum) {
+    if (accum > result) return;
 
-  if (remaining.size() <= 0) {
-    if (accum < result) result = accum;
+    if (remaining.size() <= 0) {
+      if (accum < result) result = accum;
 
-    return;
-  }
-
-  int startday = remaining[0];
-
-  remaining.erase(remaining.begin());
-
-  // 1 day
-
-  traverse(remaining, accum+_costs[0]);
-
-  // 7 day
-
-  int nextday = startday + 7;
-
-  while (remaining.size() > 0 && remaining[0] < nextday) {
-    remaining.erase(remaining.begin());
-  }
-
-  traverse(remaining, accum+_costs[1]);
-
-  nextday = startday + 30;
-
-  // probably can somehow combine this with the above block
-  while (remaining.size() > 0 && remaining[0] < nextday)  {
-    remaining.erase(remaining.begin());
-  }
-
-  traverse(remaining, accum+_costs[2]);
-}
-
-public:
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
-      _costs = costs;
-
-      traverse(days, 0);
-      return result;
+      return;
     }
+
+    int startday = remaining[0];
+
+    int nextday = startday + 30;
+
+    vector<int> rem30 = remaining;
+    vector<int> rem7 = remaining;
+
+    // probably can somehow combine this with the above block
+    while (rem30.size() > 0 && rem30[0] < nextday) {
+      rem30.erase(rem30.begin());
+    }
+
+    traverse(rem30, accum + _costs[2]);
+
+    // 7 day
+
+    nextday = startday + 7;
+
+    while (rem7.size() > 0 && rem7[0] < nextday) {
+      rem7.erase(rem7.begin());
+    }
+
+    traverse(remaining, accum + _costs[1]);
+
+    remaining.erase(remaining.begin());
+
+    traverse(remaining, accum + _costs[0]);
+
+    // 1 day
+  }
+
+ public:
+  int mincostTickets(vector<int>& days, vector<int>& costs) {
+    _costs = costs;
+
+    traverse(days, 0);
+    return result;
+  }
 };
 
 int main() {
-  vector<int> test1 = {1,2,3};
+  vector<int> test1 = {1, 2, 3};
   test1.erase(test1.begin());
   test1.erase(test1.begin());
   cerr << "expect size 1: " << test1.size() << endl;
-  cerr << "expect 3: " << test1[0] <<endl;
+  cerr << "expect 3: " << test1[0] << endl;
 }
 
 /*
 
 start - traverse it as a tree
 
+TLE on case 26
+- flip the order of calculation
+- do 30 day first
+- then 7 day
+- then 2 day
+
+failing case
+days [1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21,24,25,27,28,29,30,31,34,37,38,39,41,43,44,45,47,48,49,54,57,60,62,63,66,69,70,72,74,76,78,80,81,82,83,84,85,88,89,91,93,94,97,99]
+costs [9,38,134]
 */
