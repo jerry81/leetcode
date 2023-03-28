6ebdd802a8b3607b40db261ab43a0046ab36cfae
Raskolnikov <jerry.tann@endress.com>
2023-03-28 13:04:32 +0800

    ⚡️ with binary search

--- Exercises/76-100/84MinCostForTix/minCostTickets.cpp
+++ Exercises/76-100/84MinCostForTix/minCostTickets.cpp
@@ -67,6 +67,21 @@ class Solution {
   int result = INT_MAX;
   vector<int> _costs;
   vector<int> _days;
+  int bsearch(int start, int target) {
+    int end = _days.size() - 1;
+    while (start <= end) {
+      int mid = (start + end) / 2;
+      if (_days[mid] == target) {
+        return mid;
+      } else if (_days[mid] < target) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+    return end + 1;
+  }
+
   void traverse(int idx, int accum) {
     if (accum > result) return;
 
@@ -81,24 +96,13 @@ class Solution {
 
     int nextday = startday + 30;
 
-    int curIdx = idx;
-    // probably can somehow combine this with the above block
-    while (curIdx < _days.size() && _days[curIdx] < nextday) {
-      ++curIdx;
-    }
-
-    traverse(curIdx, accum + _costs[2]);
+    traverse(bsearch(idx, nextday), accum + _costs[2]);
 
     // 7 day
 
     nextday = startday + 7;
 
-    curIdx = idx;
-    while (curIdx < _days.size() && _days[curIdx] < nextday) {
-      ++curIdx;
-    }
-
-    traverse(curIdx, accum + _costs[1]);
+    traverse(bsearch(idx, nextday), accum + _costs[1]);
 
     traverse(idx+1, accum + _costs[0]);
 
@@ -135,6 +139,10 @@ TLE on case 26
 - still TLE
 
 - try using ptr instead of doing erase ops
+
+- better but fails on case 46
+days [6,9,10,14,15,16,17,18,20,22,23,24,29,30,31,33,35,37,38,40,41,46,47,51,54,57,59,65,70,76,77,81,85,87,90,91,93,94,95,97,98,100,103,104,105,106,107,111,112,113,114,116,117,118,120,124,128,129,135,137,139,145,146,151,152,153,157,165,166,173,174,179,181,182,185,187,188,190,191,192,195,196,204,205,206,208,210,214,218,219,221,225,229,231,233,235,239,240,245,247,249,251,252,258,261,263,268,270,273,274,275,276,280,283,285,286,288,289,290,291,292,293,296,298,299,301,303,307,313,314,319,323,325,327,329,334,339,340,341,342,344,346,349,352,354,355,356,357,358,359,363,364]
+costs [21,115,345]
 
 failing case
 days [1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21,24,25,27,28,29,30,31,34,37,38,39,41,43,44,45,47,48,49,54,57,60,62,63,66,69,70,72,74,76,78,80,81,82,83,84,85,88,89,91,93,94,97,99]
