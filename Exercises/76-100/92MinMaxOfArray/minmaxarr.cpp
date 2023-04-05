1869f8943c71860db6c4299b3162308657a91c4f
Raskolnikov <jerry.tann@endress.com>
2023-04-05 17:31:33 +0800

    ✨ idea - binary search tree?

--- Exercises/76-100/92MinMaxOfArray/minmaxarr.cpp
+++ Exercises/76-100/92MinMaxOfArray/minmaxarr.cpp
@@ -58,3 +58,72 @@ public:
 
     }
 };
+
+// [1,5,7,3,4, 2, 7]
+// [3,3,7,3,4,4,5]
+// [3,5,5,3,4,4,5]
+// try in one pass
+// diff array
+// [4,2,-4,1,-2,5]
+// if positive, half of diff
+// [2,1,0,.5,0,2.5]
+// apply
+// [3,4,6,3,4,4,5] - nope
+
+// try priority queue?
+
+// struct nodes - idx, value
+// put nodes into pq (maxheap)
+// reduce the max (adding to max-1) - so pq must have efficient modify
+
+// BST better
+// binary search tree
+// logn to search index
+
+//       4
+//   2       6
+//  1  3   5   7
+
+
+/*
+  8
+
+
+If the list is sorted, you can simply remove the element you're about to change from the list, and after changing it, you could "binary-insert" it, no? That would take an average of log(n) steps.
+
+If you can, change from an array to a java.util.TreeMap: both removal and insertion will be log(n) operations: which will be faster than your O(1) access + O(n) re-insertion solution using an array.
+
+c++ has no tree
+
+3,7,1,6 as tree
+
+     3
+  1     6
+           7
+
+  remove 7 and 3
+  bst becomes
+    6
+  1
+  3,7 become
+  5,5
+  bst becomes
+     6
+  1
+    5/5
+
+  remove 6
+  1
+    5/5
+  remove 1
+  5/5
+  modify 6 and 1
+  3,4
+  insert back
+    5/5
+3
+  4
+  5,5,3,4
+  5,5 nothing can be, so done
+*/
+
