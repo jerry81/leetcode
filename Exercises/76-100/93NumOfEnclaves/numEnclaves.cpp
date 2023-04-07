ebdf83e4163cdbb953ac50b3077f1ab9b8dc603a
Raskolnikov <jerry.tann@endress.com>
2023-04-07 15:57:29 +0800

    ✅ pass daily

--- Exercises/76-100/93NumOfEnclaves/numEnclaves.cpp
+++ Exercises/76-100/93NumOfEnclaves/numEnclaves.cpp
@@ -1,3 +1,49 @@
+/*
+
+1020. Number of Enclaves
+Medium
+2.6K
+47
+Companies
+You are given an m x n binary matrix grid, where 0 represents a sea cell and 1
+represents a land cell.
+
+A move consists of walking from one land cell to another adjacent
+(4-directionally) land cell or walking off the boundary of the grid.
+
+Return the number of land cells in grid for which we cannot walk off the
+boundary of the grid in any number of moves.
+
+
+
+Example 1:
+
+
+Input: grid = [[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]]
+Output: 3
+Explanation: There are three 1s that are enclosed by 0s, and one 1 that is not
+enclosed because its on the boundary. Example 2:
+
+
+Input: grid = [[0,1,1,0],[0,0,1,0],[0,0,1,0],[0,0,0,0]]
+Output: 0
+Explanation: All 1s are either on the boundary or can reach the boundary.
+
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 500
+grid[i][j] is either 0 or 1.
+
+*/
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
 
 class Solution {
   bool* visited;
@@ -45,22 +91,22 @@ class Solution {
       Point rp = Point(rx,cy);
       Point up = Point(cx,uy);
       Point dp = Point(cx,dy);
-      if (lx >= 0 && !isDone(lp) && orig[lx][cy]) {
+      if (lx >= 0 && !isDone(lp) && (orig[cy][lx] == 1)) {
         remain.push(lp);
         visit(lp);
       }
 
-      if (rx < width && !isDone(rp) && orig[rx][cy]) {
+      if (rx < width && !isDone(rp) && (orig[cy][rx] == 1)) {
         remain.push(rp);
         visit(rp);
       }
 
-      if (uy >= 0 && !isDone(up) && orig[cx][uy]) {
+      if (uy >= 0 && !isDone(up) && (orig[uy][cx] == 1)) {
         remain.push(up);
         visit(up);
       }
 
-      if (dy < height && !isDone(dp) && orig[cx][dy]) {
+      if (dy < height && !isDone(dp) && (orig[dy][cx] == 1)) {
         remain.push(dp);
         visit(dp);
       }
@@ -93,3 +139,13 @@ class Solution {
     return total;
   }
 };
+/*
+
+this is a bfs islands problem.
+additional check for if node of island is on edge
+
+fail on 3rd test case lol
+
+[[0,0,0,1,1,1,0,1,0,0],[1,1,0,0,0,1,0,1,1,1],[0,0,0,1,1,1,0,1,0,0],[0,1,1,0,0,0,1,0,1,0],[0,1,1,1,1,1,0,0,1,0],[0,0,1,0,1,1,1,1,0,1],[0,1,1,0,0,0,1,1,1,1],[0,0,1,0,0,1,0,1,0,1],[1,0,1,0,1,1,0,0,0,0],[0,0,0,0,1,1,0,0,0,1]]
+
+*/
