05c9837924254fcc0e848a1ffa96892a23ce3332
Raskolnikov <jerry.tann@endress.com>
2023-04-07 15:48:02 +0800

    ✅ pass examples

--- Exercises/76-100/93NumOfEnclaves/numEnclaves.cpp
+++ Exercises/76-100/93NumOfEnclaves/numEnclaves.cpp
@@ -1,49 +1,3 @@
-/*
-
-1020. Number of Enclaves
-Medium
-2.6K
-47
-Companies
-You are given an m x n binary matrix grid, where 0 represents a sea cell and 1
-represents a land cell.
-
-A move consists of walking from one land cell to another adjacent
-(4-directionally) land cell or walking off the boundary of the grid.
-
-Return the number of land cells in grid for which we cannot walk off the
-boundary of the grid in any number of moves.
-
-
-
-Example 1:
-
-
-Input: grid = [[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]]
-Output: 3
-Explanation: There are three 1s that are enclosed by 0s, and one 1 that is not
-enclosed because its on the boundary. Example 2:
-
-
-Input: grid = [[0,1,1,0],[0,0,1,0],[0,0,1,0],[0,0,0,0]]
-Output: 0
-Explanation: All 1s are either on the boundary or can reach the boundary.
-
-
-Constraints:
-
-m == grid.length
-n == grid[i].length
-1 <= m, n <= 500
-grid[i][j] is either 0 or 1.
-
-*/
-
-#include <iostream>
-#include <queue>
-#include <vector>
-
-using namespace std;
 
 class Solution {
   bool* visited;
@@ -56,9 +10,10 @@ class Solution {
     Point(int x, int y) : x(x), y(y){};
   };
 
-  bool isDone(Point p) { return visited[p.y * height + p.x]; }
+  bool isDone(Point p) {
+    return visited[(p.y * width) + p.x]; }
 
-  void visit(Point p) { visited[p.y * height + p.x] = true; }
+  void visit(Point p) { visited[(p.y * width) + p.x] = true; }
 
   int bfs(Point start) {
     if (isDone(start)) return 0;
@@ -90,22 +45,22 @@ class Solution {
       Point rp = Point(rx,cy);
       Point up = Point(cx,uy);
       Point dp = Point(cx,dy);
-      if (lx >= 0 && !isDone(lp)) {
+      if (lx >= 0 && !isDone(lp) && orig[lx][cy]) {
         remain.push(lp);
         visit(lp);
       }
 
-      if (rx < width && !isDone(rp)) {
+      if (rx < width && !isDone(rp) && orig[rx][cy]) {
         remain.push(rp);
         visit(rp);
       }
 
-      if (uy >= 0 && !isDone(up)) {
+      if (uy >= 0 && !isDone(up) && orig[cx][uy]) {
         remain.push(up);
         visit(up);
       }
 
-      if (dy >= 0 && !isDone(dp)) {
+      if (dy < height && !isDone(dp) && orig[cx][dy]) {
         remain.push(dp);
         visit(dp);
       }
@@ -122,11 +77,14 @@ class Solution {
     height = (int)grid.size();
     int m = (int)grid[0].size();
     width = m;
-    visited = new bool[height * m];
-    memset(visited, false, sizeof(visited));
+    int totaldim = height*width;
+
+    visited = new bool[totaldim];
+    memset(visited, false, totaldim);
     int total = 0;
-    for (int i = 0; i < height; ++i) {
-      for (int j = 0; j < width: ++j) {
+    for (int i = 1; i < height-1; ++i) {
+      for (int j = 1; j < width-1; ++j) {
+            if (!orig[i][j]) continue;
             Point p = Point(j,i);
             total += bfs(p);
       }
@@ -135,10 +93,3 @@ class Solution {
     return total;
   }
 };
-
-/*
-
-this is a bfs islands problem.
-additional check for if node of island is on edge
-
-*/
