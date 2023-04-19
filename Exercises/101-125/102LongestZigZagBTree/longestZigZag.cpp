e645a3c53e6ee2f62d7f3dc56e6d1c892569bb95
Raskolnikov <jerry.tann@endress.com>
2023-04-19 11:10:37 +0800

    🚧 working

--- Exercises/101-125/102LongestZigZagBTree/longestZigZag.cpp
+++ Exercises/101-125/102LongestZigZagBTree/longestZigZag.cpp
@@ -61,9 +61,24 @@ Acceptance Rate
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
 class Solution {
+int zzr(TreeNode* cur,  bool prevR=false, bool initial=false) {
+}
 public:
     int longestZigZag(TreeNode* root) {
-
+      return zzr(root, false, true);
     }
 };
