03cf14cc1977b77b0bb2d54d5f1c6870a199bacd
Raskolnikov <jerry.tann@endress.com>
2023-04-20 12:10:48 +0800

    ✨ first attempt

--- Exercises/101-125/103BinTreeMaxWidth/maxWidthBinTree.cpp
+++ Exercises/101-125/103BinTreeMaxWidth/maxWidthBinTree.cpp
@@ -79,7 +79,37 @@ public:
       q.push(root);
 
       while (itemcount > 0) {
+        itemcount = 0;
+        int curItem = 0;
+        queue<TreeNode*> nq;
+        int lp = -1;
+        int rp = -1;
+        while (!q.empty()) {
+          TreeNode* tn = q.front();
+          q.pop();
+          if (tn == nullptr) {
+            nq.push(nullptr);
+            nq.push(nullptr);
+          } else {
+            if (tn->left != nullptr) {
+              itemcount += 1;
+              if (lp < 0) lp = 2*nq.size();
+              if (2*nq.size() > rp) rp = 2*nq.size();
+            }
 
+            nq.push(tn->left);
+
+            if (tn->right != nullptr) {
+              itemcount += 1;
+              if (lp < 0) lp = nq.size()+1;
+              if ((2*nq.size()+1) > rp) rp = (2*nq.size()+1);
+            }
+
+            nq.push(tn->right);
+          }
+          if ((rp - lp) > maxW) maxW = rp - lp;
+          q = nq;
+        }
       }
       return maxW;
     }
