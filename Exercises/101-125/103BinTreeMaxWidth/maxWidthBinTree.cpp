255e6761d5f4548c5fba0f178e51af6a45ea54dc
Raskolnikov <jerry.tann@endress.com>
2023-04-20 13:16:17 +0800

    🎨 format, error on single item edge case

--- Exercises/101-125/103BinTreeMaxWidth/maxWidthBinTree.cpp
+++ Exercises/101-125/103BinTreeMaxWidth/maxWidthBinTree.cpp
@@ -9,7 +9,10 @@ Given the root of a binary tree, return the maximum width of the given tree.
 
 The maximum width of a tree is the maximum width among all levels.
 
-The width of one level is defined as the length between the end-nodes (the leftmost and rightmost non-null nodes), where the null nodes between the end-nodes that would be present in a complete binary tree extending down to that level are also counted into the length calculation.
+The width of one level is defined as the length between the end-nodes (the
+leftmost and rightmost non-null nodes), where the null nodes between the
+end-nodes that would be present in a complete binary tree extending down to that
+level are also counted into the length calculation.
 
 It is guaranteed that the answer will in the range of a 32-bit signed integer.
 
@@ -20,14 +23,14 @@ Example 1:
 
 Input: root = [1,3,2,5,3,null,9]
 Output: 4
-Explanation: The maximum width exists in the third level with length 4 (5,3,null,9).
-Example 2:
+Explanation: The maximum width exists in the third level with length 4
+(5,3,null,9). Example 2:
 
 
 Input: root = [1,3,2,5,null,null,9,6,null,7]
 Output: 7
-Explanation: The maximum width exists in the fourth level with length 7 (6,null,null,null,null,null,7).
-Example 3:
+Explanation: The maximum width exists in the fourth level with length 7
+(6,null,null,null,null,null,7). Example 3:
 
 
 Input: root = [1,3,2,5]
@@ -56,65 +59,67 @@ Acceptance Rate
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 
 #include <queue>
 using namespace std;
- struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
 
- class Solution {
-public:
-    int widthOfBinaryTree(TreeNode* root) {
-      queue<TreeNode*> q;
-      int maxW = 0;
-      int itemcount = (root != nullptr) ? 1: 0;
-      q.push(root);
+class Solution {
+ public:
+  int widthOfBinaryTree(TreeNode *root) {
+    queue<TreeNode *> q;
+    int maxW = 0;
+    int itemcount = (root != nullptr) ? 1 : 0;
+    q.push(root);
 
-      while (itemcount > 0) {
-        itemcount = 0;
-        int curItem = 0;
-        queue<TreeNode*> nq;
-        int lp = -1;
-        int rp = -1;
-        while (!q.empty()) {
-          TreeNode* tn = q.front();
-          q.pop();
-          if (tn == nullptr) {
-            nq.push(nullptr);
-            nq.push(nullptr);
-          } else {
-            if (tn->left != nullptr) {
-              itemcount += 1;
-              if (lp < 0) lp = nq.size();
+    while (itemcount > 0) {
+      itemcount = 0;
+      int curItem = 0;
+      queue<TreeNode *> nq;
+      int lp = -1;
+      int rp = -1;
+      while (!q.empty()) {
+        TreeNode *tn = q.front();
+        q.pop();
+        if (tn == nullptr) {
+          nq.push(nullptr);
+          nq.push(nullptr);
+        } else {
+          if (tn->left != nullptr) {
+            itemcount += 1;
+            if (lp < 0) lp = nq.size();
 
-              if ((int)nq.size()+1 > rp) rp = (int)nq.size()+1;
-            }
-
-            nq.push(tn->left);
-
-            if (tn->right != nullptr) {
-              itemcount += 1;
-              if (lp < 0) lp = nq.size()+1;
-              if ((nq.size()+1) > rp) rp = (nq.size()+1);
-            }
-
-            nq.push(tn->right);
+            if ((int)nq.size() + 1 > rp) rp = (int)nq.size() + 1;
           }
+
+          nq.push(tn->left);
+
+          if (tn->right != nullptr) {
+            itemcount += 1;
+            if (lp < 0) lp = nq.size() + 1;
+            if ((nq.size() + 1) > rp) rp = (nq.size() + 1);
+          }
+
+          nq.push(tn->right);
         }
-        if ((rp - lp) > maxW) maxW = rp - lp;
-        q = nq;
       }
-      return maxW;
+      if ((rp - lp) > maxW) maxW = rp - lp;
+      q = nq;
     }
+    return maxW;
+  }
 };
 
 // bfs
