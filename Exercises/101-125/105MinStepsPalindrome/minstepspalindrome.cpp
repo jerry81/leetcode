8086bfbd60fcbe9c0b8f481ef160cf195868bc1f
Raskolnikov <jerry.tann@endress.com>
2023-04-22 14:06:58 +0800

    ✨ daily, initial

---
+++ Exercises/101-125/105MinStepsPalindrome/minstepspalindrome.cpp
@@ -0,0 +1,54 @@
+/*
+1312. Minimum Insertion Steps to Make a String Palindrome
+Hard
+3.4K
+41
+Companies
+Given a string s. In one step you can insert any character at any index of the string.
+
+Return the minimum number of steps to make s palindrome.
+
+A Palindrome String is one that reads the same backward as well as forward.
+
+
+
+Example 1:
+
+Input: s = "zzazz"
+Output: 0
+Explanation: The string "zzazz" is already palindrome we do not need any insertions.
+Example 2:
+
+Input: s = "mbadm"
+Output: 2
+Explanation: String can be "mbdadbm" or "mdbabdm".
+Example 3:
+
+Input: s = "leetcode"
+Output: 5
+Explanation: Inserting 5 characters the string becomes "leetcodocteel".
+
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of lowercase English letters.
+Accepted
+87.8K
+Submissions
+130.3K
+Acceptance Rate
+67.3%
+
+*/
+
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int minInsertions(string s) {
+
+    }
+};
+
