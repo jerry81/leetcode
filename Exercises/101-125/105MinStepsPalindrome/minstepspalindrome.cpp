fa1f49721c15e9d18cff7ba2e309b577cb1848b9
Raskolnikov <jerry.tann@endress.com>
2023-04-22 14:44:41 +0800

    🚧 analysis

--- Exercises/101-125/105MinStepsPalindrome/minstepspalindrome.cpp
+++ Exercises/101-125/105MinStepsPalindrome/minstepspalindrome.cpp
@@ -52,3 +52,88 @@ public:
     }
 };
 
+/*
+01234556
+leetcode
+leetcoded
+leetcodedo
+leetcodedoc
+leetcodedoct
+leetcodedocte
+leetcodedoctee
+leetcodedocteel
+
+leetcode
+leetcodoe -> o's cancel (  leetcde )
+leetcodoce -> c's cancel (leetde)
+leetcodocte -> ts cancel leede
+leetcodoctee -> led
+leetcodocteel -> led
+
+theory: any palindrome can be built by "balancing" two sides' freq maps
+false:
+"donedone"
+- donedone - 5 inserts
+
+- doneenod - 0 inserts
+
+makeshift
+
+makeshiftfihsekam
+
+steely -> expect 4
+steelyl
+steelyle
+steelylee
+steelyleet
+steelyleets
+
+steely
+
+stleely
+styleely
+styleelyt
+styleelyts
+
+- something to do with repeats
+
+morgenstein
+
+naive -> 10
+
+ietsnegrom
+
+but just 8 b/c of repeat e and repeat n?
+
+instagram - expect 6
+
+- naive
+instagram argatsni - 8
+
+i - 1
+n - 1
+s - 1
+t - 1
+a - 2
+g - 1
+r - 1
+m - 1
+
+- so n-1 -1 b/c of one repeat
+
+i n s t a g r a m   0
+i n s t a g r g a m   1
+i n s t m a g r g a m  2
+i n s t m a g r g a m t 3
+
+distance of repeat made a difference
+
+- multiple repeats
+
+topics are string and DP
+
+- make palindrome
+- naive: - insert starting from last index and insert at end moving pointer backwards to 0
+
+
+*/
