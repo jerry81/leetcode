91761ef4bc45aa02368e1f91943f3014c023eeb8
Raskolnikov <jerry.tann@endress.com>
2023-04-27 12:21:51 +0800

    ✅ examples

--- Exercises/101-125/110BulbSwitcher/bulbswitcher.cpp
+++ Exercises/101-125/110BulbSwitcher/bulbswitcher.cpp
@@ -5,9 +5,12 @@ Medium
 1.4K
 2.1K
 Companies
-There are n bulbs that are initially off. You first turn on all the bulbs, then you turn off every second bulb.
+There are n bulbs that are initially off. You first turn on all the bulbs, then
+you turn off every second bulb.
 
-On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). For the ith round, you toggle every i bulb. For the nth round, you only toggle the last bulb.
+On the third round, you toggle every third bulb (turning on if it's off or
+turning off if it's on). For the ith round, you toggle every i bulb. For the nth
+round, you only toggle the last bulb.
 
 Return the number of bulbs that are on after n rounds.
 
@@ -46,20 +49,28 @@ Acceptance Rate
 */
 
 class Solution {
-public:
-    int bulbSwitch(int n) {
-      bool switches[n];
-      int count = n;
-      if (n == 1) return 1;
-      for (int i = 1; i < n; ++n) {
-        for (int j = i; j < n/i; j+=i) {
-          bool sw = !switches[j];
-          if (sw) count--;
-          if (!sw) count++;
-        }
-      }
-      return count;
+ public:
+  int bulbSwitch(int n) {
+    if (n == 0) return 0;
+
+    if (n == 1) return 1;
+
+    vector<bool> switches;
+    for (int i = 0; i < n; ++i) {
+      switches.push_back(true);
     }
+    cerr << "switches.size " << switches.size() << endl;
+    int count = n;
+
+    for (int i = 1; i < n; ++i) {
+      for (int j = i; j < n / i; j += i) {
+        switches[j] = !switches[j];
+        if (switches[j]) count++;
+        if (!switches[j]) count--;
+      }
+    }
+    return count;
+  }
 };
 
 // naive - do the loop
