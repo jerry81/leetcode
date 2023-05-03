b1781d2f7c09b3fd7845d829a5953d220a2e5892
Jerry Tann <jerry.tann@endress.com>
2023-05-03 15:31:15 +0800

    Create arraydiff.cpp

---
+++ Exercises/101-125/117arraydiff/arraydiff.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+       vector<vector<int>> ret;
+       vector<int> ans1;
+       vector<int> ans2;
+        ans1=nums1;
+        ans2=nums2;
+    }
+};
