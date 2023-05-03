ed382dfccf3723df928f3c3d15052a0fb1c9f814
Jerry Tann <jerry.tann@endress.com>
2023-05-03 15:59:23 +0800

    Update arraydiff.cpp

--- Exercises/101-125/117arraydiff/arraydiff.cpp
+++ Exercises/101-125/117arraydiff/arraydiff.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
        vector<vector<int>> ret;
-       vector<int> ans1;
-       vector<int> ans2;
+       set<int> anss1;
+       set<int> anss2;
         
         unordered_map<int,bool> cont1;
         unordered_map<int,bool> cont2;
@@ -12,11 +12,15 @@ public:
         }
         for (int i:nums2){
             cont2[i] =true;
-            if (cont1[i]) ans2.push_back(i);
+            if (!cont1[i]) anss2.insert(i);
         }
         for (int i:nums1){
-            if (cont2[i]) ans1.push_back(i);
+            if (!cont2[i]) anss1.insert(i);
         }
+        
+        vector<int> ans1(anss1.begin(), anss1.end());
+        
+        vector<int> ans2(anss2.begin(), anss2.end());
         
         ret.push_back(ans1);
         ret.push_back(ans2);
