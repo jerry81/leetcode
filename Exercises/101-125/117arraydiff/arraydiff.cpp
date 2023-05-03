41ef1f660dc796047fb3501251afdfd9b847d6a8
Jerry Tann <jerry.tann@endress.com>
2023-05-03 15:46:54 +0800

    Update arraydiff.cpp

--- Exercises/101-125/117arraydiff/arraydiff.cpp
+++ Exercises/101-125/117arraydiff/arraydiff.cpp
@@ -11,7 +11,17 @@ public:
             cont1[i] = true;
         }
         for (int i:nums2){
-            
+            cont2[i] =true;
+            if (cont1[i]) ans2.push_back(i);
         }
+        for (int i:nums1){
+            if (cont2[i]) ans1.push_back(i);
+        }
+        
+        ret.push_back(ans1);
+        ret.push_back(ans2);
+        
+        return ret;
     }
+    
 };
