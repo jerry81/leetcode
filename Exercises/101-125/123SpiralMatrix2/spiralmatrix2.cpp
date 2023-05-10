1fd48d71b753285365ce4a6a83eb8118c4c76cc5
Raskolnikov <jerry.tann@endress.com>
2023-05-10 12:49:31 +0800

    ✨ daily, initial

---
+++ Exercises/101-125/123SpiralMatrix2/spiralmatrix2.cpp
@@ -0,0 +1,40 @@
+/*
+
+59. Spiral Matrix II
+Medium
+5.1K
+216
+Companies
+Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+
+
+
+Example 1:
+
+
+Input: n = 3
+Output: [[1,2,3],[8,9,4],[7,6,5]]
+Example 2:
+
+Input: n = 1
+Output: [[1]]
+
+
+Constraints:
+
+1 <= n <= 20
+Accepted
+449.1K
+Submissions
+659.7K
+Acceptance Rate
+68.1%
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+
+    }
+};
