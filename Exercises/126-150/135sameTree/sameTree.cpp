7987f0befbd02bf31450388d56f35c11392df9c6
Raskolnikov <jerry.tann@endress.com>
2023-05-18 09:07:00 +0800

    🐛 edge case, already refactored

--- Exercises/126-150/135sameTree/sameTree.cpp
+++ Exercises/126-150/135sameTree/sameTree.cpp
@@ -62,9 +62,14 @@ Acceptance Rate
      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
 
+
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr || q == nullptr) {
+            return p == q;
+        }
+
       if (p->val != q->val) return false;
 
       return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
