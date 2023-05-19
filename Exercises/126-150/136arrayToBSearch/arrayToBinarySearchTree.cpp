360eec6915596a3cda8ad3ef95d5d58956ff8542
Raskolnikov <jerry.tann@endress.com>
2023-05-19 08:01:55 +0800

    ✨ array to bin search tree

---
+++ Exercises/126-150/136arrayToBSearch/arrayToBinarySearchTree.cpp
@@ -0,0 +1,72 @@
+/*
+
+108. Convert Sorted Array to Binary Search Tree
+Easy
+9.5K
+476
+Companies
+Given an integer array nums where the elements are sorted in ascending order, convert it to a
+height-balanced
+ binary search tree.
+
+
+
+Example 1:
+
+
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Example 2:
+
+
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in a strictly increasing order.
+Accepted
+992.4K
+Submissions
+1.4M
+Acceptance Rate
+70.0%
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ *
+ */
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+ #include <vector>
+ using namespace std;
+
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+
+    }
+};
