2e5e63816d2fa1128e480e145a6e8cf894f94f48
Raskolnikov <jerry.tann@endress.com>
2023-05-19 15:23:01 +0800

    ✨ heightbalanced initial

---
+++ Exercises/126-150/138HeightBalanced/heightbalanced.cpp
@@ -0,0 +1,68 @@
+/*
+
+110. Balanced Binary Tree
+Easy
+8.9K
+502
+Companies
+Given a binary tree, determine if it is
+height-balanced
+.
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+Example 3:
+
+Input: root = []
+Output: true
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-104 <= Node.val <= 104
+Accepted
+1.1M
+Submissions
+2.2M
+Acceptance Rate
+49.3%
+
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+
+    }
+};
