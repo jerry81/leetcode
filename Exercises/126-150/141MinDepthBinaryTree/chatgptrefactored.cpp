f707a50e8ad77457ee8de81d1214d6aecd144c10
Raskolnikov <jerry.tann@endress.com>
2023-05-21 14:29:07 +0800

    🎨 refactored

---
+++ Exercises/126-150/141MinDepthBinaryTree/chatgptrefactored.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        if (root->left == nullptr) return 1 + minDepth(root->right);
+        if (root->right == nullptr) return 1 + minDepth(root->left);
+        return 1 + std::min(minDepth(root->left), minDepth(root->right));
+    }
+};
