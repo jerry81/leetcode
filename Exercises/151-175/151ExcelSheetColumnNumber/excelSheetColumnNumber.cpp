9fdbdbb52523e975e8e4835860f341d85ffc16c1
Raskolnikov <jerry.tann@endress.com>
2023-05-26 17:08:45 +0800

    ✅ pass online tests

--- Exercises/151-175/151ExcelSheetColumnNumber/excelSheetColumnNumber.cpp
+++ Exercises/151-175/151ExcelSheetColumnNumber/excelSheetColumnNumber.cpp
@@ -67,21 +67,3 @@ public:
       return sum;
     }
 };
-
-class SolutionOld {
-public:
-    std::string convertToTitle(int columnNumber) {
-        if (columnNumber <= 0)
-            return "";
-
-        std::string result;
-        while (columnNumber > 0) {
-            int remainder = (columnNumber - 1) % 26;
-            char digit = 'A' + remainder;
-            result = digit + result;
-            columnNumber = (columnNumber - 1) / 26;
-        }
-
-        return result;
-    }
-};
