cbce7d0bc7bbf2e99e72c1dbd51bb2a932cfe810
Raskolnikov <jerry.tann@endress.com>
2023-05-26 17:04:05 +0800

    🚧 titleToNumber initial

---
+++ Exercises/151-175/151ExcelSheetColumnNumber/excelSheetColumnNumber.cpp
@@ -0,0 +1,77 @@
+/*
+
+171. Excel Sheet Column Number
+Easy
+4.2K
+331
+Companies
+Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28
+...
+
+
+Example 1:
+
+Input: columnTitle = "A"
+Output: 1
+Example 2:
+
+Input: columnTitle = "AB"
+Output: 28
+Example 3:
+
+Input: columnTitle = "ZY"
+Output: 701
+
+
+Constraints:
+
+1 <= columnTitle.length <= 7
+columnTitle consists only of uppercase English letters.
+columnTitle is in the range ["A", "FXSHRXW"].
+Accepted
+600.1K
+Submissions
+964K
+Acceptance Rate
+62.3%
+
+*/
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+
+    }
+};
+
+class SolutionOld {
+public:
+    std::string convertToTitle(int columnNumber) {
+        if (columnNumber <= 0)
+            return "";
+
+        std::string result;
+        while (columnNumber > 0) {
+            int remainder = (columnNumber - 1) % 26;
+            char digit = 'A' + remainder;
+            result = digit + result;
+            columnNumber = (columnNumber - 1) / 26;
+        }
+
+        return result;
+    }
+};
