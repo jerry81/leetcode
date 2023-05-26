a57dee4b6836282a874d85171110aef14a8d6808
Raskolnikov <jerry.tann@endress.com>
2023-05-26 17:08:17 +0800

    ✅ pass locals

--- Exercises/151-175/151ExcelSheetColumnNumber/excelSheetColumnNumber.cpp
+++ Exercises/151-175/151ExcelSheetColumnNumber/excelSheetColumnNumber.cpp
@@ -54,7 +54,17 @@ using namespace std;
 class Solution {
 public:
     int titleToNumber(string columnTitle) {
+      int sum = 0;
+      int power = 0;
+      for (int i = columnTitle.size()-1; i >= 0; --i) {
 
+        char c = columnTitle[i];
+        int append = pow(26,power);
+        append*=(c - 'A' + 1);
+        sum += append;
+        power++;
+      }
+      return sum;
     }
 };
 
