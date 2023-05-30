aa73146dacb92f41a4db2214816b716a3294f7f8
Raskolnikov <jerry.tann@endress.com>
2023-05-30 17:12:01 +0800

    ✨ first attempt

--- Exercises/151-175/159BinTreePaths/btreepaths.cpp
+++ Exercises/151-175/159BinTreePaths/btreepaths.cpp
@@ -51,8 +51,30 @@ Acceptance Rate
 #include <string>
 using namespace std;
 class Solution {
+vector<string> paths;
+string toS(vector<int> inp) {
+  bool first = true;
+  string ret = "";
+  for (int i:inp) {
+    if (first) {
+      ret+=to_string(i);
+      first = false;
+    } else {
+      ret+="->";
+      ret+=to_string(i);
+    }
+  }
+}
+void t(TreeNode* root, vector<int> curPath) {
+  if (root == nullptr) paths.push_back(toS(curPath));
+  curPath.push_back(root->val);
+  t(root->left, curPath);
+  t(root->right, curPath);
+}
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
-
+      vector<int> empty;
+      t(root, empty);
+      return paths;
     }
 };
