fd314bec984c3c4d7d2391a589f34781ac895c97
Raskolnikov <jerry.tann@endress.com>
2023-05-30 17:51:19 +0800

    🐛 fix cases but TLE

--- Exercises/151-175/160UglyNumber/uglyNumber.cpp
+++ Exercises/151-175/160UglyNumber/uglyNumber.cpp
@@ -43,6 +43,51 @@ Acceptance Rate
 #include <cmath>
 
 using namespace std;
+/*
+
+263. Ugly Number
+Easy
+2.9K
+1.5K
+Companies
+An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+Given an integer n, return true if n is an ugly number.
+
+
+
+Example 1:
+
+Input: n = 6
+Output: true
+Explanation: 6 = 2 × 3
+Example 2:
+
+Input: n = 1
+Output: true
+Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5.
+Example 3:
+
+Input: n = 14
+Output: false
+Explanation: 14 is not ugly since it includes the prime factor 7.
+
+
+Constraints:
+
+-231 <= n <= 231 - 1
+Accepted
+428.5K
+Submissions
+1M
+Acceptance Rate
+42.2%
+
+*/
+
+#include <cmath>
+
+using namespace std;
 
 class Solution {
   bool isPrime(int n) {
@@ -53,6 +98,10 @@ class Solution {
   }
 public:
     bool isUgly(int n) {
+
+      if (n<0) return false;
+      if (n==0) return false;
+
       for (int i = 5; i < n/2+1; i=i+2) {
         if (i%3 == 0) continue;
 
@@ -60,6 +109,9 @@ public:
 
         if (n%i == 0 && isPrime(i)) return false;
       }
+
+      // final check
+      if (n>=7 && isPrime(n)) return false;
 
       return true;
     }
@@ -69,4 +121,11 @@ public:
 /*
   try all primes?
   directly get factors and check primeness
+*/
+
+// hmm..
+/*
+  try all primes?
+  directly get factors and check primeness
+  - negative number doesn't work
 */
