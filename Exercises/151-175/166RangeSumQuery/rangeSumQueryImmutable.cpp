93dbf99a5685a4a79fd1bbf616900d9bb42d4ab4
Raskolnikov <jerry.tann@endress.com>
2023-06-02 13:58:07 +0800

    ✨ range sum query immutable

---
+++ Exercises/151-175/166RangeSumQuery/rangeSumQueryImmutable.cpp
@@ -0,0 +1,66 @@
+/*
+
+303. Range Sum Query - Immutable
+Easy
+2.8K
+1.8K
+Companies
+Given an integer array nums, handle multiple queries of the following type:
+
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
+
+NumArray(int[] nums) Initializes the object with the integer array nums.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+
+
+Example 1:
+
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
+
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+
+
+Constraints:
+
+1 <= nums.length <= 104
+-105 <= nums[i] <= 105
+0 <= left <= right < nums.length
+At most 104 calls will be made to sumRange.
+Accepted
+441.2K
+Submissions
+735K
+Acceptance Rate
+60.0%
+
+*/
+
+#include <vector>
+
+using namespace std;
+
+class NumArray {
+public:
+    NumArray(vector<int>& nums) {
+
+    }
+
+    int sumRange(int left, int right) {
+
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
