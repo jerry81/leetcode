d37210e6d6f232b11dce979897c97f3b328bf02c
Raskolnikov <jerry.tann@endress.com>
2023-06-04 18:34:07 +0800

    ✨ counting bits, initial

---
+++ Exercises/151-175/170CountingBits/countingBits.cpp
@@ -0,0 +1,60 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+
+    }
+};
+
+/*
+
+338. Counting Bits
+Easy
+8.9K
+424
+Companies
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+
+
+Example 1:
+
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+Example 2:
+
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+
+
+Constraints:
+
+0 <= n <= 105
+
+
+Follow up:
+
+It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in linear time O(n) and possibly in a single pass?
+Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?
+Accepted
+778K
+Submissions
+1M
+Acceptance Rate
+75.9%
+
+*/
