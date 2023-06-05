827d01f0df7e48904ed09fb60de7057817a32926
Raskolnikov <jerry.tann@endress.com>
2023-06-05 12:31:43 +0800

    🐛 edge case

--- Exercises/151-175/171CheckIfStraight/checkIfStraight.cpp
+++ Exercises/151-175/171CheckIfStraight/checkIfStraight.cpp
@@ -46,10 +46,16 @@ public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
       int dy = coordinates[1][1] - coordinates[0][1];
       int dx = coordinates[1][0] - coordinates[0][0];
-      double slope = (double)dy / (double)dx;
+      bool inf = dx == 0;
+      double slope = inf ? 0 : (double)dy / (double)dx;
       for (int i = 2; i < coordinates.size(); ++i) {
         dy = coordinates[i][1] - coordinates[i-1][1];
         dx = coordinates[i][0] - coordinates[i-1][0];
+        if (inf) {
+          if (dx != 0) return false;
+
+          continue;
+        }
         double temp = (double)dy/(double)dx;
         if (temp != slope) return false;
       }
