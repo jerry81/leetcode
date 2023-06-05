2ef45f730a14d6a12066c14ee249cafac9a6c581
Raskolnikov <jerry.tann@endress.com>
2023-06-06 07:57:28 +0800

    📝 rev vowels of string

---
+++ Exercises/151-175/173ReverseVowelsString/revVowelsOfString.cpp
@@ -0,0 +1,47 @@
+/*
+
+345. Reverse Vowels of a String
+Easy
+3.5K
+2.4K
+Companies
+Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+
+
+Example 1:
+
+Input: s = "hello"
+Output: "holle"
+Example 2:
+
+Input: s = "leetcode"
+Output: "leotcede"
+
+
+Constraints:
+
+1 <= s.length <= 3 * 105
+s consist of printable ASCII characters.
+Accepted
+516.9K
+Submissions
+1M
+Acceptance Rate
+50.4%
+
+*/
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+
+    }
+};
+
