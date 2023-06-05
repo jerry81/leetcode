f67dc3e16d55ff9e0aae57caeee685200c00bf39
Raskolnikov <jerry.tann@endress.com>
2023-06-05 12:25:30 +0800

    🚧 first attempt

--- Exercises/151-175/171CheckIfStraight/checkIfStraight.cpp
+++ Exercises/151-175/171CheckIfStraight/checkIfStraight.cpp
@@ -44,6 +44,15 @@ using namespace std;
 class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
-
+      int dy = coordinates[1][1] - coordinates[0][1];
+      int dx = coordinates[1][0] - coordinates[0][0];
+      double slope = (double)dy / (double)dx;
+      for (int i = 2; i < coordinates.size(); ++i) {
+        dy = coordinates[i][1] - coordinates[i-1][1];
+        dx = coordinates[i][0] - coordinates[i-1][0];
+        double temp = (double)dy/(double)dx;
+        if (temp != slope) return false;
+      }
+      return true;
     }
 };
