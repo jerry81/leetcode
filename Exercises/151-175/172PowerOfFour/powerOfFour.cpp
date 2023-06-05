280b7ece8493ff71840e29f5a2706423d8f1ef62
Raskolnikov <jerry.tann@endress.com>
2023-06-05 13:19:04 +0800

    🚧 first attempt

--- Exercises/151-175/172PowerOfFour/powerOfFour.cpp
+++ Exercises/151-175/172PowerOfFour/powerOfFour.cpp
@@ -43,6 +43,10 @@ Acceptance Rate
 class Solution {
 public:
     bool isPowerOfFour(int n) {
+      if (n == 0) return false;
 
+      if (n == 1) return true;
+
+      return (n%4) == 0 && isPowerOfFour(n/4);
     }
 };
