692fb98b2e937f6e3b9f239c1664c79b8efc7f80
Raskolnikov <jerry.tann@endress.com>
2023-06-08 15:08:31 +0800

    ✨ guesshigherlower init

---
+++ Exercises/176-200/179GuessNumberHigherLower/guessHigherLower.cpp
@@ -0,0 +1,64 @@
+/*
+
+374. Guess Number Higher or Lower
+Easy
+3K
+369
+Companies
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+
+
+Example 1:
+
+Input: n = 10, pick = 6
+Output: 6
+Example 2:
+
+Input: n = 1, pick = 1
+Output: 1
+Example 3:
+
+Input: n = 2, pick = 1
+Output: 1
+
+
+Constraints:
+
+1 <= n <= 231 - 1
+1 <= pick <= n
+Accepted
+500.8K
+Submissions
+962.3K
+Acceptance Rate
+52.0%
+
+*/
+
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+
+    }
+};
