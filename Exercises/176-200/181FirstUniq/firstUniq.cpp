d151364108ccf5d9e20fd5532e8b4988a223a3cc
Raskolnikov <jerry.tann@endress.com>
2023-06-09 10:37:01 +0800

    ✨ first uniq

---
+++ Exercises/176-200/181FirstUniq/firstUniq.cpp
@@ -0,0 +1,48 @@
+/*
+
+387. First Unique Character in a String
+Easy
+7.8K
+255
+Companies
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters.
+Accepted
+1.4M
+Submissions
+2.4M
+Acceptance Rate
+59.8%
+
+*/
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+
+    }
+};
