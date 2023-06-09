9a269a585db1319a0fedf4ac2d21bb3a4992160e
Raskolnikov <jerry.tann@endress.com>
2023-06-09 10:30:15 +0800

    🐛 pass examples

--- Exercises/176-200/180FindSmallestLetterGTTarget/smallestLetterGTTarget.cpp
+++ Exercises/176-200/180FindSmallestLetterGTTarget/smallestLetterGTTarget.cpp
@@ -5,9 +5,13 @@ Easy
 3K
 2K
 Companies
-You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+You are given an array of characters letters that is sorted in non-decreasing
+order, and a character target. There are at least two different characters in
+letters.
 
-Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+Return the smallest character in letters that is lexicographically greater than
+target. If such a character does not exist, return the first character in
+letters.
 
 
 
@@ -15,17 +19,18 @@ Example 1:
 
 Input: letters = ["c","f","j"], target = "a"
 Output: "c"
-Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
-Example 2:
+Explanation: The smallest character that is lexicographically greater than 'a'
+in letters is 'c'. Example 2:
 
 Input: letters = ["c","f","j"], target = "c"
 Output: "f"
-Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
-Example 3:
+Explanation: The smallest character that is lexicographically greater than 'c'
+in letters is 'f'. Example 3:
 
 Input: letters = ["x","x","y","y"], target = "z"
 Output: "x"
-Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].
+Explanation: There are no characters in letters that is lexicographically
+greater than 'z' so we return letters[0].
 
 
 Constraints:
@@ -49,17 +54,17 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    char nextGreatestLetter(vector<char>& letters, char target) {
-      int minc = (int) 'z' + 1;
-      for (char c:letters) {
-        if ((int) c > (int) target) {
-          if ((int) c < minc) {
-            minc = c;
-          }
+ public:
+  char nextGreatestLetter(vector<char>& letters, char target) {
+    int minc = (int)'z' + 1;
+    for (char c : letters) {
+      if ((int)c > (int)target) {
+        if ((int)c < minc) {
+          minc = c;
         }
       }
-
-      return minc < (int)target ? char(minc): letters[0];
     }
+
+    return (minc > (int)target && minc <= (int)'z') ? char(minc) : letters[0];
+  }
 };
