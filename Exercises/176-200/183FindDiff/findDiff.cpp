68f38f4845a382f4eeda517c11512697ccbf60f2
Raskolnikov <jerry.tann@endress.com>
2023-06-10 14:57:28 +0800

    🚧 first attempt

--- Exercises/176-200/183FindDiff/findDiff.cpp
+++ Exercises/176-200/183FindDiff/findDiff.cpp
@@ -39,12 +39,20 @@ Acceptance Rate
 */
 
 #include <string>
+#include <map>
 
 using namespace std;
 
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-
+      map<char, int> freq;
+      for (char c: s) {
+        freq[c]++;
+      }
+      for (char c: t) {
+        freq[c]--;
+        if (freq[c] < 0) return c;
+      }
     }
 };
