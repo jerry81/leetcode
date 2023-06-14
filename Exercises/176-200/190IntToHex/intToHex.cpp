56c36690e73f3ba35a26fc8bed3bea8950e68ac1
Raskolnikov <jerry.tann@endress.com>
2023-06-14 15:28:21 +0800

    ✅ gotta pass em all

--- Exercises/176-200/190IntToHex/intToHex.cpp
+++ Exercises/176-200/190IntToHex/intToHex.cpp
@@ -41,7 +41,6 @@ Acceptance Rate
 #include <string>
 
 using namespace std;
-
 class Solution {
   char hexTable[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
                        '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
@@ -52,8 +51,7 @@ class Solution {
     string sol = "";
     uint unum;
     if (num < 0) {
-      num*=-1;
-      unum = num;
+      unum = (long long)num * -1;
       unum = ~unum;
       unum+=1;
     } else {
