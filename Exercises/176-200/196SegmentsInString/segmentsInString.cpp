8bd253090bf113df15655ad7c009f238778f0b02
Raskolnikov <jerry.tann@endress.com>
2023-06-17 15:23:31 +0800

    ✨ count segments

---
+++ Exercises/176-200/196SegmentsInString/segmentsInString.cpp
@@ -0,0 +1,49 @@
+/*
+
+434. Number of Segments in a String
+Easy
+670
+1.2K
+Companies
+Given a string s, return the number of segments in the string.
+
+A segment is defined to be a contiguous sequence of non-space characters.
+
+
+
+Example 1:
+
+Input: s = "Hello, my name is John"
+Output: 5
+Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+Example 2:
+
+Input: s = "Hello"
+Output: 1
+
+
+Constraints:
+
+0 <= s.length <= 300
+s consists of lowercase and uppercase English letters, digits, or one of the following characters "!@#$%^&*()_+-=',.:".
+The only space character in s is ' '.
+Accepted
+147.8K
+Submissions
+400.8K
+Acceptance Rate
+36.9%
+
+
+*/
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int countSegments(string s) {
+
+    }
+};
