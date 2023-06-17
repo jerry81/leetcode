4c166d48cf42efb59dcd02965c5bd59e087754cf
Raskolnikov <jerry.tann@endress.com>
2023-06-17 12:52:44 +0800

    🚧 first attempt

--- Exercises/176-200/194MakeArrayStrictlyIncrease/makeArrStrictlyIncreasing.cpp
+++ Exercises/176-200/194MakeArrayStrictlyIncrease/makeArrStrictlyIncreasing.cpp
@@ -5,9 +5,11 @@ Hard
 829
 19
 Companies
-Given two integer arrays arr1 and arr2, return the minimum number of operations (possibly zero) needed to make arr1 strictly increasing.
+Given two integer arrays arr1 and arr2, return the minimum number of operations
+(possibly zero) needed to make arr1 strictly increasing.
 
-In one operation, you can choose two indices 0 <= i < arr1.length and 0 <= j < arr2.length and do the assignment arr1[i] = arr2[j].
+In one operation, you can choose two indices 0 <= i < arr1.length and 0 <= j <
+arr2.length and do the assignment arr1[i] = arr2[j].
 
 If there is no way to make arr1 strictly increasing, return -1.
 
@@ -45,42 +47,84 @@ Acceptance Rate
 
 */
 
+#include <map>
 #include <vector>
 
 using namespace std;
 
 class Solution {
-int bsearch(vector<int>& arr, int tgt) {
-  int retval = INT_MAX;
-  int retidx = -1;
-  int low = 0;
-  int high = arr.size()-1;
+  map<pair<int, int>, int> lkup;
 
-  while (low < high) {
-    int mid = (low+high) / 2;
-    int val = arr[mid];
-    if (val > tgt) {
+  int bsearch(vector<int>& arr, int tgt) {
+    int retval = INT_MAX;
+    int retidx = -1;
+    int low = 0;
+    int high = arr.size() - 1;
 
-      high = mid-1;
-      if (retval > val) {
-        retval = val;
-        retidx = mid;
+    while (low < high) {
+      int mid = (low + high) / 2;
+      int val = arr[mid];
+      if (val > tgt) {
+        high = mid - 1;
+        if (retval > val) {
+          retval = val;
+          retidx = mid;
+        }
+      } else {
+        low = mid + 1;
       }
-    } else {
-      low = mid+1;
     }
+    return retidx;
   }
-  return retidx;
-}
 
-public:
-    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
-      // sort arr2
-      sort(arr2.begin(), arr2.end());
-      // binary search helper to find first idx greater than val
-      // for each item in arr1
-      // if find a decrease
-      // replace with binary search (greedy)
-      // if no available then return -1.
+  int dfs(int idx, int prev, vector<int>& arr, vector<int>& arr2) {
+    // e.g. prev = 1, cur = 5
+    if (lkup.find({idx, prev}) != lkup.end()) return lkup[{idx, prev}];
+    int lt_prev_idx = bsearch(arr2, prev);
+    int replaced = lt_prev_idx > -1 ? arr2[lt_prev_idx] : -1;
+
+    int cur = arr[idx];
+
+    if (cur > prev) {
+      // 2 choices - keep current 5
+      int withCur = (lkup.find({idx + 1, cur}) != lkup.end())
+                        ? lkup[{idx + 1, cur}]
+                        : dfs(idx + 1, cur, arr, arr2);
+      // replace current with 2
+      if (replaced > -1) {
+        int withReplace = (lkup.find({idx + 1, replaced}) != lkup.end())
+                              ? lkup[{idx + 1, replaced}]
+                              : 1 + dfs(idx + 1, replaced, arr, arr2);
+        lkup[{idx,prev}] = min(withReplace, withCur);
+      } else {
+        lkup[{idx,prev}] = withCur;
+      }
+      return lkup[{idx,prev}];
+    } else {
+      // e.g. prev = 5, cur = 3
+      // replace current with greater than prev or bust
+      if (replaced > -1) {
+        lkup[{idx,prev}] = (lkup.find({idx + 1, replaced}) != lkup.end())
+                              ? lkup[{idx + 1, replaced}]
+                              : 1 + dfs(idx + 1, replaced, arr, arr2);
+      } else {
+        lkup[{idx,prev}] = INT_MAX;
+      }
     }
+    return lkup[{idx,prev}];
+  }
+
+ public:
+  int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
+    // sort arr2
+    sort(arr2.begin(), arr2.end());
+    // binary search helper to find first idx greater than val
+    // for each item in arr1
+    int res = dfs(0, -1, arr1, arr2);
+    return res <= arr1.size() ? res : -1;
+
+    // if find a decrease
+    // replace with binary search (greedy)
+    // if no available then return -1.
+  }
 };
