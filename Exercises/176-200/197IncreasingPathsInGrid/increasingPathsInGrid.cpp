58d58b7f9877da0dcf0a62c9a6b0776f757df472
Raskolnikov <jerry.tann@endress.com>
2023-06-18 12:24:07 +0800

    🐛 close but not quite

--- Exercises/176-200/197IncreasingPathsInGrid/increasingPathsInGrid.cpp
+++ Exercises/176-200/197IncreasingPathsInGrid/increasingPathsInGrid.cpp
@@ -5,11 +5,15 @@ Hard
 868
 21
 Companies
-You are given an m x n integer matrix grid, where you can move from a cell to any adjacent cell in all 4 directions.
+You are given an m x n integer matrix grid, where you can move from a cell to
+any adjacent cell in all 4 directions.
 
-Return the number of strictly increasing paths in the grid such that you can start from any cell and end at any cell. Since the answer may be very large, return it modulo 109 + 7.
+Return the number of strictly increasing paths in the grid such that you can
+start from any cell and end at any cell. Since the answer may be very large,
+return it modulo 109 + 7.
 
-Two paths are considered different if they do not have exactly the same sequence of visited cells.
+Two paths are considered different if they do not have exactly the same sequence
+of visited cells.
 
 
 
@@ -49,73 +53,86 @@ Acceptance Rate
 
 */
 
+#include <map>
+#include <queue>
+#include <string>
 #include <vector>
 
 using namespace std;
 
 class Solution {
-vector<vector<int>> memo;
-struct gridItem {
-  int x=-1;
-  int y=-1;
-  int val=0;
-  gridItem(int x, int y, int v) : x(x), y(y), val(v) {}
-};
-static bool compareGI(gridItem* a, gridItem* b) {
-  return (a->val) < (b->val);
-}
-public:
-    int countPaths(vector<vector<int>>& grid) {
-      int h = grid.size();
-      int w = grid[0].size();
-      vector<vector<int>> memo(h,vector<int>(w,1));
-      vector<gridItem*> processed;
-      for (int i = 0; i < h; ++i) {
-        auto row = grid[i];
-        for (int j = 0; j < row.size(); ++j) {
-          gridItem* g = new gridItem(j,i,row[j]);
-          processed.push_back(g);
+  vector<vector<long long>> memo;
+  vector<vector<int>> incr = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+  long long mod = 1e9 + 7;
+  struct gridItem {
+    int x = -1;
+    int y = -1;
+    int val = 0;
+
+    gridItem(int x, int y, int v) : x(x), y(y), val(v) {}
+  };
+  string hash(int y, int x) {
+    string s = to_string(x) + "," + to_string(y);
+    return s;
+  }
+  static bool compareGI(gridItem* a, gridItem* b) {
+    return (a->val) < (b->val);
+  }
+  void bfs(gridItem* gi, vector<vector<int>>& memo, vector<vector<int>>& g) {
+    queue<pair<int, int>> neighbors;
+    neighbors.push({gi->y, gi->x});
+    map<string, bool> visited;
+    visited[hash(gi->y, gi->x)] = true;
+    while (!neighbors.empty()) {
+      queue<pair<int, int>> nextNeighbors;
+      while (!neighbors.empty()) {
+        auto cur = neighbors.front();
+        neighbors.pop();
+        int toApply = memo[cur.first][cur.second] % mod;
+        for (auto diffs : incr) {
+          int newy = diffs[0] + cur.first;
+          int newx = diffs[1] + cur.second;
+          if (newy < 0) continue;
+          if (newx < 0) continue;
+          if (newy >= memo.size()) continue;
+          if (newx >= memo[0].size()) continue;
+          if (visited[hash(newy, newx)]) continue;
+          if (g[newy][newx] <= g[cur.first][cur.second]) continue;
+
+          visited[hash(newy, newx)] = true;
+          memo[newy][newx] += (toApply % mod);
+          nextNeighbors.push({newy, newx});
         }
       }
-      sort(processed.begin(), processed.end(), compareGI);
-      for (auto a: processed) {
-        cout << "gi " << a->x << "," << a->y << endl;
-      }
-      return 0;
+      neighbors = nextNeighbors;
     }
+  }
+
+ public:
+  int countPaths(vector<vector<int>>& grid) {
+    int h = grid.size();
+    int w = grid[0].size();
+    vector<vector<int>> memo(h, vector<int>(w, 1));
+    vector<gridItem*> processed;
+    for (int i = 0; i < h; ++i) {
+      auto row = grid[i];
+      for (int j = 0; j < row.size(); ++j) {
+        gridItem* g = new gridItem(j, i, row[j]);
+        processed.push_back(g);
+      }
+    }
+    sort(processed.begin(), processed.end(), compareGI);
+    long long tally = 0;
+
+    for (gridItem* gi : processed) {
+      bfs(gi, memo, grid);
+    }
+    for (auto a : memo) {
+      for (auto b : a) {
+        tally += b;
+        tally %= mod;
+      }
+    }
+    return tally;
+  }
 };
-
-/*
-1 1
-3 4
-
-labels for each
-00, 01
-10, 11
-
-100000 items to iterate at most
-1-3-4 found
-mark 00 01 10 as visited
-
-- relevant items
-1-3-4 - 3 + 2 + 1 = 6
-1-4 - 1+1 - can't count this "4" twice
-1,3,4,1
-1-3, 3-4, 1-4
-1-3-4
-
-
-1 2
-4 3
-
-14
-234
-1234
-34
-
-- sort
-1234
-234
-34, 14
-
-*/
