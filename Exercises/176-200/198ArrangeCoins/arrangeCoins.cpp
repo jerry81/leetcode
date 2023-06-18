2af005937e5936e202bc3ff185cba7773adff3ca
Raskolnikov <jerry.tann@endress.com>
2023-06-18 13:56:35 +0800

    ✨ arr coins, initial

---
+++ Exercises/176-200/198ArrangeCoins/arrangeCoins.cpp
@@ -0,0 +1,45 @@
+/*
+
+441. Arranging Coins
+Easy
+3.4K
+1.2K
+Companies
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+Given the integer n, return the number of complete rows of the staircase you will build.
+
+
+
+Example 1:
+
+
+Input: n = 5
+Output: 2
+Explanation: Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+
+Input: n = 8
+Output: 3
+Explanation: Because the 4th row is incomplete, we return 3.
+
+
+Constraints:
+
+1 <= n <= 231 - 1
+Accepted
+363.9K
+Submissions
+786.4K
+Acceptance Rate
+46.3%
+
+*/
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+
+    }
+};
