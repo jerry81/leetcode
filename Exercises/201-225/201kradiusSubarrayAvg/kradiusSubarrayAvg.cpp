b5237bf3e39f64068c828eb2a8b0a7519062282f
Raskolnikov <jerry.tann@endress.com>
2023-06-20 10:29:35 +0800

    🚧 fix setup

--- Exercises/201-225/201kradiusSubarrayAvg/kradiusSubarrayAvg.cpp
+++ Exercises/201-225/201kradiusSubarrayAvg/kradiusSubarrayAvg.cpp
@@ -7,13 +7,20 @@ Medium
 Companies
 You are given a 0-indexed array nums of n integers, and an integer k.
 
-The k-radius average for a subarray of nums centered at some index i with the radius k is the average of all elements in nums between the indices i - k and i + k (inclusive). If there are less than k elements before or after the index i, then the k-radius average is -1.
+The k-radius average for a subarray of nums centered at some index i with the
+radius k is the average of all elements in nums between the indices i - k and i
++ k (inclusive). If there are less than k elements before or after the index i,
+then the k-radius average is -1.
 
-Build and return an array avgs of length n where avgs[i] is the k-radius average for the subarray centered at index i.
+Build and return an array avgs of length n where avgs[i] is the k-radius average
+for the subarray centered at index i.
 
-The average of x elements is the sum of the x elements divided by x, using integer division. The integer division truncates toward zero, which means losing its fractional part.
+The average of x elements is the sum of the x elements divided by x, using
+integer division. The integer division truncates toward zero, which means losing
+its fractional part.
 
-For example, the average of four elements 2, 3, 1, and 5 is (2 + 3 + 1 + 5) / 4 = 11 / 4 = 2.75, which truncates to 2.
+For example, the average of four elements 2, 3, 1, and 5 is (2 + 3 + 1 + 5) / 4
+= 11 / 4 = 2.75, which truncates to 2.
 
 
 Example 1:
@@ -22,13 +29,16 @@ Example 1:
 Input: nums = [7,4,3,9,1,8,5,2,6], k = 3
 Output: [-1,-1,-1,5,4,4,-1,-1,-1]
 Explanation:
-- avg[0], avg[1], and avg[2] are -1 because there are less than k elements before each index.
-- The sum of the subarray centered at index 3 with radius 3 is: 7 + 4 + 3 + 9 + 1 + 8 + 5 = 37.
-  Using integer division, avg[3] = 37 / 7 = 5.
-- For the subarray centered at index 4, avg[4] = (4 + 3 + 9 + 1 + 8 + 5 + 2) / 7 = 4.
-- For the subarray centered at index 5, avg[5] = (3 + 9 + 1 + 8 + 5 + 2 + 6) / 7 = 4.
-- avg[6], avg[7], and avg[8] are -1 because there are less than k elements after each index.
-Example 2:
+- avg[0], avg[1], and avg[2] are -1 because there are less than k elements
+before each index.
+- The sum of the subarray centered at index 3 with radius 3 is: 7 + 4 + 3 + 9 +
+1 + 8 + 5 = 37. Using integer division, avg[3] = 37 / 7 = 5.
+- For the subarray centered at index 4, avg[4] = (4 + 3 + 9 + 1 + 8 + 5 + 2) / 7
+= 4.
+- For the subarray centered at index 5, avg[5] = (3 + 9 + 1 + 8 + 5 + 2 + 6) / 7
+= 4.
+- avg[6], avg[7], and avg[8] are -1 because there are less than k elements after
+each index. Example 2:
 
 Input: nums = [100000], k = 0
 Output: [100000]
@@ -61,23 +71,27 @@ Acceptance Rate
 
 using namespace std;
 
-int intavg(int sum, int count) {
-  return sum/count;
-}
+int intavg(int sum, int count) { return sum / count; }
 
 class Solution {
-public:
-    vector<int> getAverages(vector<int>& nums, int k) {
-      int count = 2*k + 1;
-      vector<int> res;
-      for (int i = 0; i < k; ++i) {
-        res.push_back(-1);
-      }
-      for (int i = k; i < nums.size()-k; ++i) {
+ public:
+  vector<int> getAverages(vector<int>& nums, int k) {
+    int count = 2 * k + 1;
+    vector<int> res;
+    for (int i = 0; i < k; ++i) {
+      res.push_back(-1);
+    }
+    if (nums.size() >= k) {
+      for (int i = k; i < nums.size() - k; ++i) {
         res.push_back(0);
       }
-      for (int i = nums.size()-k; i < nums.size(); ++i) {
+    }
+
+    if (nums.size() >= nums.size() - k) {
+      for (int i = nums.size() - k; i < nums.size(); ++i) {
         res.push_back(-1);
       }
     }
+    return res;
+  }
 };
