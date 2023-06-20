7cc96fa93f8fc49900bd98a9e21fb079beba93ad
Raskolnikov <jerry.tann@endress.com>
2023-06-20 10:51:11 +0800

    ✨ repeat substring pattern

---
+++ Exercises/201-225/202RepeatedSubstringPattern/repeatedSubstringPattern.cpp
@@ -0,0 +1,46 @@
+/*
+
+459. Repeated Substring Pattern
+Easy
+4.5K
+395
+Companies
+Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+
+
+
+Example 1:
+
+Input: s = "abab"
+Output: true
+Explanation: It is the substring "ab" twice.
+Example 2:
+
+Input: s = "aba"
+Output: false
+Example 3:
+
+Input: s = "abcabcabcabc"
+Output: true
+Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of lowercase English letters.
+Accepted
+301.3K
+Submissions
+689.8K
+Acceptance Rate
+43.7%
+
+*/
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+
+    }
+};
