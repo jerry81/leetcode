6ccbede9aff6bd29573624e138d8f8243a3ba0ea
Raskolnikov <jerry.tann@endress.com>
2023-06-21 15:47:20 +0800

    ✨ hamming distance initial

---
+++ Exercises/201-225/204HammingDist/hamingDistance.cpp
@@ -0,0 +1,46 @@
+/*
+
+461. Hamming Distance
+Easy
+3.6K
+211
+Companies
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, return the Hamming distance between them.
+
+
+
+Example 1:
+
+Input: x = 1, y = 4
+Output: 2
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+The above arrows point to positions where the corresponding bits are different.
+Example 2:
+
+Input: x = 3, y = 1
+Output: 1
+
+
+Constraints:
+
+0 <= x, y <= 231 - 1
+Accepted
+529K
+Submissions
+704.9K
+Acceptance Rate
+75.0%
+
+*/
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+
+    }
+};
