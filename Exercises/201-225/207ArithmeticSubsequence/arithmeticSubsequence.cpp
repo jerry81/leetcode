e812f2d11ae60ded0edaab5a39c000595086ff51
Raskolnikov <jerry.tann@endress.com>
2023-06-23 19:47:15 +0800

    ✅ pass examples

--- Exercises/201-225/207ArithmeticSubsequence/arithmeticSubsequence.cpp
+++ Exercises/201-225/207ArithmeticSubsequence/arithmeticSubsequence.cpp
@@ -5,20 +5,23 @@ Medium
 3.1K
 142
 Companies
-Given an array nums of integers, return the length of the longest arithmetic subsequence in nums.
+Given an array nums of integers, return the length of the longest arithmetic
+subsequence in nums.
 
 Note that:
 
-A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
-A sequence seq is arithmetic if seq[i + 1] - seq[i] are all the same value (for 0 <= i < seq.length - 1).
+A subsequence is an array that can be derived from another array by deleting
+some or no elements without changing the order of the remaining elements. A
+sequence seq is arithmetic if seq[i + 1] - seq[i] are all the same value (for 0
+<= i < seq.length - 1).
 
 
 Example 1:
 
 Input: nums = [3,6,9,12]
 Output: 4
-Explanation:  The whole array is an arithmetic sequence with steps of length = 3.
-Example 2:
+Explanation:  The whole array is an arithmetic sequence with steps of length
+= 3. Example 2:
 
 Input: nums = [9,4,7,2,10]
 Output: 3
@@ -48,38 +51,49 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-vector<vector<int>> lookup;
-public:
-    int longestArithSeqLength(vector<int>& nums) {
-      if (nums.empty()) return 0;
+  vector<vector<int>> lookup;
 
-      if (nums.size() == 1) return 1;
+ public:
+  int longestArithSeqLength(vector<int>& nums) {
+    if (nums.empty()) return 0;
 
-      int res = 2;
-      lookup.resize(501,vector<int>(0));
-      for (int i=0;i<nums.size();++i) {
-        int num = nums[i];
-        lookup[num].push_back(i);
-      }
-      for (int i = 0; i < nums.size()-1; ++i) {
-        for (int j = 1; j < nums.size(); ++j) {
-          int cur = 2;
-          int curnum = nums[j];
+    if (nums.size() == 1) return 1;
 
-          int diff = nums[i]-curnum;
-          int k = j+1;
-          while (k < nums.size()) {
-            int expect = curnum+diff;
-            if (expect != nums[k]) break;
-            ++k;
-            curnum = nums[k];
-            ++cur;
-            if (cur > res) res = cur;
-          }
+    int res = 2;
+    lookup.resize(501, vector<int>(0));
+    for (int i = 0; i < nums.size(); ++i) {
+      int num = nums[i];
+      lookup[num].push_back(i);
+    }
+    for (int i = 0; i < nums.size() - 1; ++i) {
+      for (int j = 1; j < nums.size(); ++j) {
+        int cur = 2;
+        int curnum = nums[j];
+
+        int diff = curnum - nums[i];
+        int k = j;
+        while (k < nums.size()) {
+          cerr << "k is " << k << endl;
+          int expect = curnum + diff;
+          if (expect < 0) break;
+
+          if (lookup[expect].empty()) break;
+
+          auto ptr =
+              lower_bound(lookup[expect].begin(), lookup[expect].end(), k + 1);
+
+          if (ptr == lookup[expect].end()) break;
+
+          curnum = nums[*ptr];
+          ++cur;
+          if (cur > res) res = cur;
+
+          k = *ptr;
         }
       }
-      return res;
     }
+    return res;
+  }
 };
 
 /*
