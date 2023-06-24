bd9338cf17f9bc1f003bfe6726ff447b15c7382e
Raskolnikov <jerry.tann@endress.com>
2023-06-24 12:20:00 +0800

    🚧 try brute force

--- Exercises/201-225/209TallestBillboard/tallestBillboard.cpp
+++ Exercises/201-225/209TallestBillboard/tallestBillboard.cpp
@@ -5,11 +5,16 @@ Hard
 1K
 35
 Companies
-You are installing a billboard and want it to have the largest height. The billboard will have two steel supports, one on each side. Each steel support must be an equal height.
+You are installing a billboard and want it to have the largest height. The
+billboard will have two steel supports, one on each side. Each steel support
+must be an equal height.
 
-You are given a collection of rods that can be welded together. For example, if you have rods of lengths 1, 2, and 3, you can weld them together to make a support of length 6.
+You are given a collection of rods that can be welded together. For example, if
+you have rods of lengths 1, 2, and 3, you can weld them together to make a
+support of length 6.
 
-Return the largest possible height of your billboard installation. If you cannot support the billboard, return 0.
+Return the largest possible height of your billboard installation. If you cannot
+support the billboard, return 0.
 
 
 
@@ -17,13 +22,13 @@ Example 1:
 
 Input: rods = [1,2,3,6]
 Output: 6
-Explanation: We have two disjoint subsets {1,2,3} and {6}, which have the same sum = 6.
-Example 2:
+Explanation: We have two disjoint subsets {1,2,3} and {6}, which have the same
+sum = 6. Example 2:
 
 Input: rods = [1,2,3,4,5,6]
 Output: 10
-Explanation: We have two disjoint subsets {2,3,5} and {4,6}, which have the same sum = 10.
-Example 3:
+Explanation: We have two disjoint subsets {2,3,5} and {4,6}, which have the same
+sum = 10. Example 3:
 
 Input: rods = [1,2]
 Output: 0
@@ -49,8 +54,82 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    int tallestBillboard(vector<int>& rods) {
+ public:
+  int tallestBillboard(vector<int>& rods) {
+    if (rods.size() < 2) return 0;
 
+    int res = 0;
+    vector<pair<int, int>> possibleSupports;
+
+    possibleSupports.push_back({rods[0], {rods[1]}});
+    possibleSupports.push_back({0, rods[0] + rods[1]});
+    if (rods[0] == rods[1] && rods[0] > res) res = rods[0];
+    for (int i = 2; i < rods.size(); ++i) {
+      vector<pair<int,int>> nextSupports;
+      for (auto a: possibleSupports) {
+        if (a.first == a.second) {
+        } else {
+        }
+      }
     }
+  }
 };
+
+/*
+
+example analysis
+Input: rods = [1,2,3,4,5,6]
+Output: 10
+Explanation: We have two disjoint subsets {2,3,5} and {4,6}, which have the same
+sum = 10.
+
+- so... disjoint subsets the way to go?
+[1] [1,2] ... [1,2,3,4,5]
+[2] [2,3] ... [2,3,4,5,6]
+
+base case -
+[1,2] - 2 items, equal return 2,2
+- not equal return 0
+
+[1] return 0
+[] return 0
+
+larger case
+
+[1,3,4]
+
+[3,4] -> 0
+1+3, 4 - > 4
+3, 4+1 -> 0
+3+4,1 -> 0  - res is useless
+0 8
+1 7
+4 4
+3 5
+
+another player [8 1 3 4]
+8 [1,3,4]
+
+0 16
+8 8
+9 7
+1 15
+12 4
+11 5
+3 13
+
+12 [8 1 3 4]
+
+0 28 past half sum drop it
+12 16
+20 8
+12+9 7
+9 7+9
+1+12 15
+1 15+12
+...
+
+rod count small <= 20
+try this brute force method
+
+*/
