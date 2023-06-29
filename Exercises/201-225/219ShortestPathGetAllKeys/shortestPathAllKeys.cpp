de04fd664f260e74ac362359e65772a0fc00062c
Raskolnikov <jerry.tann@endress.com>
2023-06-29 20:28:42 +0800

    🚧 convert java to c++

--- Exercises/201-225/219ShortestPathGetAllKeys/shortestPathAllKeys.cpp
+++ Exercises/201-225/219ShortestPathGetAllKeys/shortestPathAllKeys.cpp
@@ -12,13 +12,21 @@ You are given an m x n grid grid where:
 '@' is the starting point.
 Lowercase letters represent keys.
 Uppercase letters represent locks.
-You start at the starting point and one move consists of walking one space in one of the four cardinal directions. You cannot walk outside the grid, or walk into a wall.
+You start at the starting point and one move consists of walking one space in
+one of the four cardinal directions. You cannot walk outside the grid, or walk
+into a wall.
 
-If you walk over a key, you can pick it up and you cannot walk over a lock unless you have its corresponding key.
+If you walk over a key, you can pick it up and you cannot walk over a lock
+unless you have its corresponding key.
 
-For some 1 <= k <= 6, there is exactly one lowercase and one uppercase letter of the first k letters of the English alphabet in the grid. This means that there is exactly one key for each lock, and one lock for each key; and also that the letters used to represent the keys and locks were chosen in the same order as the English alphabet.
+For some 1 <= k <= 6, there is exactly one lowercase and one uppercase letter of
+the first k letters of the English alphabet in the grid. This means that there
+is exactly one key for each lock, and one lock for each key; and also that the
+letters used to represent the keys and locks were chosen in the same order as
+the English alphabet.
 
-Return the lowest number of moves to acquire all keys. If it is impossible, return -1.
+Return the lowest number of moves to acquire all keys. If it is impossible,
+return -1.
 
 
 
@@ -27,8 +35,8 @@ Example 1:
 
 Input: grid = ["@.a..","###.#","b.A.B"]
 Output: 8
-Explanation: Note that the goal is to obtain all the keys not to open all the locks.
-Example 2:
+Explanation: Note that the goal is to obtain all the keys not to open all the
+locks. Example 2:
 
 
 Input: grid = ["@..aA","..B#.","....b"]
@@ -58,20 +66,95 @@ Acceptance Rate
 
 */
 
-#include <vector>
+#include <queue>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    int shortestPathAllKeys(vector<string>& grid) {
-      for (int i = 0; i < grid.size(); ++i) {
-        string row = grid[i];
-        for (int j = 0; j < row.size(); ++j) {
-          char item = row[j];
+ public:
+  int shortestPathAllKeys(vector<string>& grid) {
+    int m = grid.size(), n = grid[0].size();
+    queue<vector<int>> q;
+    vector<vector<int>> moves = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
 
+    // seen['key'] is only for BFS with key state equals 'keys'
+    unordered_map<int, unordered_set<pair<int, int>>> seen;
+
+    unordered_set<char> keySet;
+    unordered_set<char> lockSet;
+    int allKeys = 0;
+    int startR = -1, startC = -1;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        char cell = grid[i][j];
+        if (cell >= 'a' && cell <= 'f') {
+          allKeys += (1 << (cell - 'a'));  // what fuckery is this
+          keySet.insert(cell);
+        }
+        if (cell >= 'A' && cell <= 'F') {
+          lockSet.insert(cell);
+        }
+        if (cell == '@') {
+          startR = i;
+          startC = j;
         }
       }
     }
+
+    // [row, column, key state, distance]
+    q.push({startR, startC, 0, 0});
+    seen[0].insert({startR, startC});
+
+    while (!q.empty()) {
+      auto cur = q.front();
+      q.pop();
+      int curR = cur[0], curC = cur[1], keys = cur[2], dist = cur[3];
+      for (auto move : moves) {
+        int newR = curR + move[0], newC = curC + move[1];
+
+        // If this cell (newR, newC) is reachable.
+        if (newR >= 0 && newR < m && newC >= 0 && newC < n &&
+            grid[newR][newC] != '#') {
+          char cell = grid[newR][newC];
+
+          // If it is a key:
+          if (keySet.count(cell) > 0) {
+            // If we have collected it before, no need to revisit this cell.
+            if (((1 << (cell - 'a')) & keys) != 0) {
+              continue;
+            }
+
+            // Otherwise, we can walk to this cell and pick it up.
+            int newKeys = (keys | (1 << (cell - 'a')));
+
+            // If we collect all keys, return dist + 1.
+            // Otherwise, just add this state to seen and queue.
+            if (newKeys == allKeys) {
+              return dist + 1;
+            }
+            seen[newKeys].insert({newR, newC});
+            q.push({newR, newC, newKeys, dist + 1});
+          }
+
+          // If it is a lock and we don't have its key, continue.
+          if (lockSet.count(cell) > 0 && ((keys & (1 << (cell - 'A'))) == 0)) {
+            continue;
+          }
+
+          // We can walk to this cell if we haven't been here before with the
+          // same key state.
+          else if (seen[keys].count({newR, newC}) <= 0) {
+            seen[keys].insert({newR, newC});
+            q.push({newR, newC, keys, dist + 1});
+          }
+        }
+      }
+    }
+
+    return -1;
+  }
 };
