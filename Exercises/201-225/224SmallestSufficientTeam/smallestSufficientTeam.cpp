c52753b8faadea0e3293c3aa6e9454a7ee6e8393
Raskolnikov <jerry.tann@endress.com>
2023-07-16 09:43:47 +0200

    ✨ attempt

--- Exercises/201-225/224SmallestSufficientTeam/smallestSufficientTeam.cpp
+++ Exercises/201-225/224SmallestSufficientTeam/smallestSufficientTeam.cpp
@@ -5,12 +5,17 @@ Hard
 1.2K
 26
 Companies
-In a project, you have a list of required skills req_skills, and a list of people. The ith person people[i] contains a list of skills that the person has.
+In a project, you have a list of required skills req_skills, and a list of
+people. The ith person people[i] contains a list of skills that the person has.
 
-Consider a sufficient team: a set of people such that for every required skill in req_skills, there is at least one person in the team who has that skill. We can represent these teams by the index of each person.
+Consider a sufficient team: a set of people such that for every required skill
+in req_skills, there is at least one person in the team who has that skill. We
+can represent these teams by the index of each person.
 
-For example, team = [0, 1, 3] represents the people with skills people[0], people[1], and people[3].
-Return any sufficient team of the smallest possible size, represented by the index of each person. You may return the answer in any order.
+For example, team = [0, 1, 3] represents the people with skills people[0],
+people[1], and people[3]. Return any sufficient team of the smallest possible
+size, represented by the index of each person. You may return the answer in any
+order.
 
 It is guaranteed an answer exists.
 
@@ -18,11 +23,12 @@ It is guaranteed an answer exists.
 
 Example 1:
 
-Input: req_skills = ["java","nodejs","reactjs"], people = [["java"],["nodejs"],["nodejs","reactjs"]]
-Output: [0,2]
-Example 2:
+Input: req_skills = ["java","nodejs","reactjs"], people =
+[["java"],["nodejs"],["nodejs","reactjs"]] Output: [0,2] Example 2:
 
-Input: req_skills = ["algorithms","math","java","reactjs","csharp","aws"], people = [["algorithms","math","java"],["algorithms","math","reactjs"],["java","csharp","aws"],["reactjs","csharp"],["csharp","math"],["aws","java"]]
+Input: req_skills = ["algorithms","math","java","reactjs","csharp","aws"],
+people =
+[["algorithms","math","java"],["algorithms","math","reactjs"],["java","csharp","aws"],["reactjs","csharp"],["csharp","math"],["aws","java"]]
 Output: [1,2]
 
 
@@ -48,43 +54,65 @@ Acceptance Rate
 
 */
 
-#include <vector>
-#include <string>
 #include <iostream>
 #include <map>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
+  int minCount = INT_MAX;
+  long long tgt = 0;
+  vector<int> res;
+  map<string, long long> valueLookup;
+  vector<int> peopleMasks;
+  void traverse(vector<int> cur, int curIdx, long long curval, int curSize) {
+    if (curSize > minCount) return;
 
-public:
-    vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
-      long long tgt = 0;
-      map<string, long long> valueLookup;
-      for (int i = 0; i < req_skills.size(); ++i) {
-        long long cur = pow(2,i);
-        valueLookup[req_skills[i]] = cur;
-        tgt+=cur;
+    if (curval == tgt) {
+      if (minCount > curSize) {
+        minCount = curSize;
+        res = cur;
+        return;
       }
-      // masks of people
-      vector<int> peopleMasks;
-      for (int i = 0; i < people.size(); ++i) {
-        vector<string> temp = people[i];
-        long long cur = 0;
-        for (string s: temp) {
-          cur += valueLookup[s];
-        }
-        peopleMasks.push_back(cur);
-      }
-      for (auto a:peopleMasks) {
-        cout << "a is " << a << endl;
-      }
-      vector<int> ret;
-      return ret;
     }
+
+    vector<int> nextcur = cur;
+    nextcur.push_back(curIdx);
+    traverse(cur, curIdx+1, curval, curSize);
+    traverse(nextcur, curIdx+1, curval+peopleMasks[curIdx], curSize+1);
+  }
+
+ public:
+  vector<int> smallestSufficientTeam(vector<string>& req_skills,
+                                     vector<vector<string>>& people) {
+
+
+    for (int i = 0; i < req_skills.size(); ++i) {
+      long long cur = pow(2, i);
+      valueLookup[req_skills[i]] = cur;
+      tgt += cur;
+    }
+    // masks of people
+
+    for (int i = 0; i < people.size(); ++i) {
+      vector<string> temp = people[i];
+      long long cur = 0;
+      for (string s : temp) {
+        cur += valueLookup[s];
+      }
+      peopleMasks.push_back(cur);
+    }
+    vector<int> init;
+    // permuations time
+    traverse(init, 0, 0, 0);
+
+    return res;
+  }
 };
 
-int main () {
+int main() {
   cerr << "expect 2 " << (2 | 0) << endl;
   cerr << "expect 5 " << (1 | 4) << endl;
   int test = 2;
