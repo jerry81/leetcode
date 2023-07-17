0d02ffac2250ed09eea8613c1306c7e71b577f2a
Raskolnikov <jerry.tann@endress.com>
2023-07-17 16:48:10 +0200

    ✅ bang

--- Exercises/226-250/227PerfectNumber/perfectNumber.cpp
+++ Exercises/226-250/227PerfectNumber/perfectNumber.cpp
@@ -38,7 +38,9 @@ Acceptance Rate
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
+      if (num == 1) return false;
       int sum = 0;
+
       for (int i = 1; i <= (num/2 + 1); ++i) {
         if (num%i == 0) sum+=i;
       }
