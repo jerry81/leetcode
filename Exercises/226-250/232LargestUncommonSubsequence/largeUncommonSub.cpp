344bc3bf9cb11e50426baa8e88bfd29452ca72b1
Raskolnikov <jerry.tann@endress.com>
2023-07-20 11:37:51 +0200

    🚧 made it too complicated, didn't read well

---
+++ Exercises/226-250/232LargestUncommonSubsequence/largeUncommonSub.cpp
@@ -0,0 +1,109 @@
+/*
+
+521. Longest Uncommon Subsequence I
+Easy
+752
+6.3K
+Companies
+Given two strings a and b, return the length of the longest uncommon subsequence between a and b. If the longest uncommon subsequence does not exist, return -1.
+
+An uncommon subsequence between two strings is a string that is a subsequence of one but not the other.
+
+A subsequence of a string s is a string that can be obtained after deleting any number of characters from s.
+
+For example, "abc" is a subsequence of "aebdc" because you can delete the underlined characters in "aebdc" to get "abc". Other subsequences of "aebdc" include "aebdc", "aeb", and "" (empty string).
+
+
+Example 1:
+
+Input: a = "aba", b = "cdc"
+Output: 3
+Explanation: One longest uncommon subsequence is "aba" because "aba" is a subsequence of "aba" but not "cdc".
+Note that "cdc" is also a longest uncommon subsequence.
+Example 2:
+
+Input: a = "aaa", b = "bbb"
+Output: 3
+Explanation: The longest uncommon subsequences are "aaa" and "bbb".
+Example 3:
+
+Input: a = "aaa", b = "aaa"
+Output: -1
+Explanation: Every subsequence of string a is also a subsequence of string b. Similarly, every subsequence of string b is also a subsequence of string a.
+
+
+Constraints:
+
+1 <= a.length, b.length <= 100
+a and b consist of lower-case English letters.
+Accepted
+99K
+Submissions
+164K
+Acceptance Rate
+60.4%
+
+*/
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+      // find a in b
+      if (a == b) return -1;
+      if (a.size() == b.size()) return a.size();
+      return max(a.size(), b.size());
+    }
+};
+
+/*
+aebecede
+
+zabcde
+
+Recursion?
+
+aebecede
+zabcde
+"cancelling"?
+zabcde
+aebecede
+z abcde
+a...e
+
+z becede
+bcde
+
+aebecede
+zabcde
+
+aebecede
+abcde
+
+ebecede
+bcde
+
+ebecede
+bcde
+e becede
+bcde
+eecede
+cde
+eeede
+de
+eeee
+e
+
+aebecede
+zedcba
+
+aebecede
+
+
+eee
+or
+z
+*/
