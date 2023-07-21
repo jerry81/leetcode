6f50632f9ff33a5af5af9058b6a99dee69e68556
Raskolnikov <jerry.tann@endress.com>
2023-07-21 09:56:08 +0200

    🚧 some ideas

--- Exercises/226-250/234LongestIncreasingSubSeq/longestIncSubSeq.cpp
+++ Exercises/226-250/234LongestIncreasingSubSeq/longestIncSubSeq.cpp
@@ -9,7 +9,8 @@ Medium
 5.5K
 226
 Companies
-Given an integer array nums, return the number of longest increasing subsequences.
+Given an integer array nums, return the number of longest increasing
+subsequences.
 
 Notice that the sequence has to be strictly increasing.
 
@@ -19,12 +20,13 @@ Example 1:
 
 Input: nums = [1,3,5,4,7]
 Output: 2
-Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3, 5, 7].
-Example 2:
+Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3,
+5, 7]. Example 2:
 
 Input: nums = [2,2,2,2,2]
 Output: 5
-Explanation: The length of the longest increasing subsequence is 1, and there are 5 increasing subsequences of length 1, so output 5.
+Explanation: The length of the longest increasing subsequence is 1, and there
+are 5 increasing subsequences of length 1, so output 5.
 
 
 Constraints:
@@ -40,15 +42,41 @@ Acceptance Rate
 
 */
 
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    int findNumberOfLIS(vector<int>& nums) {
-
+  unordered_map<int, int> lookup;
+  int r(vector<int>& nums, int idx, int last) {
+    if (idx < 0) return 0;
+    if (lookup.find(idx) != lookup.end()) return lookup[idx];
+    if (idx >= nums.size()-1) return 1;
+    int cur = nums[idx];
+    if (cur < last) {
+      // take or leave
+      lookup[idx] = max()
+    } else {
     }
+  }
+
+ public:
+  int findNumberOfLIS(vector<int>& nums) {
+    int ret = 1;
+    int mx = 0;
+    for (int i = nums.size()-1; i >= 0; --i) {
+      int maxIS = r(nums, i);
+      if (maxIS > mx) {
+        mx = maxIS;
+        ret = 1;
+      }
+      if (maxIS == mx) {
+        ret++;
+      }
+    }
+    return ret;
+  }
 };
 
 // increasing subsequence from i
