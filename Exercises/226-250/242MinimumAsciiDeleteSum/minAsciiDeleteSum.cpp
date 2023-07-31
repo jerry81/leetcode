1dabe8af9e3d7ebbf241b66797bc7c4886883a92
Raskolnikov <jerry.tann@endress.com>
2023-07-31 16:46:50 +0800

    🚧 base cases

--- Exercises/226-250/242MinimumAsciiDeleteSum/minAsciiDeleteSum.cpp
+++ Exercises/226-250/242MinimumAsciiDeleteSum/minAsciiDeleteSum.cpp
@@ -5,7 +5,8 @@ Medium
 3.1K
 81
 Companies
-Given two strings s1 and s2, return the lowest ASCII sum of deleted characters to make two strings equal.
+Given two strings s1 and s2, return the lowest ASCII sum of deleted characters
+to make two strings equal.
 
 
 
@@ -13,18 +14,19 @@ Example 1:
 
 Input: s1 = "sea", s2 = "eat"
 Output: 231
-Explanation: Deleting "s" from "sea" adds the ASCII value of "s" (115) to the sum.
-Deleting "t" from "eat" adds 116 to the sum.
-At the end, both strings are equal, and 115 + 116 = 231 is the minimum sum possible to achieve this.
-Example 2:
+Explanation: Deleting "s" from "sea" adds the ASCII value of "s" (115) to the
+sum. Deleting "t" from "eat" adds 116 to the sum. At the end, both strings are
+equal, and 115 + 116 = 231 is the minimum sum possible to achieve this. Example
+2:
 
 Input: s1 = "delete", s2 = "leet"
 Output: 403
 Explanation: Deleting "dee" from "delete" to turn the string into "let",
 adds 100[d] + 101[e] + 101[e] to the sum.
 Deleting "e" from "leet" adds 101[e] to the sum.
-At the end, both strings are equal to "let", and the answer is 100+101+101+101 = 403.
-If instead we turned both strings into "lee" or "eet", we would get answers of 433 or 417, which are higher.
+At the end, both strings are equal to "let", and the answer is 100+101+101+101 =
+403. If instead we turned both strings into "lee" or "eet", we would get answers
+of 433 or 417, which are higher.
 
 
 Constraints:
@@ -46,19 +48,49 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-string first;
-string second;
-unordered_map<string, int> dp;
-int r(int i1, int i2) {
-}
-string to_hash(int i1, int i2) {
-  return to_string(i1) + "," + to_string(i2);
-}
-public:
-    int minimumDeleteSum(string s1, string s2) {
-      first = s1;
-      second = s2;
+  string first;
+  string second;
+  unordered_map<string, int> dp;
+  int r(int i1, int i2) {
+    string as_hash = to_hash(i1, i2);
+    if (dp.find(as_hash) != dp.end()) return dp[as_hash];
+
+    if (i1 < 0 && i2 < 0) {
+      dp[as_hash] = 0;
+      return 0;
     }
+
+    // termination cases
+    if (i1 < 0) {
+      int sum = 0;
+      while (i2 >= 0) {
+        sum += second[i2];
+        i2--;
+      }
+      dp[as_hash] = sum;
+      return sum;
+    }
+
+    if (i2 < 0) {
+      int sum = 0;
+      while (i1 >= 0) {
+        sum += first[i1];
+        i1--;
+      }
+      dp[as_hash] = sum;
+      return sum;
+    }
+
+    if (first[i1] == second[i2]) {
+    }
+  }
+  string to_hash(int i1, int i2) { return to_string(i1) + "," + to_string(i2); }
+
+ public:
+  int minimumDeleteSum(string s1, string s2) {
+    first = s1;
+    second = s2;
+  }
 };
 
 /*
@@ -66,7 +98,8 @@ public:
 2 stages
 
 1.  ways to delete to make equal
-  a.  something with subsequences? - longest common subsequence finds the min diff
+  a.  something with subsequences? - longest common subsequence finds the min
+diff
 2.  optimize sums
 
 official solutions
