a7d069cfd136a86e7bb25db11f42ccf028ad9ad2
Raskolnikov <jerry.tann@endress.com>
2023-08-02 10:54:27 +0800

    ✨ daily initial

---
+++ Exercises/226-250/244Permutations/permutations.cpp
@@ -0,0 +1,49 @@
+/*
+
+46. Permutations
+Medium
+16.7K
+268
+Companies
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+
+Input: nums = [1]
+Output: [[1]]
+
+
+Constraints:
+
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+All the integers of nums are unique.
+Accepted
+1.7M
+Submissions
+2.2M
+Acceptance Rate
+76.4%
+
+*/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+
+    }
+};
