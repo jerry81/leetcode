437ee891c90c4e93a5e6e481123a4bc427a7e824
Raskolnikov <jerry.tann@endress.com>
2023-08-04 12:58:18 +0800

    🚧 ghetto memo

--- Exercises/226-250/247WordBreak/wordBreak.cpp
+++ Exercises/226-250/247WordBreak/wordBreak.cpp
@@ -47,25 +47,31 @@ Submissions
 #include <vector>
 using namespace std;
 
+int _counter = 0;
 struct TrieNode {
   char c;
   bool ender = false;
+  int id;
   unordered_map<char, TrieNode*> nexts;
-  TrieNode(char c) : c(c){};
+  TrieNode(char c) : c(c){
+    id = _counter;
+    _counter++;
+  };
 };
 class Solution {
-  unordered_map<char, TrieNode*> root;
+  TrieNode* root;
+  unordered_map<string, bool> lookup;
 
-  void buildTreeR(unordered_map<char, TrieNode*> &root, string s) {
+  void buildTreeR(TrieNode* &root, string s) {
     if (s.empty()) return;
 
     char f = s[0];
     s.erase(s.begin());
-    if (root.find(f) == root.end()) root[f] = new TrieNode(f);
+    if (root->nexts.find(f) == root->nexts.end()) root->nexts[f] = new TrieNode(f);
 
-    if (s.empty()) root[f]->ender = true;
+    if (s.empty()) root->nexts[f]->ender = true;
 
-    buildTreeR(root[f]->nexts, s);
+    buildTreeR(root->nexts[f], s);
   }
 
   void buildTrie(vector<string>& wordDict) {
@@ -74,30 +80,42 @@ class Solution {
     }
   }
 
-  bool verify(string s, unordered_map<char, TrieNode*> cur) {
-    if (s.size() == 1) {
-      if (cur.find(s[0]) == cur.end()) return false;
+  string to_hash(string s, int id) {
+    return s + to_string(id);
+  }
 
-      return (cur[s[0]]->ender);
+  bool verify(string s, TrieNode* cur) {
+    string hsh = to_hash(s, cur->id);
+    if (lookup.find(hsh) != lookup.end()) return lookup[hsh];
+
+    if (s.size() == 1) {
+      if (cur->nexts.find(s[0]) == cur->nexts.end()) return false;
+
+      return (cur->nexts[s[0]]->ender);
     }
 
     string cpy = s;
     char first = cpy[0];
 
     cpy.erase(cpy.begin());
-    if (cur.find(first) == cur.end()) return false;
+    if (cur->nexts.find(first) == cur->nexts.end()) {
+      lookup[hsh] = false;
+      return false;
+    }
 
     bool ret = true;
-    if (cur[first]->ender) {
-      ret = verify(cpy,root) || verify(cpy,cur[first]->nexts);
+    if (cur->nexts[first]->ender) {
+      ret = verify(cpy,root) || verify(cpy,cur->nexts[first]);
     } else {
-      ret = verify(cpy, cur[first]->nexts);
+      ret = verify(cpy, cur->nexts[first]);
     }
+    lookup[hsh] = ret;
     return ret;
   }
 
  public:
   bool wordBreak(string s, vector<string>& wordDict) {
+    root = new TrieNode('_');
     buildTrie(wordDict);
     return verify(s, root);
   }
