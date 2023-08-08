871cf2da94e540fc1bb88be7d29b003d911de39b
Raskolnikov <jerry.tann@endress.com>
2023-08-08 11:39:39 +0800

    🚧 attempt 1

--- Exercises/251-275/255SearchRotatedSortedArray/searchRotatedSortedArray.cpp
+++ Exercises/251-275/255SearchRotatedSortedArray/searchRotatedSortedArray.cpp
@@ -7,9 +7,14 @@ Medium
 Companies
 There is an integer array nums sorted in ascending order (with distinct values).
 
-Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+Prior to being passed to your function, nums is possibly rotated at an unknown
+pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k],
+nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For
+example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become
+[4,5,6,7,0,1,2].
 
-Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+Given the array nums after the possible rotation and an integer target, return
+the index of target if it is in nums, or -1 if it is not in nums.
 
 You must write an algorithm with O(log n) runtime complexity.
 
@@ -50,44 +55,45 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-      // find rotation point by examining last item - can't be done as the numbers are random (though sequential)
-      // binary search to find pivot point (smallest number)
-      // [4,5,6,7,0,1,2]
-      int sz = nums.size();
-      int curmin = nums.front(); // 4
-      int curmax = nums.back(); // 2
-      if (curmax == target) return sz-1;
+ public:
+  int search(vector<int>& nums, int target) {
+    // find rotation point by examining last item - can't be done as the numbers
+    // are random (though sequential) binary search to find pivot point
+    // (smallest number) [4,5,6,7,0,1,2]
+    int sz = nums.size();
+    int curmin = nums.front();  // 4
+    int curmax = nums.back();   // 2
+    if (curmax == target) return sz - 1;
 
-      if (curmin == target) return 0;
+    if (curmin == target) return 0;
 
-      int mi = 0;
-      int mxi = sz-1;
+    int mi = 0;
+    int mxi = sz - 1;
 
+    int midi = 0;
+    while (mi <= mxi) {
       if (curmin > curmax) {
         int tmp = mxi;
-        mxi = mi+sz;
+        mxi = mi + sz;
         mi = tmp;
         int tmpv = curmin;
         curmin = curmax;
         curmax = tmpv;
       }
-      int midi = 0;
-      while (mi <= mxi) {
-        int nmin = mi % nums.size();
-        int nmax = mxi % nums.size();
-        midi = (mi + mxi) / 2; // 3
-        int midv = nums[midi]; // 7
-        if (midv == target) return midv;
+      int nmin = mi;
+      int nmax = mxi;
+      midi = ((mi + mxi) / 2)%sz;  // 3
+      int midv = nums[midi];  // 7
+      if (midv == target) return midv;
 
-        if (midv < curmin && midv < target) { // false
-          curmin = midv;
-          mi = midi+1;
-        }
-
+      if (midv < target) {
+        mi = midi+1;
       }
-      return 0;
-    }
-};
 
+      if (midv < target) {
+        mxi = midi-1;
+      }
+    }
+    return -1;
+  }
+};
