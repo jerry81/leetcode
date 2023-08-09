9e38f8614d79f2cf61fdb2b0f01f04d148e9a90e
Raskolnikov <jerry.tann@endress.com>
2023-08-09 11:06:53 +0800

    🚧 that sucked

--- Exercises/251-275/257MinTheMaxDiffInPairs/minMaxDiff.cpp
+++ Exercises/251-275/257MinTheMaxDiffInPairs/minMaxDiff.cpp
@@ -5,11 +5,15 @@ Medium
 467
 21
 Companies
-You are given a 0-indexed integer array nums and an integer p. Find p pairs of indices of nums such that the maximum difference amongst all the pairs is minimized. Also, ensure no index appears more than once amongst the p pairs.
+You are given a 0-indexed integer array nums and an integer p. Find p pairs of
+indices of nums such that the maximum difference amongst all the pairs is
+minimized. Also, ensure no index appears more than once amongst the p pairs.
 
-Note that for a pair of elements at the index i and j, the difference of this pair is |nums[i] - nums[j]|, where |x| represents the absolute value of x.
+Note that for a pair of elements at the index i and j, the difference of this
+pair is |nums[i] - nums[j]|, where |x| represents the absolute value of x.
 
-Return the minimum maximum difference among all p pairs. We define the maximum of an empty set to be zero.
+Return the minimum maximum difference among all p pairs. We define the maximum
+of an empty set to be zero.
 
 
 
@@ -17,13 +21,15 @@ Example 1:
 
 Input: nums = [10,1,2,7,1,3], p = 2
 Output: 1
-Explanation: The first pair is formed from the indices 1 and 4, and the second pair is formed from the indices 2 and 5.
-The maximum difference is max(|nums[1] - nums[4]|, |nums[2] - nums[5]|) = max(0, 1) = 1. Therefore, we return 1.
+Explanation: The first pair is formed from the indices 1 and 4, and the second
+pair is formed from the indices 2 and 5. The maximum difference is max(|nums[1]
+- nums[4]|, |nums[2] - nums[5]|) = max(0, 1) = 1. Therefore, we return 1.
 Example 2:
 
 Input: nums = [4,2,1,2], p = 1
 Output: 0
-Explanation: Let the indices 1 and 3 form a pair. The difference of that pair is |2 - 2| = 0, which is the minimum we can attain.
+Explanation: Let the indices 1 and 3 form a pair. The difference of that pair is
+|2 - 2| = 0, which is the minimum we can attain.
 
 
 Constraints:
@@ -40,45 +46,43 @@ Acceptance Rate
 
 */
 
-#include <vector>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-int countPairs(vector<int>& nums, int thresh) {
-  int i = 0;
-  int count = 0;
-  while (i < nums.size()-1) {
-    int j = nums[i];
-    int k = nums[i+1];
-    if (abs(j-k) < thresh) {
-      ++count;
+  int countPairs(vector<int>& nums, int thresh) {
+    int i = 0;
+    int count = 0;
+    while (i < nums.size() - 1) {
+      int j = nums[i];
+      int k = nums[i + 1];
+      if (abs(j - k) <= thresh) {
+        ++count;
+        ++i;
+      }
       ++i;
     }
-    ++i;
+    return count;
   }
-  return count;
-}
-public:
-    int minimizeMax(vector<int>& nums, int p) {
 
-      sort(nums.begin(), nums.end());
-      int mx = abs(nums.back() - nums.front());
-      int mn = 0;
-      int res = mx;
-      while (mn < mx) {
-        int mid = mn + (mx-mn) / 2;
-        int pairs = countPairs(nums, mid);
-        if (pairs >= p) {
-          res = mid;
-          mx = mid-1;
-        } else {
-          mn = mid+1;
-        }
+ public:
+  int minimizeMax(vector<int>& nums, int p) {
+    sort(nums.begin(), nums.end());
+    int mx = abs(nums.back() - nums.front());
+    int mn = 0;
+    while (mn < mx) {
+      int mid = mn + (mx - mn) / 2;
+      int pairs = countPairs(nums, mid);
+      if (pairs >= p) {
+        mx = mid;
+      } else {
+        mn = mid + 1;
       }
-      return res;
     }
+    return mn;
+  }
 };
 
 /*
