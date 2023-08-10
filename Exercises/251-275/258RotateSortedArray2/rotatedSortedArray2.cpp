591153b4180300c8899ac0b837a64f443fa0b194
Raskolnikov <jerry.tann@endress.com>
2023-08-10 10:55:12 +0800

    🚧 must figure how to find pivot with repeats

--- Exercises/251-275/258RotateSortedArray2/rotatedSortedArray2.cpp
+++ Exercises/251-275/258RotateSortedArray2/rotatedSortedArray2.cpp
@@ -37,12 +37,47 @@ Follow up: This problem is similar to Search in Rotated Sorted Array, but nums m
 
 */
 
-#include <vector>;
-using namespace std;
+#include <vector>
 
+using namespace std;
 class Solution {
+private:
+    int findPivotIndex(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
 public:
     bool search(vector<int>& nums, int target) {
+        int pivot = findPivotIndex(nums);
+        int left = 0;
+        int right = nums.size() - 1;
 
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int rotatedMid = (mid + pivot) % nums.size();
+
+            if (nums[rotatedMid] == target) {
+                return true;
+            } else if (nums[rotatedMid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
     }
 };
