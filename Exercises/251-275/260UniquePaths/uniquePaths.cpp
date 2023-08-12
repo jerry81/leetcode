20d0d0ca2fa39fbe72cd897ab43f5348eef6d515
Raskolnikov <jerry.tann@endress.com>
2023-08-12 10:02:35 +0800

    🚧 add dp?

--- Exercises/251-275/260UniquePaths/uniquePaths.cpp
+++ Exercises/251-275/260UniquePaths/uniquePaths.cpp
@@ -5,13 +5,19 @@ Medium
 7.5K
 452
 Companies
-You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+You are given an m x n integer array grid. There is a robot initially located at
+the top-left corner (i.e., grid[0][0]). The robot tries to move to the
+bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either
+down or right at any point in time.
 
-An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+An obstacle and space are marked as 1 or 0 respectively in grid. A path that the
+robot takes cannot include any square that is an obstacle.
 
-Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+Return the number of possible unique paths that the robot can take to reach the
+bottom-right corner.
 
-The testcases are generated so that the answer will be less than or equal to 2 * 109.
+The testcases are generated so that the answer will be less than or equal to 2 *
+109.
 
 
 
@@ -50,22 +56,32 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-int res = 0;
-void r(int y, int x, vector<vector<int>>& obstacleGrid, int h, int w) {
-  if (y >= h) return;
-  if (x >= w) return;
-  if (obstacleGrid[y][x] == 1) return;
-  if (y == (h-1) && x == (w-1)) res++;
-  int right = x+1;
-  int d = y+1;
-  r(d,x,obstacleGrid,h,w);
-  r(y,right,obstacleGrid,h,w);
-}
-public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-      r(0,0,obstacleGrid,obstacleGrid.size(),obstacleGrid[0].size());
-      return res;
-    }
+  int r(int y, int x, vector<vector<int>>& obstacleGrid, int h, int w, vector<vector<int>> &dp) {
+    if (y >= h) return 0;
+
+    if (x >= w) return 0;
+
+    if (obstacleGrid[y][x] == 1) return 0;
+
+    if (y == (h - 1) && x == (w - 1)) return 1;
+
+    if (dp[y][x] >= 0) return dp[y][x];
+
+    int right = x + 1;
+    int d = y + 1;
+    int xsum = r(d, x, obstacleGrid, h, w, dp);
+    int ysum = r(y, right, obstacleGrid, h, w, dp);
+    dp[y][x] = xsum + ysum;
+    return dp[y][x];
+  }
+
+ public:
+  int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    int h = obstacleGrid.size();
+    int w = obstacleGrid[0].size();
+    vector<vector<int>> dp(h,vector<int>(w,-1));
+    return r(0, 0, obstacleGrid, h,w,dp);
+  }
 };
 
 // definitely a dfs
