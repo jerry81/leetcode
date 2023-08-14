57c27c0b44c23f6cc3f68b2e0d26599843dc8c99
Raskolnikov <jerry.tann@endress.com>
2023-08-14 16:06:21 +0800

    ✨ max product 3 numbers

---
+++ Exercises/251-275/265MaxProductOfThree/maxProductThreeNumbers.cpp
@@ -0,0 +1,42 @@
+/*
+
+628. Maximum Product of Three Numbers
+Easy
+3.9K
+634
+Companies
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: 6
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: 24
+Example 3:
+
+Input: nums = [-1,-2,-3]
+Output: -6
+
+
+Constraints:
+
+3 <= nums.length <= 104
+-1000 <= nums[i] <= 1000
+
+*/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+
+    }
+};
