d45d326f0c887ab0dad1440b43645fd6876db74c
Raskolnikov <jerry.tann@endress.com>
2023-08-18 15:42:07 +0800

    🚧 annoying final step

--- Exercises/251-275/271MaxNetworkRank/maxNetworkRank.cpp
+++ Exercises/251-275/271MaxNetworkRank/maxNetworkRank.cpp
@@ -5,13 +5,19 @@ Medium
 1.6K
 252
 Companies
-There is an infrastructure of n cities with some number of roads connecting these cities. Each roads[i] = [ai, bi] indicates that there is a bidirectional road between cities ai and bi.
+There is an infrastructure of n cities with some number of roads connecting
+these cities. Each roads[i] = [ai, bi] indicates that there is a bidirectional
+road between cities ai and bi.
 
-The network rank of two different cities is defined as the total number of directly connected roads to either city. If a road is directly connected to both cities, it is only counted once.
+The network rank of two different cities is defined as the total number of
+directly connected roads to either city. If a road is directly connected to both
+cities, it is only counted once.
 
-The maximal network rank of the infrastructure is the maximum network rank of all pairs of different cities.
+The maximal network rank of the infrastructure is the maximum network rank of
+all pairs of different cities.
 
-Given the integer n and the array roads, return the maximal network rank of the entire infrastructure.
+Given the integer n and the array roads, return the maximal network rank of the
+entire infrastructure.
 
 
 
@@ -21,7 +27,8 @@ Example 1:
 
 Input: n = 4, roads = [[0,1],[0,3],[1,2],[1,3]]
 Output: 4
-Explanation: The network rank of cities 0 and 1 is 4 as there are 4 roads that are connected to either 0 or 1. The road between 0 and 1 is only counted once.
+Explanation: The network rank of cities 0 and 1 is 4 as there are 4 roads that
+are connected to either 0 or 1. The road between 0 and 1 is only counted once.
 Example 2:
 
 
@@ -33,7 +40,8 @@ Example 3:
 
 Input: n = 8, roads = [[0,1],[1,2],[2,3],[2,4],[5,6],[5,7]]
 Output: 5
-Explanation: The network rank of 2 and 5 is 5. Notice that all the cities do not have to be connected.
+Explanation: The network rank of 2 and 5 is 5. Notice that all the cities do not
+have to be connected.
 
 
 Constraints:
@@ -64,35 +72,33 @@ struct City {
   City(){};
 };
 
-bool comp(City* a, City* b) {
-  return a->indegree > b->indegree;
-}
+bool comp(City* a, City* b) { return a->indegree > b->indegree; }
 
 class Solution {
-public:
-    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-      // iterate roads
-      // build neighbors
-      vector<City*> cities;
-      for (int i = 0; i < n; ++i) {
-        cities.push_back(new City());
-        cities[i]-idx = i;
-        cities[i]->neighbors.resize(n, false);
-      }
-
-
-      for (auto r: roads) {
-
-        cities[r[0]]->neighbors[r[1]] = true;
-        cities[r[1]]->neighbors[r[0]] = true;
-        cities[r[1]]->indegree++;
-        cities[r[0]]->indegree++;
-      }
-
-      vector<City*> cloned = cities;
-      sort(cities.begin(), cities.end(), comp);
-      bool connected = cloned[cities[0]->idx]->neighbors[cities[1]->idx];
-      return cities[0]->indegree + cities[1]->indegree - connected;;
+ public:
+  int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+    // iterate roads
+    // build neighbors
+    vector<City*> cities;
+    for (int i = 0; i < n; ++i) {
+      cities.push_back(new City());
+      cities[i]->idx = i;
+      cities[i]->neighbors.resize(n, false);
     }
+
+    for (auto r : roads) {
+      cities[r[0]]->neighbors[r[1]] = true;
+      cities[r[1]]->neighbors[r[0]] = true;
+      cities[r[1]]->indegree++;
+      cities[r[0]]->indegree++;
+    }
+
+    vector<City*> cloned = cities;
+    sort(cities.begin(), cities.end(), comp);
+    int highIdx = 0;
+    int lowIdx = 1;
+    int highVal = cities[highIdx]->indegree;
+    int lowVal = cities[lowIdx]->indegree;
+  }
 };
 // sort by in degrees
