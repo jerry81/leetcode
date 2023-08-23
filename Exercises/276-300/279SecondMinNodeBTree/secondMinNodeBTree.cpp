44a876dcbc4577d192f4846ee02c6a26b30b4d0c
Raskolnikov <jerry.tann@endress.com>
2023-08-23 15:14:34 +0800

    🐛 kablammmo

--- Exercises/276-300/279SecondMinNodeBTree/secondMinNodeBTree.cpp
+++ Exercises/276-300/279SecondMinNodeBTree/secondMinNodeBTree.cpp
@@ -5,9 +5,14 @@ Easy
 1.7K
 1.8K
 Companies
-Given a non-empty special binary tree consisting of nodes with the non-negative value, where each node in this tree has exactly two or zero sub-node. If the node has two sub-nodes, then this node's value is the smaller value among its two sub-nodes. More formally, the property root.val = min(root.left.val, root.right.val) always holds.
+Given a non-empty special binary tree consisting of nodes with the non-negative
+value, where each node in this tree has exactly two or zero sub-node. If the
+node has two sub-nodes, then this node's value is the smaller value among its
+two sub-nodes. More formally, the property root.val = min(root.left.val,
+root.right.val) always holds.
 
-Given such a binary tree, you need to output the second minimum value in the set made of all the nodes' value in the whole tree.
+Given such a binary tree, you need to output the second minimum value in the set
+made of all the nodes' value in the whole tree.
 
 If no such second minimum value exists, output -1 instead.
 
@@ -33,13 +38,8 @@ Constraints:
 
 The number of nodes in the tree is in the range [1, 25].
 1 <= Node.val <= 231 - 1
-root.val == min(root.left.val, root.right.val) for each internal node of the tree.
-Accepted
-175K
-Submissions
-395.9K
-Acceptance Rate
-44.2%
+root.val == min(root.left.val, root.right.val) for each internal node of the
+tree. Accepted 175K Submissions 395.9K Acceptance Rate 44.2%
 
 */
 
@@ -51,7 +51,8 @@ Acceptance Rate
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 
@@ -59,44 +60,37 @@ Acceptance Rate
 using namespace std;
 
 struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
 
 class Solution {
+  vector<int> values;
 
-vector<int> values;
+  void r(TreeNode *root) {
+    // inorder traversal
 
-void r(TreeNode* root) {
-  // inorder traversal
-  if (root == nullptr) return;
+    if (root == nullptr) return;
 
-  if (root->left == nullptr) {
-    values.push_back(root->val);
-  } else {
     r(root->left);
+    values.push_back(root->val);
+    r(root->right);
   }
 
-  r(root->right);
-}
-
-public:
-    int findSecondMinimumValue(TreeNode* root) {
-       r(root);
-       int lowest = INT_MAX;
-       bool found = false;
-       for (int v: values) {
-         if (v < lowest) {
-           if (found == true) return lowest;
-
-           found = true;
-           lowest = v;
-         }
-       }
-       return -1;
+ public:
+  int findSecondMinimumValue(TreeNode *root) {
+    r(root);
+    bool found = false;
+    sort(values.begin(), values.end());
+    int lowest = values[0];
+    for (int i = 1; i < values.size(); ++i) {
+      if (lowest != values[i]) return values[i];
     }
+    return -1;
+  }
 };
