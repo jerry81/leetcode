80f2ef38077b8cebe8de8cdf1e9f054556c4e092
Raskolnikov <jerry.tann@endress.com>
2023-08-23 10:42:11 +0800

    🚧 step 1

--- Exercises/276-300/278ReorgString/reorgString.cpp
+++ Exercises/276-300/278ReorgString/reorgString.cpp
@@ -35,14 +35,33 @@ Acceptance Rate
 */
 
 #include <string>
+#include <vector>
+#include <map>
 
 using namespace std;
 
+struct Item {
+  int freq;
+  char c;
+  Item(int f, char c):freq(f), c(c) {}
+};
+
+bool compareItem(Item a, Item b) {
+  return a.freq > b.freq;
+}
 class Solution {
 public:
     string reorganizeString(string s) {
       // recursion & backtracking
-
+      map<char,int> freqMap;
+      vector<Item> items;
+      for (char c: s) {
+        if (freqMap.find(c) == freqMap.end()) {
+          freqMap[c] = 1;
+        } else {
+          freqMap[c]++;
+        }
+      }
     }
 };
 
@@ -60,4 +79,12 @@ public:
 this would be brute force
 
 or - find clever tricks with duplicates
+
+official editorial:
+first determine if possible by counting frequency.  if highest freq duplicates over half the size of string u fucked
+
+next - just find a possible arrangement
+- we have freq map
+- sort freq map
+- then place them apart from each other by taking odd/even indexes
 */
