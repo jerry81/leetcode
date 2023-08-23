8b5f2f3912d7552192e510a4b257714ebe8ed1fd
Raskolnikov <jerry.tann@endress.com>
2023-08-23 11:30:20 +0800

    🚧 attempt!

--- Exercises/276-300/278ReorgString/reorgString.cpp
+++ Exercises/276-300/278ReorgString/reorgString.cpp
@@ -5,7 +5,8 @@ Medium
 6.5K
 208
 Companies
-Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+Given a string s, rearrange the characters of s so that any two adjacent
+characters are not the same.
 
 Return any possible rearrangement of s or return "" if not possible.
 
@@ -34,60 +35,65 @@ Acceptance Rate
 
 */
 
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
 
 using namespace std;
 
 struct Item {
   int freq;
   char c;
-  Item(int f, char c):freq(f), c(c) {}
+  Item(int f, char c) : freq(f), c(c) {}
 };
 
-bool compareItem(Item a, Item b) {
-  return a.freq > b.freq;
-}
+bool compareItem(Item a, Item b) { return a.freq > b.freq; }
 class Solution {
-public:
-    string reorganizeString(string s) {
-      // recursion & backtracking
-      map<char,int> freqMap;
-      string ret(s.size(),' ');
-      vector<Item> items;
-      for (char c: s) {
-        if (freqMap.find(c) == freqMap.end()) {
-          freqMap[c] = 1;
-        } else {
-          freqMap[c]++;
-        }
+ public:
+  string reorganizeString(string s) {
+    // recursion & backtracking
+    map<char, int> freqMap;
+    string ret(s.size(), ' ');
+    vector<Item> items;
+    for (char c : s) {
+      if (freqMap.find(c) == freqMap.end()) {
+        freqMap[c] = 1;
+      } else {
+        freqMap[c]++;
       }
-      for (auto [k,v]:freqMap) {
-        items.push_back(Item(v,k));
-      }
-      sort(items.begin(), items.end(), compareItem);
-
-      int lastOdd = 1;
-      int lastEven = 0;
-      bool isEven = true;
-
-      for (Item i: items) {
-        for (int j = 0; j < i.freq; j++) {
-            int lastSeen = 0;
-            if (lastEven < lastOdd) {
-              isEven = true;
-              lastSeen = lastEven;
-            } else {
-              isEven = false;
-              lastSeen = true;
-            }
-
-            ret[lastSeen+(j*2)+(int)(!isEven)] = i.c;
-        }
-      }
-      return ret;
     }
+    for (auto [k, v] : freqMap) {
+      items.push_back(Item(v, k));
+    }
+    sort(items.begin(), items.end(), compareItem);
+
+    int lastOdd = 1;
+    int lastEven = 0;
+    bool isEven = true;
+
+    for (Item i : items) {
+      int lastSeen = 0;
+      int lastIdx = 0;
+      if (lastEven < lastOdd) {
+        isEven = true;
+        lastSeen = lastEven;
+      } else {
+        isEven = false;
+        lastSeen = true;
+      }
+      for (int j = 0; j < i.freq; j++) {
+        lastIdx = lastSeen + (j * 2) + (int)(!isEven);
+        ret[lastIdx] = i.c;
+      }
+      if (isEven) {
+        lastEven = lastIdx;
+      } else {
+        lastOdd = lastIdx;
+      }
+      isEven = !isEven;
+    }
+    return ret;
+  }
 };
 
 /*
@@ -106,7 +112,8 @@ this would be brute force
 or - find clever tricks with duplicates
 
 official editorial:
-first determine if possible by counting frequency.  if highest freq duplicates over half the size of string u fucked
+first determine if possible by counting frequency.  if highest freq duplicates
+over half the size of string u fucked
 
 next - just find a possible arrangement
 - we have freq map
