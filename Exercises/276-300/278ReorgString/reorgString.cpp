da818e3960c7a8b7659c01a0806409163524a3c8
Raskolnikov <jerry.tann@endress.com>
2023-08-23 10:22:04 +0800

    ✨ daily initial

---
+++ Exercises/276-300/278ReorgString/reorgString.cpp
@@ -0,0 +1,46 @@
+/*
+
+767. Reorganize String
+Medium
+6.5K
+208
+Companies
+Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+
+Return any possible rearrangement of s or return "" if not possible.
+
+
+
+Example 1:
+
+Input: s = "aab"
+Output: "aba"
+Example 2:
+
+Input: s = "aaab"
+Output: ""
+
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of lowercase English letters.
+Accepted
+278.2K
+Submissions
+523.4K
+Acceptance Rate
+53.2%
+
+*/
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string reorganizeString(string s) {
+
+    }
+};
