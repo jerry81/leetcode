8a5ee55cd9f78111e18a0c3bb0e0d9c039fdd531
Raskolnikov <jerry.tann@endress.com>
2023-08-25 20:07:52 +0800

    🚧 is memo necessary?

--- Exercises/276-300/282InterleavingString/interleavingStr.cpp
+++ Exercises/276-300/282InterleavingString/interleavingStr.cpp
@@ -5,17 +5,17 @@ Medium
 7.2K
 424
 Companies
-Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
+Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1
+and s2.
 
-An interleaving of two strings s and t is a configuration where s and t are divided into n and m
-substrings
- respectively, such that:
+An interleaving of two strings s and t is a configuration where s and t are
+divided into n and m substrings respectively, such that:
 
 s = s1 + s2 + ... + sn
 t = t1 + t2 + ... + tm
 |n - m| <= 1
-The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
-Note: a + b is the concatenation of strings a and b.
+The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3
++ s3 + ... Note: a + b is the concatenation of strings a and b.
 
 
 
@@ -26,14 +26,14 @@ Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
 Output: true
 Explanation: One way to obtain s3 is:
 Split s1 into s1 = "aa" + "bc" + "c", and s2 into s2 = "dbbc" + "a".
-Interleaving the two splits, we get "aa" + "dbbc" + "bc" + "a" + "c" = "aadbbcbcac".
-Since s3 can be obtained by interleaving s1 and s2, we return true.
-Example 2:
+Interleaving the two splits, we get "aa" + "dbbc" + "bc" + "a" + "c" =
+"aadbbcbcac". Since s3 can be obtained by interleaving s1 and s2, we return
+true. Example 2:
 
 Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
 Output: false
-Explanation: Notice how it is impossible to interleave s2 with any other string to obtain s3.
-Example 3:
+Explanation: Notice how it is impossible to interleave s2 with any other string
+to obtain s3. Example 3:
 
 Input: s1 = "", s2 = "", s3 = ""
 Output: true
@@ -62,8 +62,19 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    bool isInterleave(string s1, string s2, string s3) {
+  bool r(int p1, int p2, int p3, bool is1, string &s1, string &s2, string &s3) {
+  }
 
-    }
+ public:
+  bool isInterleave(string s1, string s2, string s3) {
+    // params - p1, p2, p3, is1
+    // base cases active pointer >= len and p3 >= len - true
+    // active pointer < len but p3 done - false
+    // active pointer > len p3 < len - false
+    // if first letter doesn't match then false
+    // else for each matching letter dfs
+    // s1 match n chars
+    // must try 1 to n chars
+    //
+  }
 };
