7093e6ff2f1b67556615c01303e8568804cfeafc
Raskolnikov <jerry.tann@endress.com>
2023-08-25 20:57:49 +0800

    ✨ valid palindrome, initial

---
+++ Exercises/276-300/283ValidPalindrome/validPalindrome.cpp
@@ -0,0 +1,47 @@
+/*
+
+680. Valid Palindrome II
+Easy
+7.6K
+384
+Companies
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+
+
+Example 1:
+
+Input: s = "aba"
+Output: true
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+Example 3:
+
+Input: s = "abc"
+Output: false
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+Accepted
+621.8K
+Submissions
+1.6M
+Acceptance Rate
+39.5%
+
+*/
+
+#include <string>
+
+class Solution {
+public:
+    bool validPalindrome(string s) {
+
+    }
+};
