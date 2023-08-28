3377de95ce32a09a845bfdd9189bde0ed4060a69
Raskolnikov <jerry.tann@endress.com>
2023-08-28 11:32:29 +0800

    🚧 messy

--- Exercises/276-300/288CountBinarySubstrings/countBinarySubstrings.cpp
+++ Exercises/276-300/288CountBinarySubstrings/countBinarySubstrings.cpp
@@ -5,7 +5,9 @@ Easy
 3.8K
 814
 Companies
-Given a binary string s, return the number of non-empty substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+Given a binary string s, return the number of non-empty substrings that have the
+same number of 0's and 1's, and all the 0's and all the 1's in these substrings
+are grouped consecutively.
 
 Substrings that occur multiple times are counted the number of times they occur.
 
@@ -15,14 +17,16 @@ Example 1:
 
 Input: s = "00110011"
 Output: 6
-Explanation: There are 6 substrings that have equal number of consecutive 1's and 0's: "0011", "01", "1100", "10", "0011", and "01".
-Notice that some of these substrings repeat and are counted the number of times they occur.
-Also, "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
-Example 2:
+Explanation: There are 6 substrings that have equal number of consecutive 1's
+and 0's: "0011", "01", "1100", "10", "0011", and "01". Notice that some of these
+substrings repeat and are counted the number of times they occur. Also,
+"00110011" is not a valid substring because all the 0's (and 1's) are not
+grouped together. Example 2:
 
 Input: s = "10101"
 Output: 4
-Explanation: There are 4 substrings: "10", "01", "10", "01" that have equal number of consecutive 1's and 0's.
+Explanation: There are 4 substrings: "10", "01", "10", "01" that have equal
+number of consecutive 1's and 0's.
 
 
 Constraints:
@@ -39,12 +43,33 @@ Acceptance Rate
 */
 
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    int countBinarySubstrings(string s) {
-
+ public:
+  int countBinarySubstrings(string s) {
+    int res = 0;
+    for (int start = 0; start < s.size()-1; ++start) {
+      int count = 1;
+      int startV = s[start] - '0';
+      int idx = start+1;
+      while (idx < s.size()) {
+        int cur = s[idx] - '0';
+        if (startV == cur) {
+          count++;
+        } else {
+          break;
+        }
+        idx++;
+      }
+      int compV = !startV;
     }
+    return res;
+  }
 };
+
+// prefix sums?
+// nope - another condition - 1s and 0s grouped together
+//
