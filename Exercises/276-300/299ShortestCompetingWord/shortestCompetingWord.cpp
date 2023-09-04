1521bf5f0ea8d649edd88ecd2292ee2e03b73640
Raskolnikov <jerry.tann@endress.com>
2023-09-04 19:36:07 +0800

    🚧 attempt

--- Exercises/276-300/299ShortestCompetingWord/shortestCompetingWord.cpp
+++ Exercises/276-300/299ShortestCompetingWord/shortestCompetingWord.cpp
@@ -5,13 +5,21 @@ Easy
 490
 1K
 Companies
-Given a string licensePlate and an array of strings words, find the shortest completing word in words.
+Given a string licensePlate and an array of strings words, find the shortest
+completing word in words.
 
-A completing word is a word that contains all the letters in licensePlate. Ignore numbers and spaces in licensePlate, and treat letters as case insensitive. If a letter appears more than once in licensePlate, then it must appear in the word the same number of times or more.
+A completing word is a word that contains all the letters in licensePlate.
+Ignore numbers and spaces in licensePlate, and treat letters as case
+insensitive. If a letter appears more than once in licensePlate, then it must
+appear in the word the same number of times or more.
 
-For example, if licensePlate = "aBc 12c", then it contains letters 'a', 'b' (ignoring case), and 'c' twice. Possible completing words are "abccdef", "caaacab", and "cbca".
+For example, if licensePlate = "aBc 12c", then it contains letters 'a', 'b'
+(ignoring case), and 'c' twice. Possible completing words are "abccdef",
+"caaacab", and "cbca".
 
-Return the shortest completing word in words. It is guaranteed an answer exists. If there are multiple shortest completing words, return the first one that occurs in words.
+Return the shortest completing word in words. It is guaranteed an answer exists.
+If there are multiple shortest completing words, return the first one that
+occurs in words.
 
 
 
@@ -19,17 +27,17 @@ Example 1:
 
 Input: licensePlate = "1s3 PSt", words = ["step","steps","stripe","stepple"]
 Output: "steps"
-Explanation: licensePlate contains letters 's', 'p', 's' (ignoring case), and 't'.
-"step" contains 't' and 'p', but only contains 1 's'.
-"steps" contains 't', 'p', and both 's' characters.
-"stripe" is missing an 's'.
-"stepple" is missing an 's'.
-Since "steps" is the only word containing all the letters, that is the answer.
-Example 2:
+Explanation: licensePlate contains letters 's', 'p', 's' (ignoring case), and
+'t'. "step" contains 't' and 'p', but only contains 1 's'. "steps" contains 't',
+'p', and both 's' characters. "stripe" is missing an 's'. "stepple" is missing
+an 's'. Since "steps" is the only word containing all the letters, that is the
+answer. Example 2:
 
 Input: licensePlate = "1s3 456", words = ["looks","pest","stew","show"]
 Output: "pest"
-Explanation: licensePlate only contains the letter 's'. All the words contain 's', but among these "pest", "stew", and "show" are shortest. The answer is "pest" because it is the word that appears earliest of the 3.
+Explanation: licensePlate only contains the letter 's'. All the words contain
+'s', but among these "pest", "stew", and "show" are shortest. The answer is
+"pest" because it is the word that appears earliest of the 3.
 
 
 Constraints:
@@ -48,24 +56,45 @@ Acceptance Rate
 
 */
 
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
 
 using namespace std;
 
 class Solution {
-public:
-    string shortestCompletingWord(string licensePlate, vector<string>& words) {
-      map<string, map<char,int>> lookup;
-      map<char, int> freq;
-      for (string s: words) {
-        for (char c: s) {
-          lookup[s][c]++;
-        }
-      }
-      for (char c: licensePlate) {
-        if (isalpha(c)) freq[c]++;
+  bool isComplete(map<char, int> baseline, map<char, int> comp) {
+    for (auto [k,v]: baseline) {
+      if (comp.find(k) == comp.end()) return false;
+
+      if (comp[k] < v) return false;
+    }
+    return true;
+  }
+
+ public:
+  string shortestCompletingWord(string licensePlate, vector<string>& words) {
+    map<string, map<char, int>> lookup;
+    map<char, int> freq;
+    for (string s : words) {
+      for (char c : s) {
+        lookup[s][c]++;
       }
     }
+    for (char c : licensePlate) {
+      if (isalpha(c)) freq[tolower(c)]++;
+    }
+
+    int minSz = INT_MAX;
+    string res;
+    for (auto [k,mp]: lookup) {
+      if (isComplete(freq, mp)) {
+        if (k.size() < minSz) {
+          minSz = k.size();
+          res = k;
+        }
+      }
+    }
+    return res;
+  }
 };
