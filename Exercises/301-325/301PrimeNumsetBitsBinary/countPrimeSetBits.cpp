1783c2a0cc86e37bcc5c0eaa7edb0d05f67c36aa
Raskolnikov <jerry.tann@endress.com>
2023-09-05 10:50:10 +0800

    ✅ edge cases

--- Exercises/301-325/301PrimeNumsetBitsBinary/countPrimeSetBits.cpp
+++ Exercises/301-325/301PrimeNumsetBitsBinary/countPrimeSetBits.cpp
@@ -59,6 +59,10 @@ using namespace std;
 
 class Solution {
   bool isPrime(int n) {
+    if (n == 0) return false;
+
+    if (n == 1) return false;
+
     for (int i = 2; i <= sqrt(n); ++i) {
       if (n % i == 0) return false;
     }
@@ -76,7 +80,7 @@ class Solution {
  public:
   int countPrimeSetBits(int left, int right) {
     /* def: set bits - number of 1s when written in binary */
-    int ret;
+    int ret = 0;
     for (int i = left; i <= right; ++i) {
       if (isPrime(countSetBits(i))) ++ret;
     }
