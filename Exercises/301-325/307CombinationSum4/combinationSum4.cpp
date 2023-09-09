45e1cf88a6db83087db8737a8806a0eb0425d377
Raskolnikov <jerry.tann@endress.com>
2023-09-09 12:27:42 +0800

    🚧 try bottom up ?

--- Exercises/301-325/307CombinationSum4/combinationSum4.cpp
+++ Exercises/301-325/307CombinationSum4/combinationSum4.cpp
@@ -5,7 +5,8 @@ Medium
 6.2K
 596
 Companies
-Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+Given an array of distinct integers nums and a target integer target, return the
+number of possible combinations that add up to target.
 
 The test cases are generated so that the answer can fit in a 32-bit integer.
 
@@ -39,7 +40,9 @@ All the elements of nums are unique.
 1 <= target <= 1000
 
 
-Follow up: What if negative numbers are allowed in the given array? How does it change the problem? What limitation we need to add to the question to allow negative numbers?
+Follow up: What if negative numbers are allowed in the given array? How does it
+change the problem? What limitation we need to add to the question to allow
+negative numbers?
 
 Accepted
 386.8K
@@ -55,8 +58,37 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    int combinationSum4(vector<int>& nums, int target) {
+  int r(int remaining, vector<int>& dp, vector<int>& sorted) {
+    // recurrence - remaining number?
+    if (dp[remaining] >= 0) return dp[remaining];
+    int count = 0;
+    for (int i: sorted) {
+      if (i > remaining) break;
 
+      count+=r(remaining-i, dp, sorted);
     }
+    dp[remaining] = count;
+    return dp[remaining];
+  }
+
+ public:
+  int combinationSum4(vector<int>& nums, int target) {
+    // analysis
+    // dp + recursion probably
+    // base case - options > than target then return 0
+    /*
+      Input: nums = [9], target = 3
+      Output: 0
+      maybe sorted doesn't matter
+    */
+    // sort to optimize breaking out
+    // sort from low to high
+    vector<int> sorted = nums;
+    vector<int> dp(target + 1, -1);
+    sort(sorted.begin(), sorted.end());
+    for (int i = 1; i <= target; ++i) {
+      if (dp[i] < 0) dp[i] = r(i, dp, sorted);
+    }
+    return dp[target];
+  }
 };
