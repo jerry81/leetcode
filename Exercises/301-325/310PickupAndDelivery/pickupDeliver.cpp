08443c4b6b5bfea0e6034ba687a5b6be952e9a5a
Raskolnikov <jerry.tann@endress.com>
2023-09-10 11:17:23 +0800

    ✨ idears

--- Exercises/301-325/310PickupAndDelivery/pickupDeliver.cpp
+++ Exercises/301-325/310PickupAndDelivery/pickupDeliver.cpp
@@ -6,7 +6,8 @@ Hard
 Companies
 Given n orders, each order consist in pickup and delivery services.
 
-Count all valid pickup/delivery possible sequences such that delivery(i) is always after of pickup(i).
+Count all valid pickup/delivery possible sequences such that delivery(i) is
+always after of pickup(i).
 
 Since the answer may be too large, return it modulo 10^9 + 7.
 
@@ -22,9 +23,9 @@ Example 2:
 Input: n = 2
 Output: 6
 Explanation: All possible orders:
-(P1,P2,D1,D2), (P1,P2,D2,D1), (P1,D1,P2,D2), (P2,P1,D1,D2), (P2,P1,D2,D1) and (P2,D2,P1,D1).
-This is an invalid order (P1,D2,P2,D1) because Pickup 2 is after of Delivery 2.
-Example 3:
+(P1,P2,D1,D2), (P1,P2,D2,D1), (P1,D1,P2,D2), (P2,P1,D1,D2), (P2,P1,D2,D1) and
+(P2,D2,P1,D1). This is an invalid order (P1,D2,P2,D1) because Pickup 2 is after
+of Delivery 2. Example 3:
 
 Input: n = 3
 Output: 90
@@ -40,10 +41,27 @@ Submissions
 Acceptance Rate
 63.1%
 */
+#include <map>
+#include <vector>
 using namespace std;
 class Solution {
-public:
-    int countOrders(int n) {
+  map<string, int> lookup;
 
+ public:
+  int countOrders(int n) {
+    vector<int> ap;
+    for (int i = 0; i < n; ++i) {
+      ap.push_back(i);
     }
+    vector<bool> ad(n);
+  }
 };
+
+/*
+
+pool - P1,p2,p3,d1,d2,d3
+first spot , available p1,p2,p3
+p1-> then d1 becomes option, pool p2p3d1d2d3
+second spot available p2,p3,d1
+
+*/
