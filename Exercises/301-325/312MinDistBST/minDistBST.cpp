553e997331f10880c120db8262b815f793864d02
Raskolnikov <jerry.tann@endress.com>
2023-09-11 10:49:31 +0800

    min Dist BST ✨

---
+++ Exercises/301-325/312MinDistBST/minDistBST.cpp
@@ -0,0 +1,68 @@
+/*
+
+783. Minimum Distance Between BST Nodes
+Easy
+3.3K
+402
+Companies
+Given the root of a Binary Search Tree (BST), return the minimum difference
+between the values of any two different nodes in the tree.
+
+
+
+Example 1:
+
+
+Input: root = [4,2,6,1,3]
+Output: 1
+Example 2:
+
+
+Input: root = [1,0,48,null,null,12,49]
+Output: 1
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 100].
+0 <= Node.val <= 105
+
+
+Note: This question is the same as 530:
+https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+
+Accepted
+228.1K
+Submissions
+383.5K
+Acceptance Rate
+59.5%
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+class Solution {
+ public:
+  int minDiffInBST(TreeNode *root) {}
+};
