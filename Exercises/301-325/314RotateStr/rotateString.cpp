73f58b9728fc920fb5acd8ed0edcce5d0a3ffc11
Raskolnikov <jerry.tann@endress.com>
2023-09-12 12:24:06 +0800

    ✨ rotateStr init

---
+++ Exercises/301-325/314RotateStr/rotateString.cpp
@@ -0,0 +1,48 @@
+/*
+
+796. Rotate String
+Easy
+3.2K
+124
+Companies
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+
+
+Example 1:
+
+Input: s = "abcde", goal = "cdeab"
+Output: true
+Example 2:
+
+Input: s = "abcde", goal = "abced"
+Output: false
+
+
+Constraints:
+
+1 <= s.length, goal.length <= 100
+s and goal consist of lowercase English letters.
+Accepted
+240.1K
+Submissions
+426.5K
+Acceptance Rate
+56.3%
+
+*/
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+
+public:
+    bool rotateString(string s, string goal) {
+
+    }
+};
