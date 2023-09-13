0531fae529352c8e2161f14bb1ac11d1934c6e03
Raskolnikov <jerry.tann@endress.com>
2023-09-13 13:00:06 +0800

    🚧 attempt

--- Exercises/301-325/316UniqueMorse/uniqueMorse.cpp
+++ Exercises/301-325/316UniqueMorse/uniqueMorse.cpp
@@ -5,18 +5,23 @@ Easy
 2.4K
 1.5K
 Companies
-International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+International Morse Code defines a standard encoding where each letter is mapped
+to a series of dots and dashes, as follows:
 
 'a' maps to ".-",
 'b' maps to "-...",
 'c' maps to "-.-.", and so on.
-For convenience, the full table for the 26 letters of the English alphabet is given below:
+For convenience, the full table for the 26 letters of the English alphabet is
+given below:
 
 [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+Given an array of strings words where each word can be written as a
+concatenation of the Morse code of each letter.
 
-For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
-Return the number of different transformations among all words we have.
+For example, "cab" can be written as "-.-..--...", which is the concatenation of
+"-.-.", ".-", and "-...". We will call such a concatenation the transformation
+of a word. Return the number of different transformations among all words we
+have.
 
 
 
@@ -50,14 +55,28 @@ Acceptance Rate
 
 */
 
-#include <vector>
 #include <string>
+#include <vector>
+#include <set>
 
 using namespace std;
 
 class Solution {
-public:
-    int uniqueMorseRepresentations(vector<string>& words) {
-
+ public:
+  int uniqueMorseRepresentations(vector<string>& words) {
+    vector<string> lookup = {
+        ".-",   "-...", "-.-.", "-..",  ".",   "..-.", "--.",  "....", "..",
+        ".---", "-.-",  ".-..", "--",   "-.",  "---",  ".--.", "--.-", ".-.",
+        "...",  "-",    "..-",  "...-", ".--", "-..-", "-.--", "--.."};
+    set<string> resW;
+    for (string w: words) {
+      string morse = "";
+      for (char c: w) {
+        int idx = 'a' - c;
+        morse += lookup[idx];
+      }
+      resW.insert(morse);
     }
+    return resW.size();
+  }
 };
