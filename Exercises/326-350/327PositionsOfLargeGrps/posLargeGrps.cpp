ab48d84072635351952ab7d80bcb6b6d5aad66e0
Raskolnikov <jerry.tann@endress.com>
2023-09-19 10:48:54 +0800

    🚧 gain a nose for edge cases

--- Exercises/326-350/327PositionsOfLargeGrps/posLargeGrps.cpp
+++ Exercises/326-350/327PositionsOfLargeGrps/posLargeGrps.cpp
@@ -5,15 +5,20 @@ Easy
 821
 121
 Companies
-In a string s of lowercase letters, these letters form consecutive groups of the same character.
+In a string s of lowercase letters, these letters form consecutive groups of the
+same character.
 
-For example, a string like s = "abbxxxxzyy" has the groups "a", "bb", "xxxx", "z", and "yy".
+For example, a string like s = "abbxxxxzyy" has the groups "a", "bb", "xxxx",
+"z", and "yy".
 
-A group is identified by an interval [start, end], where start and end denote the start and end indices (inclusive) of the group. In the above example, "xxxx" has the interval [3,6].
+A group is identified by an interval [start, end], where start and end denote
+the start and end indices (inclusive) of the group. In the above example, "xxxx"
+has the interval [3,6].
 
 A group is considered large if it has 3 or more characters.
 
-Return the intervals of every large group sorted in increasing order by start index.
+Return the intervals of every large group sorted in increasing order by start
+index.
 
 
 
@@ -47,25 +52,28 @@ Acceptance Rate
 
 */
 
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    vector<vector<int>> largeGroupPositions(string s) {
-      vector<vector<int>> res;
-      char cur = ' ';
-      int starti = 0;
-      for (int i = 0; i < s.size(); ++i) {
-        char c = s[i];
-        if (c != cur) {
-          cur = c;
-          if ((i-starti) >= 2) res.push_back({starti, i});
-          starti = i;
-        }
+ public:
+  vector<vector<int>> largeGroupPositions(string s) {
+    vector<vector<int>> res;
+    int sz = s.size();
+    char cur = ' ';
+    int starti = 0;
+    for (int i = 0; i < sz; ++i) {
+      char c = s[i];
+      if (c != cur) {
+        cur = c;
+        if ((i - starti) > 2) res.push_back({starti, i - 1});
+        starti = i;
       }
-      return res;
     }
+    if ((sz - starti) > 2) res.push_back({starti, sz - 1});
+
+    return res;
+  }
 };
