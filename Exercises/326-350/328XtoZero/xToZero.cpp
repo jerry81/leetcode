a471a3681fe0544f2029c2b55a4ad8bfafc9db05
Raskolnikov <jerry.tann@endress.com>
2023-09-20 10:27:34 +0800

    🚧

--- Exercises/326-350/328XtoZero/xToZero.cpp
+++ Exercises/326-350/328XtoZero/xToZero.cpp
@@ -5,9 +5,13 @@ Medium
 4.2K
 83
 Companies
-You are given an integer array nums and an integer x. In one operation, you can either remove the leftmost or the rightmost element from the array nums and subtract its value from x. Note that this modifies the array for future operations.
+You are given an integer array nums and an integer x. In one operation, you can
+either remove the leftmost or the rightmost element from the array nums and
+subtract its value from x. Note that this modifies the array for future
+operations.
 
-Return the minimum number of operations to reduce x to exactly 0 if it is possible, otherwise, return -1.
+Return the minimum number of operations to reduce x to exactly 0 if it is
+possible, otherwise, return -1.
 
 
 
@@ -15,8 +19,8 @@ Example 1:
 
 Input: nums = [1,1,4,2,3], x = 5
 Output: 2
-Explanation: The optimal solution is to remove the last two elements to reduce x to zero.
-Example 2:
+Explanation: The optimal solution is to remove the last two elements to reduce x
+to zero. Example 2:
 
 Input: nums = [5,6,7,8,9], x = 4
 Output: -1
@@ -24,7 +28,8 @@ Example 3:
 
 Input: nums = [3,2,20,1,1,3], x = 10
 Output: 5
-Explanation: The optimal solution is to remove the last three elements and the first two elements (5 operations in total) to reduce x to zero.
+Explanation: The optimal solution is to remove the last three elements and the
+first two elements (5 operations in total) to reduce x to zero.
 
 
 Constraints:
@@ -46,8 +51,28 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    int minOperations(vector<int>& nums, int x) {
-
+ public:
+  int minOperations(vector<int>& nums, int x) {
+    // prefix sums forward
+    int n = nums.size();
+    vector<int> pf;
+    int curSum = 0;
+    for (int i = 0; i < n; ++i) {
+      curSum += nums[i];
+      pf.push_back(curSum);
     }
+    curSum = 0;
+    vector<int> rf;
+    for (int i = n - 1; i >= 0; --i) {
+      curSum += nums[i];
+      rf.push_back(curSum);
+    }
+    for (int i: pf) {
+      cout << "pf " << i << endl;
+    }
+
+    for (int i: rf) {
+      cout << "rf " << i << endl;
+    }
+  }
 };
