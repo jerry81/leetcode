4670fad7fa8e4c55a056fd330a00c559d08d2e3f
Raskolnikov <jerry.tann@endress.com>
2023-09-26 12:10:43 +0800

    🔥 start over

---
+++ Exercises/326-350/337RmvDupLetters/removeDuplicateLetters2.cpp
@@ -0,0 +1,60 @@
+/*
+
+316. Remove Duplicate Letters
+Medium
+7.4K
+474
+Companies
+Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is
+the smallest in lexicographical order
+ among all possible results.
+
+
+
+Example 1:
+
+Input: s = "bcabc"
+Output: "abc"
+Example 2:
+
+Input: s = "cbacdcbc"
+Output: "acdb"
+
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of lowercase English letters.
+
+
+Note: This question is the same as 1081: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+
+Accepted
+247.8K
+Submissions
+538.5K
+Acceptance Rate
+46.0%
+
+*/
+
+#include <string>
+#include <unordered_map>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+      unordered_map<char, int> chars;
+      int fullmask = 0;
+      for (int i = 0; i < s.size(); ++i) {
+        char c = s[i];
+        if (chars.find(c) == chars.end()) chars[c] = i;
+      }
+      // bitmask can be used to check if a char has been "used"
+      // knapsack, take or leave - leave if duplicate, also leave voluntarily
+      // further optimization -> start with each character (lowest index of)
+    }
+};
