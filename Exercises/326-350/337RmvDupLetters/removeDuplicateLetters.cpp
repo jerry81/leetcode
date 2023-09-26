9e86dd43f3915c67af245420deebdc382030e871
Raskolnikov <jerry.tann@endress.com>
2023-09-26 11:06:06 +0800

    🚧 oops

--- Exercises/326-350/337RmvDupLetters/removeDuplicateLetters.cpp
+++ Exercises/326-350/337RmvDupLetters/removeDuplicateLetters.cpp
@@ -5,9 +5,9 @@ Medium
 7.3K
 466
 Companies
-Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is
-the smallest in lexicographical order
- among all possible results.
+Given a string s, remove duplicate letters so that every letter appears once and
+only once. You must make sure your result is the smallest in lexicographical
+order among all possible results.
 
 
 
@@ -27,7 +27,8 @@ Constraints:
 s consists of lowercase English letters.
 
 
-Note: This question is the same as 1081: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+Note: This question is the same as 1081:
+https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
 
 Accepted
 245K
@@ -38,11 +39,11 @@ Acceptance Rate
 
 */
 
-#include <string>
-#include <map>
-#include <vector>
 #include <algorithm>
 #include <iterator>
+#include <map>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -51,40 +52,66 @@ struct CWithI {
   int i;
 };
 class Solution {
-map<char, vector<int>> lookups;
+  map<char, vector<int>> lookups;
 
-vector<CWithI> res;
+  vector<CWithI> res;
 
-void r(int lastIdx, vector<CWithI> cur, int &sz, vector<char> keys) {
-
-  if (cur.size() == sz) res = cur;
-
-  if (keys.empty()) return;
-
-  vector<int> v = lookups[c];
-  auto uppr = upper_bound(v.begin(), v.end(), lastIdx);
-  auto lwr = lower_bound(v.begin(), v.end(), lastIdx);
-
-  if (uppr != v.end()) {
-    int nextIdx = distance(v.begin(), uppr);
-    r()
-  }
-
-  if (lwr != v.end()) {
-    int nextIdx = distance(v.begin(), lwr);
-  }
-}
-public:
-    string removeDuplicateLetters(string s) {
-      for (int i = 0; i < s.size(); ++i) {
-        lookups[s[i]].push_back(i);
-      }
-      int sz = lookups.size();
-      vector<char> keys;
-      for (auto [k,_]: lookups) {
-        keys.push_back(k);
-      }
-      r(-1, {}, sz, keys);
-      return "";
+  void r(int lastIdx, vector<CWithI> cur, int &sz, vector<char> keys) {
+    if (cur.size() == sz) {
+      res = cur;
+      cout << "res found " << endl;
     }
+
+    if (keys.empty()) return;
+
+    char c = keys.front();
+
+    vector<char> nextKeys = keys;
+    nextKeys.erase(nextKeys.begin());
+
+    cout << "c is " << c << endl;
+    vector<int> v = lookups[c];
+    auto uppr = upper_bound(v.begin(), v.end(), lastIdx);
+    auto lwr = lower_bound(v.begin(), v.end(), lastIdx);
+
+    if (uppr != v.end()) {
+      int nextIdx = distance(v.begin(), uppr);
+      cout << "nextIdx is " << nextIdx << endl;
+      CWithI ci;
+      ci.c = c;
+      ci.i = nextIdx;
+      vector<CWithI> cpy = cur;
+      cpy.push_back(ci);
+
+      r(nextIdx, cpy, sz, nextKeys);
+    }
+
+    if (lwr != v.end()) {
+      int nextIdx = distance(v.begin(), lwr);
+      CWithI ci;
+      ci.c = c;
+      ci.i = nextIdx;
+      vector<CWithI> cpy = cur;
+      cpy.push_back(ci);
+
+      r(nextIdx, cpy, sz, nextKeys);
+    }
+  }
+
+ public:
+  string removeDuplicateLetters(string s) {
+    for (int i = 0; i < s.size(); ++i) {
+      lookups[s[i]].push_back(i);
+    }
+    int sz = lookups.size();
+    vector<char> keys;
+    for (auto [k, _] : lookups) {
+      keys.push_back(k);
+    }
+    for (char c: keys) {
+      cout << "key is " << c << endl;
+    }
+    r(-1, {}, sz, keys);
+    return "";
+  }
 };
