bf6b90636069e3bc8e558751b5cfe83352d6606d
Raskolnikov <jerry.tann@endress.com>
2023-09-30 12:47:28 +0800

    ✨ daily

--- Exercises/326-350/344FairCandySwap/fairCandySwap.cpp
+++ Exercises/326-350/344FairCandySwap/fairCandySwap.cpp
@@ -5,11 +5,20 @@ Easy
 2K
 346
 Companies
-Alice and Bob have a different total number of candies. You are given two integer arrays aliceSizes and bobSizes where aliceSizes[i] is the number of candies of the ith box of candy that Alice has and bobSizes[j] is the number of candies of the jth box of candy that Bob has.
+Alice and Bob have a different total number of candies. You are given two
+integer arrays aliceSizes and bobSizes where aliceSizes[i] is the number of
+candies of the ith box of candy that Alice has and bobSizes[j] is the number of
+candies of the jth box of candy that Bob has.
 
-Since they are friends, they would like to exchange one candy box each so that after the exchange, they both have the same total amount of candy. The total amount of candy a person has is the sum of the number of candies in each box they have.
+Since they are friends, they would like to exchange one candy box each so that
+after the exchange, they both have the same total amount of candy. The total
+amount of candy a person has is the sum of the number of candies in each box
+they have.
 
-Return an integer array answer where answer[0] is the number of candies in the box that Alice must exchange, and answer[1] is the number of candies in the box that Bob must exchange. If there are multiple answers, you may return any one of them. It is guaranteed that at least one answer exists.
+Return an integer array answer where answer[0] is the number of candies in the
+box that Alice must exchange, and answer[1] is the number of candies in the box
+that Bob must exchange. If there are multiple answers, you may return any one of
+them. It is guaranteed that at least one answer exists.
 
 
 
@@ -47,8 +56,23 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
-
+ public:
+  vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+    int sum1 = 0;
+    int sum2 = 0;
+    for (int i = 0; i < aliceSizes.size(); ++i) {
+      sum1 += aliceSizes[i];
     }
+    for (int j = 0; j < bobSizes.size(); ++j) {
+      sum2 += bobSizes[j];
+    }
+
+    for (int a : aliceSizes) {
+      for (int b : bobSizes) {
+        if ((sum2 - b + a) == (sum1 - a + b)) return {a,b};
+      }
+    }
+
+    return {0,0};
+  }
 };
