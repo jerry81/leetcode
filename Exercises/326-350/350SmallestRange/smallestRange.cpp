d341ac95f40b123b05e69bd31e07f0eed3860773
Raskolnikov <jerry.tann@endress.com>
2023-10-03 18:35:43 +0800

    ✅ small tweaks, pass examples

--- Exercises/326-350/350SmallestRange/smallestRange.cpp
+++ Exercises/326-350/350SmallestRange/smallestRange.cpp
@@ -7,11 +7,15 @@ Easy
 Companies
 You are given an integer array nums and an integer k.
 
-In one operation, you can choose any index i where 0 <= i < nums.length and change nums[i] to nums[i] + x where x is an integer from the range [-k, k]. You can apply this operation at most once for each index i.
+In one operation, you can choose any index i where 0 <= i < nums.length and
+change nums[i] to nums[i] + x where x is an integer from the range [-k, k]. You
+can apply this operation at most once for each index i.
 
-The score of nums is the difference between the maximum and minimum elements in nums.
+The score of nums is the difference between the maximum and minimum elements in
+nums.
 
-Return the minimum score of nums after applying the mentioned operation at most once for each index in it.
+Return the minimum score of nums after applying the mentioned operation at most
+once for each index in it.
 
 
 
@@ -24,12 +28,13 @@ Example 2:
 
 Input: nums = [0,10], k = 2
 Output: 6
-Explanation: Change nums to be [2, 8]. The score is max(nums) - min(nums) = 8 - 2 = 6.
-Example 3:
+Explanation: Change nums to be [2, 8]. The score is max(nums) - min(nums) = 8 -
+2 = 6. Example 3:
 
 Input: nums = [1,3,6], k = 3
 Output: 0
-Explanation: Change nums to be [4, 4, 4]. The score is max(nums) - min(nums) = 4 - 4 = 0.
+Explanation: Change nums to be [4, 4, 4]. The score is max(nums) - min(nums) = 4
+- 4 = 0.
 
 
 Constraints:
@@ -51,11 +56,11 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    int smallestRangeI(vector<int>& nums, int k) {
-      auto mn = min_element(nums.begin(), nums.end());
-      auto mx = max_element(nums.begin(), nums.end());
-      int absDiff = abs(*mn-*mx);
-      return min(absDiff-k, 0);
-    }
+ public:
+  int smallestRangeI(vector<int>& nums, int k) {
+    auto mn = min_element(nums.begin(), nums.end());
+    auto mx = max_element(nums.begin(), nums.end());
+    int absDiff = abs(*mn - *mx);
+    return max(absDiff - (2 * k), 0);
+  }
 };
