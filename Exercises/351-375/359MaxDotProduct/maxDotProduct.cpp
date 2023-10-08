8aab8c50a060a3ed755415ef78539a6cae68b96f
Raskolnikov <jerry.tann@endress.com>
2023-10-08 11:41:04 +0800

    🚧

--- Exercises/351-375/359MaxDotProduct/maxDotProduct.cpp
+++ Exercises/351-375/359MaxDotProduct/maxDotProduct.cpp
@@ -7,9 +7,13 @@ Hard
 Companies
 Given two arrays nums1 and nums2.
 
-Return the maximum dot product between non-empty subsequences of nums1 and nums2 with the same length.
+Return the maximum dot product between non-empty subsequences of nums1 and nums2
+with the same length.
 
-A subsequence of a array is a new array which is formed from the original array by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, [2,3,5] is a subsequence of [1,2,3,4,5] while [1,5,3] is not).
+A subsequence of a array is a new array which is formed from the original array
+by deleting some (can be none) of the characters without disturbing the relative
+positions of the remaining characters. (ie, [2,3,5] is a subsequence of
+[1,2,3,4,5] while [1,5,3] is not).
 
 
 
@@ -17,9 +21,8 @@ Example 1:
 
 Input: nums1 = [2,1,-2,5], nums2 = [3,0,-6]
 Output: 18
-Explanation: Take subsequence [2,-2] from nums1 and subsequence [3,-6] from nums2.
-Their dot product is (2*3 + (-2)*(-6)) = 18.
-Example 2:
+Explanation: Take subsequence [2,-2] from nums1 and subsequence [3,-6] from
+nums2. Their dot product is (2*3 + (-2)*(-6)) = 18. Example 2:
 
 Input: nums1 = [3,-2], nums2 = [2,-6,7]
 Output: 21
@@ -51,8 +54,17 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+  vector<vector<int>> memo;
+  int r(int i1, int i2, vector<int>& nums1, vector<int>& nums2) {}
 
-    }
+ public:
+  int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+    // dp try all subsequences?
+    // greedy?
+    int mx1 = *max_element(nums1.begin(), nums1.end());
+    int mn1 = *min_element(nums1.begin(), nums1.end());
+    int mx2 = *max_element(nums2.begin(), nums2.end());
+    int mn2 = *min_element(nums2.begin(), nums2.end());
+    if (mx1 < 0 && mn2
+  }
 };
