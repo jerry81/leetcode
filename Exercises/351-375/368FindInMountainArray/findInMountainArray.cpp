58fdfd94626cc69d44aee6bc829985c005689239
Raskolnikov <jerry.tann@endress.com>
2023-10-12 10:06:30 +0800

    🚧 foist attempt

--- Exercises/351-375/368FindInMountainArray/findInMountainArray.cpp
+++ Exercises/351-375/368FindInMountainArray/findInMountainArray.cpp
@@ -13,13 +13,17 @@ arr.length >= 3
 There exists some i with 0 < i < arr.length - 1 such that:
 arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
 arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
-Given a mountain array mountainArr, return the minimum index such that mountainArr.get(index) == target. If such an index does not exist, return -1.
+Given a mountain array mountainArr, return the minimum index such that
+mountainArr.get(index) == target. If such an index does not exist, return -1.
 
-You cannot access the mountain array directly. You may only access the array using a MountainArray interface:
+You cannot access the mountain array directly. You may only access the array
+using a MountainArray interface:
 
 MountainArray.get(k) returns the element of the array at index k (0-indexed).
 MountainArray.length() returns the length of the array.
-Submissions making more than 100 calls to MountainArray.get will be judged Wrong Answer. Also, any solutions that attempt to circumvent the judge will result in disqualification.
+Submissions making more than 100 calls to MountainArray.get will be judged Wrong
+Answer. Also, any solutions that attempt to circumvent the judge will result in
+disqualification.
 
 
 
@@ -27,8 +31,8 @@ Example 1:
 
 Input: array = [1,2,3,4,5,3,1], target = 3
 Output: 2
-Explanation: 3 exists in the array, at index=2 and index=5. Return the minimum index, which is 2.
-Example 2:
+Explanation: 3 exists in the array, at index=2 and index=5. Return the minimum
+index, which is 2. Example 2:
 
 Input: array = [0,1,2,4,2,1], target = 3
 Output: -1
@@ -59,18 +63,68 @@ Acceptance Rate
  * };
  */
 
- class MountainArray {
-   public:
-     int get(int index);
-     int length();
- };
+class MountainArray {
+ public:
+  int get(int index);
+  int length();
+};
 
 class Solution {
-public:
-    int findInMountainArray(int target, MountainArray &mountainArr) {
-      // binary search find peak
-      // binary search find target in first half
-      // binary search find target in second half
-      return -1;
+ public:
+  int findInMountainArray(int target, MountainArray &mountainArr) {
+    // binary search find peak
+    // get candidate - check next item compare
+    // greater than - peak to right
+    // less than - peak to left
+    int l = 0;
+    int len = mountainArr.length();
+    int r = len - 1;
+    while (l < r) {
+      int mid = (l + r) / 2;
+      int v = mountainArr.get(mid);
+
+      int vnext = mountainArr.get(mid + 1);
+
+      if (vnext > v) {
+        // still increasing
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
     }
+
+    int l = 0;
+    int peak = r;
+    while (l < r) {
+      int mid = (l + r) / 2;
+      int v = mountainArr.get(mid);
+      if (v == target) return mid;
+
+      if (target > v) {
+        // still increasing
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+
+    if (target == mountainArr.get(l)) return l;
+    int l = peak + 1;
+    int r = len - 1;
+
+    while (l < r) {
+      int mid = (l + r) / 2;
+      int v = mountainArr.get(mid);
+      if (v == target) return mid;
+
+      if (target > v) {
+        // still increasing
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+
+    return -1;
+  }
 };
