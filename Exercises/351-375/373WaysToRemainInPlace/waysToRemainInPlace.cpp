05f108a20d76d476bda17afa4461164e8eaec226
Raskolnikov <jerry.tann@endress.com>
2023-10-15 10:52:16 +0800

    🚧 attempt 1

--- Exercises/351-375/373WaysToRemainInPlace/waysToRemainInPlace.cpp
+++ Exercises/351-375/373WaysToRemainInPlace/waysToRemainInPlace.cpp
@@ -5,9 +5,13 @@ Hard
 772
 42
 Companies
-You have a pointer at index 0 in an array of size arrLen. At each step, you can move 1 position to the left, 1 position to the right in the array, or stay in the same place (The pointer should not be placed outside the array at any time).
+You have a pointer at index 0 in an array of size arrLen. At each step, you can
+move 1 position to the left, 1 position to the right in the array, or stay in
+the same place (The pointer should not be placed outside the array at any time).
 
-Given two integers steps and arrLen, return the number of ways such that your pointer is still at index 0 after exactly steps steps. Since the answer may be too large, return it modulo 109 + 7.
+Given two integers steps and arrLen, return the number of ways such that your
+pointer is still at index 0 after exactly steps steps. Since the answer may be
+too large, return it modulo 109 + 7.
 
 
 
@@ -46,9 +50,40 @@ Acceptance Rate
 
 */
 
-class Solution {
-public:
-    int numWays(int steps, int arrLen) {
+#include <cmath>
+#include <vector>
 
+using namespace std;
+
+class Solution {
+  const int MOD = pow(10, 9) + 7;
+  vector<vector<int>> memo;
+  int r(int steps, int idx, int &l) {
+    if (idx < 0) return 0;
+
+    if (idx >= l) return 0;
+
+    if (steps < 0) return 0;
+
+    if (memo[steps][idx] >= 0) return memo[steps][idx];
+
+    if (idx == 0) {
+      if (steps > 0) return 0;
+
+      return 1;
     }
+
+    memo[steps][idx] =
+        (r(steps - 1, idx, l) % MOD + r(steps - 1, idx - 1, l) % MOD +
+         r(steps - 1, idx + 1, l) % MOD) %
+        MOD;
+    return memo[steps][idx];
+  }
+
+ public:
+  int numWays(int steps, int arrLen) {
+    // recursion
+    memo.resize(steps, vector<int>(arrLen, -1));
+    return r(steps, 0, arrLen);
+  }
 };
