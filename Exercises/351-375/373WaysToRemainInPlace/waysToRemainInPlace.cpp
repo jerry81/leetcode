1be381f6bb7b1e614d7f3e9c46f3f65249a931c9
Raskolnikov <jerry.tann@endress.com>
2023-10-15 09:40:04 +0800

    ✨ dairy

---
+++ Exercises/351-375/373WaysToRemainInPlace/waysToRemainInPlace.cpp
@@ -0,0 +1,54 @@
+/*
+
+1269. Number of Ways to Stay in the Same Place After Some Steps
+Hard
+772
+42
+Companies
+You have a pointer at index 0 in an array of size arrLen. At each step, you can move 1 position to the left, 1 position to the right in the array, or stay in the same place (The pointer should not be placed outside the array at any time).
+
+Given two integers steps and arrLen, return the number of ways such that your pointer is still at index 0 after exactly steps steps. Since the answer may be too large, return it modulo 109 + 7.
+
+
+
+Example 1:
+
+Input: steps = 3, arrLen = 2
+Output: 4
+Explanation: There are 4 differents ways to stay at index 0 after 3 steps.
+Right, Left, Stay
+Stay, Right, Left
+Right, Stay, Left
+Stay, Stay, Stay
+Example 2:
+
+Input: steps = 2, arrLen = 4
+Output: 2
+Explanation: There are 2 differents ways to stay at index 0 after 2 steps
+Right, Left
+Stay, Stay
+Example 3:
+
+Input: steps = 4, arrLen = 2
+Output: 8
+
+
+Constraints:
+
+1 <= steps <= 500
+1 <= arrLen <= 106
+Accepted
+35.9K
+Submissions
+81.6K
+Acceptance Rate
+44.0%
+
+*/
+
+class Solution {
+public:
+    int numWays(int steps, int arrLen) {
+
+    }
+};
