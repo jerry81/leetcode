c1b525d912281c5e923c79c3627bd49b41029718
Raskolnikov <jerry.tann@endress.com>
2023-10-16 10:57:41 +0800

    ✨ ez 3

---
+++ Exercises/376-400/378CommonChars/commonChars.cpp
@@ -0,0 +1,46 @@
+/*
+
+1002. Find Common Characters
+Easy
+3.4K
+269
+Companies
+Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
+
+
+
+Example 1:
+
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
+Example 2:
+
+Input: words = ["cool","lock","cook"]
+Output: ["c","o"]
+
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] consists of lowercase English letters.
+Accepted
+194.1K
+Submissions
+282.8K
+Acceptance Rate
+68.6%
+
+*/
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+
+    }
+};
