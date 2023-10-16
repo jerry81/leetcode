d66fb52d546bab2a52ffe7647a2c5c4775431e07
Raskolnikov <jerry.tann@endress.com>
2023-10-16 10:16:58 +0800

    ✨ dairy

---
+++ Exercises/351-375/375Pascals2/pascals2.cpp
@@ -0,0 +1,52 @@
+/*
+119. Pascal's Triangle II
+Easy
+4.2K
+313
+Companies
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+
+
+Example 1:
+
+Input: rowIndex = 3
+Output: [1,3,3,1]
+Example 2:
+
+Input: rowIndex = 0
+Output: [1]
+Example 3:
+
+Input: rowIndex = 1
+Output: [1,1]
+
+
+Constraints:
+
+0 <= rowIndex <= 33
+
+
+Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?
+
+Accepted
+739.3K
+Submissions
+1.2M
+Acceptance Rate
+61.9%
+*/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+
+    }
+};
