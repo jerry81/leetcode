fd0ba8833920dac014d52cb1928534a57d88372c
Raskolnikov <jerry.tann@endress.com>
2023-10-16 10:38:43 +0800

    🐛

--- Exercises/376-400/376FindTownJudge/findTownJudge.cpp
+++ Exercises/376-400/376FindTownJudge/findTownJudge.cpp
@@ -4,16 +4,20 @@ Easy
 5.9K
 467
 Companies
-In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
+In a town, there are n people labeled from 1 to n. There is a rumor that one of
+these people is secretly the town judge.
 
 If the town judge exists, then:
 
 The town judge trusts nobody.
 Everybody (except for the town judge) trusts the town judge.
 There is exactly one person that satisfies properties 1 and 2.
-You are given an array trust where trust[i] = [ai, bi] representing that the person labeled ai trusts the person labeled bi. If a trust relationship does not exist in trust array, then such a trust relationship does not exist.
+You are given an array trust where trust[i] = [ai, bi] representing that the
+person labeled ai trusts the person labeled bi. If a trust relationship does not
+exist in trust array, then such a trust relationship does not exist.
 
-Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
+Return the label of the town judge if the town judge exists and can be
+identified, or return -1 otherwise.
 
 
 
@@ -49,23 +53,22 @@ Acceptance Rate
 
 #include <vector>
 
-
 using namespace std;
 
 class Solution {
-public:
-    int findJudge(int n, vector<vector<int>>& trust) {
-      // make in/outdegree map
-      // return indegree n outdegree 0
-      vector<int> indegrees(n+1, 0);
-      vector<int> outdegrees(n+1, 0);
-      for (auto a: trust) {
-        outdegrees[a[0]]++;
-        indegrees[a[1]]++;
-      }
-      for (int i = 1; i <= n; ++i) {
-        if (outdegrees[i] == 0 && indegrees[i] == n) return i;
-      }
-      return 0;
+ public:
+  int findJudge(int n, vector<vector<int>>& trust) {
+    // make in/outdegree map
+    // return indegree n outdegree 0
+    vector<int> indegrees(n + 1, 0);
+    vector<int> outdegrees(n + 1, 0);
+    for (auto a : trust) {
+      outdegrees[a[0]]++;
+      indegrees[a[1]]++;
     }
+    for (int i = 1; i <= n; ++i) {
+      if (outdegrees[i] == 0 && indegrees[i] == n - 1) return i;
+    }
+    return -1;
+  }
 };
