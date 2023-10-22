42299c07f52100978f8f0ea05aab5fb656accdd8
Raskolnikov <jerry.tann@endress.com>
2023-10-22 13:50:33 +0800

    🚧 attempt 1

--- Exercises/376-400/388RmOuterParens/removeOuterParens.cpp
+++ Exercises/376-400/388RmOuterParens/removeOuterParens.cpp
@@ -5,14 +5,19 @@ Easy
 2.6K
 1.5K
 Companies
-A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A
+and B are valid parentheses strings, and + represents string concatenation.
 
-For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
-A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+For example, "", "()", "(())()", and "(()(()))" are all valid parentheses
+strings. A valid parentheses string s is primitive if it is nonempty, and there
+does not exist a way to split it into s = A + B, with A and B nonempty valid
+parentheses strings.
 
-Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+Given a valid parentheses string s, consider its primitive decomposition: s = P1
++ P2 + ... + Pk, where Pi are primitive valid parentheses strings.
 
-Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+Return s after removing the outermost parentheses of every primitive string in
+the primitive decomposition of s.
 
 
 
@@ -21,16 +26,16 @@ Example 1:
 Input: s = "(()())(())"
 Output: "()()()"
 Explanation:
-The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
-After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
-Example 2:
+The input string is "(()())(())", with primitive decomposition "(()())" +
+"(())". After removing outer parentheses of each part, this is "()()" + "()" =
+"()()()". Example 2:
 
 Input: s = "(()())(())(()(()))"
 Output: "()()()()(())"
 Explanation:
-The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
-After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
-Example 3:
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())"
++ "(())" + "(()(()))". After removing outer parentheses of each part, this is
+"()()" + "()" + "()(())" = "()()()()(())". Example 3:
 
 Input: s = "()()"
 Output: ""
@@ -58,8 +63,19 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    string removeOuterParentheses(string s) {
-
+ public:
+  string removeOuterParentheses(string s) {
+    int cnt = 0;
+    string res = "";
+    for (char c : s) {
+      if (c == '(') {
+        if (cnt > 0) res+= "(";
+        cnt++;
+      } else {
+        if (cnt > 0) res+= ")";
+        cnt--;
+      }
     }
+    return res;
+  }
 };
