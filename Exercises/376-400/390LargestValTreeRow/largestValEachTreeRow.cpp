4635cd5fb8164a80dfb3e1f639d429ca6611bc66
Raskolnikov <jerry.tann@endress.com>
2023-10-24 10:03:20 +0800

    ✨ daily

---
+++ Exercises/376-400/390LargestValTreeRow/largestValEachTreeRow.cpp
@@ -0,0 +1,66 @@
+/*
+
+515. Find Largest Value in Each Tree Row
+Medium
+2.9K
+101
+Companies
+Given the root of a binary tree, return an array of the largest value in each
+row of the tree (0-indexed).
+
+
+
+Example 1:
+
+
+Input: root = [1,3,2,5,3,null,9]
+Output: [1,3,9]
+Example 2:
+
+Input: root = [1,2,3]
+Output: [1,3]
+
+
+Constraints:
+
+The number of nodes in the tree will be in the range [0, 104].
+-231 <= Node.val <= 231 - 1
+Accepted
+247.5K
+Submissions
+383K
+Acceptance Rate
+64.6%
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+class Solution {
+ public:
+  vector<int> largestValues(TreeNode *root) {}
+};
