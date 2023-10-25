83966a51eaeb5da303cf788dde8fdb37ab696694
Raskolnikov <jerry.tann@endress.com>
2023-10-25 12:57:28 +0800

    🚧

--- Exercises/376-400/393MatrixCellsDistanceOrder/matrixCellsDistanceOrder.rs
+++ Exercises/376-400/393MatrixCellsDistanceOrder/matrixCellsDistanceOrder.rs
@@ -48,6 +48,7 @@ Acceptance Rate
 
 impl Solution {
   pub fn all_cells_dist_order(rows: i32, cols: i32, r_center: i32, c_center: i32) -> Vec<Vec<i32>> {
-
+    let mut ret : Vec<vec<i32>>;
+    return ret;
   }
 }
