326e72517de046dda8d65b7025d168295e26a104
Raskolnikov <jerry.tann@endress.com>
2023-10-26 11:39:23 +0800

    🚧 attempt 1

--- Exercises/376-400/394BTreeWFactors/BTreeWithFactors.cpp
+++ Exercises/376-400/394BTreeWFactors/BTreeWithFactors.cpp
@@ -5,11 +5,15 @@ Medium
 2.6K
 192
 Companies
-Given an array of unique integers, arr, where each integer arr[i] is strictly greater than 1.
+Given an array of unique integers, arr, where each integer arr[i] is strictly
+greater than 1.
 
-We make a binary tree using these integers, and each number may be used for any number of times. Each non-leaf node's value should be equal to the product of the values of its children.
+We make a binary tree using these integers, and each number may be used for any
+number of times. Each non-leaf node's value should be equal to the product of
+the values of its children.
 
-Return the number of binary trees we can make. The answer may be too large so return the answer modulo 109 + 7.
+Return the number of binary trees we can make. The answer may be too large so
+return the answer modulo 109 + 7.
 
 
 
@@ -22,7 +26,8 @@ Example 2:
 
 Input: arr = [2,4,5,10]
 Output: 7
-Explanation: We can make these trees: [2], [4], [5], [10], [4, 2, 2], [10, 2, 5], [10, 5, 2].
+Explanation: We can make these trees: [2], [4], [5], [10], [4, 2, 2], [10, 2,
+5], [10, 5, 2].
 
 
 Constraints:
@@ -39,13 +44,36 @@ Acceptance Rate
 
 */
 
+#include <cmath>
 #include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    int numFactoredBinaryTrees(vector<int>& arr) {
+  const int MOD = pow(10, 9) + 7;
 
+ public:
+  int numFactoredBinaryTrees(vector<int>& arr) {
+    vector<int> lookup = vector<int>(1001, 0);
+
+    long long res = 0;
+    for (int cur : arr) {
+      // get factors up to sqrt(cur);
+      lookup[cur] = 1;  // add the inevitable one
+      int upper = sqrt(cur);
+      for (int i = 2; i < upper; ++i) {
+        if (cur % i != 0) continue;
+
+        int f = cur / i;
+        int addend = lookup[f] * lookup[i];
+        if (f != i) {
+          addend *= 2;
+        }
+        lookup[cur] += addend;
+        res += lookup[cur];
+        res %= MOD;
+      }
     }
+    return res;
+  }
 };
