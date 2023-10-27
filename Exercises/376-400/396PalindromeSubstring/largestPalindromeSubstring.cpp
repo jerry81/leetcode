127bf0f337ab105a3e2bce4085d53a8dd80c6554
Raskolnikov <jerry.tann@endress.com>
2023-10-27 09:53:38 +0800

    🚧

--- Exercises/376-400/396PalindromeSubstring/largestPalindromeSubstring.cpp
+++ Exercises/376-400/396PalindromeSubstring/largestPalindromeSubstring.cpp
@@ -42,8 +42,30 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
+pair<int,int> check(int ptr1, int ptr2, string &s) {
+  cout << "checking " << ptr1 << "," << ptr2 << endl;
+  return {0,0};
+}
 public:
     string longestPalindrome(string s) {
+      int sz = s.size();
+      if (sz == 1) return s;
 
+      string res = "";
+      int mxsz = 0;
+      int ptr1 = 0;
+      for (int ptr1 = 0; ptr1 < sz-1; ++ptr1) {
+        for (int i: {1,2}) {
+          int ptr2 = ptr1 + i;
+          if (s[ptr1] != s[ptr2]) continue;
+          pair<int,int> pr = check(ptr1, ptr2, &s);
+        }
+      }
+      return res;
     }
 };
+
+/*
+some kind of 2 ptr
+check adjacent and off by 1 cases
+*/
