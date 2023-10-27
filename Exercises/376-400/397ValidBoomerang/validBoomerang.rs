bc677e692a3c3192603f0af1babb3f245f2cfa30
Raskolnikov <jerry.tann@endress.com>
2023-10-27 10:23:31 +0800

    ✨ ez?

---
+++ Exercises/376-400/397ValidBoomerang/validBoomerang.rs
@@ -0,0 +1,42 @@
+/*
+
+1037. Valid Boomerang
+Easy
+381
+507
+Companies
+Given an array points where points[i] = [xi, yi] represents a point on the X-Y plane, return true if these points are a boomerang.
+
+A boomerang is a set of three points that are all distinct and not in a straight line.
+
+
+
+Example 1:
+
+Input: points = [[1,1],[2,3],[3,2]]
+Output: true
+Example 2:
+
+Input: points = [[1,1],[2,2],[3,3]]
+Output: false
+
+
+Constraints:
+
+points.length == 3
+points[i].length == 2
+0 <= xi, yi <= 100
+Accepted
+46.2K
+Submissions
+125.5K
+Acceptance Rate
+36.8%
+
+*/
+
+impl Solution {
+  pub fn is_boomerang(points: Vec<Vec<i32>>) -> bool {
+
+  }
+}
