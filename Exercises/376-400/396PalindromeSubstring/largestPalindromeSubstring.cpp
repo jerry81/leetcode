e43b76d4c5b9339eb45ee487037b0870d47ab763
Raskolnikov <jerry.tann@endress.com>
2023-10-27 09:46:53 +0800

    ✨

---
+++ Exercises/376-400/396PalindromeSubstring/largestPalindromeSubstring.cpp
@@ -0,0 +1,49 @@
+/*
+
+5. Longest Palindromic Substring
+Medium
+27.5K
+1.6K
+Companies
+Given a string s, return the longest
+palindromic
+
+substring
+ in s.
+
+
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+
+
+Constraints:
+
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+Accepted
+2.7M
+Submissions
+8.1M
+Acceptance Rate
+32.9%
+
+*/
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+
+    }
+};
