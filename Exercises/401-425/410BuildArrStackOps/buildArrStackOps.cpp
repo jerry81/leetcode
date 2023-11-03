c130339d90dea14682723eedfe64afd4b798fbd5
Raskolnikov <jerry.tann@endress.com>
2023-11-03 20:33:51 +0800

    ✅ examples

--- Exercises/401-425/410BuildArrStackOps/buildArrStackOps.cpp
+++ Exercises/401-425/410BuildArrStackOps/buildArrStackOps.cpp
@@ -13,12 +13,16 @@ You have an empty stack with the two following operations:
 "Pop": removes the integer on the top of the stack.
 You also have a stream of the integers in the range [1, n].
 
-Use the two stack operations to make the numbers in the stack (from the bottom to the top) equal to target. You should follow the following rules:
+Use the two stack operations to make the numbers in the stack (from the bottom
+to the top) equal to target. You should follow the following rules:
 
-If the stream of the integers is not empty, pick the next integer from the stream and push it to the top of the stack.
-If the stack is not empty, pop the integer at the top of the stack.
-If, at any moment, the elements in the stack (from the bottom to the top) are equal to target, do not read new integers from the stream and do not do more operations on the stack.
-Return the stack operations needed to build target following the mentioned rules. If there are multiple valid answers, return any of them.
+If the stream of the integers is not empty, pick the next integer from the
+stream and push it to the top of the stack. If the stack is not empty, pop the
+integer at the top of the stack. If, at any moment, the elements in the stack
+(from the bottom to the top) are equal to target, do not read new integers from
+the stream and do not do more operations on the stack. Return the stack
+operations needed to build target following the mentioned rules. If there are
+multiple valid answers, return any of them.
 
 
 
@@ -26,28 +30,26 @@ Example 1:
 
 Input: target = [1,3], n = 3
 Output: ["Push","Push","Pop","Push"]
-Explanation: Initially the stack s is empty. The last element is the top of the stack.
-Read 1 from the stream and push it to the stack. s = [1].
-Read 2 from the stream and push it to the stack. s = [1,2].
-Pop the integer on the top of the stack. s = [1].
-Read 3 from the stream and push it to the stack. s = [1,3].
+Explanation: Initially the stack s is empty. The last element is the top of the
+stack. Read 1 from the stream and push it to the stack. s = [1]. Read 2 from the
+stream and push it to the stack. s = [1,2]. Pop the integer on the top of the
+stack. s = [1]. Read 3 from the stream and push it to the stack. s = [1,3].
 Example 2:
 
 Input: target = [1,2,3], n = 3
 Output: ["Push","Push","Push"]
-Explanation: Initially the stack s is empty. The last element is the top of the stack.
-Read 1 from the stream and push it to the stack. s = [1].
-Read 2 from the stream and push it to the stack. s = [1,2].
-Read 3 from the stream and push it to the stack. s = [1,2,3].
-Example 3:
+Explanation: Initially the stack s is empty. The last element is the top of the
+stack. Read 1 from the stream and push it to the stack. s = [1]. Read 2 from the
+stream and push it to the stack. s = [1,2]. Read 3 from the stream and push it
+to the stack. s = [1,2,3]. Example 3:
 
 Input: target = [1,2], n = 4
 Output: ["Push","Push"]
-Explanation: Initially the stack s is empty. The last element is the top of the stack.
-Read 1 from the stream and push it to the stack. s = [1].
-Read 2 from the stream and push it to the stack. s = [1,2].
-Since the stack (from the bottom to the top) is equal to target, we stop the stack operations.
-The answers that read integer 3 from the stream are not accepted.
+Explanation: Initially the stack s is empty. The last element is the top of the
+stack. Read 1 from the stream and push it to the stack. s = [1]. Read 2 from the
+stream and push it to the stack. s = [1,2]. Since the stack (from the bottom to
+the top) is equal to target, we stop the stack operations. The answers that read
+integer 3 from the stream are not accepted.
 
 
 Constraints:
@@ -65,14 +67,26 @@ Acceptance Rate
 
 */
 
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    vector<string> buildArray(vector<int>& target, int n) {
-
+ public:
+  vector<string> buildArray(vector<int>& target, int n) {
+    int cur = 1;
+    vector<string> res;
+    for (int i : target) {
+      int diff = i - cur;
+      cur = i + 1;
+      while (diff > 0) {
+        res.push_back("Push");
+        res.push_back("Pop");
+        --diff;
+      }
+      res.push_back("Push");
     }
+    return res;
+  }
 };
