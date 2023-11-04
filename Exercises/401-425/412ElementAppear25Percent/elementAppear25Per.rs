c92aa31249c0d8558250792b64f17e916d02a2e5
Raskolnikov <jerry.tann@endress.com>
2023-11-04 13:37:28 +0800

    ✨ ez

---
+++ Exercises/401-425/412ElementAppear25Percent/elementAppear25Per.rs
@@ -0,0 +1,39 @@
+/*
+
+1287. Element Appearing More Than 25% In Sorted Array
+Easy
+908
+45
+Companies
+Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time, return that integer.
+
+
+
+Example 1:
+
+Input: arr = [1,2,2,6,6,6,6,7,10]
+Output: 6
+Example 2:
+
+Input: arr = [1,1]
+Output: 1
+
+
+Constraints:
+
+1 <= arr.length <= 104
+0 <= arr[i] <= 105
+Accepted
+86K
+Submissions
+145.6K
+Acceptance Rate
+59.1%
+
+*/
+
+impl Solution {
+  pub fn find_special_integer(arr: Vec<i32>) -> i32 {
+
+  }
+}
