a8894eddb00ba2fe3960768803c02f2908033b81
Raskolnikov <jerry.tann@endress.com>
2023-11-05 12:18:26 +0800

    🚧 attempt

--- Exercises/401-425/414EquivalentDominoPairs/equivDominoPairs.rs
+++ Exercises/401-425/414EquivalentDominoPairs/equivDominoPairs.rs
@@ -37,13 +37,22 @@ Acceptance Rate
 
 impl Solution {
   pub fn num_equiv_domino_pairs(dominoes: Vec<Vec<i32>>) -> i32 {
-    int sz = dominoes.len();
+    let sz = dominoes.len();
+    let mut res = 0;
     for i in 0..sz-1 {
       for j in i+1..sz {
-        let cura = dominoes[i];
-        let curb = dominoes[j];
-        if cura[0] == curb[0] && cura[1] == curb[1]
+        let cura = &dominoes[i];
+        let curb = &dominoes[j];
+        if cura[0] == curb[0] && cura[1] == curb[1] {
+          res+=1;
+          continue;
+        }
+
+        if (cura[0] == curb[1] && cura[1] == curb[0]) {
+          res+=1;
+        }
       }
     }
+    res
   }
 }
