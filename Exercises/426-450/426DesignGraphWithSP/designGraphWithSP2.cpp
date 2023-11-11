ee3e4422e9679feac935e11fd04e0cd74935454a
Raskolnikov <jerry.tann@endress.com>
2023-11-11 20:00:03 +0800

    🚧 compile, distances wrong

--- Exercises/426-450/426DesignGraphWithSP/designGraphWithSP2.cpp
+++ Exercises/426-450/426DesignGraphWithSP/designGraphWithSP2.cpp
@@ -16,28 +16,36 @@ class Graph {
 
  public:
   Graph(int n, vector<vector<int>>& edges) {
+    _n = n;
     _edges.resize(n);
+    cout << "resized " << endl;
     for (auto a : edges) {
       _edges[a[0]].push_back({a[1], a[2]});
+      cout << "from " << a[0] << " to " << a[1] << "with weight " << a[2] << endl;
     }
+
   }
 
   void addEdge(vector<int> edge) {
     _edges[edge[0]].push_back({edge[1], edge[2]});
+    cout << "added " << edge[0] << " to " << edge[1] << "with weight " << edge[2] << endl;
   }
 
   int shortestPath(int node1, int node2) {
+    cout << "sp start " << endl;
     vector<int> dists(_n, INT_MAX);
     priority_queue<vector<int>, vector<vector<int>>, ComparePQ> q;
     unordered_set<string> visited;
 
     dists[node1] = 0;
+    cout << "dist 0 set " << endl;
     auto startingEdges = _edges[node1];
     for (auto v: startingEdges) {
-      q.push(v);
+      q.push({node1,v[0],v[1]});
     }
     while (!q.empty()) {
       auto cur = q.top();
+      cout << "cur popped and is " << cur[0] << ","<<cur[1]<<","<<cur[2]<<endl;
       q.pop();
       int src = cur[0];
       int accumWeight = dists[src];
@@ -50,10 +58,10 @@ class Graph {
       for (auto newEdge: neighs) {
         if (visited.find(hashEdge(newEdge[0], newEdge[1])) != visited.end()) continue;
 
-        newEdge[2] += accumWeight;
-        q.push(newEdge);
+        q.push({dest,newEdge[0],newEdge[1]+accumWeight});
       }
     }
+    cout << "sp end " << endl;
     return dists[node2] < INT_MAX ? dists[node2] : -1;
   }
 };
