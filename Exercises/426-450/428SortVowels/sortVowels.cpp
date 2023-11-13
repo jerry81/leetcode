ee1849c5f5f4d492808093ffac3798da2c66bc9b
Raskolnikov <jerry.tann@endress.com>
2023-11-13 13:28:34 +0800

    🚧 attempt 1

--- Exercises/426-450/428SortVowels/sortVowels.cpp
+++ Exercises/426-450/428SortVowels/sortVowels.cpp
@@ -7,11 +7,15 @@ Medium
 Companies
 Given a 0-indexed string s, permute s to get a new string t such that:
 
-All consonants remain in their original places. More formally, if there is an index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
-The vowels must be sorted in the nondecreasing order of their ASCII values. More formally, for pairs of indices i, j with 0 <= i < j < s.length such that s[i] and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
+All consonants remain in their original places. More formally, if there is an
+index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
+The vowels must be sorted in the nondecreasing order of their ASCII values. More
+formally, for pairs of indices i, j with 0 <= i < j < s.length such that s[i]
+and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
 Return the resulting string.
 
-The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase. Consonants comprise all letters that are not vowels.
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or
+uppercase. Consonants comprise all letters that are not vowels.
 
 
 
@@ -19,12 +23,14 @@ Example 1:
 
 Input: s = "lEetcOde"
 Output: "lEOtcede"
-Explanation: 'E', 'O', and 'e' are the vowels in s; 'l', 't', 'c', and 'd' are all consonants. The vowels are sorted according to their ASCII values, and the consonants remain in the same places.
-Example 2:
+Explanation: 'E', 'O', and 'e' are the vowels in s; 'l', 't', 'c', and 'd' are
+all consonants. The vowels are sorted according to their ASCII values, and the
+consonants remain in the same places. Example 2:
 
 Input: s = "lYmpH"
 Output: "lYmpH"
-Explanation: There are no vowels in s (all characters in s are consonants), so we return "lYmpH".
+Explanation: There are no vowels in s (all characters in s are consonants), so
+we return "lYmpH".
 
 
 Constraints:
@@ -46,13 +52,23 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    string sortVowels(string s) {
-      vector<char> vowels;
-      vector<char> vowelsref = {'a','e','i','o','u'};
-      for (char c: s) {
-        if (find(vowelsref.begin(), vowelsref.end(), tolower(c) != vowelsref.end()) {
-        }
+ public:
+  string sortVowels(string s) {
+    vector<char> vowels;
+    vector<char> vowelsref = {'a', 'e', 'i', 'o', 'u'};
+    for (char c : s) {
+      if (find(vowelsref.begin(), vowelsref.end(), tolower(c)) !=
+          vowelsref.end())
+        vowels.push_back(c);
+    }
+    sort(vowels.begin(), vowels.end(), greater());
+    for (int i = 0; i < s.size(); ++i) {
+      if (find(vowelsref.begin(), vowelsref.end(), tolower(s[i])) !=
+          vowelsref.end()) {
+        s[i] = vowels.back();
+        vowels.pop_back();
       }
     }
+    return s;
+  }
 };
