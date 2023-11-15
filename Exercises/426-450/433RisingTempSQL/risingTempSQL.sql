44f1f102d112cf122bb3e1b17a5818b44f85eff6
Raskolnikov <jerry.tann@endress.com>
2023-11-15 10:56:46 +0800

    🐛 not the way to use LAG

---
+++ Exercises/426-450/433RisingTempSQL/risingTempSQL.sql
@@ -0,0 +1,63 @@
+/*
+197. Rising Temperature
+Easy
+2.4K
+554
+Companies
+SQL Schema
+Pandas Schema
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+This table contains information about the temperature on a certain day.
+
+
+Write a solution to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output:
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation:
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+Accepted
+466K
+Submissions
+1M
+Acceptance Rate
+46.0%
+*/
+select id from weather where LAG(temperature) OVER ( order by id ) < temperature;
+/*
+1.  googled "postgres get previous row"
+2.  found postgresql LAG function https://www.postgresqltutorial.com/postgresql-window-function/postgresql-lag-function/
+*/
