beeb7b16c71dffe686b88c5f987c0c373146cada
Raskolnikov <jerry.tann@endress.com>
2023-11-15 10:34:25 +0800

    🚧 naive fails, contining on

--- Exercises/426-450/432MaxElementAfterDecrease/maxElementAfterDecrease.cpp
+++ Exercises/426-450/432MaxElementAfterDecrease/maxElementAfterDecrease.cpp
@@ -5,15 +5,19 @@ Medium
 372
 85
 Companies
-You are given an array of positive integers arr. Perform some operations (possibly none) on arr so that it satisfies these conditions:
+You are given an array of positive integers arr. Perform some operations
+(possibly none) on arr so that it satisfies these conditions:
 
 The value of the first element in arr must be 1.
-The absolute difference between any 2 adjacent elements must be less than or equal to 1. In other words, abs(arr[i] - arr[i - 1]) <= 1 for each i where 1 <= i < arr.length (0-indexed). abs(x) is the absolute value of x.
-There are 2 types of operations that you can perform any number of times:
+The absolute difference between any 2 adjacent elements must be less than or
+equal to 1. In other words, abs(arr[i] - arr[i - 1]) <= 1 for each i where 1 <=
+i < arr.length (0-indexed). abs(x) is the absolute value of x. There are 2 types
+of operations that you can perform any number of times:
 
 Decrease the value of any element of arr to a smaller positive integer.
 Rearrange the elements of arr to be in any order.
-Return the maximum possible value of an element in arr after performing the operations to satisfy the conditions.
+Return the maximum possible value of an element in arr after performing the
+operations to satisfy the conditions.
 
 
 
@@ -39,7 +43,8 @@ Example 3:
 
 Input: arr = [1,2,3,4,5]
 Output: 5
-Explanation: The array already satisfies the conditions, and the largest element is 5.
+Explanation: The array already satisfies the conditions, and the largest element
+is 5.
 
 
 Constraints:
@@ -60,8 +65,11 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-
-    }
+ public:
+  int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+    // dp and recursion?
+    sort(arr.begin(), arr.end());
+    return min((int)arr.size(), *max_element(arr.begin(), arr.end()));
+    // oops forgot some cases
+  }
 };
