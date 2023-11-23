10e4ad40656be81e5bc68eed32b70bfeed04f5ea
Raskolnikov <jerry.tann@endress.com>
2023-11-23 10:13:19 +0900

    🚧 attempt

--- Exercises/426-450/445ArithmeticSubarray/arithmeticSubarray.cpp
+++ Exercises/426-450/445ArithmeticSubarray/arithmeticSubarray.cpp
@@ -5,7 +5,10 @@ Medium
 1.2K
 123
 Companies
-A sequence of numbers is called arithmetic if it consists of at least two elements, and the difference between every two consecutive elements is the same. More formally, a sequence s is arithmetic if and only if s[i+1] - s[i] == s[1] - s[0] for all valid i.
+A sequence of numbers is called arithmetic if it consists of at least two
+elements, and the difference between every two consecutive elements is the same.
+More formally, a sequence s is arithmetic if and only if s[i+1] - s[i] == s[1] -
+s[0] for all valid i.
 
 For example, these are arithmetic sequences:
 
@@ -15,9 +18,13 @@ For example, these are arithmetic sequences:
 The following sequence is not arithmetic:
 
 1, 1, 2, 5, 7
-You are given an array of n integers, nums, and two arrays of m integers each, l and r, representing the m range queries, where the ith query is the range [l[i], r[i]]. All the arrays are 0-indexed.
+You are given an array of n integers, nums, and two arrays of m integers each, l
+and r, representing the m range queries, where the ith query is the range [l[i],
+r[i]]. All the arrays are 0-indexed.
 
-Return a list of boolean elements answer, where answer[i] is true if the subarray nums[l[i]], nums[l[i]+1], ... , nums[r[i]] can be rearranged to form an arithmetic sequence, and false otherwise.
+Return a list of boolean elements answer, where answer[i] is true if the
+subarray nums[l[i]], nums[l[i]+1], ... , nums[r[i]] can be rearranged to form an
+arithmetic sequence, and false otherwise.
 
 
 
@@ -26,13 +33,14 @@ Example 1:
 Input: nums = [4,6,5,9,3,7], l = [0,0,2], r = [2,3,5]
 Output: [true,false,true]
 Explanation:
-In the 0th query, the subarray is [4,6,5]. This can be rearranged as [6,5,4], which is an arithmetic sequence.
-In the 1st query, the subarray is [4,6,5,9]. This cannot be rearranged as an arithmetic sequence.
-In the 2nd query, the subarray is [5,9,3,7]. This can be rearranged as [3,5,7,9], which is an arithmetic sequence.
-Example 2:
+In the 0th query, the subarray is [4,6,5]. This can be rearranged as [6,5,4],
+which is an arithmetic sequence. In the 1st query, the subarray is [4,6,5,9].
+This cannot be rearranged as an arithmetic sequence. In the 2nd query, the
+subarray is [5,9,3,7]. This can be rearranged as [3,5,7,9], which is an
+arithmetic sequence. Example 2:
 
-Input: nums = [-12,-9,-3,-12,-6,15,20,-25,-20,-15,-10], l = [0,1,6,4,8,7], r = [4,4,9,7,9,10]
-Output: [false,true,false,false,true,true]
+Input: nums = [-12,-9,-3,-12,-6,15,20,-25,-20,-15,-10], l = [0,1,6,4,8,7], r =
+[4,4,9,7,9,10] Output: [false,true,false,false,true,true]
 
 
 Constraints:
@@ -57,8 +65,29 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
-      vector<bool> res;
+ public:
+  vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l,
+                                        vector<int>& r) {
+    vector<bool> res;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < l.size(); ++i) {
+      int cl = l[i];
+      int cr = r[i];
+      if (cl == cr) {
+        res.push_back(true);
+        continue;
+      }
+      int diff = res[cl + 1] - res[cl];
+      int checkPass = true;
+      for (int j = cl + 1; j < cr; ++j) {
+        if (res[j + 1] - res[j] != diff) {
+          res.push_back(false);
+          checkPass = false;
+          break;
+        }
+      }
+      if (checkPass) res.push_back(true);
     }
+    return res;
+  }
 };
