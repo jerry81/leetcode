25d2080f680ffb19a58038f9889e402f947a9030
Raskolnikov <jerry.tann@endress.com>
2023-11-26 10:09:44 +0800

    🚧

--- Exercises/426-450/448LargestSubmatrix/largestSubmatrix.cpp
+++ Exercises/426-450/448LargestSubmatrix/largestSubmatrix.cpp
@@ -5,9 +5,11 @@ Medium
 1K
 32
 Companies
-You are given a binary matrix matrix of size m x n, and you are allowed to rearrange the columns of the matrix in any order.
+You are given a binary matrix matrix of size m x n, and you are allowed to
+rearrange the columns of the matrix in any order.
 
-Return the area of the largest submatrix within matrix where every element of the submatrix is 1 after reordering the columns optimally.
+Return the area of the largest submatrix within matrix where every element of
+the submatrix is 1 after reordering the columns optimally.
 
 
 
@@ -29,7 +31,8 @@ Example 3:
 
 Input: matrix = [[1,1,0],[1,0,1]]
 Output: 2
-Explanation: Notice that you must rearrange entire columns, and there is no way to make a submatrix of 1s larger than an area of 2.
+Explanation: Notice that you must rearrange entire columns, and there is no way
+to make a submatrix of 1s larger than an area of 2.
 
 
 Constraints:
@@ -52,8 +55,13 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    int largestSubmatrix(vector<vector<int>>& matrix) {
-
+ public:
+  int largestSubmatrix(vector<vector<int>>& matrix) {
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+    for (int i = 0; i < rows; ++i) {
+      for (int j = 0; j < cols; ++j) {
+      }
     }
+  }
 };
