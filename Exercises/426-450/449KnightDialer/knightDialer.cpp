8536d572d3bb1a3edaec9c060441f9d8b27e727f
Raskolnikov <jerry.tann@endress.com>
2023-11-27 10:59:18 +0800

    🚧 come back to this later

--- Exercises/426-450/449KnightDialer/knightDialer.cpp
+++ Exercises/426-450/449KnightDialer/knightDialer.cpp
@@ -68,28 +68,15 @@ class Solution {
       {4, 8},                      // 3
       {3, 9, 0},                   // 4
       {},                          // 5
-      {1, 7},                      // 6
+      {1, 7, 0},                      // 6
       {2, 6},                      // 7
       {1, 3},                      // 8
       {4, 2}};
 
-  vector<int> dp = vector<int>(5001, );
+  vector<int> dp = vector<int>(5001, 0);
 
   const int MOD = pow(10, 9) + 7;
 
-  long long int r(int cur, int rem) {
-    if (rem == 0) return 1;
-
-    long long int ret = 0;
-    vector<int> options = movemap[cur];
-    for (int o:options) {
-      if (o == 5) {}
-
-      ret+= r(o, rem-1);
-      ret%=MOD;
-    }
-    return ret;
-  }
 
  public:
   int knightDialer(int n) {
@@ -97,10 +84,8 @@ class Solution {
     // start on any square
     // handle 10 squares (base case)
     long long int res = 0;
-    for (int i = 0; i < 10; ++i) {
-      res+=r(i,n-1);
-      res%=MOD;
-    }
+    if (n==1) return 10;
+
     return res;
   }
 };
