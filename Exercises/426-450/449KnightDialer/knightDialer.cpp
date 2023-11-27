edaaa8d1c8058896f1139aac09a11ceb8d6a3a6c
Raskolnikov <jerry.tann@endress.com>
2023-11-27 10:33:29 +0800

    🚧 setuping

--- Exercises/426-450/449KnightDialer/knightDialer.cpp
+++ Exercises/426-450/449KnightDialer/knightDialer.cpp
@@ -4,17 +4,24 @@ Medium
 2.2K
 396
 Companies
-The chess knight has a unique movement, it may move two squares vertically and one square horizontally, or two squares horizontally and one square vertically (with both forming the shape of an L). The possible movements of chess knight are shown in this diagaram:
+The chess knight has a unique movement, it may move two squares vertically and
+one square horizontally, or two squares horizontally and one square vertically
+(with both forming the shape of an L). The possible movements of chess knight
+are shown in this diagaram:
 
 A chess knight can move as indicated in the chess diagram below:
 
 
-We have a chess knight and a phone pad as shown below, the knight can only stand on a numeric cell (i.e. blue cell).
+We have a chess knight and a phone pad as shown below, the knight can only stand
+on a numeric cell (i.e. blue cell).
 
 
-Given an integer n, return how many distinct phone numbers of length n we can dial.
+Given an integer n, return how many distinct phone numbers of length n we can
+dial.
 
-You are allowed to place the knight on any numeric cell initially and then you should perform n - 1 jumps to dial a number of length n. All jumps should be valid knight jumps.
+You are allowed to place the knight on any numeric cell initially and then you
+should perform n - 1 jumps to dial a number of length n. All jumps should be
+valid knight jumps.
 
 As the answer may be very large, return the answer modulo 109 + 7.
 
@@ -24,13 +31,13 @@ Example 1:
 
 Input: n = 1
 Output: 10
-Explanation: We need to dial a number of length 1, so placing the knight over any numeric cell of the 10 cells is sufficient.
-Example 2:
+Explanation: We need to dial a number of length 1, so placing the knight over
+any numeric cell of the 10 cells is sufficient. Example 2:
 
 Input: n = 2
 Output: 20
-Explanation: All the valid number we can dial are [04, 06, 16, 18, 27, 29, 34, 38, 40, 43, 49, 60, 61, 67, 72, 76, 81, 83, 92, 94]
-Example 3:
+Explanation: All the valid number we can dial are [04, 06, 16, 18, 27, 29, 34,
+38, 40, 43, 49, 60, 61, 67, 72, 76, 81, 83, 92, 94] Example 3:
 
 Input: n = 3131
 Output: 136006598
@@ -48,9 +55,28 @@ Acceptance Rate
 52.1%
 */
 
-class Solution {
-public:
-    int knightDialer(int n) {
+#include <vector>
 
-    }
+using namespace std;
+
+class Solution {
+  vector<vector<int>> movemap = {  // idx 0 is number of moves
+      {6, 4},                      // 0
+      {8, 6},                      // 1
+      {7, 9},                      // 2
+      {4, 8},                      // 3
+      {3, 9, 0},                   // 4
+      {},                          // 5
+      {1, 7},                      // 6
+      {2, 6},                      // 7
+      {1, 3},                      // 8
+      {4, 2}};
+
+ public:
+  int knightDialer(int n) {
+    // dp/recursion problem i thinks
+    // start on any square
+    // handle 10 squares (base case)
+    // 0
+  }
 };
