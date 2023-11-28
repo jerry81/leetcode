d8469bec054c0ed08e85862e0b2c974a0068907a
Raskolnikov <jerry.tann@endress.com>
2023-11-28 10:39:44 +0800

    🚧 dp + recursion

--- Exercises/451-475/451DivideLongCorridor/divideLongCorridor.cpp
+++ Exercises/451-475/451DivideLongCorridor/divideLongCorridor.cpp
@@ -5,13 +5,22 @@ Hard
 413
 44
 Companies
-Along a long library corridor, there is a line of seats and decorative plants. You are given a 0-indexed string corridor of length n consisting of letters 'S' and 'P' where each 'S' represents a seat and each 'P' represents a plant.
+Along a long library corridor, there is a line of seats and decorative plants.
+You are given a 0-indexed string corridor of length n consisting of letters 'S'
+and 'P' where each 'S' represents a seat and each 'P' represents a plant.
 
-One room divider has already been installed to the left of index 0, and another to the right of index n - 1. Additional room dividers can be installed. For each position between indices i - 1 and i (1 <= i <= n - 1), at most one divider can be installed.
+One room divider has already been installed to the left of index 0, and another
+to the right of index n - 1. Additional room dividers can be installed. For each
+position between indices i - 1 and i (1 <= i <= n - 1), at most one divider can
+be installed.
 
-Divide the corridor into non-overlapping sections, where each section has exactly two seats with any number of plants. There may be multiple ways to perform the division. Two ways are different if there is a position with a room divider installed in the first way but not in the second way.
+Divide the corridor into non-overlapping sections, where each section has
+exactly two seats with any number of plants. There may be multiple ways to
+perform the division. Two ways are different if there is a position with a room
+divider installed in the first way but not in the second way.
 
-Return the number of ways to divide the corridor. Since the answer may be very large, return it modulo 109 + 7. If there is no way, return 0.
+Return the number of ways to divide the corridor. Since the answer may be very
+large, return it modulo 109 + 7. If there is no way, return 0.
 
 
 
@@ -21,21 +30,22 @@ Example 1:
 Input: corridor = "SSPPSPS"
 Output: 3
 Explanation: There are 3 different ways to divide the corridor.
-The black bars in the above image indicate the two room dividers already installed.
-Note that in each of the ways, each section has exactly two seats.
+The black bars in the above image indicate the two room dividers already
+installed. Note that in each of the ways, each section has exactly two seats.
 Example 2:
 
 
 Input: corridor = "PPSPSP"
 Output: 1
-Explanation: There is only 1 way to divide the corridor, by not installing any additional dividers.
-Installing any would create some section that does not have exactly two seats.
-Example 3:
+Explanation: There is only 1 way to divide the corridor, by not installing any
+additional dividers. Installing any would create some section that does not have
+exactly two seats. Example 3:
 
 
 Input: corridor = "S"
 Output: 0
-Explanation: There is no way to divide the corridor because there will always be a section that does not have exactly two seats.
+Explanation: There is no way to divide the corridor because there will always be
+a section that does not have exactly two seats.
 
 
 Constraints:
@@ -52,13 +62,44 @@ Acceptance Rate
 
 */
 
+#include <cmath>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
-public:
-    int numberOfWays(string corridor) {
+  unordered_map<string, long long int> memo;
 
+  string hsh(int idx, int cnt) { return to_string(idx) + "," + to_string(cnt); }
+
+  const int MOD = pow(10, 9) + 7;
+
+  long long int r(int idx, int cnt, string &c) {
+    if (idx < 0) return 0;
+
+    if (cnt > 0) return 0;
+
+    char cur = c[idx];
+    if (cur == 'S') cnt++;
+
+    if (idx == 0) return (cnt == 2) ? 1 : 0;
+    string as_h = hsh(idx, cnt);
+    if (memo.find(as_h) != memo.end()) return memo[as_h];
+
+    if (cnt == 2) {
+      // can place divider
+      memo[as_h] = r(idx - 1, 0, c) % MOD + r(idx - 1, cnt, c) % MOD;
+    } else {
+      // cannot place divider
+      memo[as_h] = r(idx - 1, cnt, c) % MOD;
     }
+    return memo[as_h] % MOD;
+  }
+
+ public:
+  int numberOfWays(string corridor) {
+    int idx = corridor.size() - 1;
+    return r(idx, 0, corridor);
+  }
 };
