94a994cf3195bde85851c098f89bfc2c595de135
Raskolnikov <jerry.tann@endress.com>
2023-11-30 14:25:02 +0800

    🚧 a simplification

--- Exercises/451-475/455BitOpsMakeZero/bitOpsMakeZero.cpp
+++ Exercises/451-475/455BitOpsMakeZero/bitOpsMakeZero.cpp
@@ -5,11 +5,13 @@ Hard
 477
 433
 Companies
-Given an integer n, you must transform it into 0 using the following operations any number of times:
+Given an integer n, you must transform it into 0 using the following operations
+any number of times:
 
 Change the rightmost (0th) bit in the binary representation of n.
-Change the ith bit in the binary representation of n if the (i-1)th bit is set to 1 and the (i-2)th through 0th bits are set to 0.
-Return the minimum number of operations to transform n into 0.
+Change the ith bit in the binary representation of n if the (i-1)th bit is set
+to 1 and the (i-2)th through 0th bits are set to 0. Return the minimum number of
+operations to transform n into 0.
 
 
 
@@ -25,10 +27,9 @@ Example 2:
 Input: n = 6
 Output: 4
 Explanation: The binary representation of 6 is "110".
-"110" -> "010" with the 2nd operation since the 1st bit is 1 and 0th through 0th bits are 0.
-"010" -> "011" with the 1st operation.
-"011" -> "001" with the 2nd operation since the 0th bit is 1.
-"001" -> "000" with the 1st operation.
+"110" -> "010" with the 2nd operation since the 1st bit is 1 and 0th through 0th
+bits are 0. "010" -> "011" with the 1st operation. "011" -> "001" with the 2nd
+operation since the 0th bit is 1. "001" -> "000" with the 1st operation.
 
 
 Constraints:
@@ -43,9 +44,17 @@ Acceptance Rate
 
 */
 
-class Solution {
-public:
-    int minimumOneBitOperations(int n) {
+#include <cmath>
 
-    }
+using namespace std;
+
+class Solution {
+  bool isPowOf2(int n) { return n > 0 && fmod(log2(n), 1) == 0; }
+
+ public:
+  int minimumOneBitOperations(int n) {
+    if (isPowOf2(n)) return n*2-1;
+
+    return 0;
+  }
 };
