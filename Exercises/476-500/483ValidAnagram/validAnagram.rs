b6ce0524871f05dc90ec8f9ce87603154425f64d
Raskolnikov <jerry.tann@endress.com>
2023-12-16 09:23:00 +0800

    ✨

---
+++ Exercises/476-500/483ValidAnagram/validAnagram.rs
@@ -0,0 +1,45 @@
+/*
+
+242. Valid Anagram
+Easy
+11.1K
+353
+Companies
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+Accepted
+2.9M
+Submissions
+4.5M
+Acceptance Rate
+63.5%
+
+*/
+
+impl Solution {
+  pub fn is_anagram(s: String, t: String) -> bool {
+
+  }
+}
