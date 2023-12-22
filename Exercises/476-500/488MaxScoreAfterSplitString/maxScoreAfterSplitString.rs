7ae3e55f9320bd38a7dd3be7b4db5766ceab064f
Raskolnikov <jerry.tann@endress.com>
2023-12-22 15:34:51 +0800

    🚧

--- Exercises/476-500/488MaxScoreAfterSplitString/maxScoreAfterSplitString.rs
+++ Exercises/476-500/488MaxScoreAfterSplitString/maxScoreAfterSplitString.rs
@@ -1,5 +1,18 @@
 impl Solution {
   pub fn max_score(s: String) -> i32 {
-
+    // prefix sums
+    let mut zeroes_ps = Vec<i32>::new();
+    let mut ones_ps = Vec<i32>::new();
+    let mut zc = 0;
+    let mut oc = 0;
+    for c in s.chars() {
+      if c == '0' {
+        zc+=1;
+      } else {
+        oc+=1;
+      }
+      zeroes_ps.push(zc);
+      ones_ps.push(oc);
+    }
   }
 }
