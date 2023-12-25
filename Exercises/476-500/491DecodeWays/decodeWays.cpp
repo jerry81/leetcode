eb6cc5a1b89115c8e6692cb0e08bc83cea2ad356
Raskolnikov <jerry.tann@endress.com>
2023-12-25 15:15:00 +0800

    🚧

--- Exercises/476-500/491DecodeWays/decodeWays.cpp
+++ Exercises/476-500/491DecodeWays/decodeWays.cpp
@@ -5,17 +5,21 @@ Medium
 11K
 4.4K
 Companies
-A message containing letters from A-Z can be encoded into numbers using the following mapping:
+A message containing letters from A-Z can be encoded into numbers using the
+following mapping:
 
 'A' -> "1"
 'B' -> "2"
 ...
 'Z' -> "26"
-To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+To decode an encoded message, all the digits must be grouped then mapped back
+into letters using the reverse of the mapping above (there may be multiple
+ways). For example, "11106" can be mapped into:
 
 "AAJF" with the grouping (1 1 10 6)
 "KJF" with the grouping (11 10 6)
-Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into
+'F' since "6" is different from "06".
 
 Given a string s containing only digits, return the number of ways to decode it.
 
@@ -32,12 +36,13 @@ Example 2:
 
 Input: s = "226"
 Output: 3
-Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
-Example 3:
+Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2
+6). Example 3:
 
 Input: s = "06"
 Output: 0
-Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
+Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is
+different from "06").
 
 
 Constraints:
@@ -58,8 +63,26 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    int numDecodings(string s) {
+  int r(int idx, int carry, string &s, int &len) {
+    if (idx >= len) return 1;
+    char cur = s[idx];
+    int as_i = cur - '0';
+    if (as_i == 0 && carry != 1 && carry != 2) return 0;
 
+    if (as_i == 1 || as_i == 2) {
+      // use it immediately
+      int used = r(idx+1, -1, s,len);
+      int unused = r(idx+1, as_i, s,len);
+      return used + unused;
     }
+
+    if (as_i > 6 && carry == 2) return 0;
+
+    return r(idx+1, -1, s, len);
+
+
+  }
+
+ public:
+  int numDecodings(string s) {}
 };
