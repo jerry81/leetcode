a32800746035e5a17482e80815f1224f8bacf28f
Raskolnikov <jerry.tann@endress.com>
2023-12-27 08:41:35 +0800

    🚧

--- Exercises/476-500/493MakeRopeColorful/makeRopecolorful_simplified.cpp
+++ Exercises/476-500/493MakeRopeColorful/makeRopecolorful_simplified.cpp
@@ -5,17 +5,27 @@
 using namespace std;
 
 class Solution {
-public:
-    int minCost(string colors, vector<int>& neededTime) {
-      // get blocks of a single color
-      // pick the min out of those blocks.
-      char prev_color = ' ';
-      int cur_time = INT_MAX;
-      int res = 0;
-      for (int i = 0; i < colors.size(); ++i) {
-        char cur_color = colors[i];
+ public:
+  int minCost(string colors, vector<int>& neededTime) {
+    // get blocks of a single color
+    // pick the min out of those blocks.
+    char prev_color = ' ';
+    int cur_time_max = 0;
+    int cur_time_accum = 0;
+    int res = 0;
+    for (int i = 0; i < colors.size(); ++i) {
+      char cur_color = colors[i];
+      int cur_time = neededTime[i];
+      if (cur_color == prev_color) {
+        cur_time_max = max(cur_time_max, cur_time);
+        cur_time_accum += cur_time;
+      } else {
+        res += (cur_time_accum - cur_time_max);
+        cur_time_accum = cur_time;
+        cur_time_max = cur_time;
       }
-
-      return res;
     }
+
+    return res;
+  }
 };
