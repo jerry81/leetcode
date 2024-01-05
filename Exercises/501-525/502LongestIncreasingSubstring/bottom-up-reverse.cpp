a88e1f47a7bf844d3bc1ecb11e82d0c7206eaeb3
Raskolnikov <jerry.tann@endress.com>
2024-01-05 16:34:07 +0800

    📝

--- Exercises/501-525/502LongestIncreasingSubstring/bottom-up-reverse.cpp
+++ Exercises/501-525/502LongestIncreasingSubstring/bottom-up-reverse.cpp
@@ -7,9 +7,9 @@ public:
     int lengthOfLIS(vector<int>& nums) {
       int sz = nums.size();
       vector<int> dp(sz,1);
-      for (int i = 1; i < sz; ++i) {
-        for (int j = 0; j < i; ++j) {
-          if (nums[i] > nums[j]) {
+      for (int i = sz-2; i >= 0; --i) {
+        for (int j = sz-1; j > i; --j) {
+          if (nums[i] < nums[j]) {
             dp[i] = max(dp[i], 1+dp[j]);
           }
         }
