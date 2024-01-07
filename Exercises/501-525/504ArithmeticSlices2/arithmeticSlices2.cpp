465115d1559b7578cc0e99b5e616df38b408cb4b
Raskolnikov <jerry.tann@endress.com>
2024-01-07 17:58:18 +0800

    🚧

--- Exercises/501-525/504ArithmeticSlices2/arithmeticSlices2.cpp
+++ Exercises/501-525/504ArithmeticSlices2/arithmeticSlices2.cpp
@@ -5,13 +5,16 @@ Hard
 2.5K
 121
 Companies
-Given an integer array nums, return the number of all the arithmetic subsequences of nums.
+Given an integer array nums, return the number of all the arithmetic
+subsequences of nums.
 
-A sequence of numbers is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+A sequence of numbers is called arithmetic if it consists of at least three
+elements and if the difference between any two consecutive elements is the same.
 
-For example, [1, 3, 5, 7, 9], [7, 7, 7, 7], and [3, -1, -5, -9] are arithmetic sequences.
-For example, [1, 1, 2, 5, 7] is not an arithmetic sequence.
-A subsequence of an array is a sequence that can be formed by removing some elements (possibly none) of the array.
+For example, [1, 3, 5, 7, 9], [7, 7, 7, 7], and [3, -1, -5, -9] are arithmetic
+sequences. For example, [1, 1, 2, 5, 7] is not an arithmetic sequence. A
+subsequence of an array is a sequence that can be formed by removing some
+elements (possibly none) of the array.
 
 For example, [2,5,10] is a subsequence of [1,2,1,2,4,1,5,10].
 The test cases are generated so that the answer fits in 32-bit integer.
@@ -50,13 +53,29 @@ Acceptance Rate
 
 */
 
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    int numberOfArithmeticSlices(vector<int>& nums) {
+ public:
+  int numberOfArithmeticSlices(vector<int>& nums) {
+    int sz = nums.size();
+    vector<vector<unordered_map<int, int>>> memo(
+        sz, vector<unordered_map<int, int>>(sz, unordered_map<int, int>()));
+    for (int i = sz-1; i >= 1; --i) {
+      int anchor = nums[i];
+      for (int j = i-1; j >= 0; --j) {
+        int comp = nums[j];
+        int diff = anchor - comp;
 
+        if (memo[i][j].find(diff) == memo[i][j].end()) {
+          memo[i][j][diff] = 0;
+        } else {
+          memo[i][j][diff]
+        }
+      }
     }
+  }
 };
