47cebc8f77304960fbf3ff92052dc599b1613d7d
Raskolnikov <jerry.tann@endress.com>
2024-01-08 09:32:17 +0800

    ✅ woot

--- Exercises/501-525/504ArithmeticSlices2/arithmeticSlices2.cpp
+++ Exercises/501-525/504ArithmeticSlices2/arithmeticSlices2.cpp
@@ -52,7 +52,6 @@ Acceptance Rate
 47.9%
 
 */
-
 #include <unordered_map>
 #include <vector>
 
@@ -62,22 +61,20 @@ class Solution {
  public:
   int numberOfArithmeticSlices(vector<int>& nums) {
     int sz = nums.size();
-    vector<unordered_map<int, int>> memo(
-        sz, unordered_map<int, int>());
+    vector<unordered_map<long long, int>> memo(sz, unordered_map<long long int, int>());
     int res = 0;
-    for (int i = sz-1; i >= 1; --i) {
+    for (int i = sz - 1; i >= 0; --i) {
       int anchor = nums[i];
-      for (int j = i-1; j >= 0; --j) {
+      for (int j = i - 1; j >= 0; --j) {
         int comp = nums[j];
-        int diff = anchor - comp;
+        long long diff = (long long)anchor - comp;
 
         if (memo[i].find(diff) == memo[i].end()) {
-          memo[j][diff] = 1;
+          memo[j][diff] += 1;
         } else {
-            int prev = memo[i][diff];
-            memo[j][diff] = 1 + memo[i][diff];
-            res+=prev;
-          }
+          int prev = memo[i][diff];
+          memo[j][diff] += 1 + memo[i][diff];
+          res += prev;
         }
       }
     }
