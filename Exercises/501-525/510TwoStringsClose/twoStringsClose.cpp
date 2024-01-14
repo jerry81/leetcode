2be3189fe893c48a7c62b0145fab2ab75273509a
Raskolnikov <jerry.tann@endress.com>
2024-01-14 12:01:34 +0800

    🚧

--- Exercises/501-525/510TwoStringsClose/twoStringsClose.cpp
+++ Exercises/501-525/510TwoStringsClose/twoStringsClose.cpp
@@ -69,8 +69,13 @@ class Solution {
   bool closeStrings(string word1, string word2) {
     unordered_map<char, int> freq1;
     unordered_map<char, int> freq2;
-    for (char c: word1) {
+    if (word1.size() != word2.size()) return false;
+
+    for (char c : word1) {
       freq1[c]++;
+    }
+    for (char c : word2) {
+      freq2[c]++;
     }
   }
 };
