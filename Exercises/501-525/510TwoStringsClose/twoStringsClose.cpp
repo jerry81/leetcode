43b1ba51269d5310221eb42f62310b615fdacb7a
Raskolnikov <jerry.tann@endress.com>
2024-01-14 12:00:17 +0800

    🚧

--- Exercises/501-525/510TwoStringsClose/twoStringsClose.cpp
+++ Exercises/501-525/510TwoStringsClose/twoStringsClose.cpp
@@ -5,15 +5,18 @@ Medium
 Topics
 Companies
 Hint
-Two strings are considered close if you can attain one from the other using the following operations:
+Two strings are considered close if you can attain one from the other using the
+following operations:
 
 Operation 1: Swap any two existing characters.
 For example, abcde -> aecdb
-Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
-For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
-You can use the operations on either string as many times as necessary.
+Operation 2: Transform every occurrence of one existing character into another
+existing character, and do the same with the other character. For example,
+aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's) You can use
+the operations on either string as many times as necessary.
 
-Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
+Given two strings, word1 and word2, return true if word1 and word2 are close,
+and false otherwise.
 
 
 
@@ -28,8 +31,8 @@ Example 2:
 
 Input: word1 = "a", word2 = "aa"
 Output: false
-Explanation: It is impossible to attain word2 from word1, or vice versa, in any number of operations.
-Example 3:
+Explanation: It is impossible to attain word2 from word1, or vice versa, in any
+number of operations. Example 3:
 
 Input: word1 = "cabbba", word2 = "abbccc"
 Output: true
@@ -57,12 +60,17 @@ Acceptance Rate
 */
 
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
-public:
-    bool closeStrings(string word1, string word2) {
-
+ public:
+  bool closeStrings(string word1, string word2) {
+    unordered_map<char, int> freq1;
+    unordered_map<char, int> freq2;
+    for (char c: word1) {
+      freq1[c]++;
     }
+  }
 };
