ee95089f2ccfca48d1c3e63e31692e599d23d4b2
Raskolnikov <jerry.tann@endress.com>
2024-01-16 10:32:09 +0800

    🚧 attempt 1

--- Exercises/501-525/516SplitBalancedStr/splitBalancedStr.rs
+++ Exercises/501-525/516SplitBalancedStr/splitBalancedStr.rs
@@ -52,6 +52,21 @@ Acceptance Rate
 
 impl Solution {
   pub fn balanced_string_split(s: String) -> i32 {
-
+    let mut l = 0;
+    let mut r = 0;
+    let mut res = 0;
+    for c in s.chars() {
+      if c == 'L' {
+        l+=1;
+      } else {
+        r+=1;
+      }
+      if l==r {
+        res+=1;
+        l = 0;
+        r = 0;
+      }
+      res
+    }
   }
 }
