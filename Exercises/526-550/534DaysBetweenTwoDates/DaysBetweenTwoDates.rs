7d6d54a7d06c89a0ce3868b1b1604eabb86dad6a
Raskolnikov <jerry.tann@endress.com>
2024-01-23 14:11:28 +0800

    ✨

---
+++ Exercises/526-550/534DaysBetweenTwoDates/DaysBetweenTwoDates.rs
@@ -0,0 +1,44 @@
+/*
+
+1360. Number of Days Between Two Dates
+Easy
+Topics
+Companies
+Hint
+Write a program to count the number of days between two dates.
+
+The two dates are given as strings, their format is YYYY-MM-DD as shown in the examples.
+
+
+
+Example 1:
+
+Input: date1 = "2019-06-29", date2 = "2019-06-30"
+Output: 1
+Example 2:
+
+Input: date1 = "2020-01-15", date2 = "2019-12-31"
+Output: 15
+
+
+Constraints:
+
+The given dates are valid dates between the years 1971 and 2100.
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+47.4K
+Submissions
+96.2K
+Acceptance Rate
+49.3%
+
+*/
+
+impl Solution {
+  pub fn days_between_dates(date1: String, date2: String) -> i32 {
+
+  }
+}
