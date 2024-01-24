294c1ca99273360b583fe27171ba86cd7e6f4e20
Raskolnikov <jerry.tann@endress.com>
2024-01-24 11:53:20 +0800

    🚧 attempt 1

--- Exercises/526-550/536NumsSmallerThanCur/numsSmaller.rs
+++ Exercises/526-550/536NumsSmallerThanCur/numsSmaller.rs
@@ -48,8 +48,20 @@ Acceptance Rate
 
 */
 
+use std::collections::HashMap;
+
 impl Solution {
   pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
-
+      let mut sorted = &nums;
+      sorted.sort();
+      let mut hm: HashMap<i32,i32> = HashMap::new();
+      for idx,v in enumerate(sorted) {
+        hm.entry(v).or_insert(idx);
+      }
+      let mut res: Vec<i32> = Vec::new();
+      for i in nums {
+        res.push(hm[&i]);
+      }
+      res
   }
 }
