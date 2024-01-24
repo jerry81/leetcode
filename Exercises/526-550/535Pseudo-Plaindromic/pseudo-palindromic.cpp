7d0ca893048a6f0a0a139fa8c4efa583167ba019
Raskolnikov <jerry.tann@endress.com>
2024-01-24 10:45:04 +0800

    ✨

--- Exercises/526-550/535Pseudo-Plaindromic/pseudo-palindromic.cpp
+++ Exercises/526-550/535Pseudo-Plaindromic/pseudo-palindromic.cpp
@@ -5,9 +5,12 @@ Medium
 Topics
 Companies
 Hint
-Given a binary tree where node values are digits from 1 to 9. A path in the binary tree is said to be pseudo-palindromic if at least one permutation of the node values in the path is a palindrome.
+Given a binary tree where node values are digits from 1 to 9. A path in the
+binary tree is said to be pseudo-palindromic if at least one permutation of the
+node values in the path is a palindrome.
 
-Return the number of pseudo-palindromic paths going from the root node to leaf nodes.
+Return the number of pseudo-palindromic paths going from the root node to leaf
+nodes.
 
 
 
@@ -17,14 +20,21 @@ Example 1:
 
 Input: root = [2,3,1,3,1,null,1]
 Output: 2
-Explanation: The figure above represents the given binary tree. There are three paths going from the root node to leaf nodes: the red path [2,3,3], the green path [2,1,1], and the path [2,3,1]. Among these paths only red path and green path are pseudo-palindromic paths since the red path [2,3,3] can be rearranged in [3,2,3] (palindrome) and the green path [2,1,1] can be rearranged in [1,2,1] (palindrome).
-Example 2:
+Explanation: The figure above represents the given binary tree. There are three
+paths going from the root node to leaf nodes: the red path [2,3,3], the green
+path [2,1,1], and the path [2,3,1]. Among these paths only red path and green
+path are pseudo-palindromic paths since the red path [2,3,3] can be rearranged
+in [3,2,3] (palindrome) and the green path [2,1,1] can be rearranged in [1,2,1]
+(palindrome). Example 2:
 
 
 
 Input: root = [2,1,1,1,3,null,null,null,null,null,1]
 Output: 1
-Explanation: The figure above represents the given binary tree. There are three paths going from the root node to leaf nodes: the green path [2,1,1], the path [2,1,3,1], and the path [2,1]. Among these paths only the green path is pseudo-palindromic since [2,1,1] can be rearranged in [1,2,1] (palindrome).
+Explanation: The figure above represents the given binary tree. There are three
+paths going from the root node to leaf nodes: the green path [2,1,1], the path
+[2,1,3,1], and the path [2,1]. Among these paths only the green path is
+pseudo-palindromic since [2,1,1] can be rearranged in [1,2,1] (palindrome).
 Example 3:
 
 Input: root = [9]
@@ -56,22 +66,28 @@ Acceptance Rate
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 
- struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+#include <vector>
+
+using namespace std;
 
 class Solution {
-public:
-    int pseudoPalindromicPaths (TreeNode* root) {
+  bool is_pp(vector<int> tested) {}
 
-    }
+ public:
+  int pseudoPalindromicPaths(TreeNode *root) {}
 };
