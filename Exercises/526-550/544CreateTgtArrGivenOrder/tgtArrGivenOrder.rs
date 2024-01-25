979181e358d0684761c63272bb2a9ecf2221ba7d
Raskolnikov <jerry.tann@endress.com>
2024-01-25 14:04:03 +0800

    ✅

--- Exercises/526-550/544CreateTgtArrGivenOrder/tgtArrGivenOrder.rs
+++ Exercises/526-550/544CreateTgtArrGivenOrder/tgtArrGivenOrder.rs
@@ -65,6 +65,10 @@ Acceptance Rate
 
 impl Solution {
   pub fn create_target_array(nums: Vec<i32>, index: Vec<i32>) -> Vec<i32> {
-
+    let mut res:Vec<i32> = Vec::new();
+    for i in 0..nums.len() {
+      res.insert(index[i] as usize, nums[i]);
+    }
+    res
   }
 }
