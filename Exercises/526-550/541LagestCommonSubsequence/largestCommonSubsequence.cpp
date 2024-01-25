90f013dd163aed7071d8dd05cfefa61606229434
Raskolnikov <jerry.tann@endress.com>
2024-01-25 11:30:21 +0800

    🚧 attempt1

--- Exercises/526-550/541LagestCommonSubsequence/largestCommonSubsequence.cpp
+++ Exercises/526-550/541LagestCommonSubsequence/largestCommonSubsequence.cpp
@@ -10,12 +10,16 @@ Medium
 Topics
 Companies
 Hint
-Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+Given two strings text1 and text2, return the length of their longest common
+subsequence. If there is no common subsequence, return 0.
 
-A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+A subsequence of a string is a new string generated from the original string
+with some characters (can be none) deleted without changing the relative order
+of the remaining characters.
 
 For example, "ace" is a subsequence of "abcde".
-A common subsequence of two strings is a subsequence that is common to both strings.
+A common subsequence of two strings is a subsequence that is common to both
+strings.
 
 
 
@@ -53,16 +57,47 @@ Acceptance Rate
 
 */
 
+#include <map>
 #include <string>
-#includde <unordered_map>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-int r(string &t1, string &t2, int &s1, int &s2, int idx1, int idx2) {
-}
-public:
-    int longestCommonSubsequence(string text1, string text2) {
+  map<pair<int, int>, int> dp;
 
+  int r(string &t1, string &t2, map<char, vector<int>> m1, map<char, vector<int>> m2, int &s1, int &s2, int idx1, int idx2) {
+    if (s1 <= idx1 || s2 <= idx2) return 0;
+
+    if (dp.find({idx1, idx2}) != dp.end()) return dp[{idx1,idx2}];
+    char c = t1[idx1];
+    int take_cnt = 0;
+    // take
+    if (m2.find(c) != m2.end()) {
+      for (int i = 0; i < m2[c].size(); ++i) {
+        if (m2[c][i] >= idx2) {
+          take_cnt = 1+r(t1,t2,m1,m2,s1,s2,idx1+1, m2[c][i]);
+          break;
+        }
+      }
     }
+    // leave
+    int leave_cnt = r(t1,t2,m1,m2,s1,s2,idx1+1, idx2);
+    dp[{idx1,idx2}] = max(take_cnt, leave_cnt);
+  }
+
+ public:
+  int longestCommonSubsequence(string text1, string text2) {
+    int s1 = text1.size();
+    int s2 = text2.size();
+    map<char, vector<int>> s1_map;
+    map<char, vector<int>> s2_map;
+    for (int i = 0; i < s1; ++i) {
+      s1_map[text1[i]].push_back(i);
+    }
+    for (int i = 0; i < s2; ++i) {
+      s2_map[text2[i]].push_back(i);
+    }
+    return r(text1, text2, s1_map, s2_map, s1, s2, 0, 0);
+  }
 };
