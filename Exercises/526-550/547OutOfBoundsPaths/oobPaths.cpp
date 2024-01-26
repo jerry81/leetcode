43f856ad259d0905448558e44ff816b3035d8408
Raskolnikov <jerry.tann@endress.com>
2024-01-26 11:07:19 +0800

    🚧 attempt 1

--- Exercises/526-550/547OutOfBoundsPaths/oobPaths.cpp
+++ Exercises/526-550/547OutOfBoundsPaths/oobPaths.cpp
@@ -5,9 +5,14 @@ Medium
 Topics
 Companies
 Hint
-There is an m x n grid with a ball. The ball is initially at the position [startRow, startColumn]. You are allowed to move the ball to one of the four adjacent cells in the grid (possibly out of the grid crossing the grid boundary). You can apply at most maxMove moves to the ball.
+There is an m x n grid with a ball. The ball is initially at the position
+[startRow, startColumn]. You are allowed to move the ball to one of the four
+adjacent cells in the grid (possibly out of the grid crossing the grid
+boundary). You can apply at most maxMove moves to the ball.
 
-Given the five integers m, n, maxMove, startRow, startColumn, return the number of paths to move the ball out of the grid boundary. Since the answer can be very large, return it modulo 109 + 7.
+Given the five integers m, n, maxMove, startRow, startColumn, return the number
+of paths to move the ball out of the grid boundary. Since the answer can be very
+large, return it modulo 109 + 7.
 
 
 
@@ -42,9 +47,41 @@ Acceptance Rate
 
 */
 
-class Solution {
-public:
-    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+#include <vector>
 
+using namespace std;
+
+class Solution {
+  const int MOD = 10e9 + 7;
+  vector<vector<int>> dp;
+  const vector<vector<int>> NEIGHBORS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+  int r(int y, int x, int movesLeft, int &h, int &w) {
+    if (movesLeft <= 0) return 0;
+
+    if (y >= h || x >= w) return 0;
+
+    if (y < 0 || x < 0) return 0;
+
+    if (dp[y][x] >= 0) return dp[y][x];
+
+    long long int total = 0;
+    for (auto v: NEIGHBORS) {
+      int dy = v[0];
+      int dx = v[1];
+      int ny = dy+y;
+      int nx = dx+x;
+      if (ny >= h || nx >= w || ny < 0 || nx < 0) {
+        total+=1;
+      } else {
+        total+=r(ny,nx,movesLeft-1,h,w);
+      }
     }
+    return dp[y][x] = total;
+  }
+
+ public:
+  int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+    dp.resize(m + 1, vector<int>(n + 1, -1));
+    return r(startRow, startColumn, maxMove, m, n);
+  }
 };
