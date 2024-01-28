204151fc7024b881682c16a033bb1a15fce151a3
Raskolnikov <jerry.tann@endress.com>
2024-01-28 22:45:38 +0800

    🚧 attempt 1

--- Exercises/526-550/550SubMatSumToTarget/subMatrixSumToTarget.cpp
+++ Exercises/526-550/550SubMatSumToTarget/subMatrixSumToTarget.cpp
@@ -61,6 +61,10 @@ class Solution {
  public:
   int numSubmatrixSumTarget(vector<vector<int>> &matrix, int target) {
     // for each item
+    int h = matrix.size();
+    int w = matrix[0].size();
+    int res = 0;
+
     vector<vector<int>> prefix_forward;
     // vector<vector<int>> prefix_backward;
     for (int idx = 0; idx < matrix.size(); ++idx) {
@@ -70,17 +74,80 @@ class Solution {
       for (int i = 0; i < v.size(); ++i) {
         sm += v[i];
 
-        int mod= (idx > 0)? prefix_forward[idx-1][i] : 0;
-        cur.push_back(sm+mod);
+        int mod = (idx > 0) ? prefix_forward[idx - 1][i] : 0;
+        if (sm + mod == target) res++;
+        cur.push_back(sm + mod);
       }
+
       prefix_forward.push_back(cur);
     }
-    for (auto a : prefix_forward) {
-      cout << endl;
-      for (auto b : a) {
-        cout << b << ",";
+    for (auto v: prefix_forward) {
+    cout << endl;
+      for (auto w: v) {
+        cout << w << ",";
       }
     }
+    for (int ys = 0; ys < h; ++ys) {
+      for (int xs = 0; xs < w; ++xs) {
+        if (ys == 0 && xs == 0) continue;
+
+        for (int ye = ys; ye < h; ++ye) {
+          for (int xe = xs; xe < w; ++xe) {
+            // submatrix from ys, xs to ye, xe
+            int sub1 = xe > 0 ? prefix_forward[ye][xe-1] : 0;
+            int sub2 = ye > 0 ? prefix_forward[ye-1][xe] : 0;
+            int add = xe >0 && ye > 0 ? prefix_forward[ye-1][xe-1] : 0;
+            int cur = prefix_forward[ye][xe] - sub1 - sub2 + add;
+            if (cur == target) res++;
+          }
+        }
+      }
+    }
+
     return 1;
   }
 };
+
+/*
+
+0,1,1,
+1,3,4,
+1,4,5,
+
+*/
+
+/*
+  this covers 9 matrixes all submatrixes with 0,0
+  for 0,1
+  we have 0,1 by itself (0,1) - (0,0)
+  0,1 to 0,2 (0,2) - (0,0)
+  0,1 to 1,1 (1,1) - (1,0)
+  0,1 to 1,2 (1,2) - (1,0)
+  0,1 to 2,1 (2,1) - (2,0)
+  0,1 to 2,2 (2,2) - (2,0)
+
+0,1,1,
+1,3,4,
+1,4,5,
+
+  starting from 1,1
+  1,1 -> 1,1 - 1,0 - 0,1 + 0,0
+  1,1 to 1,2 -> 1,2 - 0,2 - 1,0 + 0,0
+  1,1 to 2,1 -> 2,1 - 2,0 - 0,1 + 0,0
+  1,1 to 2,2 -> 2,2 - 2,0 - 0,2 + 0,0
+
+0,1,1,
+1,3,4,
+1,4,5,
+
+starting from 1,0
+1,0 by itself 1,0 - 0,0
+1,0 to 1,1 -> 1,1 - 0,1
+1,0 to 2,0 -> 2,0 - 0,0
+1,0 to 1,2 -> 1,2 - 0,2
+
+starting from 2,0
+2,0 by itself is 2,0 - 1,0
+2,0 to 2,1 -> 2,1 - 1,1
+2,0 to 2,2 -> 2,2 - 1,2
+*/
