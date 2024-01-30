3d354bbc211882838ef8e380a9c39a6b1f59587a
Raskolnikov <jerry.tann@endress.com>
2024-01-30 11:38:35 +0800

    ✨

---
+++ Exercises/551-575/557Are1sKPlacesAway/1sKsPlacesAway.rs
@@ -0,0 +1,49 @@
+/*
+
+1437. Check If All 1's Are at Least Length K Places Away
+Easy
+Topics
+Companies
+Hint
+Given an binary array nums and an integer k, return true if all 1's are at least k places away from each other, otherwise return false.
+
+
+
+Example 1:
+
+
+Input: nums = [1,0,0,0,1,0,0,1], k = 2
+Output: true
+Explanation: Each of the 1s are at least 2 places away from each other.
+Example 2:
+
+
+Input: nums = [1,0,0,1,0,1], k = 2
+Output: false
+Explanation: The second 1 and third 1 are only one apart from each other.
+
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= k <= nums.length
+nums[i] is 0 or 1
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+74.8K
+Submissions
+128.9K
+Acceptance Rate
+58.1%
+
+*/
+
+class Solution {
+  public:
+      bool kLengthApart(vector<int>& nums, int k) {
+
+      }
+  };
