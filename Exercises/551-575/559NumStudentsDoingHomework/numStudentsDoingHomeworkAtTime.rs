9181f516fe38c97b0f47a6328c29f51c6c827f17
Raskolnikov <jerry.tann@endress.com>
2024-01-30 12:47:43 +0800

    🚧

--- Exercises/551-575/559NumStudentsDoingHomework/numStudentsDoingHomeworkAtTime.rs
+++ Exercises/551-575/559NumStudentsDoingHomework/numStudentsDoingHomeworkAtTime.rs
@@ -49,6 +49,14 @@ Acceptance Rate
 
 impl Solution {
   pub fn busy_student(start_time: Vec<i32>, end_time: Vec<i32>, query_time: i32) -> i32 {
-
+    let mut res = 0;
+    for i in 0..start_time.len() {
+      let start = start_time[i];
+      let end = end_time[i];
+      if start <=query_time && end >= query_time {
+        res+=1;
+      }
+    }
+    res
   }
 }
