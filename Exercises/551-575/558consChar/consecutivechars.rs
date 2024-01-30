76f4d7af17fdce770658e17d867a42ef57884950
Raskolnikov <jerry.tann@endress.com>
2024-01-30 12:41:10 +0800

    🚧 attempt 1

--- Exercises/551-575/558consChar/consecutivechars.rs
+++ Exercises/551-575/558consChar/consecutivechars.rs
@@ -42,6 +42,16 @@ Acceptance Rate
 
 impl Solution {
   pub fn max_power(s: String) -> i32 {
-
+    let mut cur = '?';
+    let mut cnt = 0;
+    for c in s.chars() {
+      if c == cur {
+        cnt+=1;
+      } else {
+        cnt = 0;
+        cur = c;
+      }
+    }
+    cnt
   }
 }
