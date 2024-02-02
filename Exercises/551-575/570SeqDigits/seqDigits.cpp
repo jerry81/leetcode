ccc26855cd1f56a3dce9207ee2e23221b0c53c7f
Raskolnikov <jerry.tann@endress.com>
2024-02-02 17:41:16 +0800

    🚧

---
+++ Exercises/551-575/570SeqDigits/seqDigits.cpp
@@ -0,0 +1,48 @@
+/*
+
+1291. Sequential Digits
+Medium
+Topics
+Companies
+Hint
+An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+
+Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+
+
+
+Example 1:
+
+Input: low = 100, high = 300
+Output: [123,234]
+Example 2:
+
+Input: low = 1000, high = 13000
+Output: [1234,2345,3456,4567,5678,6789,12345]
+
+
+Constraints:
+
+10 <= low <= high <= 10^9
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+134.4K
+Submissions
+211.5K
+Acceptance Rate
+63.6%
+
+*/
+
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+
+    }
+};
