2e2ee15b09d7493df6b3afae2efcf8d3cc2cf88e
Raskolnikov <jerry.tann@endress.com>
2024-02-03 09:06:01 +0800

    🚧 more regex training

---
+++ Exercises/551-575/572PatientsWithConditionSQL/patientsWithConditions.sql
@@ -0,0 +1,66 @@
+/*
+
+1527. Patients With a Condition
+Easy
+Topics
+Companies
+SQL Schema
+Pandas Schema
+Table: Patients
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| patient_id   | int     |
+| patient_name | varchar |
+| conditions   | varchar |
++--------------+---------+
+patient_id is the primary key (column with unique values) for this table.
+'conditions' contains 0 or more code separated by spaces.
+This table contains information of the patients in the hospital.
+
+
+Write a solution to find the patient_id, patient_name, and conditions of the patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Patients table:
++------------+--------------+--------------+
+| patient_id | patient_name | conditions   |
++------------+--------------+--------------+
+| 1          | Daniel       | YFEV COUGH   |
+| 2          | Alice        |              |
+| 3          | Bob          | DIAB100 MYOP |
+| 4          | George       | ACNE DIAB100 |
+| 5          | Alain        | DIAB201      |
++------------+--------------+--------------+
+Output:
++------------+--------------+--------------+
+| patient_id | patient_name | conditions   |
++------------+--------------+--------------+
+| 3          | Bob          | DIAB100 MYOP |
+| 4          | George       | ACNE DIAB100 |
++------------+--------------+--------------+
+Explanation: Bob and George both have a condition that starts with DIAB1.
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+175.1K
+Submissions
+435.6K
+Acceptance Rate
+40.2%
+
+*/
+SELECT *
+FROM Patients p
+WHERE p.conditions ~ '\y\s*DIAB1(\S*|$)';
