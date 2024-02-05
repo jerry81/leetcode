a5d3a21cfae2973c8bc4c47c7a9a2aa6011267fd
Raskolnikov <jerry.tann@endress.com>
2024-02-05 15:49:57 +0800

    🚧 attempt

--- Exercises/551-575/575ShuffleString/shuffleString.rs
+++ Exercises/551-575/575ShuffleString/shuffleString.rs
@@ -44,8 +44,21 @@ Acceptance Rate
 
 */
 
+use std::collections::HashMap;
+
 impl Solution {
   pub fn restore_string(s: String, indices: Vec<i32>) -> String {
+    let mut hm: HashMap<i32,char> = HashMap::new();
+    for (i, &idx) in indices.iter().enumerate() {
+      println!("i is {} idx is {}", i, &idx);
+      hm.insert(idx, s.chars().nth(i).unwrap());
+    }
+    let sorted_entries: Vec<(i32, char)> = hm.into_iter().collect(); // convert to vec of tuples
+    for (key, value) in sorted_entries.into_iter().sorted_by_key(|&(k, _)| k) { // iter has sorted_by_key on it
+      sorted_char_map.insert(key, value);
+  }
 
+  let ret: String = (0..s.len() as i32).map(|i| sorted_char_map[&i]).collect();
+    "".to_string();
   }
 }
