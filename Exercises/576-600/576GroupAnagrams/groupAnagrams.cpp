f98acba3031f4fd518b0dca109b1910ccbbec5f0
Raskolnikov <jerry.tann@endress.com>
2024-02-06 16:23:16 +0800

    ✨

---
+++ Exercises/576-600/576GroupAnagrams/groupAnagrams.cpp
@@ -0,0 +1,57 @@
+/*
+
+49. Group Anagrams
+Medium
+Topics
+Companies
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+2.6M
+Submissions
+3.8M
+Acceptance Rate
+67.8%
+
+*/
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+      vector<vector<stirng>> res;
+
+      return res;
+    }
+};
