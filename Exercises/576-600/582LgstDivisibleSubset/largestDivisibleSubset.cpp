6bc88d4d2c82ea7d114ad8991d89b26beb4cd24a
Raskolnikov <jerry.tann@endress.com>
2024-02-09 11:53:50 +0800

    🚧 close but no ciggy

--- Exercises/576-600/582LgstDivisibleSubset/largestDivisibleSubset.cpp
+++ Exercises/576-600/582LgstDivisibleSubset/largestDivisibleSubset.cpp
@@ -1,28 +1,29 @@
-#include <vector>
 #include <map>
+#include <vector>
 
 using namespace std;
+
 class Solution {
-  map<pair<int,int>, vector<int>> dp;
-  vector<int> get_chain(int idx, int back, vector<int>& nums, int &sz) {
+  map<pair<int, int>, vector<int>> dp;
+  vector<int> get_chain(int idx, int back, vector<int>& nums, int& sz) {
     if (idx >= sz) return {};
 
-    if (dp.find({idx,back}) != dp.end()) return dp[{idx,back}];
+    // if (dp.find({idx,back}) != dp.end()) return dp[{idx,back}];
     int cur = nums[idx];
     vector<int> curv = {cur};
     // leave case
-    vector<int> tmp = get_chain(idx+1, back, nums, sz);
+    vector<int> orig = get_chain(idx + 1, back, nums, sz);
     if (cur % back == 0 || back % cur == 0) {
-      vector<int> cmp = get_chain(idx+1, cur, nums, sz);
-      curv.insert(curv.end(), cmp.begin(),cmp.end());
+      vector<int> cmp = get_chain(idx + 1, cur, nums, sz);
+      curv.insert(curv.end(), cmp.begin(), cmp.end());
     }
-    return dp[{idx,back}] = (curv.size() >=tmp.size()) ? curv : tmp;
+    return dp[{idx, back}] = (curv.size() >= orig.size()) ? curv : orig;
   }
 
  public:
   vector<int> largestDivisibleSubset(vector<int>& nums) {
     sort(nums.begin(), nums.end());
     int sz = nums.size();
-    return get_chain(0,-1,nums,sz);
+    return get_chain(0, 1, nums, sz);
   }
 };
