ac3192a92a6906e281f836611597b7c4bafa6d32
Raskolnikov <jerry.tann@endress.com>
2024-02-10 08:53:59 +0800

    🚧 feelin it out

--- Exercises/576-600/584PalindromicSubstr/palindromicSubstr.cpp
+++ Exercises/576-600/584PalindromicSubstr/palindromicSubstr.cpp
@@ -49,15 +49,31 @@ using namespace std;
 class Solution {
 public:
     int countSubstrings(string s) {
-      if (s.size()==1) return 1;
+      int sz = s.size();
+      if (sz==1) return 1;
+      // try brute force
+      int count = 0;
+      for (int i = 0; i < sz; ++i) {
+        for (int j = 2; j < sz-i; ++j) {
+          cout << "substr from " << i << " of len " << j << endl;
+          cout << s.substr(i, j) << endl;
+        }
+      }
+      return count;
     }
 };
 
 /*
 abc
-0, 01, 012  len 1 auto yes
-1, 12
+0, 01, 012  len 1 auto yes, len 2 - characters are equal, len 3 char 1 and 3 are equal
+1, 12 - any relation to previous substrings?
 2
 */
 /*
+abccbd
+01,012,0123,01234,012345
+12,123,1234,12345
+23,234,2345
+34,345
+45
 */
