a744dd9608279b3bcc49a4fc6500ed1391953156
Raskolnikov <jerry.tann@endress.com>
2024-02-10 08:45:33 +0800

    ✨

---
+++ Exercises/576-600/584PalindromicSubstr/palindromicSubstr.cpp
@@ -0,0 +1,63 @@
+/*
+
+647. Palindromic Substrings
+Medium
+Topics
+Companies
+Hint
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of lowercase English letters.
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+671.9K
+Submissions
+982.2K
+Acceptance Rate
+68.4%
+
+*/
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int countSubstrings(string s) {
+      if (s.size()==1) return 1;
+    }
+};
+
+/*
+abc
+0, 01, 012  len 1 auto yes
+1, 12
+2
+*/
+/*
+*/
