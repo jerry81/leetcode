6d0b718969e16d2c8ec5cfdf7f6b368bc8263413
Raskolnikov <jerry.tann@endress.com>
2024-02-15 10:30:15 +0800

    🐛 overflow issues

--- Exercises/576-600/590PolygonLargestPerimeter/polygonLargestPerimeter.cpp
+++ Exercises/576-600/590PolygonLargestPerimeter/polygonLargestPerimeter.cpp
@@ -7,13 +7,17 @@ Companies
 Hint
 You are given an array of positive integers nums of length n.
 
-A polygon is a closed plane figure that has at least 3 sides. The longest side of a polygon is smaller than the sum of its other sides.
+A polygon is a closed plane figure that has at least 3 sides. The longest side
+of a polygon is smaller than the sum of its other sides.
 
-Conversely, if you have k (k >= 3) positive real numbers a1, a2, a3, ..., ak where a1 <= a2 <= a3 <= ... <= ak and a1 + a2 + a3 + ... + ak-1 > ak, then there always exists a polygon with k sides whose lengths are a1, a2, a3, ..., ak.
+Conversely, if you have k (k >= 3) positive real numbers a1, a2, a3, ..., ak
+where a1 <= a2 <= a3 <= ... <= ak and a1 + a2 + a3 + ... + ak-1 > ak, then there
+always exists a polygon with k sides whose lengths are a1, a2, a3, ..., ak.
 
 The perimeter of a polygon is the sum of lengths of its sides.
 
-Return the largest possible perimeter of a polygon whose sides can be formed from nums, or -1 if it is not possible to create a polygon.
+Return the largest possible perimeter of a polygon whose sides can be formed
+from nums, or -1 if it is not possible to create a polygon.
 
 
 
@@ -21,19 +25,21 @@ Example 1:
 
 Input: nums = [5,5,5]
 Output: 15
-Explanation: The only possible polygon that can be made from nums has 3 sides: 5, 5, and 5. The perimeter is 5 + 5 + 5 = 15.
-Example 2:
+Explanation: The only possible polygon that can be made from nums has 3 sides:
+5, 5, and 5. The perimeter is 5 + 5 + 5 = 15. Example 2:
 
 Input: nums = [1,12,1,2,5,50,3]
 Output: 12
-Explanation: The polygon with the largest perimeter which can be made from nums has 5 sides: 1, 1, 2, 3, and 5. The perimeter is 1 + 1 + 2 + 3 + 5 = 12.
-We cannot have a polygon with either 12 or 50 as the longest side because it is not possible to include 2 or more smaller sides that have a greater sum than either of them.
-It can be shown that the largest possible perimeter is 12.
-Example 3:
+Explanation: The polygon with the largest perimeter which can be made from nums
+has 5 sides: 1, 1, 2, 3, and 5. The perimeter is 1 + 1 + 2 + 3 + 5 = 12. We
+cannot have a polygon with either 12 or 50 as the longest side because it is not
+possible to include 2 or more smaller sides that have a greater sum than either
+of them. It can be shown that the largest possible perimeter is 12. Example 3:
 
 Input: nums = [5,5,50]
 Output: -1
-Explanation: There is no possible way to form a polygon from nums, as a polygon has at least 3 sides and 50 > 5 + 5.
+Explanation: There is no possible way to form a polygon from nums, as a polygon
+has at least 3 sides and 50 > 5 + 5.
 
 
 Constraints:
@@ -58,8 +64,22 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    long long largestPerimeter(vector<int>& nums) {
-
+ public:
+  long long largestPerimeter(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    vector<long long> ps;
+    long long sum = 0;
+    for (long long i : nums) {
+      sum += i;
+      ps.push_back(sum);
     }
+    for (int i = nums.size() - 1; i > 1; --i) {
+      int cur = nums[i];
+      long long prevSum = ps[i - 1];
+      if (prevSum > cur) {
+        return ps[i];
+      }
+    }
+    return -1;
+  }
 };
