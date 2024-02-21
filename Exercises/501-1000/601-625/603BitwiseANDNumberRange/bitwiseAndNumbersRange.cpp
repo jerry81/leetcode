fe1701c5700bcd0d2b0e6574161ceb94a405f26b
Raskolnikov <jerry.tann@endress.com>
2024-02-21 11:35:08 +0800

    ✨

---
+++ Exercises/501-1000/601-625/603BitwiseANDNumberRange/bitwiseAndNumbersRange.cpp
@@ -0,0 +1,48 @@
+/*
+
+
+201. Bitwise AND of Numbers Range
+Medium
+Topics
+Companies
+Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+
+
+
+Example 1:
+
+Input: left = 5, right = 7
+Output: 4
+Example 2:
+
+Input: left = 0, right = 0
+Output: 0
+Example 3:
+
+Input: left = 1, right = 2147483647
+Output: 0
+
+
+Constraints:
+
+0 <= left <= right <= 231 - 1
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+284.4K
+Submissions
+652.7K
+Acceptance Rate
+43.6%
+
+
+*/
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+
+    }
+};
