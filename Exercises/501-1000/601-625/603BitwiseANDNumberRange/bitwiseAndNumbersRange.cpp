f914734b2e4b3ed895ae5351f6ff29394ee3b9ce
Raskolnikov <jerry.tann@endress.com>
2024-02-21 11:37:14 +0800

    🚧 couldn't be this easy

--- Exercises/501-1000/601-625/603BitwiseANDNumberRange/bitwiseAndNumbersRange.cpp
+++ Exercises/501-1000/601-625/603BitwiseANDNumberRange/bitwiseAndNumbersRange.cpp
@@ -43,6 +43,10 @@ Acceptance Rate
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-
+      int res = left;
+      for (int i = left+1; i <=right; ++i) {
+        res&=i;
+      }
+      return res;
     }
 };
