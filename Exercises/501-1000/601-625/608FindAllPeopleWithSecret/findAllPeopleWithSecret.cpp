490a1d7c59ed825819e542be3400c0265093d7ce
Raskolnikov <jerry.tann@endress.com>
2024-02-24 09:17:54 +0800

    ✅ verify first step

--- Exercises/501-1000/601-625/608FindAllPeopleWithSecret/findAllPeopleWithSecret.cpp
+++ Exercises/501-1000/601-625/608FindAllPeopleWithSecret/findAllPeopleWithSecret.cpp
@@ -5,13 +5,23 @@ Hard
 Topics
 Companies
 Hint
-You are given an integer n indicating there are n people numbered from 0 to n - 1. You are also given a 0-indexed 2D integer array meetings where meetings[i] = [xi, yi, timei] indicates that person xi and person yi have a meeting at timei. A person may attend multiple meetings at the same time. Finally, you are given an integer firstPerson.
+You are given an integer n indicating there are n people numbered from 0 to n
+- 1. You are also given a 0-indexed 2D integer array meetings where meetings[i]
+= [xi, yi, timei] indicates that person xi and person yi have a meeting at
+timei. A person may attend multiple meetings at the same time. Finally, you are
+given an integer firstPerson.
 
-Person 0 has a secret and initially shares the secret with a person firstPerson at time 0. This secret is then shared every time a meeting takes place with a person that has the secret. More formally, for every meeting, if a person xi has the secret at timei, then they will share the secret with person yi, and vice versa.
+Person 0 has a secret and initially shares the secret with a person firstPerson
+at time 0. This secret is then shared every time a meeting takes place with a
+person that has the secret. More formally, for every meeting, if a person xi has
+the secret at timei, then they will share the secret with person yi, and vice
+versa.
 
-The secrets are shared instantaneously. That is, a person may receive the secret and share it with people in other meetings within the same time frame.
+The secrets are shared instantaneously. That is, a person may receive the secret
+and share it with people in other meetings within the same time frame.
 
-Return a list of all the people that have the secret after all the meetings have taken place. You may return the answer in any order.
+Return a list of all the people that have the secret after all the meetings have
+taken place. You may return the answer in any order.
 
 
 
@@ -40,10 +50,10 @@ Input: n = 5, meetings = [[3,4,2],[1,2,1],[2,3,1]], firstPerson = 1
 Output: [0,1,2,3,4]
 Explanation:
 At time 0, person 0 shares the secret with person 1.
-At time 1, person 1 shares the secret with person 2, and person 2 shares the secret with person 3.
-Note that person 2 can share the secret at the same time as receiving it.
-At time 2, person 3 shares the secret with person 4.
-Thus, people 0, 1, 2, 3, and 4 know the secret after all the meetings.
+At time 1, person 1 shares the secret with person 2, and person 2 shares the
+secret with person 3. Note that person 2 can share the secret at the same time
+as receiving it. At time 2, person 3 shares the secret with person 4. Thus,
+people 0, 1, 2, 3, and 4 know the secret after all the meetings.
 
 
 Constraints:
@@ -68,23 +78,31 @@ Acceptance Rate
 
 */
 
-#include <vector>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-      unordered_map<int, vector<pair<int,int>>> neighbors;
-      for (vector<int> v: meetings) {
-        int p1 = v[0];
-        int p2 = v[1];
-        int t = v[2];
-        pair<int,int> pair1 = {p1,t};
-        pair<int,int> pair2 = {p2,t};
-        neighbors[p1].push_back(pair2);
-        neighbors[p2].push_back(pair1);
+ public:
+  vector<int> findAllPeople(int n, vector<vector<int>>& meetings,
+                            int firstPerson) {
+    unordered_map<int, vector<pair<int, int>>> neighbors;
+    for (vector<int> v : meetings) {
+      int p1 = v[0];
+      int p2 = v[1];
+      int t = v[2];
+      pair<int, int> pair1 = {p1, t};
+      pair<int, int> pair2 = {p2, t};
+      neighbors[p1].push_back(pair2);
+      neighbors[p2].push_back(pair1);
+    }
+    for (auto [k, v] : neighbors) {
+      cout << "neighbors of " << k << endl;
+      for (auto [f, s] : v) {
+        cout << f << " at time " << s << endl;
       }
     }
+    return {0};
+  }
 };
