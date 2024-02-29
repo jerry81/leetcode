87dd94901d4a4b6ef230a9234a9cd9081dfebb21
Raskolnikov <jerry.tann@endress.com>
2024-02-29 16:38:42 +0800

    🚧 restart with level order

--- Exercises/501-1000/601-625/617EvenOddTree/evenOddTree.cpp
+++ Exercises/501-1000/601-625/617EvenOddTree/evenOddTree.cpp
@@ -7,10 +7,13 @@ Companies
 Hint
 A binary tree is named Even-Odd if it meets the following conditions:
 
-The root of the binary tree is at level index 0, its children are at level index 1, their children are at level index 2, etc.
-For every even-indexed level, all nodes at the level have odd integer values in strictly increasing order (from left to right).
-For every odd-indexed level, all nodes at the level have even integer values in strictly decreasing order (from left to right).
-Given the root of a binary tree, return true if the binary tree is Even-Odd, otherwise return false.
+The root of the binary tree is at level index 0, its children are at level index
+1, their children are at level index 2, etc. For every even-indexed level, all
+nodes at the level have odd integer values in strictly increasing order (from
+left to right). For every odd-indexed level, all nodes at the level have even
+integer values in strictly decreasing order (from left to right). Given the root
+of a binary tree, return true if the binary tree is Even-Odd, otherwise return
+false.
 
 
 
@@ -24,8 +27,8 @@ Level 0: [1]
 Level 1: [10,4]
 Level 2: [3,7,9]
 Level 3: [12,8,6,2]
-Since levels 0 and 2 are all odd and increasing and levels 1 and 3 are all even and decreasing, the tree is Even-Odd.
-Example 2:
+Since levels 0 and 2 are all odd and increasing and levels 1 and 3 are all even
+and decreasing, the tree is Even-Odd. Example 2:
 
 
 Input: root = [5,4,2,3,3,7]
@@ -34,8 +37,8 @@ Explanation: The node values on each level are:
 Level 0: [5]
 Level 1: [4,2]
 Level 2: [3,3,7]
-Node values in level 2 must be in strictly increasing order, so the tree is not Even-Odd.
-Example 3:
+Node values in level 2 must be in strictly increasing order, so the tree is not
+Even-Odd. Example 3:
 
 
 Input: root = [5,9,1,3,5,7]
@@ -68,31 +71,41 @@ Acceptance Rate
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 
- struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
 
+#include <queue>
+using namespace std;
 class Solution {
-bool r(bool isOdd, TreeNode* root) {
-  if (!root) return true;
-  if (isOdd) {
-    if (root->val % 2 != 0) return false;
-  } else {
-    if (root->val % 2 == 0) return false;
-  }
-  return r(!isOdd,root->left) && r(!isOdd,root->right);
-}
-public:
-    bool isEvenOddTree(TreeNode* root) {
-      return r(false, root);
+ public:
+  bool isEvenOddTree(TreeNode *root) {
+    queue<TreeNode *> q;
+    q.push(root);
+    bool odd = false;
+    while (!q.empty()) {
+      queue<TreeNode*> nq;
+      while (!q.empty()) {
+        TreeNode* cur = q.front();
+        q.pop();
+        if (odd) {
+          if (cur->val % 2 != 0) return false;
+        } else {
+          if (cur->val % 2 == 1) return false;
+        }
+      }
+      odd=!odd;
     }
+  }
 };
