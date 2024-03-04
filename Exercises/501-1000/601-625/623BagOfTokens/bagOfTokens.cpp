f4f46cb134f78a3f6c0995bcdccc55b0ba7cc285
Raskolnikov <jerry.tann@endress.com>
2024-03-04 10:05:40 +0800

    🚧 attempt - greedy

--- Exercises/501-1000/601-625/623BagOfTokens/bagOfTokens.cpp
+++ Exercises/501-1000/601-625/623BagOfTokens/bagOfTokens.cpp
@@ -4,13 +4,19 @@
 Medium
 Topics
 Companies
-You start with an initial power of power, an initial score of 0, and a bag of tokens given as an integer array tokens, where each tokens[i] donates the value of tokeni.
+You start with an initial power of power, an initial score of 0, and a bag of
+tokens given as an integer array tokens, where each tokens[i] donates the value
+of tokeni.
 
-Your goal is to maximize the total score by strategically playing these tokens. In one move, you can play an unplayed token in one of the two ways (but not both for the same token):
+Your goal is to maximize the total score by strategically playing these tokens.
+In one move, you can play an unplayed token in one of the two ways (but not both
+for the same token):
 
-Face-up: If your current power is at least tokens[i], you may play tokeni, losing tokens[i] power and gaining 1 score.
-Face-down: If your current score is at least 1, you may play tokeni, gaining tokens[i] power and losing 1 score.
-Return the maximum possible score you can achieve after playing any number of tokens.
+Face-up: If your current power is at least tokens[i], you may play tokeni,
+losing tokens[i] power and gaining 1 score. Face-down: If your current score is
+at least 1, you may play tokeni, gaining tokens[i] power and losing 1 score.
+Return the maximum possible score you can achieve after playing any number of
+tokens.
 
 
 
@@ -20,7 +26,9 @@ Input: tokens = [100], power = 50
 
 Output: 0
 
-Explanation: Since your score is 0 initially, you cannot play the token face-down. You also cannot play it face-up since your power (50) is less than tokens[0] (100).
+Explanation: Since your score is 0 initially, you cannot play the token
+face-down. You also cannot play it face-up since your power (50) is less than
+tokens[0] (100).
 
 Example 2:
 
@@ -28,9 +36,11 @@ Input: tokens = [200,100], power = 150
 
 Output: 1
 
-Explanation: Play token1 (100) face-up, reducing your power to 50 and increasing your score to 1.
+Explanation: Play token1 (100) face-up, reducing your power to 50 and increasing
+your score to 1.
 
-There is no need to play token0, since you cannot play it face-up to add to your score. The maximum score achievable is 1.
+There is no need to play token0, since you cannot play it face-up to add to your
+score. The maximum score achievable is 1.
 
 Example 3:
 
@@ -70,8 +80,29 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    int bagOfTokensScore(vector<int>& tokens, int power) {
+ public:
+  int bagOfTokensScore(vector<int>& tokens, int power) {
+    sort(tokens.begin(), tokens.end());
+    int score = 0;
+    while (!tokens.empty()) {
+      int smallest = tokens[0];
+      if (power < smallest && score < 1) return score;
 
+      // but do we continue?
+      if (power >= smallest) {
+        power-=smallest;
+        score+=1;
+        tokens.erase(tokens.begin());
+      }
+
+      int largest = tokens.back();
+      int nextback = tokens.front();
+      if (largest > nextback) {
+        tokens.pop_back();
+        power+=largest;
+        score-=1;
+      }
     }
+    return score;
+  }
 };
