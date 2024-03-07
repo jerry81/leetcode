d0f1d17fe34b2a5f66087c1efaeefc95cbb91646
Raskolnikov <jerry.tann@endress.com>
2024-03-07 10:19:30 +0800

    ✅

--- Exercises/501-1000/626-650/652DailyLeadsAndPartnersSQL/dailyLeadsAndPartners.sql
+++ Exercises/501-1000/626-650/652DailyLeadsAndPartnersSQL/dailyLeadsAndPartners.sql
@@ -68,5 +68,6 @@
 -- Acceptance Rate
 -- 86.5%
 
-select date_id, make_name, count(distinct(lead_id)) as unique_leads, count(distinct(partner_id))
-from DailySale;
+select date_id, make_name, count(distinct(lead_id)) as unique_leads, count(distinct(partner_id)) as unique_partners
+from DailySales
+group by date_id, make_name;
