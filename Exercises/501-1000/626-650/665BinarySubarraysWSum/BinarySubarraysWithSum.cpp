382b347c7fef34b259f5aede18d3c402db3a3b99
Raskolnikov <jerry.tann@endress.com>
2024-03-14 10:07:46 +0800

    ✨

---
+++ Exercises/501-1000/626-650/665BinarySubarraysWSum/BinarySubarraysWithSum.cpp
@@ -0,0 +1,60 @@
+/*
+
+
+Code
+Testcase
+Test Result
+Test Result
+930. Binary Subarrays With Sum
+Medium
+Topics
+Companies
+Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+
+A subarray is a contiguous part of the array.
+
+
+
+Example 1:
+
+Input: nums = [1,0,1,0,1], goal = 2
+Output: 4
+Explanation: The 4 subarrays are bolded and underlined below:
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+Example 2:
+
+Input: nums = [0,0,0,0,0], goal = 0
+Output: 15
+
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+nums[i] is either 0 or 1.
+0 <= goal <= nums.length
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+106K
+Submissions
+186.1K
+Acceptance Rate
+57.0%
+
+*/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+
+    }
+};
