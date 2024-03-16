ebdccc29bdf21795024434a5c62f45031510c513
Raskolnikov <jerry.tann@endress.com>
2024-03-16 11:23:55 +0800

    ✨

---
+++ Exercises/501-1000/626-649/627ContiguousArray/continguousArray.cpp
@@ -0,0 +1,46 @@
+/*
+
+
+525. Contiguous Array
+Medium
+Topics
+Companies
+Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+
+
+
+Example 1:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
+Example 2:
+
+Input: nums = [0,1,0]
+Output: 2
+Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+340.3K
+Submissions
+722.8K
+Acceptance Rate
+47.1%
+
+*/
+
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+
+    }
+};
