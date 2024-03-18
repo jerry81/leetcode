885e3bf1447c6392711a45ee5e28091588d82087
Raskolnikov <jerry.tann@endress.com>
2024-03-18 10:55:23 +0800

    ✨ ez

---
+++ Exercises/501-1000/626-649/632SumUnique/sumUnique.rs
@@ -0,0 +1,52 @@
+/*
+
+1748. Sum of Unique Elements
+Easy
+Topics
+Companies
+Hint
+You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+
+Return the sum of all the unique elements of nums.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,2]
+Output: 4
+Explanation: The unique elements are [1,3], and the sum is 4.
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: 0
+Explanation: There are no unique elements, and the sum is 0.
+Example 3:
+
+Input: nums = [1,2,3,4,5]
+Output: 15
+Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+158.3K
+Submissions
+204.8K
+Acceptance Rate
+77.3%
+
+*/
+
+impl Solution {
+  pub fn sum_of_unique(nums: Vec<i32>) -> i32 {
+
+  }
+}
