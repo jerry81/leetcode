d8774daac628dc3c3082cc6d16a2a01ee40c4106
Raskolnikov <jerry.tann@endress.com>
2024-03-19 10:54:15 +0800

    🚧

--- Exercises/501-1000/626-649/taskScheduler.cpp
+++ Exercises/501-1000/626-649/taskScheduler.cpp
@@ -3,7 +3,10 @@
 Medium
 Topics
 Companies
-You are given an array of CPU tasks, each represented by letters A to Z, and a cooling time, n. Each cycle or interval allows the completion of one task. Tasks can be completed in any order, but there's a constraint: identical tasks must be separated by at least n intervals due to cooling time.
+You are given an array of CPU tasks, each represented by letters A to Z, and a
+cooling time, n. Each cycle or interval allows the completion of one task. Tasks
+can be completed in any order, but there's a constraint: identical tasks must be
+separated by at least n intervals due to cooling time.
 
 ​Return the minimum number of intervals required to complete all tasks.
 
@@ -17,7 +20,9 @@ Output: 8
 
 Explanation: A possible sequence is: A -> B -> idle -> A -> B -> idle -> A -> B.
 
-After completing task A, you must wait two cycles before doing A again. The same applies to task B. In the 3rd interval, neither A nor B can be done, so you idle. By the 4th cycle, you can do A again as 2 intervals have passed.
+After completing task A, you must wait two cycles before doing A again. The same
+applies to task B. In the 3rd interval, neither A nor B can be done, so you
+idle. By the 4th cycle, you can do A again as 2 intervals have passed.
 
 Example 2:
 
@@ -35,9 +40,11 @@ Input: tasks = ["A","A","A", "B","B","B"], n = 3
 
 Output: 10
 
-Explanation: A possible sequence is: A -> B -> idle -> idle -> A -> B -> idle -> idle -> A -> B.
+Explanation: A possible sequence is: A -> B -> idle -> idle -> A -> B -> idle ->
+idle -> A -> B.
 
-There are only two types of tasks, A and B, which need to be separated by 3 intervals. This leads to idling twice between repetitions of these tasks.
+There are only two types of tasks, A and B, which need to be separated by 3
+intervals. This leads to idling twice between repetitions of these tasks.
 
 
 
@@ -58,33 +65,35 @@ Acceptance Rate
 58.4%
 */
 
-#include <vector>
+#include <queue>
 #include <tuple>
 #include <unordered_map>
-#include <queue>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-unordered_map<char, int> freq;
-unordered_map<char, int> lastIdx;
-bool customSort(char a, char b) {
-  if (lastIdx[a] < lastIdx[b]) return true;
+  unordered_map<char, int> freq;
+  unordered_map<char, int> lastIdx;
+  bool customSort(char a, char b) {
+    if (lastIdx[a] < lastIdx[b]) return true;
 
-  return freq[a] > freq[b];
-}
-public:
-    int leastInterval(vector<char>& tasks, int n) {
-      for (char c: tasks) {
-        if (freq.find(c) != freq.end()) {
-          freq[c]++;
-        } else {
-          freq[c] = 1;
-        }
+    return freq[a] > freq[b];
+  }
+
+ public:
+  int leastInterval(vector<char>& tasks, int n) {
+    std::priority_queue<int, std::vector<int>, decltype(&customSort)> pq(
+        customSort);  // new way to do custom sort, interesting
+
+    for (char c : tasks) {
+      if (freq.find(c) != freq.end()) {
+        freq[c]++;
+      } else {
+        freq[c] = 1;
+        pq.push(c);
         lastIdx[c] = -1;
       }
-      std::priority_queue<int, std::vector<int>, decltype(&customSort)> pq(customSort); // new way to do custom sort, interesting
-
-
     }
+  }
 };
