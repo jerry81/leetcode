a4701c5cb7bde32e06eb7d9a733e989924fc0c3c
Raskolnikov <jerry.tann@endress.com>
2024-03-20 10:48:06 +0800

    ✅ very ugly

--- Exercises/501-1000/626-649/635MergeInBetweenLL/mergeInBetweenLL.cpp
+++ Exercises/501-1000/626-649/635MergeInBetweenLL/mergeInBetweenLL.cpp
@@ -7,7 +7,8 @@ Companies
 Hint
 You are given two linked lists: list1 and list2 of sizes n and m respectively.
 
-Remove list1's nodes from the ath node to the bth node, and put list2 in their place.
+Remove list1's nodes from the ath node to the bth node, and put list2 in their
+place.
 
 The blue edges and nodes in the following figure indicate the result:
 
@@ -21,13 +22,15 @@ Example 1:
 
 Input: list1 = [10,1,13,6,9,5], a = 3, b = 4, list2 = [1000000,1000001,1000002]
 Output: [10,1,13,1000000,1000001,1000002,5]
-Explanation: We remove the nodes 3 and 4 and put the entire list2 in their place. The blue edges and nodes in the above figure indicate the result.
-Example 2:
+Explanation: We remove the nodes 3 and 4 and put the entire list2 in their
+place. The blue edges and nodes in the above figure indicate the result. Example
+2:
 
 
-Input: list1 = [0,1,2,3,4,5,6], a = 2, b = 5, list2 = [1000000,1000001,1000002,1000003,1000004]
-Output: [0,1,1000000,1000001,1000002,1000003,1000004,6]
-Explanation: The blue edges and nodes in the above figure indicate the result.
+Input: list1 = [0,1,2,3,4,5,6], a = 2, b = 5, list2 =
+[1000000,1000001,1000002,1000003,1000004] Output:
+[0,1,1000000,1000001,1000002,1000003,1000004,6] Explanation: The blue edges and
+nodes in the above figure indicate the result.
 
 
 Constraints:
@@ -59,46 +62,48 @@ Acceptance Rate
  * };
  */
 
- struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
-     ListNode(int x) : val(x), next(nullptr) {}
-     ListNode(int x, ListNode *next) : val(x), next(next) {}
- };
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 class Solution {
-public:
-    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
-      int cnt = 0;
-      ListNode *cur = list1;
-      ListNode *ret = cur;
-      ListNode *cpyhead = new ListNode(list1->val);
-      ListNode *cpy = cpyhead;
-      ListNode *cpyptr = list1;
-      while (cpyptr!=nullptr) {
-        ListNode *next = new ListNode(cpyptr->val);
-        cpy->next = next;
-        cpy=cpy->next;
-        cpyptr = cpyptr->next;
-      }
-      while (cnt != a) {
-        cnt++;
-        cur = cur->next;
-
-
-      }
-      cur->next = list2;
-      cur = cur->next;
-      while (cur->next != nullptr) {
-         cur = cur->next;
-      }
-      while (cnt != b) {
-        cnt++;
-        shadow = shadow->next;
-      }
-      cout << "shadow is now " << shadow->val << endl;
-      cur->next = shadow->next;
-      return ret;
+ public:
+  ListNode *mergeInBetween(ListNode *list1, int a, int b, ListNode *list2) {
+    /* first clone the list */
+    ListNode *cur = list1;
+    ListNode *ret = cur;
+    ListNode *cpyhead = new ListNode(list1->val);
+    ListNode *cpy = cpyhead;
+    ListNode *cpyptr = list1;
+    while (cpyptr != nullptr) {
+      ListNode *next = new ListNode(cpyptr->val);
+      cpy->next = next;
+      cpy = cpy->next;
+      cpyptr = cpyptr->next;
     }
+
+    /* keep track of "index"*/
+    int cnt = 1;
+    while (cnt < a) {
+      cnt++;
+      cur = cur->next;
+      cpyhead = cpyhead->next;
+    }
+    cur->next = list2;
+    cur = cur->next;
+    while (cur->next != nullptr) {
+      cur = cur->next;
+    }
+    while (cnt <= b) {
+      cpyhead = cpyhead->next;
+      cnt++;
+    }
+    if (cpyhead->next) cpyhead = cpyhead->next;
+    cur->next = cpyhead->next;
+    return ret;
+  }
 };
