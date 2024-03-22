f17689e52efd6c88e34e9f0e93fdbe6c86a8ff24
Raskolnikov <jerry.tann@endress.com>
2024-03-22 17:15:03 +0800

    ✨ ez

---
+++ Exercises/501-1000/626-649/638CountItemsMatching/countItemsMatching.rs
@@ -0,0 +1,54 @@
+/*
+
+1773. Count Items Matching a Rule
+Easy
+Topics
+Companies
+Hint
+You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+
+The ith item is said to match the rule if one of the following is true:
+
+ruleKey == "type" and ruleValue == typei.
+ruleKey == "color" and ruleValue == colori.
+ruleKey == "name" and ruleValue == namei.
+Return the number of items that match the given rule.
+
+
+
+Example 1:
+
+Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+Output: 1
+Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+Example 2:
+
+Input: items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+Output: 2
+Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"] and ["phone","gold","iphone"]. Note that the item ["computer","silver","phone"] does not match.
+
+
+Constraints:
+
+1 <= items.length <= 104
+1 <= typei.length, colori.length, namei.length, ruleValue.length <= 10
+ruleKey is equal to either "type", "color", or "name".
+All strings consist only of lowercase letters.
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+210.5K
+Submissions
+248.4K
+Acceptance Rate
+84.8%
+
+*/
+
+impl Solution {
+  pub fn count_matches(items: Vec<Vec<String>>, rule_key: String, rule_value: String) -> i32 {
+
+  }
+}
