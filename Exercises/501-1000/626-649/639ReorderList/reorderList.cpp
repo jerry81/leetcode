c429fa01781c8e94a698c442f40a211cbd7c993a
Raskolnikov <jerry.tann@endress.com>
2024-03-23 11:03:18 +0800

    🚧

--- Exercises/501-1000/626-649/639ReorderList/reorderList.cpp
+++ Exercises/501-1000/626-649/639ReorderList/reorderList.cpp
@@ -53,6 +53,14 @@ Acceptance Rate
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
 class Solution {
 public:
     void reorderList(ListNode* head) {
