82782e625214d212223065660686baaac6bcbd37
Raskolnikov <jerry.tann@endress.com>
2024-03-25 11:16:33 +0800

    ✨

---
+++ Exercises/501-1000/626-649/642FindAllDuplicates/findallduplicates.cpp
@@ -0,0 +1,56 @@
+/*
+
+
+442. Find All Duplicates in an Array
+Medium
+Topics
+Companies
+Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+
+You must write an algorithm that runs in O(n) time and uses only constant extra space.
+
+
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [2,3]
+Example 2:
+
+Input: nums = [1,1,2]
+Output: [1]
+Example 3:
+
+Input: nums = [1]
+Output: []
+
+
+Constraints:
+
+n == nums.length
+1 <= n <= 105
+1 <= nums[i] <= n
+Each element in nums appears once or twice.
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+622.5K
+Submissions
+842.2K
+Acceptance Rate
+73.9%
+
+*/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+
+    }
+};
