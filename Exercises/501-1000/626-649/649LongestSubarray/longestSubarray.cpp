695e29a0859b8ad15199a2088e274b88cf0fd1e0
Raskolnikov <jerry.tann@endress.com>
2024-03-28 08:28:53 +0800

    🚧 att1

--- Exercises/501-1000/626-649/649LongestSubarray/longestSubarray.cpp
+++ Exercises/501-1000/626-649/649LongestSubarray/longestSubarray.cpp
@@ -10,7 +10,8 @@ You are given an integer array nums and an integer k.
 
 The frequency of an element x is the number of times it occurs in an array.
 
-An array is called good if the frequency of each element in this array is less than or equal to k.
+An array is called good if the frequency of each element in this array is less
+than or equal to k.
 
 Return the length of the longest good subarray of nums.
 
@@ -22,20 +23,23 @@ Example 1:
 
 Input: nums = [1,2,3,1,2,3,1,2], k = 2
 Output: 6
-Explanation: The longest possible good subarray is [1,2,3,1,2,3] since the values 1, 2, and 3 occur at most twice in this subarray. Note that the subarrays [2,3,1,2,3,1] and [3,1,2,3,1,2] are also good.
-It can be shown that there are no good subarrays with length more than 6.
-Example 2:
+Explanation: The longest possible good subarray is [1,2,3,1,2,3] since the
+values 1, 2, and 3 occur at most twice in this subarray. Note that the subarrays
+[2,3,1,2,3,1] and [3,1,2,3,1,2] are also good. It can be shown that there are no
+good subarrays with length more than 6. Example 2:
 
 Input: nums = [1,2,1,2,1,2,1,2], k = 1
 Output: 2
-Explanation: The longest possible good subarray is [1,2] since the values 1 and 2 occur at most once in this subarray. Note that the subarray [2,1] is also good.
-It can be shown that there are no good subarrays with length more than 2.
+Explanation: The longest possible good subarray is [1,2] since the values 1 and
+2 occur at most once in this subarray. Note that the subarray [2,1] is also
+good. It can be shown that there are no good subarrays with length more than 2.
 Example 3:
 
 Input: nums = [5,5,5,5,5,5,5], k = 4
 Output: 4
-Explanation: The longest possible good subarray is [5,5,5,5] since the value 5 occurs 4 times in this subarray.
-It can be shown that there are no good subarrays with length more than 4.
+Explanation: The longest possible good subarray is [5,5,5,5] since the value 5
+occurs 4 times in this subarray. It can be shown that there are no good
+subarrays with length more than 4.
 
 
 Constraints:
@@ -56,24 +60,37 @@ Acceptance Rate
 
 */
 
-#include <vector>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    int maxSubarrayLength(vector<int>& nums, int k) {
-      // contiguous, therefore, don't need dp?
-      int sz = nums.size();
-      if (k == 1) return 1;
-      if (sz == 1) return 1;
-      unordered_map<int,int> freq;
-      int ptr1 = 0;
-      int ptr2 = 0;
-
-      int res = 0;
-
-      return res;
+ public:
+  int maxSubarrayLength(vector<int>& nums, int k) {
+    // contiguous, therefore, don't need dp?
+    int sz = nums.size();
+    if (k == 1) return 1;
+    if (sz == 1) return 1;
+    unordered_map<int, int> freq;
+    int ptr1 = 0;
+    int ptr2 = 0;
+    int res = 0;
+    while (ptr2 < sz && ptr1 < sz) {
+      int cur = nums[ptr2];
+      freq[cur] += 1;
+      if (freq[cur] < k) {
+        res = max(res, (ptr2-ptr1)+1);
+      } else {
+        while (nums[ptr1]!=cur) {
+          freq[nums[ptr1]]--;
+          ptr1++;
+        }
+        nums[ptr1]--;
+      }
+      ptr2++;
     }
+
+    return res;
+  }
 };
