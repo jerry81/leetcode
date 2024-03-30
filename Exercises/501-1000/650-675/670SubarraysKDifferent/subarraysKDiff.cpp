585a25298803c6805bd31ae8f9bdbc6b9ab06261
Raskolnikov <jerry.tann@endress.com>
2024-03-30 21:20:50 +0800

    🚧

--- Exercises/501-1000/650-675/670SubarraysKDifferent/subarraysKDiff.cpp
+++ Exercises/501-1000/650-675/670SubarraysKDifferent/subarraysKDiff.cpp
@@ -5,9 +5,11 @@
 Hard
 Topics
 Companies
-Given an integer array nums and an integer k, return the number of good subarrays of nums.
+Given an integer array nums and an integer k, return the number of good
+subarrays of nums.
 
-A good array is an array where the number of different integers in that array is exactly k.
+A good array is an array where the number of different integers in that array is
+exactly k.
 
 For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.
 A subarray is a contiguous part of an array.
@@ -18,12 +20,13 @@ Example 1:
 
 Input: nums = [1,2,1,2,3], k = 2
 Output: 7
-Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2]
-Example 2:
+Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1],
+[1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2] Example 2:
 
 Input: nums = [1,2,1,3,4], k = 3
 Output: 3
-Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
+Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3],
+[2,1,3], [1,3,4].
 
 
 Constraints:
@@ -43,13 +46,32 @@ Acceptance Rate
 
 */
 
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    int subarraysWithKDistinct(vector<int>& nums, int k) {
+ public:
+  int subarraysWithKDistinct(vector<int>& nums, int k) {
+    unordered_map<int, int> cur;
+    int sz = nums.size();
+    int uptok = 0;
+    int starti = 0;
+    int endi = 0;
 
+    while (starti < sz && endi < sz) {
+      while (cur.size() <= k) {
+        cur[nums[endi]]++;
+        uptok += (endi - starti + 1);
+      }
+      while (cur.size() > k) {
+        cur[nums[starti]]--;
+        if (cur[nums[starti]] == 0) cur.erase(nums[starti]);
+        starti++;
+      }
     }
+    cout << "count is " << uptok << endl;
+    return uptok;
+  }
 };
