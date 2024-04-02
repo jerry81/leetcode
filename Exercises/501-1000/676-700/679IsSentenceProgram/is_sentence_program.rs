fc6c0edc7efc3e55f980f4e08b23e0f36aacac32
Raskolnikov <jerry.tann@endress.com>
2024-04-02 16:51:19 +0800

    ✨ ez 2

---
+++ Exercises/501-1000/676-700/679IsSentenceProgram/is_sentence_program.rs
@@ -0,0 +1,47 @@
+/*
+
+1832. Check if the Sentence Is Pangram
+Easy
+Topics
+Companies
+Hint
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+
+
+Example 1:
+
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains at least one of every letter of the English alphabet.
+Example 2:
+
+Input: sentence = "leetcode"
+Output: false
+
+
+Constraints:
+
+1 <= sentence.length <= 1000
+sentence consists of lowercase English letters.
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+319K
+Submissions
+383.8K
+Acceptance Rate
+83.1%
+
+*/
+
+
+impl Solution {
+  pub fn check_if_pangram(sentence: String) -> bool {
+
+  }
+}
