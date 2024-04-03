ad38b49d62335efc8db3e83c847e28117c61d4d9
Raskolnikov <jerry.tann@endress.com>
2024-04-03 10:32:16 +0800

    🚧

--- Exercises/501-1000/676-700/681WordSearch/wordSearch.cpp
+++ Exercises/501-1000/676-700/681WordSearch/wordSearch.cpp
@@ -5,27 +5,28 @@
 Medium
 Topics
 Companies
-Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+Given an m x n grid of characters board and a string word, return true if word
+exists in the grid.
 
-The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+The word can be constructed from letters of sequentially adjacent cells, where
+adjacent cells are horizontally or vertically neighboring. The same letter cell
+may not be used more than once.
 
 
 
 Example 1:
 
 
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
-Output: true
-Example 2:
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word =
+"ABCCED" Output: true Example 2:
 
 
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
-Output: true
-Example 3:
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word =
+"SEE" Output: true Example 3:
 
 
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
-Output: false
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word =
+"ABCB" Output: false
 
 
 Constraints:
@@ -37,7 +38,8 @@ n = board[i].length
 board and word consists of only lowercase and uppercase English letters.
 
 
-Follow up: Could you use search pruning to make your solution faster with a larger board?
+Follow up: Could you use search pruning to make your solution faster with a
+larger board?
 
 Seen this question in a real interview before?
 1/4
@@ -51,13 +53,40 @@ Acceptance Rate
 41.6%
 
 */
+#include <queue>
+#include <set>
 #include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    bool exist(vector<vector<char>>& board, string word) {
-
+ public:
+  bool exist(vector<vector<char>>& board, string word) {
+    // bfs
+    vector<vector<int>> NEIGHBORS = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+    int r = board.size();
+    int c = board[0].size();
+    char start_letter = word[0];
+    for (int cur_r = 0; cur_r < r; ++cur_r) {
+      for (int cur_c = 0; cur_c < c; ++cur_c) {
+        if (board[r][c] == start_letter) {
+          set<pair<int, int>> visited;
+          queue<pair<int, int>> q;
+          q.push({cur_r, cur_c});
+          while (!q.empty()) {
+            queue<pair<int,int>> nq;
+            while (!q.empty()) {
+             pair<int,int> popped = q.front();
+             visited.insert(popped);
+             auto [y,x] = popped;
+              q.pop();
+            }
+            for (vector<int> neigh: NEIGHBORS) {
+            }
+          }
+        }
+      }
     }
+    return true;
+  }
 };
