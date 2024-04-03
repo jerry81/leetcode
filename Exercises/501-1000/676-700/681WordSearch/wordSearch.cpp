2796fa717232b3e40a4392ae270aa85226a9f4be
Raskolnikov <jerry.tann@endress.com>
2024-04-03 10:22:24 +0800

    ✨

---
+++ Exercises/501-1000/676-700/681WordSearch/wordSearch.cpp
@@ -0,0 +1,63 @@
+/*
+
+
+79. Word Search
+Medium
+Topics
+Companies
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+
+
+Example 1:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+Example 2:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+Example 3:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+
+
+Constraints:
+
+m == board.length
+n = board[i].length
+1 <= m, n <= 6
+1 <= word.length <= 15
+board and word consists of only lowercase and uppercase English letters.
+
+
+Follow up: Could you use search pruning to make your solution faster with a larger board?
+
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+1.5M
+Submissions
+3.7M
+Acceptance Rate
+41.6%
+
+*/
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+
+    }
+};
