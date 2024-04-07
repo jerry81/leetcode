7cdeb78c7cc53fbfa9b7977d73767b70234b484d
Raskolnikov <jerry.tann@endress.com>
2024-04-07 11:40:56 +0800

    🚧 pass examples

--- Exercises/501-1000/676-700/687ValidParenStr/validParenStr.cpp
+++ Exercises/501-1000/676-700/687ValidParenStr/validParenStr.cpp
@@ -5,14 +5,16 @@
 Medium
 Topics
 Companies
-Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+Given a string s containing only three types of characters: '(', ')' and '*',
+return true if s is valid.
 
 The following rules define a valid string:
 
 Any left parenthesis '(' must have a corresponding right parenthesis ')'.
 Any right parenthesis ')' must have a corresponding left parenthesis '('.
 Left parenthesis '(' must go before the corresponding right parenthesis ')'.
-'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+'*' could be treated as a single right parenthesis ')' or a single left
+parenthesis '(' or an empty string "".
 
 
 Example 1:
@@ -49,55 +51,57 @@ Acceptance Rate
 #include <string>
 
 using namespace std;
-
 class Solution {
-public:
-    bool checkValidString(string s) {
-        // iterate forwards
-        // iterate backwards
-        int wildcount = 0;
-        int lcount = 0;
-        for (char c: s) {
-            if (c == '(') {
-              lcount+=1;
-            } else if (c == ')') {
-              if (lcount < 1) {
-                if (wildcount < 1) {
-                    return false;
-                } else {
-                    wildcount-=1;
-                }
-              }
-              lcount-=1;
-            } else {
-                wildcount+=1;
-            }
+ public:
+  bool checkValidString(string s) {
+    // iterate forwards
+    // iterate backwards
+    int wildcount = 0;
+    int lcount = 0;
+    for (char c : s) {
+      if (c == '(') {
+        lcount += 1;
+      } else if (c == ')') {
+        if (lcount < 1) {
+          if (wildcount < 1) {
+            return false;
+          } else {
+            wildcount -= 1;
+          }
+        } else {
+          lcount -= 1;
         }
-        cout << "lcount " << lcount<< endl;
-        if (lcount != 0) return false;
-
-        string rev = s;
-        reverse(rev.begin(), rev.end());
-        cout << "rev is " << rev << endl;
-        lcount = 0;
-        wildcount = 0;
-        for (char c: rev) {
-            if (c == ')') {
-              lcount+=1;
-            } else if (c == '(') {
-              if (lcount < 1) {
-                if (wildcount < 1) {
-                    return false;
-                } else {
-                    wildcount-=1;
-                }
-              }
-              lcount-=1;
-            } else {
-                wildcount+=1;
-            }
-        }
-        cout << "lcount " << lcount << endl;
-        return lcount == 0;
+      } else {
+        wildcount += 1;
+      }
     }
+    cout << "lcount " << lcount << endl;
+    if (lcount > wildcount) return false;
+
+    string rev = s;
+    reverse(rev.begin(), rev.end());
+    cout << "rev is " << rev << endl;
+    lcount = 0;
+    wildcount = 0;
+    for (char c : rev) {
+      if (c == ')') {
+        lcount += 1;
+      } else if (c == '(') {
+        if (lcount < 1) {
+          if (wildcount < 1) {
+            return false;
+          } else {
+            wildcount -= 1;
+          }
+        } else {
+          lcount -= 1;
+        }
+      } else {
+        wildcount += 1;
+      }
+    }
+    cout << "lcount " << lcount << endl;
+    cout << "wildcount is " << wildcount << endl;
+    return lcount <= wildcount;
+  }
 };
