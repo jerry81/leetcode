53630e5f2cb412ff897268032873b0fb7b4131ea
Raskolnikov <jerry.tann@endress.com>
2024-04-13 12:20:43 +0800

    ✨ daily

---
+++ Exercises/501-1000/701-725/704MaxRect/maxRect.cpp
@@ -0,0 +1,52 @@
+/*
+
+
+85. Maximal Rectangle
+Hard
+Topics
+Companies
+Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+
+
+
+Example 1:
+
+
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 6
+Explanation: The maximal rectangle is shown in the above picture.
+Example 2:
+
+Input: matrix = [["0"]]
+Output: 0
+Example 3:
+
+Input: matrix = [["1"]]
+Output: 1
+
+
+Constraints:
+
+rows == matrix.length
+cols == matrix[i].length
+1 <= row, cols <= 200
+matrix[i][j] is '0' or '1'.
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+406.5K
+Submissions
+857.2K
+Acceptance Rate
+47.4%
+
+*/
+
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+
+    }
+};
