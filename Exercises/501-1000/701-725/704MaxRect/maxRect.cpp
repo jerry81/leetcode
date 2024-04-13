773732662218fc9eb7c33f48f779ad80f03069b1
Raskolnikov <jerry.tann@endress.com>
2024-04-13 12:32:55 +0800

    🚧

--- Exercises/501-1000/701-725/704MaxRect/maxRect.cpp
+++ Exercises/501-1000/701-725/704MaxRect/maxRect.cpp
@@ -5,14 +5,16 @@
 Hard
 Topics
 Companies
-Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+Given a rows x cols binary matrix filled with 0's and 1's, find the largest
+rectangle containing only 1's and return its area.
 
 
 
 Example 1:
 
 
-Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Input: matrix =
+[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
 Output: 6
 Explanation: The maximal rectangle is shown in the above picture.
 Example 2:
@@ -49,39 +51,39 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    int maximalRectangle(vector<vector<char>>& matrix) {
-      // 2 pass method
-      // left to right
-      // top to bottom
-      int h = matrix.size();
-      int w = matrix[0].size();
-      vector<vector<int>> ltr(h, vector<int>(w,0));
-      vector<vector<int>> ttb = ltr;
-      for (int i = 0; i < h; ++i) {
-        vector<char> cur_row = matrix[i];
-        for (int j = 0; j < w; ++j) {
-          char val = cur_row[j];
-          int left = (j > 0) ? ltr[i][j-1]:0;
-          if (val == '0') {
-            ltr[i][j] = 0;
-          } else {
-            ltr[i][j] = left+1;
-          }
-        }
-      }
-      int res = 0;
+ public:
+  int maximalRectangle(vector<vector<char>>& matrix) {
+    // 2 pass method
+    // left to right
+    // top to bottom
+    int h = matrix.size();
+    int w = matrix[0].size();
+    vector<vector<int>> ltr(h, vector<int>(w, 0));
+    vector<vector<int>> ttb = ltr;
+    for (int i = 0; i < h; ++i) {
+      vector<char> cur_row = matrix[i];
       for (int j = 0; j < w; ++j) {
-        for (int i = 0; i < h; ++i) {
-          int up = i > 0 ? ttb[i][j]:0;
-          if (matrix[i][j] == '0') {
-            ttb[i][j] = 0;
-          } else {
-            ttb[i][j] = up+1;
-            res = max(res, ttb[i][j]*ltr[i][j]);
-          }
+        char val = cur_row[j];
+        int left = (j > 0) ? ltr[i][j - 1] : 0;
+        if (val == '0') {
+          ltr[i][j] = 0;
+        } else {
+          ltr[i][j] = left + 1;
         }
       }
-      return res;
     }
+    int res = 0;
+    for (int j = 0; j < w; ++j) {
+      for (int i = 0; i < h; ++i) {
+        int up = i > 0 ? ttb[i][j] : 0;
+        if (matrix[i][j] == '0') {
+          ttb[i][j] = 0;
+        } else {
+          ttb[i][j] = up + 1;
+          res = max(res, ttb[i][j] * ltr[i][j]);
+        }
+      }
+    }
+    return res;
+  }
 };
