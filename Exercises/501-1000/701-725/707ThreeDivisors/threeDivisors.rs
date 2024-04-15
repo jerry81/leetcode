e334bbc2a00ab3165f64d37ce86bf16c39a3afe5
Raskolnikov <jerry.tann@endress.com>
2024-04-15 12:06:06 +0800

    ✨ ez 1

---
+++ Exercises/501-1000/701-725/707ThreeDivisors/threeDivisors.rs
@@ -0,0 +1,45 @@
+/*
+
+1952. Three Divisors
+Easy
+Topics
+Companies
+Hint
+Given an integer n, return true if n has exactly three positive divisors. Otherwise, return false.
+
+An integer m is a divisor of n if there exists an integer k such that n = k * m.
+
+
+
+Example 1:
+
+Input: n = 2
+Output: false
+Explantion: 2 has only two divisors: 1 and 2.
+Example 2:
+
+Input: n = 4
+Output: true
+Explantion: 4 has three divisors: 1, 2, and 4.
+
+
+Constraints:
+
+1 <= n <= 104
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+65.1K
+Submissions
+107.7K
+Acceptance Rate
+60.5%
+
+*/
+impl Solution {
+  pub fn is_three(n: i32) -> bool {
+
+  }
+}
