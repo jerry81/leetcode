efbc09c0d0b1672add4c906de8a86ab1b1b400b8
Raskolnikov <jerry.tann@endress.com>
2024-04-17 10:36:29 +0800

    📝

---
+++ Exercises/501-1000/701-725/713SmallestStringStartLeaf/smallestStrStartFromLeaf.cpp
@@ -0,0 +1,81 @@
+/*
+
+988. Smallest String Starting From Leaf
+Medium
+Topics
+Companies
+You are given the root of a binary tree where each node has a value in the range [0, 25] representing the letters 'a' to 'z'.
+
+Return the lexicographically smallest string that starts at a leaf of this tree and ends at the root.
+
+As a reminder, any shorter prefix of a string is lexicographically smaller.
+
+For example, "ab" is lexicographically smaller than "aba".
+A leaf of a node is a node that has no children.
+
+
+
+Example 1:
+
+
+Input: root = [0,1,2,3,4,3,4]
+Output: "dba"
+Example 2:
+
+
+Input: root = [25,1,3,1,3,0,2]
+Output: "adz"
+Example 3:
+
+
+Input: root = [2,2,1,null,1,0,null,0]
+Output: "abc"
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 8500].
+0 <= Node.val <= 25
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+84.2K
+Submissions
+160.8K
+Acceptance Rate
+52.3%
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+#include <string>
+
+using namespace std;
+class Solution {
+public:
+    string smallestFromLeaf(TreeNode* root) {
+
+    }
+};
