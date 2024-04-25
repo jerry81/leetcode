909d73b3ed8885589a2516829d20cbaf31f23670
Raskolnikov <jerry.tann@endress.com>
2024-04-25 14:40:53 +0800

    🚧 attempt

--- Exercises/501-1000/726-750/728LongestIdealSubseq/longestIdealSubseq.cpp
+++ Exercises/501-1000/726-750/728LongestIdealSubseq/longestIdealSubseq.cpp
@@ -59,11 +59,22 @@ int c_diff(char a, char b) {
 int r(string &s, int &k, int &sz, int idx, char last, bool hasLast) {
   if (idx >= sz) return 0;
 
+  char cur = s[idx];
   if (!hasLast) {
-    int take = r(s,k,sz,idx+1,s[idx],true);
+    int take = 1+r(s,k,sz,idx+1,cur,true);
     int leave = r(s,k,sz,idx+1,'a',false);
     return max(take, leave);
+  } else {
+    char cur = s[idx];
+    if (c_diff(cur, last) > k) {
+      return r(s,k,sz,idx+1,last,true);
+    } else {
+      int take = 1+r(s,k,sz,idx+1,cur,true);
+      int leave = r(s,k,sz,idx+1,'a',false);
+      return max(take,leave);
+    }
   }
+
 };
 public:
     int longestIdealString(string s, int k) {
