8cd06645b21ba545527ca917dfe8bd15adb571a7
Raskolnikov <jerry.tann@endress.com>
2024-04-27 12:20:14 +0800

    🚧

--- Exercises/501-1000/726-750/732FreedomTrail/freedomTrail.cpp
+++ Exercises/501-1000/726-750/732FreedomTrail/freedomTrail.cpp
@@ -4,16 +4,28 @@
 Hard
 Topics
 Companies
-In the video game Fallout 4, the quest "Road to Freedom" requires players to reach a metal dial called the "Freedom Trail Ring" and use the dial to spell a specific keyword to open the door.
+In the video game Fallout 4, the quest "Road to Freedom" requires players to
+reach a metal dial called the "Freedom Trail Ring" and use the dial to spell a
+specific keyword to open the door.
 
-Given a string ring that represents the code engraved on the outer ring and another string key that represents the keyword that needs to be spelled, return the minimum number of steps to spell all the characters in the keyword.
+Given a string ring that represents the code engraved on the outer ring and
+another string key that represents the keyword that needs to be spelled, return
+the minimum number of steps to spell all the characters in the keyword.
 
-Initially, the first character of the ring is aligned at the "12:00" direction. You should spell all the characters in key one by one by rotating ring clockwise or anticlockwise to make each character of the string key aligned at the "12:00" direction and then by pressing the center button.
+Initially, the first character of the ring is aligned at the "12:00" direction.
+You should spell all the characters in key one by one by rotating ring clockwise
+or anticlockwise to make each character of the string key aligned at the "12:00"
+direction and then by pressing the center button.
 
 At the stage of rotating the ring to spell the key character key[i]:
 
-You can rotate the ring clockwise or anticlockwise by one place, which counts as one step. The final purpose of the rotation is to align one of ring's characters at the "12:00" direction, where this character must equal key[i].
-If the character key[i] has been aligned at the "12:00" direction, press the center button to spell, which also counts as one step. After the pressing, you could begin to spell the next character in the key (next stage). Otherwise, you have finished all the spelling.
+You can rotate the ring clockwise or anticlockwise by one place, which counts as
+one step. The final purpose of the rotation is to align one of ring's characters
+at the "12:00" direction, where this character must equal key[i]. If the
+character key[i] has been aligned at the "12:00" direction, press the center
+button to spell, which also counts as one step. After the pressing, you could
+begin to spell the next character in the key (next stage). Otherwise, you have
+finished all the spelling.
 
 
 Example 1:
@@ -22,10 +34,10 @@ Example 1:
 Input: ring = "godding", key = "gd"
 Output: 4
 Explanation:
-For the first key character 'g', since it is already in place, we just need 1 step to spell this character.
-For the second key character 'd', we need to rotate the ring "godding" anticlockwise by two steps to make it become "ddinggo".
-Also, we need 1 more step for spelling.
-So the final output is 4.
+For the first key character 'g', since it is already in place, we just need 1
+step to spell this character. For the second key character 'd', we need to
+rotate the ring "godding" anticlockwise by two steps to make it become
+"ddinggo". Also, we need 1 more step for spelling. So the final output is 4.
 Example 2:
 
 Input: ring = "godding", key = "godding"
@@ -50,43 +62,44 @@ Acceptance Rate
 */
 
 #include <string>
-#include <vector>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    int findRotateSteps(string ring, string key) {
-      /*
-      godding
-      (0)g: { g: 0, 1,  o: 1, d: 2, 3 i: 3, n: 2}
-      */
+ public:
+  int findRotateSteps(string ring, string key) {
+    /*
+    godding
+    (0)g: { g: 0, 1,  o: 1, d: 2, 3 i: 3, n: 2}
+    */
 
-     int sz = ring.size();
-     vector<unordered_map<char, vector<pair<int,int>>>> lookup(sz);
-     // precalc distances
-     for (int i = 0; i < sz; ++i) {
-       char cur = ring[i];
-       int dist = 0;
-       pair<int,int> pr = {dist,i};
-       lookup[i][cur].push_back(pr);
-       int addend = 1;
-       int lower=0;
-       int upper=0;
-       while (true) {
-         lower = i - addend;
-         if (lower < 0) lower += sz;
-         cout << "lower " << lower << endl;
-         // process lower
-         if (lower == upper) break;
-         upper = i + addend;
-         if (upper >= sz) upper %= sz;
-         addend+=1;
-         // process upper
-         cout << "upper " << upper << endl;
-         if (lower == upper) break;
-       }
-     }
+    int sz = ring.size();
+    vector<unordered_map<char, vector<pair<int, int>>>> lookup(sz);
+    // precalc distances
+    for (int i = 0; i < sz; ++i) {
+      char cur = ring[i];
+      int dist = 0;
+      pair<int, int> pr = {dist, i};
+      lookup[i][cur].push_back(pr);
+      int addend = 1;
+      int lower = 0;
+      int upper = 0;
+      while (true) {
+        lower = i - addend;
+        if (lower < 0) lower += sz;
+        cout << "lower " << lower << endl;
+        // process lower
+        if (lower == upper) break;
+        upper = i + addend;
+        if (upper >= sz) upper %= sz;
+        addend += 1;
+        // process upper
+        cout << "upper " << upper << endl;
+        if (lower == upper) break;
+      }
     }
+    return 0;
+  }
 };
