146fb8843312877ec82f711399935830412a2a35
Raskolnikov <jerry.tann@endress.com>
2024-05-02 09:43:44 +0800

    🚧 att1

--- Exercises/501-1000/726-750/741LargestExistWithNegative/largestExistWithNegative.rs
+++ Exercises/501-1000/726-750/741LargestExistWithNegative/largestExistWithNegative.rs
@@ -46,9 +46,24 @@ Acceptance Rate
 
 */
 
+use std::collections::HashSet;
+
 impl Solution {
   pub fn find_max_k(nums: Vec<i32>) -> i32 {
-
+    let mut hs: HashSet<i32> = HashSet::new();
+    for n in nums {
+      if n > 0 {
+        hs.insert(n*-1);
+      }
+    }
+    let mut curmin = 1;
+    for k in hs {
+      if k < curmin {
+        curmin = k;
+      }
+    }
+    if curmin > 0 { return -1 }
+    -1*curmin
   }
 }
 
