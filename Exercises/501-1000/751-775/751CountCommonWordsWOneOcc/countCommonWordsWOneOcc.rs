a594cfd9b5c28bb0a840fcfd3a4e96f05b53ca38
Raskolnikov <jerry.tann@endress.com>
2024-05-08 10:51:57 +0800

    🚧

--- Exercises/501-1000/751-775/751CountCommonWordsWOneOcc/countCommonWordsWOneOcc.rs
+++ Exercises/501-1000/751-775/751CountCommonWordsWOneOcc/countCommonWordsWOneOcc.rs
@@ -49,8 +49,30 @@ Acceptance Rate
 
 */
 
+use std::collections::HashMap;
+
 impl Solution {
   pub fn count_words(words1: Vec<String>, words2: Vec<String>) -> i32 {
-
+    // more counting, freq
+    let mut freq1: HashMap<string, i32> = HashMap::new();
+    let mut freq2: HashMap<string, i32> = HashMap::new();
+    for w in words1 {
+      *freq1.entry(w).or_insert(0) +=1;
+    }
+    for w in words2 {
+      *freq2.entry(w).or_insert(0) +=1;
+    }
+    let mut res = 0;
+    for (k,v) in freq1.clone() {
+      match freq2.get(k) {
+        Some(v2) => {
+          if v == 1 && v2 == 1 { res+=1; }
+        }
+        None => {
+          continue
+        }
+      }
+    }
+    res
   }
 }
