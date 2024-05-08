a387a6b3560dcbc26188fbc664140189e013f822
Raskolnikov <jerry.tann@endress.com>
2024-05-08 10:13:34 +0800

    ✨

---
+++ Exercises/501-1000/726-750/748VowelSubstrings/vowelSubstrings.rs
@@ -0,0 +1,61 @@
+/*
+2062. Count Vowel Substrings of a String
+Easy
+Topics
+Companies
+Hint
+A substring is a contiguous (non-empty) sequence of characters within a string.
+
+A vowel substring is a substring that only consists of vowels ('a', 'e', 'i', 'o', and 'u') and has all five vowels present in it.
+
+Given a string word, return the number of vowel substrings in word.
+
+
+
+Example 1:
+
+Input: word = "aeiouu"
+Output: 2
+Explanation: The vowel substrings of word are as follows (underlined):
+- "aeiouu"
+- "aeiouu"
+Example 2:
+
+Input: word = "unicornarihan"
+Output: 0
+Explanation: Not all 5 vowels are present, so there are no vowel substrings.
+Example 3:
+
+Input: word = "cuaieuouac"
+Output: 7
+Explanation: The vowel substrings of word are as follows (underlined):
+- "cuaieuouac"
+- "cuaieuouac"
+- "cuaieuouac"
+- "cuaieuouac"
+- "cuaieuouac"
+- "cuaieuouac"
+- "cuaieuouac"
+
+
+Constraints:
+
+1 <= word.length <= 100
+word consists of lowercase English letters only.
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+39.2K
+Submissions
+58K
+Acceptance Rate
+67.5%
+*/
+
+impl Solution {
+  pub fn count_vowel_substrings(word: String) -> i32 {
+
+  }
+}
