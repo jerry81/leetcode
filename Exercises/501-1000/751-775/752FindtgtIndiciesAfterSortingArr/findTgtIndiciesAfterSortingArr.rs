c7e4c52d03814f702921c03ad6916ddc1ecffa41
Raskolnikov <jerry.tann@endress.com>
2024-05-08 10:57:21 +0800

    🚧 att1

--- Exercises/501-1000/751-775/752FindtgtIndiciesAfterSortingArr/findTgtIndiciesAfterSortingArr.rs
+++ Exercises/501-1000/751-775/752FindtgtIndiciesAfterSortingArr/findTgtIndiciesAfterSortingArr.rs
@@ -50,6 +50,15 @@ Acceptance Rate
 
 impl Solution {
   pub fn target_indices(nums: Vec<i32>, target: i32) -> Vec<i32> {
-
+    let mut sorted = nums.clone();
+    sorted.sort();
+    let sz = nums.len();
+    let mut res: Vec<i32> = vec![];
+    for i in 0..sz {
+      if sorted[i] == target {
+        res.push(i as i32);
+      }
+    }
+    res
   }
 }
