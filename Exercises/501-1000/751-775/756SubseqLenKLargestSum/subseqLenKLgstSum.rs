ca2b224cec231098845c2a56c0931a659df158f9
Raskolnikov <jerry.tann@endress.com>
2024-05-09 13:10:42 +0800

    🚧

--- Exercises/501-1000/751-775/756SubseqLenKLargestSum/subseqLenKLgstSum.rs
+++ Exercises/501-1000/751-775/756SubseqLenKLargestSum/subseqLenKLgstSum.rs
@@ -5,11 +5,16 @@ lazy_static! {
 }
 
 impl Solution {
-  fn r(idx: usize, remain: i32, sz: usize, nums: &Vec<i32>) {
+  fn r(cur: Vec<i32>, idx: usize, remain: i32, sz: usize, nums: &Vec<i32>) {
+    if idx >= sz {
+      let mut all = ALL.lock().unwrap(); // so it's a threading thing
+      all.push(cur.clone());
+      drop(all);
+     }
   }
   pub fn max_subsequence(nums: Vec<i32>, k: i32) -> Vec<i32> {
     let sz = nums.len();
-    r(0, k, sz, &nums);
+    Solution::r(vec![], 0, k, sz, &nums);
     vec![]
   }
 }
