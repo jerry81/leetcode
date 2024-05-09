1a3c33545269fd5460764ac62f8ce9763fdb4aed
Raskolnikov <jerry.tann@endress.com>
2024-05-09 13:34:27 +0800

    🚧 super struggle 2000

--- Exercises/501-1000/751-775/756SubseqLenKLargestSum/subseqLenKLgstSum.rs
+++ Exercises/501-1000/751-775/756SubseqLenKLargestSum/subseqLenKLgstSum.rs
@@ -1,10 +1,22 @@
 impl Solution {
   pub fn max_subsequence(nums: Vec<i32>, k: i32) -> Vec<i32> {
-      let sorted = nums.clone().sort();
-      let mut res = vec![];
-      let sz = sorted.len();
+      let mut sorted: Vec<(i32,usize)> = Vec::new();
+      let sz = nums.len();
+
+      for i in 0..sz {
+        sorted.push((nums[i],i));
+      }
+
+      sorted.sort_by(|a,b| { a.0.cmp(&b.0) });
+      let mut pre_res: Vec<(i32,usize)> = Vec::new();
       for i in 0..k {
-        res.push(sorted[sz-i-1]);
+        pre_res.push(sorted[sz-i-1]);
+      }
+
+      let mut res = vec![];
+      pre_res.sort_by(|a,b| { a.1.cmp(&b.1) });
+      for (v,_) in pre_res {
+        res.push(v);
       }
       res
   }
