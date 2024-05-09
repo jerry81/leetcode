84f7f222605768cb7952def2581219941f3c1b97
Raskolnikov <jerry.tann@endress.com>
2024-05-09 13:13:18 +0800

    🚧

--- Exercises/501-1000/751-775/756SubseqLenKLargestSum/subseqLenKLgstSum.rs
+++ Exercises/501-1000/751-775/756SubseqLenKLargestSum/subseqLenKLgstSum.rs
@@ -6,11 +6,18 @@ lazy_static! {
 
 impl Solution {
   fn r(cur: Vec<i32>, idx: usize, remain: i32, sz: usize, nums: &Vec<i32>) {
-    if idx >= sz {
+    if idx >= sz || remain <= 0 {
       let mut all = ALL.lock().unwrap(); // so it's a threading thing
       all.push(cur.clone());
       drop(all);
-     }
+      return
+    }
+
+    Self::r(cur, idx+1, remain, sz, nums);
+
+    let mut nxt = cur.clone();
+    nxt.push(nums[idx]);
+    Self::r(nxt, idx+1, remain-1, sz, nums);
   }
   pub fn max_subsequence(nums: Vec<i32>, k: i32) -> Vec<i32> {
     let sz = nums.len();
