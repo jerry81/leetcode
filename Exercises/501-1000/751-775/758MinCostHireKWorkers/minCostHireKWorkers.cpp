326d08638ae3d7dba454214279afacd770ad01c3
Raskolnikov <jerry.tann@endress.com>
2024-05-11 16:27:39 +0800

    🚧 TIL: double max

--- Exercises/501-1000/751-775/758MinCostHireKWorkers/minCostHireKWorkers.cpp
+++ Exercises/501-1000/751-775/758MinCostHireKWorkers/minCostHireKWorkers.cpp
@@ -4,13 +4,20 @@
 Hard
 Topics
 Companies
-There are n workers. You are given two integer arrays quality and wage where quality[i] is the quality of the ith worker and wage[i] is the minimum wage expectation for the ith worker.
+There are n workers. You are given two integer arrays quality and wage where
+quality[i] is the quality of the ith worker and wage[i] is the minimum wage
+expectation for the ith worker.
 
-We want to hire exactly k workers to form a paid group. To hire a group of k workers, we must pay them according to the following rules:
+We want to hire exactly k workers to form a paid group. To hire a group of k
+workers, we must pay them according to the following rules:
 
-Every worker in the paid group must be paid at least their minimum wage expectation.
-In the group, each worker's pay must be directly proportional to their quality. This means if a worker’s quality is double that of another worker in the group, then they must be paid twice as much as the other worker.
-Given the integer k, return the least amount of money needed to form a paid group satisfying the above conditions. Answers within 10-5 of the actual answer will be accepted.
+Every worker in the paid group must be paid at least their minimum wage
+expectation. In the group, each worker's pay must be directly proportional to
+their quality. This means if a worker’s quality is double that of another worker
+in the group, then they must be paid twice as much as the other worker. Given
+the integer k, return the least amount of money needed to form a paid group
+satisfying the above conditions. Answers within 10-5 of the actual answer will
+be accepted.
 
 
 
@@ -44,21 +51,25 @@ Acceptance Rate
 
 */
 
-#include <vector>
 #include <queue>
+#include <vector>
+#include <limits>
 
 using namespace std;
 
 class Solution {
-public:
-    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
-      vector<pair<double, int>> wage_quality;
+ public:
+  double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+    vector<pair<double, int>> wage_quality;
 
-      for (int i = 0; i < quality.size(); ++i) {
-        int q = quality[i];
-        double ratio = (double)wage[i] / (double)q;
-        wage_quality.push_back({ratio, q});
-      }
-      return 0.0;
+    for (int i = 0; i < quality.size(); ++i) {
+      int q = quality[i];
+      double ratio = (double)wage[i] / (double)q;
+      wage_quality.push_back({ratio, q});
     }
+
+    priority_queue<int> pq;
+    double res = numeric_limits<double>::max();
+    return res;
+  }
 };
