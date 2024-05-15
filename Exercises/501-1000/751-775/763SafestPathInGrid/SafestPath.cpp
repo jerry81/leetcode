fa3d8afad2af3c7ee0b2254332bae56f8a24200b
Raskolnikov <jerry.tann@endress.com>
2024-05-15 10:38:48 +0800

    ⬆️ new snippet

--- Exercises/501-1000/751-775/763SafestPathInGrid/SafestPath.cpp
+++ Exercises/501-1000/751-775/763SafestPathInGrid/SafestPath.cpp
@@ -9,15 +9,19 @@ You are given a 0-indexed 2D matrix grid of size n x n, where (r, c) represents:
 
 A cell containing a thief if grid[r][c] = 1
 An empty cell if grid[r][c] = 0
-You are initially positioned at cell (0, 0). In one move, you can move to any adjacent cell in the grid, including cells containing thieves.
+You are initially positioned at cell (0, 0). In one move, you can move to any
+adjacent cell in the grid, including cells containing thieves.
 
-The safeness factor of a path on the grid is defined as the minimum manhattan distance from any cell in the path to any thief in the grid.
+The safeness factor of a path on the grid is defined as the minimum manhattan
+distance from any cell in the path to any thief in the grid.
 
 Return the maximum safeness factor of all paths leading to cell (n - 1, n - 1).
 
-An adjacent cell of cell (r, c), is one of the cells (r, c + 1), (r, c - 1), (r + 1, c) and (r - 1, c) if it exists.
+An adjacent cell of cell (r, c), is one of the cells (r, c + 1), (r, c - 1), (r
++ 1, c) and (r - 1, c) if it exists.
 
-The Manhattan distance between two cells (a, b) and (x, y) is equal to |a - x| + |b - y|, where |val| denotes the absolute value of val.
+The Manhattan distance between two cells (a, b) and (x, y) is equal to |a - x| +
+|b - y|, where |val| denotes the absolute value of val.
 
 
 
@@ -26,24 +30,28 @@ Example 1:
 
 Input: grid = [[1,0,0],[0,0,0],[0,0,1]]
 Output: 0
-Explanation: All paths from (0, 0) to (n - 1, n - 1) go through the thieves in cells (0, 0) and (n - 1, n - 1).
-Example 2:
+Explanation: All paths from (0, 0) to (n - 1, n - 1) go through the thieves in
+cells (0, 0) and (n - 1, n - 1). Example 2:
 
 
 Input: grid = [[0,0,1],[0,0,0],[0,0,0]]
 Output: 2
-Explanation: The path depicted in the picture above has a safeness factor of 2 since:
-- The closest cell of the path to the thief at cell (0, 2) is cell (0, 0). The distance between them is | 0 - 0 | + | 0 - 2 | = 2.
-It can be shown that there are no other paths with a higher safeness factor.
-Example 3:
+Explanation: The path depicted in the picture above has a safeness factor of 2
+since:
+- The closest cell of the path to the thief at cell (0, 2) is cell (0, 0). The
+distance between them is | 0 - 0 | + | 0 - 2 | = 2. It can be shown that there
+are no other paths with a higher safeness factor. Example 3:
 
 
 Input: grid = [[0,0,0,1],[0,0,0,0],[0,0,0,0],[1,0,0,0]]
 Output: 2
-Explanation: The path depicted in the picture above has a safeness factor of 2 since:
-- The closest cell of the path to the thief at cell (0, 3) is cell (1, 2). The distance between them is | 0 - 1 | + | 3 - 2 | = 2.
-- The closest cell of the path to the thief at cell (3, 0) is cell (3, 2). The distance between them is | 3 - 3 | + | 0 - 2 | = 2.
-It can be shown that there are no other paths with a higher safeness factor.
+Explanation: The path depicted in the picture above has a safeness factor of 2
+since:
+- The closest cell of the path to the thief at cell (0, 3) is cell (1, 2). The
+distance between them is | 0 - 1 | + | 3 - 2 | = 2.
+- The closest cell of the path to the thief at cell (3, 0) is cell (3, 2). The
+distance between them is | 3 - 3 | + | 0 - 2 | = 2. It can be shown that there
+are no other paths with a higher safeness factor.
 
 
 Constraints:
@@ -65,21 +73,24 @@ Acceptance Rate
 
 */
 
-#include <vector>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    int maximumSafenessFactor(vector<vector<int>>& grid) {
-      // brute force?
-      // bfs from each theif to find safeness of grid
-      // with caveat.  bfs and start from all theives
-      // WRONG -> dfs or bfs to find all paths from 0,0 to sz,sz
+  const vector<vector<int>> NEIGHBORS = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 
+ public:
+  int maximumSafenessFactor(vector<vector<int>>& grid) {
+    // brute force?
+    // bfs from each theif to find safeness of grid
+    // with caveat.  bfs and start from all theives
+    // WRONG -> dfs or bfs to find all paths from 0,0 to sz,sz
+    int h = grid.size();
+    int w = grid[0].size();
 
-      // CORRECT: dijkstra
-      // DIJKSTRA REVIEW
-    }
+    // CORRECT: dijkstra
+    // DIJKSTRA REVIEW
+  }
 };
---
+++ utils/neighbors.cpp
@@ -0,0 +1,1 @@
+const vector<vector<int>> NEIGHBORS = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
