79b926ff5e87b448ab055925d87ece92f6c52630
Raskolnikov <jerry.tann@endress.com>
2024-05-21 11:00:58 +0800

    🐛 edge

--- Exercises/501-1000/776-800/780OpsToObtainZero/opsObtainZero.rs
+++ Exercises/501-1000/776-800/780OpsToObtainZero/opsObtainZero.rs
@@ -58,6 +58,8 @@ Acceptance Rate
 impl Solution {
   pub fn count_operations(num1: i32, num2: i32) -> i32 {
     // recursion
+    // edge
+    if num1 == 0 || num2 == 0 { return 0 }
     if num1 > num2 { return 1+Solution::count_operations(num1-num2, num2) }
     if num2 > num1 { return 1+Solution::count_operations(num1,num2-num1) }
     if num1 == num2 { return 1 }
