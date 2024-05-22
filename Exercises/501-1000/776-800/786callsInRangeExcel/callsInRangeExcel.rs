bc828824c2accdf17a70f3be211d5df5fdafe03c
Raskolnikov <jerry.tann@endress.com>
2024-05-22 11:56:54 +0800

    🎨

--- Exercises/501-1000/776-800/786callsInRangeExcel/callsInRangeExcel.rs
+++ Exercises/501-1000/776-800/786callsInRangeExcel/callsInRangeExcel.rs
@@ -56,20 +56,15 @@ impl Solution {
       let end_c = s.chars().nth(3).unwrap();
       let start_r = s.chars().nth(1).unwrap();
       let end_r = s.chars().nth(4).unwrap();
-      // col outside
-      let mut res = vec![];
-      let mut cur_c = start_c;
 
-      while cur_c <= end_c {
-        let mut cur_r = start_r;
-        while cur_r <= end_r {
-          let s:String = String::new();
-          res.push(format!("{}{}", cur_c, cur_r));
-          cur_r=(1 as u8 + cur_r as u8) as char;
-        }
-        cur_c=(1 as u8 + cur_c as u8) as char;
+      let mut res = Vec::new();
+
+      for cur_c in start_c..=end_c { // das geht
+          for cur_r in start_r..=end_r {
+              res.push(format!("{}{}", cur_c, cur_r)); // format! review
+          }
       }
-      // row inside
+
       res
   }
 }
