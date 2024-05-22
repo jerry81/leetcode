53c45fe67222a86f2c31ce43cfbc1c6497540df5
Raskolnikov <jerry.tann@endress.com>
2024-05-22 10:22:46 +0800

    ✨

---
+++ Exercises/501-1000/776-800/781PalindromePartition/palindromePartition.cpp
@@ -0,0 +1,53 @@
+/*
+
+131. Palindrome Partitioning
+Medium
+Topics
+Companies
+Given a string s, partition s such that every
+substring
+ of the partition is a
+palindrome
+. Return all possible palindrome partitioning of s.
+
+
+
+Example 1:
+
+Input: s = "aab"
+Output: [["a","a","b"],["aa","b"]]
+Example 2:
+
+Input: s = "a"
+Output: [["a"]]
+
+
+Constraints:
+
+1 <= s.length <= 16
+s contains only lowercase English letters.
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+808.2K
+Submissions
+1.2M
+Acceptance Rate
+67.8%
+
+*/
+
+#include <vector>
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+
+    }
+};
