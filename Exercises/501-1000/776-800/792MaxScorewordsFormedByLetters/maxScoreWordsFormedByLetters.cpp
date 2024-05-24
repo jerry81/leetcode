638d73976c1707d6b5cfc63deb37cb0e83ca061a
Raskolnikov <jerry.tann@endress.com>
2024-05-24 11:33:51 +0800

    🚧

--- Exercises/501-1000/776-800/792MaxScorewordsFormedByLetters/maxScoreWordsFormedByLetters.cpp
+++ Exercises/501-1000/776-800/792MaxScorewordsFormedByLetters/maxScoreWordsFormedByLetters.cpp
@@ -5,35 +5,36 @@ Hard
 Topics
 Companies
 Hint
-Given a list of words, list of  single letters (might be repeating) and score of every character.
+Given a list of words, list of  single letters (might be repeating) and score of
+every character.
 
-Return the maximum score of any valid set of words formed by using the given letters (words[i] cannot be used two or more times).
+Return the maximum score of any valid set of words formed by using the given
+letters (words[i] cannot be used two or more times).
 
-It is not necessary to use all characters in letters and each letter can only be used once. Score of letters 'a', 'b', 'c', ... ,'z' is given by score[0], score[1], ... , score[25] respectively.
+It is not necessary to use all characters in letters and each letter can only be
+used once. Score of letters 'a', 'b', 'c', ... ,'z' is given by score[0],
+score[1], ... , score[25] respectively.
 
 
 
 Example 1:
 
-Input: words = ["dog","cat","dad","good"], letters = ["a","a","c","d","d","d","g","o","o"], score = [1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0]
-Output: 23
-Explanation:
+Input: words = ["dog","cat","dad","good"], letters =
+["a","a","c","d","d","d","g","o","o"], score =
+[1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0] Output: 23 Explanation:
 Score  a=1, c=9, d=5, g=3, o=2
-Given letters, we can form the words "dad" (5+1+5) and "good" (3+2+2+5) with a score of 23.
-Words "dad" and "dog" only get a score of 21.
-Example 2:
+Given letters, we can form the words "dad" (5+1+5) and "good" (3+2+2+5) with a
+score of 23. Words "dad" and "dog" only get a score of 21. Example 2:
 
-Input: words = ["xxxz","ax","bx","cx"], letters = ["z","a","b","c","x","x","x"], score = [4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,0,10]
-Output: 27
+Input: words = ["xxxz","ax","bx","cx"], letters = ["z","a","b","c","x","x","x"],
+score = [4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,0,10] Output: 27
 Explanation:
 Score  a=4, b=4, c=4, x=5, z=10
-Given letters, we can form the words "ax" (4+5), "bx" (4+5) and "cx" (4+5) with a score of 27.
-Word "xxxz" only get a score of 25.
-Example 3:
+Given letters, we can form the words "ax" (4+5), "bx" (4+5) and "cx" (4+5) with
+a score of 27. Word "xxxz" only get a score of 25. Example 3:
 
-Input: words = ["leetcode"], letters = ["l","e","t","c","o","d"], score = [0,0,1,1,1,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0]
-Output: 0
-Explanation:
+Input: words = ["leetcode"], letters = ["l","e","t","c","o","d"], score =
+[0,0,1,1,1,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0] Output: 0 Explanation:
 Letter "e" can only be used once.
 
 
@@ -59,25 +60,40 @@ Acceptance Rate
 
 */
 
-#include <vector>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
-      // score is score[char - 'a']
-      // unordered_map for chars remaining
-      unordered_map<char, int> freq;
-      for (char c: letters) {
-        if (freq.find(c) != freq.end()) {
-          freq[c] += 1;
+ public:
+  int maxScoreWords(vector<string>& words, vector<char>& letters,
+                    vector<int>& score) {
+    // score is score[char - 'a']
+    // unordered_map for chars remaining
+    unordered_map<char, int> freq;
+    for (char c : letters) {
+      if (freq.find(c) != freq.end()) {
+        freq[c] += 1;
+      } else {
+        freq[c] = 1;
+      }
+    }
+    // map for each word?
+    int word_sz = words.size();
+    vector<unordered_map<char, int>> word_freq(word_sz);
+    for (int i = 0; i < word_sz; ++i) {
+      for (char c : words[i]) {
+        auto wfreq = word_freq[i];
+        if (wfreq.find(c) != wfreq.end()) {
+          wfreq[c] += 1;
         } else {
-          freq[c] = 1;
+          wfreq[c] = 1;
         }
       }
-      // map for each word?
     }
+    // order words by value and greedy?
+    return 0;
+  }
 };
