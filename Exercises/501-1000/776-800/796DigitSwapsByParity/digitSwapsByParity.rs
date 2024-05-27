7637127a10d99e250a533117120386eefbe3ff88
Raskolnikov <jerry.tann@endress.com>
2024-05-27 14:08:30 +0800

    📝

--- Exercises/501-1000/776-800/796DigitSwapsByParity/digitSwapsByParity.rs
+++ Exercises/501-1000/776-800/796DigitSwapsByParity/digitSwapsByParity.rs
@@ -72,7 +72,7 @@ impl Solution {
     let multiplier = 1;
     for i in 0..sz {
       let idx: i32 = (sz-1) as i32 - i as i32;
-      res+=iv[idx as usize] * 10_i32.pow(i as u32); // TIL: crazy _i32 syntax
+      res+=iv[idx as usize] * 10_i32.pow(i as u32); // TIL: crazy _i32 syntax - known as a type suffix, lets compiler know
     }
     res
   }
