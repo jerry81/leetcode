9001046b493f621f9f6998a038e289fadfd20eee
Raskolnikov <jerry.tann@endress.com>
2024-05-29 12:14:43 +0800

    🐛 precision weirdness

---
+++ Exercises/501-1000/801-825/807PercentLetterInStr/percentLetterInStr.rs
@@ -0,0 +1,52 @@
+/*
+2278. Percentage of Letter in String
+Easy
+Topics
+Companies
+Hint
+Given a string s and a character letter, return the percentage of characters in s that equal letter rounded down to the nearest whole percent.
+
+
+
+Example 1:
+
+Input: s = "foobar", letter = "o"
+Output: 33
+Explanation:
+The percentage of characters in s that equal the letter 'o' is 2 / 6 * 100% = 33% when rounded down, so we return 33.
+Example 2:
+
+Input: s = "jjjj", letter = "k"
+Output: 0
+Explanation:
+The percentage of characters in s that equal the letter 'k' is 0%, so we return 0.
+
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of lowercase English letters.
+letter is a lowercase English letter.
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+72K
+Submissions
+97.4K
+Acceptance Rate
+74.0%
+*/
+
+impl Solution {
+  pub fn percentage_letter(s: String, letter: char) -> i32 {
+    let mut cnt = 0;
+    for c in s.chars() {
+      if c == letter { cnt+=1; }
+    }
+    let sz = s.len();
+    let fl: f32 = (cnt as f32 * 100.0) / sz as f32;
+    fl as i32
+  }
+}
