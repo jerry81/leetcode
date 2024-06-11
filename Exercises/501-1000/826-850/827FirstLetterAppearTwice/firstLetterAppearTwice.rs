ffa1d15e30a76122092e6457e1c047cae88d7c22
Raskolnikov <jerry.tann@endress.com>
2024-06-11 18:07:32 +0800

    ✅

--- Exercises/501-1000/826-850/827FirstLetterAppearTwice/firstLetterAppearTwice.rs
+++ Exercises/501-1000/826-850/827FirstLetterAppearTwice/firstLetterAppearTwice.rs
@@ -54,9 +54,10 @@ use std::collections::HashSet;
 impl Solution {
   pub fn repeated_character(s: String) -> char {
     let mut hs: HashSet<char> = HashSet::new();
-    for c in s {
+    for c in s.chars() {
       if hs.contains(&c) { return c }
-      hs.insert(&c);
+      hs.insert(c);
     }
+    'a'
   }
 }
