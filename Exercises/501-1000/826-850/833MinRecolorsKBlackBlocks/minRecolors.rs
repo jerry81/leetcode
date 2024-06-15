32f9a6611f0d4e029b7484caf59207f9c8569e03
Raskolnikov <jerry.tann@endress.com>
2024-06-15 16:35:04 +0800

    ✅

--- Exercises/501-1000/826-850/833MinRecolorsKBlackBlocks/minRecolors.rs
+++ Exercises/501-1000/826-850/833MinRecolorsKBlackBlocks/minRecolors.rs
@@ -1,10 +1,10 @@
 impl Solution {
   pub fn minimum_recolors(blocks: String, k: i32) -> i32 {
-    let mut left = 0;
-    let mut right = k;
+    let mut left = 0 as usize;
+    let mut right = k as usize;
     let mut wcount = 0;
     for i in left..right {
-      if blocks.chars().ith(i) == 'W' {
+      if blocks.chars().nth(i).unwrap() == 'W' {
         wcount+=1;
       }
     }
@@ -12,10 +12,10 @@ impl Solution {
     let mut res = wcount;
 
     while right < sz {
-      if blocks.chars().ith(left) == 'W' {
+      if blocks.chars().nth(left).unwrap() == 'W' {
         wcount-=1;
       }
-      if blocks.chars().ith(right) == 'W' {
+      if blocks.chars().nth(right).unwrap() == 'W' {
         wcount+=1;
       }
       res = res.min(wcount);
