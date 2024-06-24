346fbc971e36a58c774f8112107d22c6627f099c
Raskolnikov <jerry.tann@endress.com>
2024-06-24 10:52:30 +0800

    ✅ geniuses out there

--- Exercises/501-1000/826-850/834CountNiceSubarrays/countNiceSubarr.cpp
+++ Exercises/501-1000/826-850/834CountNiceSubarrays/countNiceSubarr.cpp
@@ -6,6 +6,17 @@ using namespace std;
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-
+      unordered_map<int, int> counts;
+      int res = 0;
+      int cur = 0;
+      counts[0] = 1;
+      for (int n: nums) {
+        if (n%2 == 1) cur++;
+        if (counts.find(cur-k) != counts.end()) {
+          res+=counts[cur-k];
+        }
+        counts[cur]++;
+      }
+      return res;
     }
 };
