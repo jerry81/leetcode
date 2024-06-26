f8c90077f3399ac7aea1865e1ae78397b804c5b5
Raskolnikov <jerry.tann@endress.com>
2024-06-26 12:30:41 +0800

    ♻️ refactor 1: inorder traversal of bsearchtree gives sorted list.

---
+++ Exercises/501-1000/826-850/839BalanceBsearchTree/refactor.cpp
@@ -0,0 +1,107 @@
+/*
+
+1382. Balance a Binary Search Tree
+Medium
+Topics
+Companies
+Hint
+Given the root of a binary search tree, return a balanced binary search tree
+with the same node values. If there is more than one answer, return any of them.
+
+A binary search tree is balanced if the depth of the two subtrees of every node
+never differs by more than 1.
+
+
+
+Example 1:
+
+
+Input: root = [1,null,2,null,3,null,4,null,null]
+Output: [2,1,3,null,null,null,4]
+Explanation: This is not the only correct answer, [3,1,4,null,2] is also
+correct. Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,1,3]
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+1 <= Node.val <= 105
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+166.6K
+Submissions
+202.7K
+Acceptance Rate
+82.2%
+
+*/
+
+#include <vector>
+
+using namespace std;
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+  vector<int> items;
+  TreeNode *res = nullptr;
+  void r(TreeNode *root) {
+    if (!root) return;
+
+    r(root->left);
+    items.push_back(root->val);
+    r(root->right);
+  }
+
+  void build(TreeNode *parent, bool left,vector<int> remain) {
+    if (remain.empty()) return;
+
+
+    int sz = remain.size();
+    // mid idx
+    int mididx = sz / 2;
+    int nwval = remain[mididx];
+    TreeNode *cur = new TreeNode(nwval);
+
+
+    if (parent == nullptr && res == nullptr) {
+      res = cur;
+    } else {
+      if (left) {
+        parent->left = cur;
+      } else {
+        parent->right = cur;
+      }
+    }
+    if (remain.size() == 1) return;
+    // subseq left
+    vector<int> l(remain.begin(), remain.begin() + mididx);
+    build(cur, true, l);
+    // subseq right
+    vector<int> r(remain.begin() + mididx+1, remain.end());
+    build(cur, false, r);
+  }
+
+ public:
+  TreeNode *balanceBST(TreeNode *root) {
+    // same technique as previously - extract the elements first
+    r(root);
+    // recursively build new tree
+    build(nullptr, false, items);
+    return res;
+  }
+};
