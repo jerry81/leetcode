2b7143d206b69c145a4f52e2e356339732c61f37
Raskolnikov <jerry.tann@endress.com>
2024-06-27 16:28:01 +0800

    🚧 k

--- Exercises/501-1000/826-850/843CountDaysTogether/countDaysTogether.rs
+++ Exercises/501-1000/826-850/843CountDaysTogether/countDaysTogether.rs
@@ -46,13 +46,19 @@ Acceptance Rate
 */
 
 impl Solution {
+
   pub fn count_days_together(arrive_alice: String, leave_alice: String, arrive_bob: String, leave_bob: String) -> i32 {
-    let months:Vec<i32> = vec![31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    let (aam, aad) = Solution::parse_date(arrive_alice);
-    let (lam, lad) = Solution::parse_date(leave_alice);
-    let (abm, abd) = Solution::parse_date(arrive_bob);
-    let (lbm, lbd) = Solution::parse_date(leave_bob);
+    let (aam, aad) = Solution::parse_date(&arrive_alice);
+    let (lam, lad) = Solution::parse_date(&leave_alice);
+    let (abm, abd) = Solution::parse_date(&arrive_bob);
+    let (lbm, lbd) = Solution::parse_date(&leave_bob);
+    println!("{} is the {}th day ", aam, Solution::month_to_day_of_year(aam));
     0
+  }
+
+  fn month_to_day_of_year(m: i32) -> i32 {
+    let months:Vec<i32> = vec![31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    months[0..m].iter().sum()
   }
 
   fn parse_date(date: &str) -> (i32, i32) { // TIL tuple
