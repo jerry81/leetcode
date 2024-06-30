c6c6b0644dde1bf92e27d072f162ce0a29fb3c37
Raskolnikov <jerry.tann@endress.com>
2024-06-30 12:11:52 +0800

    🚧 unionFind, a review

---
+++ Exercises/501-1000/826-850/847RemoveMaxEdgesKeepTraversable/removeMaxEdgesKeepTraversible.cpp
@@ -0,0 +1,149 @@
+/*
+
+1579. Remove Max Number of Edges to Keep Graph Fully Traversable
+Hard
+Topics
+Companies
+Hint
+Alice and Bob have an undirected graph of n nodes and three types of edges:
+
+Type 1: Can be traversed by Alice only.
+Type 2: Can be traversed by Bob only.
+Type 3: Can be traversed by both Alice and Bob.
+Given an array edges where edges[i] = [typei, ui, vi] represents a bidirectional
+edge of type typei between nodes ui and vi, find the maximum number of edges you
+can remove so that after removing the edges, the graph can still be fully
+traversed by both Alice and Bob. The graph is fully traversed by Alice and Bob
+if starting from any node, they can reach all other nodes.
+
+Return the maximum number of edges you can remove, or return -1 if Alice and Bob
+cannot fully traverse the graph.
+
+
+
+Example 1:
+
+
+
+Input: n = 4, edges = [[3,1,2],[3,2,3],[1,1,3],[1,2,4],[1,1,2],[2,3,4]]
+Output: 2
+Explanation: If we remove the 2 edges [1,1,2] and [1,1,3]. The graph will still
+be fully traversable by Alice and Bob. Removing any additional edge will not
+make it so. So the maximum number of edges we can remove is 2. Example 2:
+
+
+
+Input: n = 4, edges = [[3,1,2],[3,2,3],[1,1,4],[2,1,4]]
+Output: 0
+Explanation: Notice that removing any edge will not make the graph fully
+traversable by Alice and Bob. Example 3:
+
+
+
+Input: n = 4, edges = [[3,2,3],[1,1,2],[2,3,4]]
+Output: -1
+Explanation: In the current graph, Alice cannot reach node 4 from the other
+nodes. Likewise, Bob cannot reach 1. Therefore it's impossible to make the graph
+fully traversable.
+
+
+
+
+Constraints:
+
+1 <= n <= 105
+1 <= edges.length <= min(105, 3 * n * (n - 1) / 2)
+edges[i].length == 3
+1 <= typei <= 3
+1 <= ui < vi <= n
+All tuples (typei, ui, vi) are distinct.
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+64.4K
+Submissions
+99K
+Acceptance Rate
+65.1%
+
+*/
+
+#include <vector>
+
+using namespace std;
+
+struct UnionFind {
+  vector<int> parent;
+
+ public:
+  *UnionFind(int n) {
+    parent.resize(n);
+    for (int i = 0; i < n; ++i) {
+      parent[i] = i;
+    }
+  }
+
+  int f(int i) {
+    if (parent[i] == i) return i;
+
+    return f(parent[i]);
+  };
+
+  void u(int a, int b) {
+    int pa = f(a);
+    int pb = f(b);
+    if (pa != pb) {
+      parent[b] = pa;
+    }
+  }
+};
+
+class Solution {
+ public:
+  int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
+    UnionFind *alice = new UnionFind(n);
+    UnionFind *bob = new UnionFind(n);
+    for (vector<int> edge: edges) {
+      int type = edge[0];
+      int n1 = edge[1];
+      int n2 = edge[2];
+      if (type == 3) {
+        alice->u(n1,n2);
+        bob->u(n1,n2);
+      } else if (type == 1) {
+        alice->u(n1,n2);
+      } else {
+        bob->u(n1,n2);
+      }
+    }
+  }
+};
+
+// example 1
+// in-degree A
+// 1: 2
+// 2: 4
+// 3: 2
+// 4: 1
+/*
+B
+1:  1
+2:  2
+3:  2
+4:  1
+
+1
+2
+3
+4
+*/
+
+/*
+  min in-degree A/B
+  1: 2
+  2: 1
+  3: 1
+  4: 1 - counting in degrees not enough
+*/
