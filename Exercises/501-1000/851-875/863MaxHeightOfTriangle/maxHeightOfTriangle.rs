9503687d056e863c0045c6983d2c1f650cfc8b4e
Raskolnikov <jerry.tann@endress.com>
2024-07-05 15:03:18 +0800

    ✨

---
+++ Exercises/501-1000/851-875/863MaxHeightOfTriangle/maxHeightOfTriangle.rs
@@ -0,0 +1,78 @@
+/*
+
+3200. Maximum Height of a Triangle
+Easy
+Topics
+Companies
+Hint
+You are given two integers red and blue representing the count of red and blue colored balls. You have to arrange these balls to form a triangle such that the 1st row will have 1 ball, the 2nd row will have 2 balls, the 3rd row will have 3 balls, and so on.
+
+All the balls in a particular row should be the same color, and adjacent rows should have different colors.
+
+Return the maximum height of the triangle that can be achieved.
+
+
+
+Example 1:
+
+Input: red = 2, blue = 4
+
+Output: 3
+
+Explanation:
+
+
+
+The only possible arrangement is shown above.
+
+Example 2:
+
+Input: red = 2, blue = 1
+
+Output: 2
+
+Explanation:
+
+
+The only possible arrangement is shown above.
+
+Example 3:
+
+Input: red = 1, blue = 1
+
+Output: 1
+
+Example 4:
+
+Input: red = 10, blue = 1
+
+Output: 2
+
+Explanation:
+
+
+The only possible arrangement is shown above.
+
+
+
+Constraints:
+
+1 <= red, blue <= 100
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+29K
+Submissions
+70.6K
+Acceptance Rate
+41.0%
+
+*/
+
+impl Solution {
+  pub fn max_height_of_triangle(red: i32, blue: i32) -> i32 {
+
+  }
+}
