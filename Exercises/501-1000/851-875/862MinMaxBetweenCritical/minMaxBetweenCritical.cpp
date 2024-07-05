766f8dc56b4662b9c4057a5a4b3c33347a00491b
Raskolnikov <jerry.tann@endress.com>
2024-07-05 13:39:41 +0800

    🚧

--- Exercises/501-1000/851-875/862MinMaxBetweenCritical/minMaxBetweenCritical.cpp
+++ Exercises/501-1000/851-875/862MinMaxBetweenCritical/minMaxBetweenCritical.cpp
@@ -5,15 +5,22 @@ Medium
 Topics
 Companies
 Hint
-A critical point in a linked list is defined as either a local maxima or a local minima.
+A critical point in a linked list is defined as either a local maxima or a local
+minima.
 
-A node is a local maxima if the current node has a value strictly greater than the previous node and the next node.
+A node is a local maxima if the current node has a value strictly greater than
+the previous node and the next node.
 
-A node is a local minima if the current node has a value strictly smaller than the previous node and the next node.
+A node is a local minima if the current node has a value strictly smaller than
+the previous node and the next node.
 
-Note that a node can only be a local maxima/minima if there exists both a previous node and a next node.
+Note that a node can only be a local maxima/minima if there exists both a
+previous node and a next node.
 
-Given a linked list head, return an array of length 2 containing [minDistance, maxDistance] where minDistance is the minimum distance between any two distinct critical points and maxDistance is the maximum distance between any two distinct critical points. If there are fewer than two critical points, return [-1, -1].
+Given a linked list head, return an array of length 2 containing [minDistance,
+maxDistance] where minDistance is the minimum distance between any two distinct
+critical points and maxDistance is the maximum distance between any two distinct
+critical points. If there are fewer than two critical points, return [-1, -1].
 
 
 
@@ -29,22 +36,25 @@ Example 2:
 Input: head = [5,3,1,2,5,1,2]
 Output: [1,3]
 Explanation: There are three critical points:
-- [5,3,1,2,5,1,2]: The third node is a local minima because 1 is less than 3 and 2.
-- [5,3,1,2,5,1,2]: The fifth node is a local maxima because 5 is greater than 2 and 1.
-- [5,3,1,2,5,1,2]: The sixth node is a local minima because 1 is less than 5 and 2.
-The minimum distance is between the fifth and the sixth node. minDistance = 6 - 5 = 1.
-The maximum distance is between the third and the sixth node. maxDistance = 6 - 3 = 3.
-Example 3:
+- [5,3,1,2,5,1,2]: The third node is a local minima because 1 is less than 3
+and 2.
+- [5,3,1,2,5,1,2]: The fifth node is a local maxima because 5 is greater than 2
+and 1.
+- [5,3,1,2,5,1,2]: The sixth node is a local minima because 1 is less than 5
+and 2. The minimum distance is between the fifth and the sixth node. minDistance
+= 6 - 5 = 1. The maximum distance is between the third and the sixth node.
+maxDistance = 6 - 3 = 3. Example 3:
 
 
 Input: head = [1,3,2,2,3,2,2,2,7]
 Output: [3,3]
 Explanation: There are two critical points:
-- [1,3,2,2,3,2,2,2,7]: The second node is a local maxima because 3 is greater than 1 and 2.
-- [1,3,2,2,3,2,2,2,7]: The fifth node is a local maxima because 3 is greater than 2 and 2.
-Both the minimum and maximum distances are between the second and the fifth node.
-Thus, minDistance and maxDistance is 5 - 2 = 3.
-Note that the last node is not considered a local maxima because it does not have a next node.
+- [1,3,2,2,3,2,2,2,7]: The second node is a local maxima because 3 is greater
+than 1 and 2.
+- [1,3,2,2,3,2,2,2,7]: The fifth node is a local maxima because 3 is greater
+than 2 and 2. Both the minimum and maximum distances are between the second and
+the fifth node. Thus, minDistance and maxDistance is 5 - 2 = 3. Note that the
+last node is not considered a local maxima because it does not have a next node.
 
 
 Constraints:
@@ -72,10 +82,30 @@ Acceptance Rate
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
- */
-class Solution {
-public:
-    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+ *
+ **/
 
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> nodesBetweenCriticalPoints(ListNode *head) {
+    // drop critical points into a vector
+    vector<int> as_v;
+    while (head != nullptr) {
+      as_v.push_back(head->val);
+      head = head->next;
     }
+    return as_v;
+  }
 };
