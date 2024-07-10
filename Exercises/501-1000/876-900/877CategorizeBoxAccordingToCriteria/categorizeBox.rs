668e76bf7aa70d512060565cf2d1c8d720fe6d97
Raskolnikov <jerry.tann@endress.com>
2024-07-10 13:00:17 +0800

    🚧 att1, trap incoming

--- Exercises/501-1000/876-900/877CategorizeBoxAccordingToCriteria/categorizeBox.rs
+++ Exercises/501-1000/876-900/877CategorizeBoxAccordingToCriteria/categorizeBox.rs
@@ -58,6 +58,16 @@ Acceptance Rate
 
 impl Solution {
   pub fn categorize_box(length: i32, width: i32, height: i32, mass: i32) -> String {
+    let is_bulky_dim = length >= 10_000 || width >= 10_000 || height >= 10000;
+    let is_bulky_vol = length * width * height >= 1_000_000_000;
+    let is_bulky = is_bulky_dim || is_bulky_vol;
+    let is_heavy = mass >= 100;
+    if is_heavy && is_bulky { return "Both" }
 
+    if !is_heavy && !is_bulky { return "Neither" }
+
+    if is_bulky && !is_heavy { return "Bulky" }
+
+    "Heavy"
   }
 }
