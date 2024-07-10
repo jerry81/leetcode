bc700419b25cee6f3bc28930401572681637020e
Raskolnikov <jerry.tann@endress.com>
2024-07-10 12:22:10 +0800

    ✨

---
+++ Exercises/501-1000/851-875/875CountDigitsDivideNum/countDigDivNum.rs
@@ -0,0 +1,52 @@
+/*
+
+2520. Count the Digits That Divide a Number
+Easy
+Topics
+Companies
+Hint
+Given an integer num, return the number of digits in num that divide num.
+
+An integer val divides nums if nums % val == 0.
+
+
+
+Example 1:
+
+Input: num = 7
+Output: 1
+Explanation: 7 divides itself, hence the answer is 1.
+Example 2:
+
+Input: num = 121
+Output: 2
+Explanation: 121 is divisible by 1, but not 2. Since 1 occurs twice as a digit, we return 2.
+Example 3:
+
+Input: num = 1248
+Output: 4
+Explanation: 1248 is divisible by all of its digits, hence the answer is 4.
+
+
+Constraints:
+
+1 <= num <= 109
+num does not contain 0 as one of its digits.
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+84.1K
+Submissions
+99.5K
+Acceptance Rate
+84.6%
+
+*/
+
+impl Solution {
+  pub fn count_digits(num: i32) -> i32 {
+
+  }
+}
