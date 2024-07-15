47c5fce21d86e64f19b2ac1e0058a80a6a4ab6d7
Raskolnikov <jerry.tann@endress.com>
2024-07-15 10:28:48 +0800

    🚧

--- Exercises/501-1000/876-900/883CreateBinaryTree/createBTree.cpp
+++ Exercises/501-1000/876-900/883CreateBinaryTree/createBTree.cpp
@@ -61,13 +61,41 @@ Acceptance Rate
  * };
  */
 
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+      unordered_map<int, TreeNode*> nodes;
+      for (auto d: descriptions) {
+        int p = d[0];
+        int c = d[1];
+        bool l = d[2];
+        if (nodes.find(p) == nodes.end()) {
+          nodes[p] = new TreeNode(p);
+        }
 
+        if (nodes.find(c) == nodes.end()) {
+          nodes[c] = new TreeNode(c);
+        }
+
+        if (l) {
+          nodes[p]->left = nodes[c];
+        } else {
+          nodes[p]->right = nodes[c];
+        }
+      }
     }
 };
