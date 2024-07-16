f90587be9ac80c11e42a2fd68208005593ae03d8
Raskolnikov <jerry.tann@endress.com>
2024-07-16 10:46:30 +0800

    ✨

---
+++ Exercises/501-1000/876-900/884StepByStepDirectionsFromBinTreeNode/stepByStepBTreeNode.cpp
@@ -0,0 +1,72 @@
+/*
+2096. Step-By-Step Directions From a Binary Tree Node to Another
+Medium
+Topics
+Companies
+Hint
+You are given the root of a binary tree with n nodes. Each node is uniquely assigned a value from 1 to n. You are also given an integer startValue representing the value of the start node s, and a different integer destValue representing the value of the destination node t.
+
+Find the shortest path starting from node s and ending at node t. Generate step-by-step directions of such path as a string consisting of only the uppercase letters 'L', 'R', and 'U'. Each letter indicates a specific direction:
+
+'L' means to go from a node to its left child node.
+'R' means to go from a node to its right child node.
+'U' means to go from a node to its parent node.
+Return the step-by-step directions of the shortest path from node s to node t.
+
+
+
+Example 1:
+
+
+Input: root = [5,1,2,3,null,6,4], startValue = 3, destValue = 6
+Output: "UURL"
+Explanation: The shortest path is: 3 → 1 → 5 → 2 → 6.
+Example 2:
+
+
+Input: root = [2,1], startValue = 2, destValue = 1
+Output: "L"
+Explanation: The shortest path is: 2 → 1.
+
+
+Constraints:
+
+The number of nodes in the tree is n.
+2 <= n <= 105
+1 <= Node.val <= n
+All the values in the tree are unique.
+1 <= startValue, destValue <= n
+startValue != destValue
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+106.3K
+Submissions
+214.1K
+Acceptance Rate
+49.7%
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#include <string>
+
+using namespace std;
+class Solution {
+public:
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+
+    }
+};
