63ee335487170dc6b96fc906de2a0cc6dfd2e852
Raskolnikov <jerry.tann@endress.com>
2024-07-22 10:39:09 +0800

    🎨 reinvented "find"

---
+++ Exercises/501-1000/876-900/895MaxDiffRemapDigit/refactor.rs
@@ -0,0 +1,12 @@
+impl Solution {
+  pub fn min_max_difference(num: i32) -> i32 {
+      let as_str = num.to_string();
+      let most_sig_mx = as_str.chars().find(|&c| c != '9').unwrap_or('9'); // o i see.  find replaces the loop
+      let most_sig_mn = as_str.chars().find(|&c| c != '0').unwrap_or('0');
+
+      let mx_str: String = as_str.chars().map(|c| if c == most_sig_mx { '9' } else { c }).collect();
+      let mn_str: String = as_str.chars().map(|c| if c == most_sig_mn { '0' } else { c }).collect();
+
+      mx_str.parse::<i32>().unwrap() - mn_str.parse::<i32>().unwrap()
+  }
+}
