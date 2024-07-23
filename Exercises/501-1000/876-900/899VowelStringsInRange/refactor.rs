cc215fbf7a0d4077e2ee63a3f35b0796b206a4be
Raskolnikov <jerry.tann@endress.com>
2024-07-23 13:06:37 +0800

    🎨

---
+++ Exercises/501-1000/876-900/899VowelStringsInRange/refactor.rs
@@ -0,0 +1,70 @@
+/*
+
+2586. Count the Number of Vowel Strings in Range
+Easy
+Topics
+Companies
+Hint
+You are given a 0-indexed array of string words and two integers left and right.
+
+A string is called a vowel string if it starts with a vowel character and ends with a vowel character where vowel characters are 'a', 'e', 'i', 'o', and 'u'.
+
+Return the number of vowel strings words[i] where i belongs to the inclusive range [left, right].
+
+
+
+Example 1:
+
+Input: words = ["are","amy","u"], left = 0, right = 2
+Output: 2
+Explanation:
+- "are" is a vowel string because it starts with 'a' and ends with 'e'.
+- "amy" is not a vowel string because it does not end with a vowel.
+- "u" is a vowel string because it starts with 'u' and ends with 'u'.
+The number of vowel strings in the mentioned range is 2.
+Example 2:
+
+Input: words = ["hey","aeo","mu","ooo","artro"], left = 1, right = 4
+Output: 3
+Explanation:
+- "aeo" is a vowel string because it starts with 'a' and ends with 'o'.
+- "mu" is not a vowel string because it does not start with a vowel.
+- "ooo" is a vowel string because it starts with 'o' and ends with 'o'.
+- "artro" is a vowel string because it starts with 'a' and ends with 'o'.
+The number of vowel strings in the mentioned range is 3.
+
+
+Constraints:
+
+1 <= words.length <= 1000
+1 <= words[i].length <= 10
+words[i] consists of only lowercase English letters.
+0 <= left <= right < words.length
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+60.8K
+Submissions
+82K
+Acceptance Rate
+74.1%
+
+*/
+
+impl Solution {
+  pub fn vowel_strings(words: Vec<String>, left: i32, right: i32) -> i32 {
+    let vowels: [char:5] = ['a','e','i','o','u']; // TIL: array init, faster than Vec
+    let mut res = 0;
+    for i in left as usize..=right as usize {
+      let mut chrs = words[i].chars();
+      let firstchr = chrs.next().unwrap();
+      let lastchr = chrs.last().unwrap_or(firstchr);
+      if vowels.contains(&firstchr) && vowels.contains(&lastchr) {
+        res+=1;
+      }
+    }
+    res
+  }
+}
