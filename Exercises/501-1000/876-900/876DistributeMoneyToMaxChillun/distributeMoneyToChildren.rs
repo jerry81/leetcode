0c46dd98d16c4a578202dff31cd156a6b1bbac34
Raskolnikov <jerry.tann@endress.com>
2024-07-24 12:57:28 +0800

    🚧 handle edge cases easier still

--- Exercises/501-1000/876-900/876DistributeMoneyToMaxChillun/distributeMoneyToChildren.rs
+++ Exercises/501-1000/876-900/876DistributeMoneyToMaxChillun/distributeMoneyToChildren.rs
@@ -1,9 +1,13 @@
 impl Solution {
   pub fn dist_money(money: i32, children: i32) -> i32 {
-      // recursive brute force?
-      if children > money { return -1; }
-
-      if money/8 == children { return money/8 }
-      0
+      // handle each case
+      // possible children_with_8
+      if children > money { return -1 }
+      let possible_children_wth_8 = children.min(money/8);
+      println!("possible {}", possible_children_wth_8);
+      if money%8 == 4 && children-possible_children_with_8 == 1 {
+        possible_children_with_8 -= 1;
+      }
+      possible_children_with_8
   }
 }
