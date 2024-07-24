50032a8dcd0ed69d2704c0eff319175a69217d1e
Raskolnikov <jerry.tann@endress.com>
2024-07-24 13:36:46 +0800

    🚧 cleaned up, try again

--- Exercises/501-1000/876-900/876DistributeMoneyToMaxChillun/distributeMoneyToChildren.rs
+++ Exercises/501-1000/876-900/876DistributeMoneyToMaxChillun/distributeMoneyToChildren.rs
@@ -2,17 +2,21 @@ impl Solution {
   pub fn dist_money(money: i32, children: i32) -> i32 {
       // handle each case
       // possible children_with_8
-      if children > money { return -1 }
-      let mut possible_children_with_8 = children.min(money/8);
+      if children > money { return 0 }
 
-      let remaining_money = money%8;
-      while remaining_money < children-possible_children_with_8 {
-        possible_children_with_8-=1;
-        if possible_children_with_8 < 0 { return 0 }
+      let mut k = money/8;
+
+      loop { // yes, rust has this
+        if k < 0 { return k }
+        let remaining_money = money - (k*8);
+        let remaining_children = children-k;
+        if remaining_children == 1 && remaining_money == 4 {
+          k-=1;
+          continue
+        }
+        if remaining_money >= remaining_children { return k }
+        k-=1;
       }
-      if money%8 == 4 && children - possible_children_with_8 == 1 {
-        possible_children_with_8 -= 1;
-      }
-      possible_children_with_8
+      -1
   }
 }
