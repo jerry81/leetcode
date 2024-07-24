cfb4fe2bfc75c44ffab985ad748b9170c9340ec8
Raskolnikov <jerry.tann@endress.com>
2024-07-24 13:19:47 +0800

    🚧 most annoying problem ever

--- Exercises/501-1000/876-900/876DistributeMoneyToMaxChillun/distributeMoneyToChildren.rs
+++ Exercises/501-1000/876-900/876DistributeMoneyToMaxChillun/distributeMoneyToChildren.rs
@@ -4,9 +4,11 @@ impl Solution {
       // possible children_with_8
       if children > money { return -1 }
       let mut possible_children_with_8 = children.min(money/8);
+
       let remaining_money = money%8;
-      if remaining_money < children-possible_children_with_8 {
+      while remaining_money < children-possible_children_with_8 {
         possible_children_with_8-=1;
+        if possible_children_with_8 < 0 { return 0 }
       }
       if money%8 == 4 && children - possible_children_with_8 == 1 {
         possible_children_with_8 -= 1;
