3da81913279bfaeafbf4239790b74c5f7f9f9d82
Raskolnikov <jerry.tann@endress.com>
2024-07-26 08:01:10 +0800

    🚧 att1

--- Exercises/501-1000/901-925/902KItemsMaxSum/kitemsMaxSum.rs
+++ Exercises/501-1000/901-925/902KItemsMaxSum/kitemsMaxSum.rs
@@ -51,6 +51,10 @@ Acceptance Rate
 
 impl Solution {
   pub fn k_items_with_maximum_sum(num_ones: i32, num_zeros: i32, num_neg_ones: i32, k: i32) -> i32 {
-
+    // greedy
+    let mut result = num_ones;
+    k-=num_ones;
+    k-=num_zeroes;
+    result-k
   }
 }
