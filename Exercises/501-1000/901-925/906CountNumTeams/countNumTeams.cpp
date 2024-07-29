55443eff5ab13cfcf4ac059df9559dc8fe38aacc
Raskolnikov <jerry.tann@endress.com>
2024-07-29 09:46:36 +0800

    ✨ daily

---
+++ Exercises/501-1000/901-925/906CountNumTeams/countNumTeams.cpp
@@ -0,0 +1,60 @@
+/*
+1395. Count Number of Teams
+Medium
+Topics
+Companies
+Hint
+There are n soldiers standing in a line. Each soldier is assigned a unique rating value.
+
+You have to form a team of 3 soldiers amongst them under the following rules:
+
+Choose 3 soldiers with index (i, j, k) with rating (rating[i], rating[j], rating[k]).
+A team is valid if: (rating[i] < rating[j] < rating[k]) or (rating[i] > rating[j] > rating[k]) where (0 <= i < j < k < n).
+Return the number of teams you can form given the conditions. (soldiers can be part of multiple teams).
+
+
+
+Example 1:
+
+Input: rating = [2,5,3,4,1]
+Output: 3
+Explanation: We can form three teams given the conditions. (2,3,4), (5,4,1), (5,3,1).
+Example 2:
+
+Input: rating = [2,1,3]
+Output: 0
+Explanation: We can't form any team given the conditions.
+Example 3:
+
+Input: rating = [1,2,3,4]
+Output: 4
+
+
+Constraints:
+
+n == rating.length
+3 <= n <= 1000
+1 <= rating[i] <= 105
+All the integers in rating are unique.
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+115.4K
+Submissions
+174.7K
+Acceptance Rate
+66.0%
+*/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+
+    }
+};
