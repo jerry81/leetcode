82fca97cda126c26c9f2502ebad247702faa0853
Raskolnikov <jerry.tann@endress.com>
2024-08-01 08:45:27 +0800

    ✨ daring attempt

--- Exercises/501-1000/901-925/919WidthColsInGrid/widthColsInGrid.rs
+++ Exercises/501-1000/901-925/919WidthColsInGrid/widthColsInGrid.rs
@@ -47,9 +47,14 @@ Acceptance Rate
 67.7%
 
 */
-
 impl Solution {
   pub fn find_column_width(grid: Vec<Vec<i32>>) -> Vec<i32> {
+      grid.iter()
+      .map(|v| {
+        v.map(|vv| {
+          vv.to_string().len()
+        }).iter().max()
+      })
 
   }
 }
