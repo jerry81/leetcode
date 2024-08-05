84841c641f87fa170d33eedc479ad1e8d6df48e8
Raskolnikov <jerry.tann@endress.com>
2024-08-05 12:30:26 +0800

    🚧 att1

---
+++ Exercises/501-1000/901-925/924GenFibSeq/genFibSeq.js
@@ -0,0 +1,74 @@
+/*
+
+2648. Generate Fibonacci Sequence
+Easy
+Companies
+Hint
+Write a generator function that returns a generator object which yields the fibonacci sequence.
+
+The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+
+The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13.
+
+
+
+Example 1:
+
+Input: callCount = 5
+Output: [0,1,1,2,3]
+Explanation:
+const gen = fibGenerator();
+gen.next().value; // 0
+gen.next().value; // 1
+gen.next().value; // 1
+gen.next().value; // 2
+gen.next().value; // 3
+Example 2:
+
+Input: callCount = 0
+Output: []
+Explanation: gen.next() is never called so nothing is outputted
+
+
+Constraints:
+
+0 <= callCount <= 50
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+28.3K
+Submissions
+34.1K
+Acceptance Rate
+83.1%
+
+*/
+
+/**
+ * @return {Generator<number>}
+ */
+ var fibGenerator = function*() {
+   let cur = 1;
+   let prev = 0;
+   let started = false;
+   function next() {
+     if (!started) {
+      started = true;
+      value = 0;
+     } else {
+      let tmp = cur;
+      cur = prev+cur;
+      prev = tmp;
+      value = cur;
+     }
+     return { value }
+   }
+ };
+
+ /**
+  * const gen = fibGenerator();
+  * gen.next().value; // 0
+  * gen.next().value; // 1
+  */
