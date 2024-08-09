7e35cfaf1a1e640b21a0be704c3045c7b7fecc00
Raskolnikov <jerry.tann@endress.com>
2024-08-09 14:44:08 +0800

    🚧

--- Exercises/501-1000/926-950/932MagicSquaresInGrid/magicSquaresInGrid.cpp
+++ Exercises/501-1000/926-950/932MagicSquaresInGrid/magicSquaresInGrid.cpp
@@ -3,11 +3,14 @@
 Medium
 Topics
 Companies
-A 3 x 3 magic square is a 3 x 3 grid filled with distinct numbers from 1 to 9 such that each row, column, and both diagonals all have the same sum.
+A 3 x 3 magic square is a 3 x 3 grid filled with distinct numbers from 1 to 9
+such that each row, column, and both diagonals all have the same sum.
 
-Given a row x col grid of integers, how many 3 x 3 contiguous magic square subgrids are there?
+Given a row x col grid of integers, how many 3 x 3 contiguous magic square
+subgrids are there?
 
-Note: while a magic square can only contain numbers from 1 to 9, grid may contain numbers up to 15.
+Note: while a magic square can only contain numbers from 1 to 9, grid may
+contain numbers up to 15.
 
 
 
@@ -51,24 +54,48 @@ Acceptance Rate
 using namespace std;
 
 class Solution {
-public:
-    int numMagicSquaresInside(vector<vector<int>>& grid) {
-        int h = grid.size();
-        int w = grid[0].size(); // safe due to constraints
-        if (h < 3 || w < 3) return 0;
-        return 0;
+  vector<vector<int>> buildSums(vector<vector<int>>& grid, int sR, int sC) {
+    // called once
+  };
+
+  vector<vector<int>> moveRight(vector<vector<int>>& grid, int sR, int sC, vector<vector<int>>& sums) {
+  };
+
+  vector<vector<int>> moveDown(vector<vector<int>>& grid, int sR, int sC, vector<vector<int>>& sums) {
+  };
+
+  bool isMagic(vector<vector<int>> toCheck) {
+    int check_against = toCheck[0][0];
+    for (auto v: toCheck) {
+      for (int i:v) {
+        if (i != check_against) return false;
+      }
     }
-    // to test
-    // 3xhorizontal
-    // 3xvertical
-    // 4xdiagonal
-    // 10 total
-    // cache -> vector<vector<int>> h, v, d
-    // loop top to bottom
-      // r = 0; r < h-2; r++
-        // if r > 0 use cached values
-    // move left to right
-      // c = 0; c < w-2; c++
-        // cache 0
-        // find magic, stop early
+    return true;
+  }
+
+ public:
+  int numMagicSquaresInside(vector<vector<int>>& grid) {
+    int h = grid.size();
+    int w = grid[0].size();  // safe due to constraints
+    if (h < 3 || w < 3) return 0;
+    int res = 0;
+    vector<vector<int>> initDiags = buildSums(grid, 0,0);
+
+
+    return res;
+  }
+  // to test
+  // 3xhorizontal
+  // 3xvertical
+  // 4xdiagonal
+  // 10 total
+  // cache -> vector<vector<int>> h, v, d
+  // loop top to bottom
+  // r = 0; r < h-2; r++
+  // if r > 0 use cached values
+  // move left to right
+  // c = 0; c < w-2; c++
+  // cache 0
+  // find magic, stop early
 };
