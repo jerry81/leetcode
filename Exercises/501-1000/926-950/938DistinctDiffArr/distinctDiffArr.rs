588f3fe6206d2128269a444b0812f4921a31f32d
Raskolnikov <jerry.tann@endress.com>
2024-08-12 14:26:09 +0800

    🚧 att2

--- Exercises/501-1000/926-950/938DistinctDiffArr/distinctDiffArr.rs
+++ Exercises/501-1000/926-950/938DistinctDiffArr/distinctDiffArr.rs
@@ -1,74 +1,21 @@
-/*
-
-2670. Find the Distinct Difference Array
-Easy
-Topics
-Companies
-Hint
-You are given a 0-indexed array nums of length n.
-
-The distinct difference array of nums is an array diff of length n such that diff[i] is equal to the number of distinct elements in the suffix nums[i + 1, ..., n - 1] subtracted from the number of distinct elements in the prefix nums[0, ..., i].
-
-Return the distinct difference array of nums.
-
-Note that nums[i, ..., j] denotes the subarray of nums starting at index i and ending at index j inclusive. Particularly, if i > j then nums[i, ..., j] denotes an empty subarray.
-
-
-
-Example 1:
-
-Input: nums = [1,2,3,4,5]
-Output: [-3,-1,1,3,5]
-Explanation: For index i = 0, there is 1 element in the prefix and 4 distinct elements in the suffix. Thus, diff[0] = 1 - 4 = -3.
-For index i = 1, there are 2 distinct elements in the prefix and 3 distinct elements in the suffix. Thus, diff[1] = 2 - 3 = -1.
-For index i = 2, there are 3 distinct elements in the prefix and 2 distinct elements in the suffix. Thus, diff[2] = 3 - 2 = 1.
-For index i = 3, there are 4 distinct elements in the prefix and 1 distinct element in the suffix. Thus, diff[3] = 4 - 1 = 3.
-For index i = 4, there are 5 distinct elements in the prefix and no elements in the suffix. Thus, diff[4] = 5 - 0 = 5.
-Example 2:
-
-Input: nums = [3,2,3,4,2]
-Output: [-2,-1,0,2,3]
-Explanation: For index i = 0, there is 1 element in the prefix and 3 distinct elements in the suffix. Thus, diff[0] = 1 - 3 = -2.
-For index i = 1, there are 2 distinct elements in the prefix and 3 distinct elements in the suffix. Thus, diff[1] = 2 - 3 = -1.
-For index i = 2, there are 2 distinct elements in the prefix and 2 distinct elements in the suffix. Thus, diff[2] = 2 - 2 = 0.
-For index i = 3, there are 3 distinct elements in the prefix and 1 distinct element in the suffix. Thus, diff[3] = 3 - 1 = 2.
-For index i = 4, there are 3 distinct elements in the prefix and no elements in the suffix. Thus, diff[4] = 3 - 0 = 3.
-
-
-Constraints:
-
-1 <= n == nums.length <= 50
-1 <= nums[i] <= 50
-Seen this question in a real interview before?
-1/5
-Yes
-No
-Accepted
-39K
-Submissions
-50.1K
-Acceptance Rate
-77.8%
-
-*/
 
 use std::collections::HashSet;
+
 impl Solution {
   pub fn distinct_difference_array(nums: Vec<i32>) -> Vec<i32> {
-    let mut pref: Vec<i32> = vec![];
-    let mut hs:HashSet<i32> = HashSet::new();
-    let mut hs2:HashSet<i32> = HashSet::new();
-    let mut suff: Vec<i32>;
     let l = nums.len();
+    let mut res: Vec<i32> = vec![];
     for i in 0..l {
-      hs.insert(nums[i]);
-      hs2.insert(nums[l-i-1]);
-      pref.push(hs.len() as i32);
-      suff.push(hs.len() as i32);
+      HashSet<i32> pref;
+      HashSet<i32> suff;
+      for j in 0..i {
+        pref.insert(nums[j]);
+      }
+      for k in i..l {
+        suff.insert(nums[k]);
+      }
+      res.push((pref.len() - suff.len()) as i32);
     }
-    suff.reverse();
-    println!("pref {:?}", pref);
-    println!("suff {:?}", suff);
-    vec![]
+    res
   }
 }
