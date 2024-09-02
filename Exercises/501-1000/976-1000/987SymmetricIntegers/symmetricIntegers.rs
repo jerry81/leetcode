e11c5ed3117b0f9b7293f6a99e775a45c037e8b3
Raskolnikov <jerry.tann@endress.com>
2024-09-02 12:30:45 +0800

    ✨

---
+++ Exercises/501-1000/976-1000/987SymmetricIntegers/symmetricIntegers.rs
@@ -0,0 +1,48 @@
+/*
+
+2843. Count Symmetric Integers
+Easy
+Topics
+Companies
+Hint
+You are given two positive integers low and high.
+
+An integer x consisting of 2 * n digits is symmetric if the sum of the first n digits of x is equal to the sum of the last n digits of x. Numbers with an odd number of digits are never symmetric.
+
+Return the number of symmetric integers in the range [low, high].
+
+
+
+Example 1:
+
+Input: low = 1, high = 100
+Output: 9
+Explanation: There are 9 symmetric integers between 1 and 100: 11, 22, 33, 44, 55, 66, 77, 88, and 99.
+Example 2:
+
+Input: low = 1200, high = 1230
+Output: 4
+Explanation: There are 4 symmetric integers between 1200 and 1230: 1203, 1212, 1221, and 1230.
+
+
+Constraints:
+
+1 <= low <= high <= 104
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+47.5K
+Submissions
+63.7K
+Acceptance Rate
+74.6%
+
+*/
+
+impl Solution {
+  pub fn count_symmetric_integers(low: i32, high: i32) -> i32 {
+
+  }
+}
