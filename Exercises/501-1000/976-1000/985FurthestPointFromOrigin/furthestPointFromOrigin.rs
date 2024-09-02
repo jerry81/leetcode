46ce81c73ea2011c5b1066864a6328404ec5d797
Raskolnikov <jerry.tann@endress.com>
2024-09-02 11:57:41 +0800

    🚧 att1

--- Exercises/501-1000/976-1000/985FurthestPointFromOrigin/furthestPointFromOrigin.rs
+++ Exercises/501-1000/976-1000/985FurthestPointFromOrigin/furthestPointFromOrigin.rs
@@ -51,6 +51,14 @@ Acceptance Rate
 
 impl Solution {
   pub fn furthest_distance_from_origin(moves: String) -> i32 {
-
+    let mut (l,r,u) = (0,0,0);
+    for c in moves.chars() {
+      match c {
+        'L' => l+=1,
+        'R' => r+=1,
+        _ => u+=1
+      }
+    }
+    l.max(r) + u
   }
 }
