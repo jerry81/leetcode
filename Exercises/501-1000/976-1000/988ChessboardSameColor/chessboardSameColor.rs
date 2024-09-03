d387540fe15003bd71a1a0e2045caba1c9a39dbe
Raskolnikov <jerry.tann@endress.com>
2024-09-03 10:27:22 +0800

    ✨ ez 1

---
+++ Exercises/501-1000/976-1000/988ChessboardSameColor/chessboardSameColor.rs
@@ -0,0 +1,66 @@
+/*
+
+3274. Check if Two Chessboard Squares Have the Same Color
+Easy
+Companies
+Hint
+You are given two strings, coordinate1 and coordinate2, representing the coordinates of a square on an 8 x 8 chessboard.
+
+Below is the chessboard for reference.
+
+
+
+Return true if these two squares have the same color and false otherwise.
+
+The coordinate will always represent a valid chessboard square. The coordinate will always have the letter first (indicating its column), and the number second (indicating its row).
+
+
+
+Example 1:
+
+Input: coordinate1 = "a1", coordinate2 = "c3"
+
+Output: true
+
+Explanation:
+
+Both squares are black.
+
+Example 2:
+
+Input: coordinate1 = "a1", coordinate2 = "h3"
+
+Output: false
+
+Explanation:
+
+Square "a1" is black and "h3" is white.
+
+
+
+Constraints:
+
+coordinate1.length == coordinate2.length == 2
+'a' <= coordinate1[0], coordinate2[0] <= 'h'
+'1' <= coordinate1[1], coordinate2[1] <= '8'
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+33.5K
+Submissions
+48.3K
+Acceptance Rate
+69.5%
+
+*/
+
+impl Solution {
+  pub fn check_two_chessboards(coordinate1: String, coordinate2: String) -> bool {
+    let c11:char = coordinate1.chars().nth(0).unwrap();
+    let c12:char = coordinate1.chars().nth(1).unwrap();
+    let c21:char = coordinate2.chars().nth(0).unwrap();
+    let c22:char = coordinate2.chars().nth(1).unwrap();
+  }
+}
