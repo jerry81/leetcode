98cff240974a988f207593b48b9d434738103056
Raskolnikov <jerry.tann@endress.com>
2024-09-05 13:10:17 +0800

    📝 treat pandas dataframe like a list and slice it

---
+++ Exercises/501-1000/976-1000/999DisplayFirst3Rows/displayFirst3Rows.py
@@ -0,0 +1,56 @@
+# 2879. Display the First Three Rows
+# Easy
+# Companies
+# Hint
+# DataFrame: employees
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | employee_id | int    |
+# | name        | object |
+# | department  | object |
+# | salary      | int    |
+# +-------------+--------+
+# Write a solution to display the first 3 rows of this DataFrame.
+
+
+
+# Example 1:
+
+# Input:
+# DataFrame employees
+# +-------------+-----------+-----------------------+--------+
+# | employee_id | name      | department            | salary |
+# +-------------+-----------+-----------------------+--------+
+# | 3           | Bob       | Operations            | 48675  |
+# | 90          | Alice     | Sales                 | 11096  |
+# | 9           | Tatiana   | Engineering           | 33805  |
+# | 60          | Annabelle | InformationTechnology | 37678  |
+# | 49          | Jonathan  | HumanResources        | 23793  |
+# | 43          | Khaled    | Administration        | 40454  |
+# +-------------+-----------+-----------------------+--------+
+# Output:
+# +-------------+---------+-------------+--------+
+# | employee_id | name    | department  | salary |
+# +-------------+---------+-------------+--------+
+# | 3           | Bob     | Operations  | 48675  |
+# | 90          | Alice   | Sales       | 11096  |
+# | 9           | Tatiana | Engineering | 33805  |
+# +-------------+---------+-------------+--------+
+# Explanation:
+# Only the first 3 rows are displayed.
+# Seen this question in a real interview before?
+# 1/5
+# Yes
+# No
+# Accepted
+# 98.1K
+# Submissions
+# 105.5K
+# Acceptance Rate
+# 92.9%
+
+import pandas as pd
+
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+  return employees[:3]
