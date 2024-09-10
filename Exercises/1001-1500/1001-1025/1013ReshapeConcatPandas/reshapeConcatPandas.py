1c3ca9d37e7381d4eb177d101f7fabf0639fdeae
Raskolnikov <jerry.tann@endress.com>
2024-09-10 11:49:15 +0800

    ✨ ez 2

---
+++ Exercises/1001-1500/1001-1025/1013ReshapeConcatPandas/reshapeConcatPandas.py
@@ -0,0 +1,75 @@
+# 2888. Reshape Data: Concatenate
+# Easy
+# Companies
+# Hint
+# DataFrame df1
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | student_id  | int    |
+# | name        | object |
+# | age         | int    |
+# +-------------+--------+
+
+# DataFrame df2
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | student_id  | int    |
+# | name        | object |
+# | age         | int    |
+# +-------------+--------+
+
+# Write a solution to concatenate these two DataFrames vertically into one DataFrame.
+
+# The result format is in the following example.
+
+
+
+# Example 1:
+
+# Input:
+# df1
+# +------------+---------+-----+
+# | student_id | name    | age |
+# +------------+---------+-----+
+# | 1          | Mason   | 8   |
+# | 2          | Ava     | 6   |
+# | 3          | Taylor  | 15  |
+# | 4          | Georgia | 17  |
+# +------------+---------+-----+
+# df2
+# +------------+------+-----+
+# | student_id | name | age |
+# +------------+------+-----+
+# | 5          | Leo  | 7   |
+# | 6          | Alex | 7   |
+# +------------+------+-----+
+# Output:
+# +------------+---------+-----+
+# | student_id | name    | age |
+# +------------+---------+-----+
+# | 1          | Mason   | 8   |
+# | 2          | Ava     | 6   |
+# | 3          | Taylor  | 15  |
+# | 4          | Georgia | 17  |
+# | 5          | Leo     | 7   |
+# | 6          | Alex    | 7   |
+# +------------+---------+-----+
+# Explanation:
+# The two DataFramess are stacked vertically, and their rows are combined.
+# Seen this question in a real interview before?
+# 1/5
+# Yes
+# No
+# Accepted
+# 57.4K
+# Submissions
+# 63.3K
+# Acceptance Rate
+# 90.6%
+
+import pandas as pd
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([df1, df2], ignore_index=True) # TIL concat is used to concatenate two DataFrames vertically.
