dc85e867e68660c56c4332b5d1511a02a79f382a
Raskolnikov <jerry.tann@endress.com>
2024-09-18 10:32:55 +0800

    ✨

---
+++ Exercises/1001-1500/1026-1050/1038LargestNum/largest_number.cpp
@@ -0,0 +1,50 @@
+/*
+
+179. Largest Number
+Medium
+Topics
+Companies
+Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+
+Since the result may be very large, so you need to return a string instead of an integer.
+
+
+
+Example 1:
+
+Input: nums = [10,2]
+Output: "210"
+Example 2:
+
+Input: nums = [3,30,34,5,9]
+Output: "9534330"
+
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 109
+
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+515.2K
+Submissions
+1.4M
+Acceptance Rate
+37.1%
+
+*/
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+
+    }
+};
