3cd80b48a1bf554884c6a5b116e8562e4dfee6c0
Raskolnikov <jerry.tann@endress.com>
2024-09-19 11:03:33 +0800

    ✨ really hard

--- Exercises/1001-1500/1026-1050/1044DiffWaysAddParens/diffWaysAddParens.cpp
+++ Exercises/1001-1500/1026-1050/1044DiffWaysAddParens/diffWaysAddParens.cpp
@@ -51,10 +51,25 @@ Acceptance Rate
 #include <vector>
 
 using namespace std;
-
 class Solution {
 public:
     vector<int> diffWaysToCompute(string expression) {
-
+        vector<int> result;
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression[i];
+            if (c == '+' || c == '-' || c == '*') {
+                vector<int> left = diffWaysToCompute(expression.substr(0, i));
+                vector<int> right = diffWaysToCompute(expression.substr(i + 1));
+                for (int l : left) {
+                    for (int r : right) {
+                        if (c == '+') result.push_back(l + r);
+                        else if (c == '-') result.push_back(l - r);
+                        else if (c == '*') result.push_back(l * r);
+                    }
+                }
+            }
+        }
+        if (result.empty()) result.push_back(stoi(expression));
+        return result;
     }
 };
