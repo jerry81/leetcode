d1dbe16a9e142a520192e33bddc9fc937721b24f
Raskolnikov <jerry.tann@endress.com>
2024-09-24 11:00:31 +0800

    ✅

--- Exercises/1001-1500/1051-1075/1054LenLongestCommonPrefix/len_longest_common_prefix.cpp
+++ Exercises/1001-1500/1051-1075/1054LenLongestCommonPrefix/len_longest_common_prefix.cpp
@@ -7,13 +7,19 @@ Companies
 Hint
 You are given two arrays with positive integers arr1 and arr2.
 
-A prefix of a positive integer is an integer formed by one or more of its digits, starting from its leftmost digit. For example, 123 is a prefix of the integer 12345, while 234 is not.
+A prefix of a positive integer is an integer formed by one or more of its
+digits, starting from its leftmost digit. For example, 123 is a prefix of the
+integer 12345, while 234 is not.
 
-A common prefix of two integers a and b is an integer c, such that c is a prefix of both a and b. For example, 5655359 and 56554 have a common prefix 565 while 1223 and 43456 do not have a common prefix.
+A common prefix of two integers a and b is an integer c, such that c is a prefix
+of both a and b. For example, 5655359 and 56554 have a common prefix 565 while
+1223 and 43456 do not have a common prefix.
 
-You need to find the length of the longest common prefix between all pairs of integers (x, y) such that x belongs to arr1 and y belongs to arr2.
+You need to find the length of the longest common prefix between all pairs of
+integers (x, y) such that x belongs to arr1 and y belongs to arr2.
 
-Return the length of the longest common prefix among all pairs. If no common prefix exists among them, return 0.
+Return the length of the longest common prefix among all pairs. If no common
+prefix exists among them, return 0.
 
 
 
@@ -30,8 +36,9 @@ Example 2:
 
 Input: arr1 = [1,2,3], arr2 = [4,4,4]
 Output: 0
-Explanation: There exists no common prefix for any pair (arr1[i], arr2[j]), hence we return 0.
-Note that common prefixes between elements of the same array do not count.
+Explanation: There exists no common prefix for any pair (arr1[i], arr2[j]),
+hence we return 0. Note that common prefixes between elements of the same array
+do not count.
 
 
 Constraints:
@@ -52,44 +59,46 @@ Acceptance Rate
 
 */
 
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-static bool cmp(pair<int,string> a, pair<int,string> b) {
-  return a.second < b.second;
-}
-
-int get_ps_length(string a, string b) {
-  int shorter = min(a.size(), b.size());
-
-  for (int i = 0; i < shorter; ++i) {
-    if (a[i] != b[i]) return i;
+  static bool cmp(pair<int, string> a, pair<int, string> b) {
+    return a.second < b.second;
   }
-  return shorter;
-};
 
-public:
-    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-      vector<pair<int,string>> all_items;
-      for (int i: arr1) {
-        all_items.push_back({1,to_string(i)});
-      }
-      for (int i: arr2) {
-        all_items.push_back({2,to_string(i)});
-      }
-      sort(all_items.begin(), all_items.end(), cmp);
-      int n = all_items.size();
-      int res = 0;
-      for (int i = 0; i < n-1; ++i) {
-        auto [a1,a2] = all_items[i];
-        auto [b1,b2] = all_items[i+1];
-        if (a1==b1) continue;
+  int get_ps_length(string a, string b) {
+    int shorter = min(a.size(), b.size());
 
-        res = max(res, get_ps_length(a2,b2));
-      }
-      return res;
+    for (int i = 0; i < shorter; ++i) {
+      if (a[i] != b[i]) return i;
     }
+    return shorter;
+  };
+
+ public:
+  int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+    vector<pair<int, string>> all_items;
+    for (int i : arr1) {
+      all_items.push_back({1, to_string(i)});
+    }
+    for (int i : arr2) {
+      all_items.push_back({2, to_string(i)});
+    }
+    sort(all_items.begin(), all_items.end(), cmp);
+    int n = all_items.size();
+    int res = 0;
+    for (int i = 0; i < n - 1; ++i) {
+      auto [a1, a2] = all_items[i];
+      auto [b1, b2] = all_items[i + 1];
+      if (a1 == b1) continue;
+
+      res = max(res, get_ps_length(a2, b2));
+    }
+    return res;
+  }
 };
+
+// TODO:  official solution, use trie
