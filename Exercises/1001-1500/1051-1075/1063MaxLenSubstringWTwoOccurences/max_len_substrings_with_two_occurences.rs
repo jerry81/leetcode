59bf982a481e2567a5803a026c50206b222a7e7d
Raskolnikov <jerry.tann@endress.com>
2024-09-25 13:41:17 +0800

    ✨ ez 3

---
+++ Exercises/1001-1500/1051-1075/1063MaxLenSubstringWTwoOccurences/max_len_substrings_with_two_occurences.rs
@@ -0,0 +1,55 @@
+/*
+
+3090. Maximum Length Substring With Two Occurrences
+Easy
+Topics
+Companies
+Hint
+Given a string s, return the maximum length of a
+substring
+ such that it contains at most two occurrences of each character.
+
+
+Example 1:
+
+Input: s = "bcbbbcba"
+
+Output: 4
+
+Explanation:
+
+The following substring has a length of 4 and contains at most two occurrences of each character: "bcbbbcba".
+Example 2:
+
+Input: s = "aaaa"
+
+Output: 2
+
+Explanation:
+
+The following substring has a length of 2 and contains at most two occurrences of each character: "aaaa".
+
+
+Constraints:
+
+2 <= s.length <= 100
+s consists only of lowercase English letters.
+
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+41.3K
+Submissions
+64.5K
+Acceptance Rate
+64.0%
+
+*/
+
+impl Solution {
+  pub fn maximum_length_substring(s: String) -> i32 {
+
+  }
+}
