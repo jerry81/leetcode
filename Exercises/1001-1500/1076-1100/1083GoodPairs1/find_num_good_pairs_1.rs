d34cde9fb3848f59b293efdaac6bb7512c207940
Raskolnikov <jerry.tann@endress.com>
2024-10-10 12:20:52 +0800

    🚧

--- Exercises/1001-1500/1076-1100/1083GoodPairs1/find_num_good_pairs_1.rs
+++ Exercises/1001-1500/1076-1100/1083GoodPairs1/find_num_good_pairs_1.rs
@@ -54,6 +54,16 @@ Acceptance Rate
 
 impl Solution {
   pub fn number_of_pairs(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> i32 {
+    let n = nums1.len();
+    let m = nums2.len();
+    let mut res = 0;
+    for i in 0..n {
+      let first = nums1[i];
+      for j in 0..m {
+        let second = nums2[j];
 
+      }
+    }
+    res
   }
 }
