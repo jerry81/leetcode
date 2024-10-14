e82d6e7d21b6f1671747c3fb57d8c97350d6df54
Raskolnikov <jerry.tann@endress.com>
2024-10-14 12:07:21 +0800

    🚧

---
+++ Exercises/1001-1500/1076-1100/1094LexocographicallySmallestStringAfterSwap/lexocographically_smallest_string_after_swap.rs
@@ -0,0 +1,83 @@
+/*
+
+3216. Lexicographically Smallest String After a Swap
+Easy
+Topics
+Companies
+Hint
+Given a string s containing only digits, return the
+lexicographically smallest string
+ that can be obtained after swapping adjacent digits in s with the same parity at most once.
+
+Digits have the same parity if both are odd or both are even. For example, 5 and 9, as well as 2 and 4, have the same parity, while 6 and 9 do not.
+
+
+
+Example 1:
+
+Input: s = "45320"
+
+Output: "43520"
+
+Explanation:
+
+s[1] == '5' and s[2] == '3' both have the same parity, and swapping them results in the lexicographically smallest string.
+
+Example 2:
+
+Input: s = "001"
+
+Output: "001"
+
+Explanation:
+
+There is no need to perform a swap because s is already the lexicographically smallest.
+
+
+
+Constraints:
+
+2 <= s.length <= 100
+s consists only of digits.
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+45.7K
+Submissions
+84.9K
+Acceptance Rate
+53.9%
+
+*/
+
+impl Solution {
+  pub fn get_smallest_string(s: String) -> String {
+    // swap smallest to front
+    let mut as_v: Vec<char> = s.chars().collect();
+    let l = as_v.len();
+    let mut swap1 = -1;
+    let mut swap2 = -1;
+    for i in 0..(l-1) {
+      let c1 = (as_v[i] as u8 - '0' as u8) as i32;
+      let mut mn_swap = 10;
+      for j in i+1..l {
+        // find largest with same parity
+        let c2 = (as_v[j] as u8 - '0' as u8) as i32;
+        if c1%2 != c2%2 { continue; }
+
+        if c2 >= c1 { continue; }
+
+        if c2 < mn_swap {
+          mn_swap = c2;
+          swap1 = i;
+          swap2 = j;
+          break;
+        }
+      }
+    }
+    println!("swap1 {}, swap2 {}", swap1, swap2);
+    "".to_string();
+  }
+}
