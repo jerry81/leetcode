c170bf2ec1d287d634c4753a136aa04f46932379
Raskolnikov <jerry.tann@endress.com>
2024-10-16 10:49:38 +0800

    🚧 att1

---
+++ Exercises/1001-1500/1076-1100/1096CoinGameWinner/coin_game_winner.rs
@@ -0,0 +1,69 @@
+/*
+
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        priority_queue<pair<int, char>> pq;
+        // Add the counts of a, b and c in priority queue.
+        if (a > 0) {
+            pq.push({a, 'a'});
+        }
+
+        if (b > 0) {
+            pq.push({b, 'b'});
+        }
+
+        if (c > 0) {
+            pq.push({c, 'c'});
+        }
+
+        string ans = "";
+        while (!pq.empty()) {
+            pair<int, char> p = pq.top();
+            pq.pop();
+            int count = p.first;
+            char character = p.second;
+            // If three consecutive characters exist, pick the second most
+            // frequent character.
+            if (ans.length() >= 2 and ans[ans.length() - 1] == p.second and
+                ans[ans.length() - 2] == p.second) {
+                if (pq.empty()) break;
+
+                pair<int, char> temp = pq.top();
+                pq.pop();
+                ans += temp.second;
+                if (temp.first - 1 > 0) {
+                    pq.push({temp.first - 1, temp.second});
+                }
+            } else {
+                count--;
+                ans = ans + character;
+            }
+
+            // If count is greater than zero, add it to priority queue.
+            if (count > 0) {
+                pq.push({count, character});
+            }
+        }
+        return ans;
+    }
+};
+
+*/
+
+impl Solution {
+  pub fn losing_player(x: i32, y: i32) -> String {
+    // the only possibility is x-1 y-4
+    let mut alice = true;
+    let mut x = x;
+    let mut y = y;
+    while x >= 1 && y >= 4 {
+      alice = !alice;
+    }
+    if alice {
+      "Bob".to_string()
+    } else {
+      "Alice".to_string()
+    }
+  }
+}
