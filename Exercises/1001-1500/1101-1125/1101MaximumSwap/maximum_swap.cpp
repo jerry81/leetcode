510bda4ea4342c2d3cf2c3f25827520a4709d5a0
Raskolnikov <jerry.tann@endress.com>
2024-10-17 09:54:07 +0800

    ✨

---
+++ Exercises/1001-1500/1101-1125/1101MaximumSwap/maximum_swap.cpp
@@ -0,0 +1,46 @@
+/*
+
+670. Maximum Swap
+Medium
+Topics
+Companies
+You are given an integer num. You can swap two digits at most once to get the maximum valued number.
+
+Return the maximum valued number you can get.
+
+
+
+Example 1:
+
+Input: num = 2736
+Output: 7236
+Explanation: Swap the number 2 and the number 7.
+Example 2:
+
+Input: num = 9973
+Output: 9973
+Explanation: No swap.
+
+
+Constraints:
+
+0 <= num <= 108
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+279.9K
+Submissions
+567.7K
+Acceptance Rate
+49.3%
+
+*/
+
+class Solution {
+public:
+    int maximumSwap(int num) {
+
+    }
+};
