d4cc043587ed8609dfc30abcb8a8a5c86c556609
Raskolnikov <jerry.tann@endress.com>
2024-10-23 10:04:55 +0800

    🚧

--- Exercises/1001-1500/1101-1125/1109BTreeCousins/cousins_in_b_tree2.cpp
+++ Exercises/1001-1500/1101-1125/1109BTreeCousins/cousins_in_b_tree2.cpp
@@ -4,13 +4,16 @@ Medium
 Topics
 Companies
 Hint
-Given the root of a binary tree, replace the value of each node in the tree with the sum of all its cousins' values.
+Given the root of a binary tree, replace the value of each node in the tree with
+the sum of all its cousins' values.
 
-Two nodes of a binary tree are cousins if they have the same depth with different parents.
+Two nodes of a binary tree are cousins if they have the same depth with
+different parents.
 
 Return the root of the modified tree.
 
-Note that the depth of a node is the number of edges in the path from the root node to it.
+Note that the depth of a node is the number of edges in the path from the root
+node to it.
 
 
 
@@ -19,7 +22,8 @@ Example 1:
 
 Input: root = [5,4,9,1,10,null,7]
 Output: [0,0,0,7,7,null,11]
-Explanation: The diagram above shows the initial binary tree and the binary tree after changing the value of each node.
+Explanation: The diagram above shows the initial binary tree and the binary tree
+after changing the value of each node.
 - Node with value 5 does not have any cousins so its sum is 0.
 - Node with value 4 does not have any cousins so its sum is 0.
 - Node with value 9 does not have any cousins so its sum is 0.
@@ -31,7 +35,8 @@ Example 2:
 
 Input: root = [3,1,2]
 Output: [0,0,0]
-Explanation: The diagram above shows the initial binary tree and the binary tree after changing the value of each node.
+Explanation: The diagram above shows the initial binary tree and the binary tree
+after changing the value of each node.
 - Node with value 3 does not have any cousins so its sum is 0.
 - Node with value 1 does not have any cousins so its sum is 0.
 - Node with value 2 does not have any cousins so its sum is 0.
@@ -61,22 +66,40 @@ Acceptance Rate
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
+#include <unordered_map>
+#include <vector>
 
- struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
 
 class Solution {
-public:
-    TreeNode* replaceValueInTree(TreeNode* root) {
+ unordered_map<int, vector<TreeNode*>> level_nodes;
+ unordered_map<int, int> level_sums;
+ void dfs(TreeNode *cur, int lvl) {
+  if (cur) {
+   level_sums[lvl]+=cur->val;
+   level_nodes[lvl].push_back(cur);
+  }
+  dfs(cur->left, lvl+1);
+  dfs(cur->right, lvl+1);
 
-    }
+ }
+ public:
+  TreeNode *replaceValueInTree(TreeNode *root) {
+    // one pass - use map to store the level nodes
+    dfs(root, 0);
+  }
 };
