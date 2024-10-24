3d906ffb36832fa730b3fae461c0286572e1d91b
Raskolnikov <jerry.tann@endress.com>
2024-10-24 10:33:32 +0800

    🚧 pass examples, no overflow handle yet

---
+++ Exercises/1001-1500/1101-1125/1114ReverseInteger/reverse_integer.cpp
@@ -0,0 +1,59 @@
+/*
+
+7. Reverse Integer
+Medium
+Topics
+Companies
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing
+x causes the value to go outside the signed 32-bit integer range [-231, 231 -
+1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or
+unsigned).
+
+
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+
+
+Constraints:
+
+-231 <= x <= 231 - 1
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+3.6M
+Submissions
+12.2M
+Acceptance Rate
+29.3%
+
+*/
+#include <string>
+using namespace std;
+class Solution {
+ public:
+  int reverse(int x) {
+    // so the challenge is testing out of bounds?
+    string as_s = to_string(x);
+    if (x < 0) {
+      std::reverse(as_s.begin() + 1, as_s.end());
+    } else {
+      std::reverse(as_s.begin(), as_s.end());
+    }
+    return stoi(as_s);
+  }
+};
