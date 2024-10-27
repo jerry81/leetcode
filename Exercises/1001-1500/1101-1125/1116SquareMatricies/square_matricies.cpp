aabf5ba1693fbd616215e4b3c8e59c058bb360aa
Raskolnikov <jerry.tann@endress.com>
2024-10-27 13:27:46 +0800

    🚧

---
+++ Exercises/1001-1500/1101-1125/1116SquareMatricies/square_matricies.cpp
@@ -0,0 +1,66 @@
+/*
+
+1277. Count Square Submatrices with All Ones
+Medium
+Topics
+Companies
+Hint
+Given a m * n matrix of ones and zeros, return how many square submatrices have all ones.
+
+
+
+Example 1:
+
+Input: matrix =
+[
+  [0,1,1,1],
+  [1,1,1,1],
+  [0,1,1,1]
+]
+Output: 15
+Explanation:
+There are 10 squares of side 1.
+There are 4 squares of side 2.
+There is  1 square of side 3.
+Total number of squares = 10 + 4 + 1 = 15.
+Example 2:
+
+Input: matrix =
+[
+  [1,0,1],
+  [1,1,0],
+  [1,1,0]
+]
+Output: 7
+Explanation:
+There are 6 squares of side 1.
+There is 1 square of side 2.
+Total number of squares = 6 + 1 = 7.
+
+
+Constraints:
+
+1 <= arr.length <= 300
+1 <= arr[0].length <= 300
+0 <= arr[i][j] <= 1
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+247.3K
+Submissions
+325.2K
+Acceptance Rate
+76.0%
+
+*/
+
+#include <vector>
+
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+
+    }
+};
