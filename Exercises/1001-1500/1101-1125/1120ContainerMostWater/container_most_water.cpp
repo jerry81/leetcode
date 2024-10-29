cb8b3cff63870ad12974dbac3ebc4a1c0b82e332
Raskolnikov <jerry.tann@endress.com>
2024-10-29 11:55:17 +0800

    ✅ 🚧 straightforward is king

--- Exercises/1001-1500/1101-1125/1120ContainerMostWater/container_most_water.cpp
+++ Exercises/1001-1500/1101-1125/1120ContainerMostWater/container_most_water.cpp
@@ -46,10 +46,24 @@ Acceptance Rate
 #include <vector>
 
 using namespace std;
-
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int max_area = 0;
 
+        while (left < right) {
+            int current_area = min(height[left], height[right]) * (right - left);
+            max_area = max(max_area, current_area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return max_area;
     }
 };
