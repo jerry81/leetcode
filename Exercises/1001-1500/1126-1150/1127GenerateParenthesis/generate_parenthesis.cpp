1cd8c4429904dffe3f270166f49cc40da32d9293
Raskolnikov <jerry.tann@endress.com>
2024-11-01 12:24:01 +0800

    ✨

---
+++ Exercises/1001-1500/1126-1150/1127GenerateParenthesis/generate_parenthesis.cpp
@@ -0,0 +1,48 @@
+/*
+
+
+22. Generate Parentheses
+Medium
+Topics
+Companies
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+
+
+Constraints:
+
+1 <= n <= 8
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+2.1M
+Submissions
+2.7M
+Acceptance Rate
+75.8%
+
+*/
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+
+    }
+};
