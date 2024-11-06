e35b9346811a2fa95d560c25a0a56380114c8e60
Raskolnikov <jerry.tann@endress.com>
2024-11-06 13:48:44 +0800

    🚧 compile, but logic needs work

--- Exercises/1001-1500/1126-1150/1135MultiplyStrings/multiply_strings.rs
+++ Exercises/1001-1500/1126-1150/1135MultiplyStrings/multiply_strings.rs
@@ -42,9 +42,9 @@ impl Solution {
   fn build_mini_products(num1: String, num2: String) -> Vec<String> {
     let mut res:Vec<String> = vec![];
     for offset1 in 0..num1.len() {
-      let n1 = (num1.chars().nth(offset1).unwrap() as u8 + '0' as u8) as i32;
+      let n1 = (num1.chars().nth(offset1).unwrap() as u8 - '0' as u8) as i32;
       for offset2 in 0..num2.len() {
-        let n2 = (num2.chars().nth(offset2).unwrap() as u8 + '0' as u8) as i32;
+        let n2 = (num2.chars().nth(offset2).unwrap() as u8 - '0' as u8) as i32;
         let prod = (n1*n2).to_string();
         let num_zeroes = offset1+offset2;
         let mut s: String = "".to_string();
