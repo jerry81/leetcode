1d4e8c058f774b907a15c624f504e7c8f77e1d88
Raskolnikov <jerry.tann@endress.com>
2024-11-07 09:25:27 +0800

    🐛 🚧 fix compile

--- Exercises/1001-1500/1126-1150/1136LargestComboBitwiseANDGT0/largest_combo_bitwise_and_gt_0.rs
+++ Exercises/1001-1500/1126-1150/1136LargestComboBitwiseANDGT0/largest_combo_bitwise_and_gt_0.rs
@@ -53,12 +53,12 @@ impl Solution {
   pub fn largest_combination(candidates: Vec<i32>) -> i32 {
     let n = candidates.len();
     let mut res = 1;
-    let i in 0..n {
+    for i in 0..n {
       let cur_prod = candidates[i];
-      let j in i+1..n {
+      for j in i+1..n {
         cur_prod = cur_prod & candidates[j];
         if cur_prod > 0 {
-          res = res.max(j-i+1)
+          res = res.max((j-i+1) as i32)
         } else {
           break;
         }
