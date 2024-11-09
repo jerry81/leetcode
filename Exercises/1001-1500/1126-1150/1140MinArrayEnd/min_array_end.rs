9facc52358c9433cf2f94742312111e3c56a73ed
Raskolnikov <jerry.tann@endress.com>
2024-11-09 12:44:18 +0800

    ✨

---
+++ Exercises/1001-1500/1126-1150/1140MinArrayEnd/min_array_end.rs
@@ -0,0 +1,56 @@
+/*
+
+3133. Minimum Array End
+Medium
+Topics
+Companies
+Hint
+You are given two integers n and x. You have to construct an array of positive integers nums of size n where for every 0 <= i < n - 1, nums[i + 1] is greater than nums[i], and the result of the bitwise AND operation between all elements of nums is x.
+
+Return the minimum possible value of nums[n - 1].
+
+
+
+Example 1:
+
+Input: n = 3, x = 4
+
+Output: 6
+
+Explanation:
+
+nums can be [4,5,6] and its last element is 6.
+
+Example 2:
+
+Input: n = 2, x = 7
+
+Output: 15
+
+Explanation:
+
+nums can be [7,15] and its last element is 15.
+
+
+
+Constraints:
+
+1 <= n, x <= 108
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+25.4K
+Submissions
+56.2K
+Acceptance Rate
+45.3%
+
+*/
+
+impl Solution {
+  pub fn min_end(n: i32, x: i32) -> i64 {
+
+  }
+}
