b0bfe4e6c7f0347970db2b45058db6529c72d4ff
Raskolnikov <jerry.tann@endress.com>
2024-11-18 09:21:54 +0800

    ✨

---
+++ Exercises/1001-1500/1126-1150/1150MergeIntervals/merge_intervals.rs
@@ -0,0 +1,46 @@
+/*
+
+
+56. Merge Intervals
+Medium
+Topics
+Companies
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+
+
+Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+
+Constraints:
+
+1 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 104
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+2.8M
+Submissions
+5.7M
+Acceptance Rate
+48.2%
+
+*/
+
+impl Solution {
+  pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+
+  }
+}
