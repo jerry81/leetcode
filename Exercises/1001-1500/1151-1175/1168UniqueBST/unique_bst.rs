c595d649f6dfac994649add75da86e728a0ea238
Raskolnikov <jerry.tann@endress.com>
2024-12-31 14:49:41 +0800

    ✨

---
+++ Exercises/1001-1500/1151-1175/1168UniqueBST/unique_bst.rs
@@ -0,0 +1,43 @@
+/*
+
+
+96. Unique Binary Search Trees
+Medium
+Topics
+Companies
+Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
+
+
+
+Example 1:
+
+
+Input: n = 3
+Output: 5
+Example 2:
+
+Input: n = 1
+Output: 1
+
+
+Constraints:
+
+1 <= n <= 19
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+721.5K
+Submissions
+1.2M
+Acceptance Rate
+61.9%
+
+*/
+
+impl Solution {
+  pub fn num_trees(n: i32) -> i32 {
+
+  }
+}
