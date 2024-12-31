98f6d1aea91daa1898306f5765b3a29d563da3a2
Raskolnikov <jerry.tann@endress.com>
2024-12-31 14:29:34 +0800

    🚧 restarting

--- Exercises/1001-1500/1151-1175/1167RestoreIpAddress/restore_ip_addresses.rs
+++ Exercises/1001-1500/1151-1175/1167RestoreIpAddress/restore_ip_addresses.rs
@@ -43,46 +43,41 @@ Acceptance Rate
 */
 
 impl Solution {
+  fn is_valid_segment(s: &str) -> bool {
+    if s.len() == 1 { return true }
+    if s.len() > 3 { return false }
+
+    if s.starts_with('0') && s.len() > 1 { return false }
+
+    let n = s.parse::<u8>().unwrap();
+    if n > 255 { return false }
+    println!("{} is valid", s);
+    true
+  }
   fn r(res: &mut Vec<String>, s: &str, idx: usize, num_dots: u8, cur: String) {
+    println!("cur is now {}", cur);
     if num_dots == 3 || idx >= s.len() { return }
 
     if num_dots == 2 {
-      res.push((cur+"."+s[idx..]).to_string());
+      let remaining_segment: String = s[idx..].to_string();
+      if Solution::is_valid_segment(&remaining_segment){
+        res.push((cur+"."+&s[idx..]).to_string());
+      }
       return
     }
-    let leading_c:char = s[idx];
-    // check leading char for 0
-    if leading_c == '0' {
-      Solution::r(res, s, idx+1, num_dots+1, cur+"0.")
-    } else if leading_c == '1' {
-      // 100 - 199
-      Solution::r(res, s, idx+1, num_dots+1, cur+"1.");
-      Solution::r(res, s, idx+2, num_dots+1, cur+s[idx..idx+2]);
-      Solution::r(res, s, idx+3, num_dots+1, cur+s[idx..idx+3]);
-    } else if leading_c == '2' {
-      // 200 - 255
-      // need to be in bounds
-      Solution::r(res, s, idx+1, num_dots+1, cur+"1.");
-      if s.len() - idx > 2 {
-        let second_c: char = s[idx+1];
-        Solution::r(res, s, idx+2, num_dots+1, cur+s[idx..idx+2]);
-        if s.len() - idx > 3 && second_c <= '5' {
-          let third_c: char = s[idx+2];
-          if third_c <= '5' {
-            Solution::r(res, s, idx+3, num_dots+1, cur+s[idx..idx+3])
-          }
-        }
-      }
-    } else {
-      // 2 digit
-      Solution::r(res, s, idx+1, num_dots+1, cur+"1.");
-      Solution::r(res, s, idx+2, num_dots+1, cur+s[idx..idx+2]);
+
+    // try len 1, 2, 3
+    let chars_remaining = 3.min(s.len() - idx - 1);
+    println!("chars_remaining is {}", chars_remaining);
+    for i in 1..=chars_remaining {
+
+      Solution::r(res,s,idx+i,num_dots+1, cur.clone()+&s[idx..idx+i]+".");
     }
   }
   pub fn restore_ip_addresses(s: String) -> Vec<String> {
     // some kind of recursion
     let mut res: Vec<String> = vec![];
-    Solution::r(&mut res, &s, 0, 0);
+    Solution::r(&mut res, &s, 0, 0, String::new());
     res
   }
 }
