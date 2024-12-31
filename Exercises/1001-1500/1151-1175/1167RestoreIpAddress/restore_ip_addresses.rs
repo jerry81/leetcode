7d38088933138011c11435027edabdfc355ccd71
Raskolnikov <jerry.tann@endress.com>
2024-12-31 14:00:37 +0800

    🚧

--- Exercises/1001-1500/1151-1175/1167RestoreIpAddress/restore_ip_addresses.rs
+++ Exercises/1001-1500/1151-1175/1167RestoreIpAddress/restore_ip_addresses.rs
@@ -43,7 +43,46 @@ Acceptance Rate
 */
 
 impl Solution {
-  pub fn restore_ip_addresses(s: String) -> Vec<String> {
+  fn r(res: &mut Vec<String>, s: &str, idx: usize, num_dots: u8, cur: String) {
+    if num_dots == 3 || idx >= s.len() { return }
 
+    if num_dots == 2 {
+      res.push((cur+"."+s[idx..]).to_string());
+      return
+    }
+    let leading_c:char = s[idx];
+    // check leading char for 0
+    if leading_c == '0' {
+      Solution::r(res, s, idx+1, num_dots+1, cur+"0.")
+    } else if leading_c == '1' {
+      // 100 - 199
+      Solution::r(res, s, idx+1, num_dots+1, cur+"1.");
+      Solution::r(res, s, idx+2, num_dots+1, cur+s[idx..idx+2]);
+      Solution::r(res, s, idx+3, num_dots+1, cur+s[idx..idx+3]);
+    } else if leading_c == '2' {
+      // 200 - 255
+      // need to be in bounds
+      Solution::r(res, s, idx+1, num_dots+1, cur+"1.");
+      if s.len() - idx > 2 {
+        let second_c: char = s[idx+1];
+        Solution::r(res, s, idx+2, num_dots+1, cur+s[idx..idx+2]);
+        if s.len() - idx > 3 && second_c <= '5' {
+          let third_c: char = s[idx+2];
+          if third_c <= '5' {
+            Solution::r(res, s, idx+3, num_dots+1, cur+s[idx..idx+3])
+          }
+        }
+      }
+    } else {
+      // 2 digit
+      Solution::r(res, s, idx+1, num_dots+1, cur+"1.");
+      Solution::r(res, s, idx+2, num_dots+1, cur+s[idx..idx+2]);
+    }
+  }
+  pub fn restore_ip_addresses(s: String) -> Vec<String> {
+    // some kind of recursion
+    let mut res: Vec<String> = vec![];
+    Solution::r(&mut res, &s, 0, 0);
+    res
   }
 }
