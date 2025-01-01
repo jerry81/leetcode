078a04aa4fdb092c0b58fbee9d53d385451eff3f
Raskolnikov <jerry.tann@endress.com>
2025-01-01 09:03:31 +0800

    ✨ med 1

---
+++ Exercises/1001-1500/1151-1175/1169ValidateBinaryTree/validate_binary_tree.rs
@@ -0,0 +1,72 @@
+/*
+
+98. Validate Binary Search Tree
+Medium
+Topics
+Companies
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left
+subtree
+ of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+
+Example 1:
+
+
+Input: root = [2,1,3]
+Output: true
+Example 2:
+
+
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-231 <= Node.val <= 231 - 1
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+2.6M
+Submissions
+7.8M
+Acceptance Rate
+33.7%
+
+*/
+
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn is_valid_bst(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+
+    }
+}
