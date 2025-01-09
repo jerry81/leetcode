16db74dbacfee2bc2b82910d329e6dedeedf6ac5
Raskolnikov <jerry.tann@endress.com>
2025-01-09 11:00:25 +0800

    ✨

---
+++ Exercises/1001-1500/1176-1200/1178FlattenBinaryTreeToLL/flatten_binary_tree_to_ll.rs
@@ -0,0 +1,71 @@
+/*
+114. Flatten Binary Tree to Linked List
+Medium
+Topics
+Companies
+Hint
+Given the root of a binary tree, flatten the tree into a "linked list":
+
+The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+
+Example 1:
+
+
+Input: root = [1,2,5,3,4,null,6]
+Output: [1,null,2,null,3,null,4,null,5,null,6]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [0]
+Output: [0]
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+
+
+Follow up: Can you flatten the tree in-place (with O(1) extra space)?
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+1.1M
+Submissions
+1.6M
+Acceptance Rate
+67.2%
+*/
+
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn flatten(root: &mut Option<Rc<RefCell<TreeNode>>>) {
+
+    }
+}
