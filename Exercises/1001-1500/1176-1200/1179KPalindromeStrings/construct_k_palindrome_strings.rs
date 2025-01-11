cee4546625cd9fbe179736cdff2aea505809364f
Raskolnikov <jerry.tann@endress.com>
2025-01-11 15:08:08 +0800

    ✨ can construct k palndromic strings

---
+++ Exercises/1001-1500/1176-1200/1179KPalindromeStrings/construct_k_palindrome_strings.rs
@@ -0,0 +1,49 @@
+/*
+1400. Construct K Palindrome Strings
+Medium
+Topics
+Companies
+Hint
+Given a string s and an integer k, return true if you can use all the characters in s to construct k palindrome strings or false otherwise.
+
+
+
+Example 1:
+
+Input: s = "annabelle", k = 2
+Output: true
+Explanation: You can construct two palindromes using all characters in s.
+Some possible constructions "anna" + "elble", "anbna" + "elle", "anellena" + "b"
+Example 2:
+
+Input: s = "leetcode", k = 3
+Output: false
+Explanation: It is impossible to construct 3 palindromes using all the characters of s.
+Example 3:
+
+Input: s = "true", k = 4
+Output: true
+Explanation: The only possible solution is to put each character in a separate string.
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+1 <= k <= 105
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+95.4K
+Submissions
+146.6K
+Acceptance Rate
+65.0%
+*/
+impl Solution {
+  pub fn can_construct(s: String, k: i32) -> bool {
+
+  }
+}
