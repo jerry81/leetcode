e7cef6676c88770365322ea8876507a3666d138f
Raskolnikov <jerry.tann@endress.com>
2025-01-15 11:38:12 +0800

    ✨

---
+++ Exercises/1001-1500/1176-1200/1182MinimizeXor/minimize_xor.rs
@@ -0,0 +1,56 @@
+/*
+
+2429. Minimize XOR
+Medium
+Topics
+Companies
+Hint
+Given two positive integers num1 and num2, find the positive integer x such that:
+
+x has the same number of set bits as num2, and
+The value x XOR num1 is minimal.
+Note that XOR is the bitwise XOR operation.
+
+Return the integer x. The test cases are generated such that x is uniquely determined.
+
+The number of set bits of an integer is the number of 1's in its binary representation.
+
+
+
+Example 1:
+
+Input: num1 = 3, num2 = 5
+Output: 3
+Explanation:
+The binary representations of num1 and num2 are 0011 and 0101, respectively.
+The integer 3 has the same number of set bits as num2, and the value 3 XOR 3 = 0 is minimal.
+Example 2:
+
+Input: num1 = 1, num2 = 12
+Output: 3
+Explanation:
+The binary representations of num1 and num2 are 0001 and 1100, respectively.
+The integer 3 has the same number of set bits as num2, and the value 3 XOR 1 = 2 is minimal.
+
+
+Constraints:
+
+1 <= num1, num2 <= 109
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+33.8K
+Submissions
+68.4K
+Acceptance Rate
+49.5%
+
+*/
+
+impl Solution {
+  pub fn minimize_xor(num1: i32, num2: i32) -> i32 {
+
+  }
+}
