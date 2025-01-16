f5b53fb037549f39a54ba7385c785c0dea009bee
Raskolnikov <jerry.tann@endress.com>
2025-01-16 11:26:31 +0800

    🚧 att1

--- Exercises/1001-1500/1176-1200/1184BitwiseXorPairings/bitwise_xor_all_pairings.rs
+++ Exercises/1001-1500/1176-1200/1184BitwiseXorPairings/bitwise_xor_all_pairings.rs
@@ -42,6 +42,12 @@ Acceptance Rate
 
 impl Solution {
   pub fn xor_all_nums(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
-
+    let mut res = 0;
+    for n1 in nums1 {
+      for n2 in nums2 {
+        res ^= n1 ^ n2;
+      }
+    }
+    res
   }
 }
