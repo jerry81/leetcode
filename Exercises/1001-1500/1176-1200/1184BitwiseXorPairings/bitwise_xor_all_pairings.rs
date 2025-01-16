fc2f39db379c6d908bf3a18c9481ae4569da023f
Raskolnikov <jerry.tann@endress.com>
2025-01-16 11:31:57 +0800

    🚧 att2

--- Exercises/1001-1500/1176-1200/1184BitwiseXorPairings/bitwise_xor_all_pairings.rs
+++ Exercises/1001-1500/1176-1200/1184BitwiseXorPairings/bitwise_xor_all_pairings.rs
@@ -43,11 +43,19 @@ Acceptance Rate
 impl Solution {
   pub fn xor_all_nums(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
     let mut res = 0;
-    for n1 in nums1 {
-      for n2 in nums2 {
-        res ^= n1 ^ n2;
+    if nums2.clone().len() % 2 != 0 {
+      for n1 in nums1.clone() {
+        res ^= n1;
       }
     }
+
+    if nums1.clone().len() % 2 != 0 {
+      for n2 in nums2.clone() {
+        res ^= n2;
+      }
+    }
+
     res
   }
 }
+// of course there is a catch:  TLE
