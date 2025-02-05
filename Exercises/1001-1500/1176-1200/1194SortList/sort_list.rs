8feb605dd6f030f63b4fc39833ca69418813b881
Raskolnikov <jerry.tann@endress.com>
2025-02-05 11:38:29 +0800

    ✨

---
+++ Exercises/1001-1500/1176-1200/1194SortList/sort_list.rs
@@ -0,0 +1,68 @@
+/*
+
+148. Sort List
+Medium
+Topics
+Companies
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+
+
+Example 1:
+
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+
+Input: head = []
+Output: []
+
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 5 * 104].
+-105 <= Node.val <= 105
+
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+955.6K
+Submissions
+1.6M
+Acceptance Rate
+60.6%
+
+*/
+
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+  pub fn sort_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+
+  }
+}
