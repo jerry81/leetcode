f43ba2972abb29a94bbb6848a5c72bfb082fbc10
Raskolnikov <jerry.tann@endress.com>
2025-02-11 12:36:51 +0800

    🚧 att1

--- Exercises/1001-1500/1201-1225/1201FindPeakElement/find_peak_element.rs
+++ Exercises/1001-1500/1201-1225/1201FindPeakElement/find_peak_element.rs
@@ -46,6 +46,16 @@ Acceptance Rate
 
 impl Solution {
   pub fn find_peak_element(nums: Vec<i32>) -> i32 {
-
+    let mut r = nums.len() -1;
+    let mut l = 0;
+    while l < r {
+      let mid = l + r / 2;
+      if nums[mid] < nums[mid+1] {
+        l = mid;
+      } else {
+        r = mid;
+      }
+    }
+    l as i32
   }
 }
