2cfe52e85628d948b5c5d1d0b0eb3125fa10306d
Raskolnikov <jerry.tann@endress.com>
2025-02-13 13:43:20 +0800

    ✨

---
+++ Exercises/1001-1500/1201-1225/1205FractionToRecurringDecimal/fraction_to_recurring_decimal.rs
@@ -0,0 +1,53 @@
+/*
+
+166. Fraction to Recurring Decimal
+Medium
+Topics
+Companies
+Hint
+Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+
+If the fractional part is repeating, enclose the repeating part in parentheses.
+
+If multiple answers are possible, return any of them.
+
+It is guaranteed that the length of the answer string is less than 104 for all the given inputs.
+
+
+
+Example 1:
+
+Input: numerator = 1, denominator = 2
+Output: "0.5"
+Example 2:
+
+Input: numerator = 2, denominator = 1
+Output: "2"
+Example 3:
+
+Input: numerator = 4, denominator = 333
+Output: "0.(012)"
+
+
+Constraints:
+
+-231 <= numerator, denominator <= 231 - 1
+denominator != 0
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+246.4K
+Submissions
+954.6K
+Acceptance Rate
+25.8%
+
+*/
+
+impl Solution {
+  pub fn fraction_to_decimal(numerator: i32, denominator: i32) -> String {
+
+  }
+}
