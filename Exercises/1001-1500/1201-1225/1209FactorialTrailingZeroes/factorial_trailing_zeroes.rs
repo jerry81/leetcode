79482bf595ce878dcfe18d9e2f38666f2dbbe0a9
Raskolnikov <jerry.tann@endress.com>
2025-02-17 12:10:21 +0800

    ✨

---
+++ Exercises/1001-1500/1201-1225/1209FactorialTrailingZeroes/factorial_trailing_zeroes.rs
@@ -0,0 +1,52 @@
+/*
+
+172. Factorial Trailing Zeroes
+Medium
+Topics
+Companies
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
+
+
+
+Example 1:
+
+Input: n = 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+Example 2:
+
+Input: n = 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+Example 3:
+
+Input: n = 0
+Output: 0
+
+
+Constraints:
+
+0 <= n <= 104
+
+
+Follow up: Could you write a solution that works in logarithmic time complexity?
+
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+529K
+Submissions
+1.2M
+Acceptance Rate
+44.4%
+*/
+
+impl Solution {
+  pub fn trailing_zeroes(n: i32) -> i32 {
+
+  }
+}
