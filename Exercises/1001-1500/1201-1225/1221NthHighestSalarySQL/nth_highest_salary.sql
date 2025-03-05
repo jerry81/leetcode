c247e24c0e034bab1e5cadbfacab95ee8a15d3fa
Raskolnikov <jerry.tann@endress.com>
2025-03-05 10:46:17 +0800

    ✨ med 1

---
+++ Exercises/1001-1500/1201-1225/1221NthHighestSalarySQL/nth_highest_salary.sql
@@ -0,0 +1,77 @@
+-- 177. Nth Highest Salary
+-- Medium
+-- Topics
+-- Companies
+-- SQL Schema
+-- Pandas Schema
+-- Table: Employee
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | salary      | int  |
+-- +-------------+------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains information about the salary of an employee.
+
+
+-- Write a solution to find the nth highest salary from the Employee table. If there is no nth highest salary, return null.
+
+-- The result format is in the following example.
+
+
+
+-- Example 1:
+
+-- Input:
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- n = 2
+-- Output:
+-- +------------------------+
+-- | getNthHighestSalary(2) |
+-- +------------------------+
+-- | 200                    |
+-- +------------------------+
+-- Example 2:
+
+-- Input:
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- +----+--------+
+-- n = 2
+-- Output:
+-- +------------------------+
+-- | getNthHighestSalary(2) |
+-- +------------------------+
+-- | null                   |
+-- +------------------------+
+-- Seen this question in a real interview before?
+-- 1/5
+-- Yes
+-- No
+-- Accepted
+-- 463.6K
+-- Submissions
+-- 1.2M
+-- Acceptance Rate
+-- 37.9%
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  RETURN QUERY (
+    -- Write your PostgreSQL query statement below.
+
+
+  );
+END;
+$$ LANGUAGE plpgsql;
