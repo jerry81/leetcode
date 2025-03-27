e677de7a22070cd24c226f71a048e49203f1d032
Raskolnikov <jerry.tann@endress.com>
2025-03-27 14:02:53 +0800

    ✨

--- Exercises/1001-1500/1226-1250/1227DepartmentHighestSalary/dept_highest_salary.sql
+++ Exercises/1001-1500/1226-1250/1227DepartmentHighestSalary/dept_highest_salary.sql
@@ -2,4 +2,7 @@
 
 -- Write your PostgreSQL query statement below
 
-
+SELECT d.name as Department, e.name as Employee, salary as Salary
+FROM Employee as e
+JOIN Department as d
+ON e.departmentId=d.id;
