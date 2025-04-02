9156f74f91250d21dc377f8f42c96fdc0291aa7e
Raskolnikov <jerry.tann@endress.com>
2025-04-02 16:48:31 +0800

    🚧 att1

--- Exercises/1001-1500/1226-1250/1229RepeatedDNASequences/repeated_dna_sequences.rs
+++ Exercises/1001-1500/1226-1250/1229RepeatedDNASequences/repeated_dna_sequences.rs
@@ -41,8 +41,21 @@ Acceptance Rate
 
 */
 
+use std::collections::HashSet;
+
 impl Solution {
   pub fn find_repeated_dna_sequences(s: String) -> Vec<String> {
-
+    // brute force with early stops?
+    // store
+    let mut res: HashSet<String> = vec![];
+    let mut hs: HashSet<String> = HashSet::new();
+    let n = s.len();
+    for i in 0..(n-10) {
+      let substr = s[i..(i+10)].to_string();
+      if hs.contains(substr) { res.push(substr); }
+      hs.insert(substr);
+    }
+    let resv: Vec<String> = res.into_iter().collect();
+    resv
   }
 }
