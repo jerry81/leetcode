2998526528ae7238b5ceba512fc4e1c2175866b3
Raskolnikov <jerry.tann@endress.com>
2025-04-02 16:53:52 +0800

    ✅

--- Exercises/1001-1500/1226-1250/1229RepeatedDNASequences/repeated_dna_sequences.rs
+++ Exercises/1001-1500/1226-1250/1229RepeatedDNASequences/repeated_dna_sequences.rs
@@ -47,12 +47,13 @@ impl Solution {
   pub fn find_repeated_dna_sequences(s: String) -> Vec<String> {
     // brute force with early stops?
     // store
-    let mut res: HashSet<String> = vec![];
+    let mut res: HashSet<String> = HashSet::new();
     let mut hs: HashSet<String> = HashSet::new();
     let n = s.len();
-    for i in 0..(n-10) {
+    if n < 10 { return vec![] }
+    for i in 0..=(n-10) {
       let substr = s[i..(i+10)].to_string();
-      if hs.contains(substr) { res.push(substr); }
+      if hs.contains(&substr) { res.insert(substr.clone()); }
       hs.insert(substr);
     }
     let resv: Vec<String> = res.into_iter().collect();
