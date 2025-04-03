9e92f15c3bab17aee4a6a0566aa7c9d54e09cfdb
Raskolnikov <jerry.tann@endress.com>
2025-04-03 13:26:56 +0800

    🚧 the setup

---
+++ Exercises/1001-1500/1226-1250/1230MaxValOrderedTripletII/max_val_ordered_triplet2.rs
@@ -0,0 +1,83 @@
+/*
+
+2874. Maximum Value of an Ordered Triplet II
+Medium
+Topics
+Companies
+Hint
+You are given a 0-indexed integer array nums.
+
+Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0.
+
+The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].
+
+
+
+Example 1:
+
+Input: nums = [12,6,1,2,7]
+Output: 77
+Explanation: The value of the triplet (0, 2, 4) is (nums[0] - nums[2]) * nums[4] = 77.
+It can be shown that there are no ordered triplets of indices with a value greater than 77.
+Example 2:
+
+Input: nums = [1,10,3,4,19]
+Output: 133
+Explanation: The value of the triplet (1, 2, 4) is (nums[1] - nums[2]) * nums[4] = 133.
+It can be shown that there are no ordered triplets of indices with a value greater than 133.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+Explanation: The only ordered triplet of indices (0, 1, 2) has a negative value of (nums[0] - nums[1]) * nums[2] = -3. Hence, the answer would be 0.
+
+
+Constraints:
+
+3 <= nums.length <= 105
+1 <= nums[i] <= 106
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+57.9K
+Submissions
+113.4K
+Acceptance Rate
+51.0%
+
+*/
+
+impl Solution {
+  pub fn maximum_triplet_value(nums: Vec<i32>) -> i64 {
+    // maximize a
+    // minimize b
+    // maximize c
+    let mut res = 0;
+    // reverse max prefix
+    let mut max_rp: Vec<i32> = vec![];
+    let mut mx = 0;
+    for i in nums.iter().rev() {
+      mx = mx.max(*i);
+      max_rp.push(mx);
+    }
+    max_rp = max_rp.iter().rev().cloned().collect();
+    // max prefix
+    let mut max_p: Vec<i32> = vec![];
+    let mut min_p: Vec<i32> = vec![];
+    let mut mn = i32::MAX;
+    mx = 0;
+    for i in nums.iter() {
+      mx = mx.max(*i);
+      max_p.push(mx);
+      mn = mn.min(*i);
+      min_p.push(mn);
+    }
+
+    println!("{:?}", max_rp);
+    println!("max_p {:?}", max_p);
+    println!("min_p {:?}", min_p);
+    res.max(0)
+  }
+}
